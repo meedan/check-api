4e504678a3b57b76d26fba35b0887cd75d1225c5
Sawy <melsawy.be7eri@gmail.com>
2016-07-19 21:44:11 +0200

    Fix tests

--- db/migrate/20160622182507_create_medias.rb
+++ db/migrate/20160622182507_create_medias.rb
@@ -2,7 +2,6 @@ class CreateMedias < ActiveRecord::Migration
   def change
     create_table :medias do |t|
       t.belongs_to :user
-      t.belongs_to :project, index: true
       t.belongs_to :account, index: true
       t.string :url
       if ActiveRecord::Base.connection.class.name === 'ActiveRecord::ConnectionAdapters::PostgreSQLAdapter'
