53f9655cff4d3df731c294d410d7e300d8a63492
Sawy <melsawy.be7eri@gmail.com>
2016-08-08 18:34:46 +0200

    Ticket #4918 - GraphQL

---
+++ app/graph/types/contact_type.rb
@@ -0,0 +1,23 @@
+ContactType = GraphQL::ObjectType.define do
+  name 'Contact'
+  description 'Contact type'
+
+  interfaces [NodeIdentification.interface]
+
+  field :id, field: GraphQL::Relay::GlobalIdField.new('Contact')
+  field :updated_at, types.String
+  field :created_at, types.String
+  field :location, types.String
+  field :phone, types.String
+  field :web, types.String
+  field :team_id, types.Int
+  field :team do
+    type -> { TeamType }
+
+    resolve -> (contact, _args, _ctx) {
+      contact.team
+    }
+  end
+
+# End of fields
+end
