152315ffc224504bc80f3dacc37c44d50622433e
Sawy <melsawy.be7eri@gmail.com>
2016-08-22 16:45:26 +0200

    Add a unique index for TeamUser table

---
+++ db/migrate/20160822140558_add_index_to_team_users.rb
@@ -0,0 +1,17 @@
+class AddIndexToTeamUsers < ActiveRecord::Migration
+  def change
+
+    tu = TeamUser.all.group_by { |x| [x.team_id, x.user_id] }
+    tu.each do |key, value|
+      value.each_with_index do |row, index|
+        unless value.length == 1
+          if index == value.length - 1
+            row.destroy
+          end
+        end
+      end
+    end
+
+    add_index :team_users, [:team_id, :user_id], unique: true
+  end
+end
