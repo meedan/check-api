aa9cba03cd5a4f013275b519c5685a7eea6f2ef2
Sawy <melsawy.be7eri@gmail.com>
2016-08-22 21:37:50 +0200

    slack authentication - fix redirect

--- app/controllers/concerns/slack_authentication.rb
+++ app/controllers/concerns/slack_authentication.rb
@@ -3,6 +3,10 @@ module SlackAuthentication
 
   # OAuth callback
   def slack
-    start_session_and_redirect(params[:destination])
+    destination = '/api'
+    if request.env.has_key?('omniauth.params')
+      destination = request.env['omniauth.params']['destination'] unless request.env['omniauth.params']['destination'].blank?
+    end
+    start_session_and_redirect(destination)
   end
 end
