a55688b41a3e67ab0d8d09c482d111baa64aa910
Benjamin Foote <git@bnf.net>
2016-08-25 12:21:28 -0700

    #4960 set permissions on uploads

--- production/bin/start.sh
+++ production/bin/start.sh
@@ -3,10 +3,17 @@
 # start.sh
 # the Dockerfile CMD
 
+UPLOADS=${DEPLOYDIR}/current/public/uploads
+
+# TODO only run this on ${PRIMARY} nodes, perhaps in an out-of-band process during major multi-node deployments
+echo "running migrations"
 bundle exec rake db:migrate
 
-echo
-echo "migration complete... starting nginx"
-echo
+echo "setting permissions for uploads"
+chown -R ${DEPLOYUSER}:www-data ${UPLOADS}
+find ${UPLOADS} -type d -exec chmod 2777 {} \; # set the sticky bit on directories to preserve permissions
+find ${UPLOADS} -type f -exec chmod 0664 {} \; # files are 664
+
+echo "starting nginx"
 
 nginx
