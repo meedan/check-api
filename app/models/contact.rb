af8e2c4ec10d0088e6ab600a46e7463dc002b33b
infojunkie <karim.ratib@gmail.com>
2016-09-09 15:11:07 -0700

    Better phone validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ActiveRecord::Base
   attr_accessible
   belongs_to :team
-  phony_normalize :phone
+  phony_normalize :phone, default_country_code: 'US'
   validates :phone, phony_plausible: true
 
 end
