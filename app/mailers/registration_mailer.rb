40ac8c5b0d6eaa7ca1c8e77998e7f42bd7f95e52
Benjamin Foote <git@bnf.net>
2016-09-13 11:37:36 -0700

    update sent URL

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -3,7 +3,7 @@ class RegistrationMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url = CONFIG['checkdesk_base_url']
+    @url = CONFIG['checkdesk_client']
     mail(to: @user.email, subject: 'New account for you on Check')
   end
 end
