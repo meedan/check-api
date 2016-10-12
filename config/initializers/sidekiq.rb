351b258b25608b8da9d7a010095074784092db2e
infojunkie <karim.ratib@gmail.com>
2016-10-12 16:04:01 -0700

    Sidekiq configs, part 2

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
-SIDEKIQ_CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'sidekiq.yml'))
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{SIDEKIQ_CONFIG[:redis_host]}:#{SIDEKIQ_CONFIG[:redis_port]}/#{SIDEKIQ_CONFIG[:redis_database]}", namespace: "sidekiq_checkapi_#{Rails.env}" }
-end
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{SIDEKIQ_CONFIG[:redis_host]}:#{SIDEKIQ_CONFIG[:redis_port]}/#{SIDEKIQ_CONFIG[:redis_database]}", namespace: "sidekiq_checkapi_#{Rails.env}" }
+if File.exists? (File.join(Rails.root, 'config', 'sidekiq.yml'))
+  SIDEKIQ_CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'sidekiq.yml'))
+  Sidekiq.configure_server do |config|
+    config.redis = { url: "redis://#{SIDEKIQ_CONFIG[:redis_host]}:#{SIDEKIQ_CONFIG[:redis_port]}/#{SIDEKIQ_CONFIG[:redis_database]}", namespace: "sidekiq_checkapi_#{Rails.env}" }
+  end
+  Sidekiq.configure_client do |config|
+    config.redis = { url: "redis://#{SIDEKIQ_CONFIG[:redis_host]}:#{SIDEKIQ_CONFIG[:redis_port]}/#{SIDEKIQ_CONFIG[:redis_database]}", namespace: "sidekiq_checkapi_#{Rails.env}" }
+  end
 end
