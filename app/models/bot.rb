736541f5d90793c58ba9598f99d8150f2a7af9f5
Sawy <melsawy.be7eri@gmail.com>
2016-10-15 03:12:55 +0200

    Add profile_image for Bot

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -3,4 +3,8 @@ class Bot < ActiveRecord::Base
   mount_uploader :avatar, ImageUploader
   validates_presence_of :name
 
+  def profile_image
+    CONFIG['checkdesk_base_url'] + self.avatar.url
+  end
+
 end
