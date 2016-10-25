b20878ce5eb50bcb931d4487b11eed359002fb1c
Caio SBA <caiosba@gmail.com>
2016-10-25 17:09:35 -0300

    Avoid 503 on ElasticSearch migration

--- db/migrate/20161020211227_convert_statuses.rb
+++ db/migrate/20161020211227_convert_statuses.rb
@@ -1,8 +1,10 @@
 class ConvertStatuses < ActiveRecord::Migration
   def change
     Status.all_sorted.each do |s|
+      puts "Converting status #{s.id}..."
       s.status = s.status.gsub(' ', '_').downcase
       s.save!
+      sleep 1
     end
   end
 end
