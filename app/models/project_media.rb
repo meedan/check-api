02a43d78bf190296f0cded0a800da6e7edbb43fb
Caio SBA <caiosba@gmail.com>
2017-01-13 22:58:19 -0300

    Ticket #5590: Fixing Code Climate issue and code coverage

--- app/models/project_media.rb
+++ app/models/project_media.rb
@@ -125,26 +125,48 @@ class ProjectMedia < ActiveRecord::Base
     end
   end
 
+  protected
+
+  def create_image
+    m = UploadedImage.new
+    m.file = self.file
+    m.save!
+    m
+  end
+
+  def create_claim
+    m = Claim.new 
+    m.quote = self.quote
+    m.save!
+    m
+  end
+
+  def create_link
+    m = Link.new
+    m.url = self.url
+    # call m.valid? to get normalized URL before caling 'find_or_create_by'
+    m.valid?
+    m = Link.find_or_create_by(url: m.url)
+    m
+  end
+
+  def create_media
+    m = nil
+    if !self.file.blank?
+      m = self.create_image
+    elsif !self.quote.blank?
+      m = self.create_claim
+    else
+      m = self.create_link
+    end
+    m
+  end
+
   private
 
   def set_media
     unless self.url.blank? && self.quote.blank? && self.file.blank?
-      m = nil
-      if !self.file.blank?
-        m = UploadedImage.new
-        m.file = self.file
-        m.save!
-      elsif !self.quote.blank?
-        m = Claim.new 
-        m.quote = self.quote
-        m.save!
-      else
-        m = Link.new
-        m.url = self.url
-        # call m.valid? to get normalized URL before caling 'find_or_create_by'
-        m.valid?
-        m = Link.find_or_create_by(url: m.url)
-      end
+      m = self.create_media
       self.media_id = m.id unless m.nil?
     end
   end
