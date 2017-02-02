a9233a16e5a30269cb83d0d5368e215aafcbdabf
Caio SBA <caiosba@gmail.com>
2017-02-01 23:55:09 -0300

    Ticket #5614: Allow a specific Sidekiq config for testing

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,7 @@
-if File.exist?(File.join(Rails.root, 'config', 'sidekiq.yml'))
-  SIDEKIQ_CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'sidekiq.yml'))
+file = File.join(Rails.root, 'config', "sidekiq-#{Rails.env}.yml")
+file = File.join(Rails.root, 'config', "sidekiq.yml") unless File.exist?(file)
+if File.exist?(file)
+  SIDEKIQ_CONFIG = YAML.load_file(file)
   
   redis_config = { url: "redis://#{SIDEKIQ_CONFIG[:redis_host]}:#{SIDEKIQ_CONFIG[:redis_port]}/#{SIDEKIQ_CONFIG[:redis_database]}", namespace: "sidekiq_checkapi_#{Rails.env}" }
   
