f5464e98c784f234fc39798d75823f2acc0e7507
Daniela Soares Feitosa <danielafeitosa@gmail.com>
2017-02-14 16:00:28 -0300

    Ticket #5580: Added back line removed when merging with develop

--- app/models/annotations/tag.rb
+++ app/models/annotations/tag.rb
@@ -5,7 +5,7 @@ class Tag < ActiveRecord::Base
   field :full_tag, String, presence: true
 
   validates_presence_of :tag
-  validates :data, uniqueness: { scope: [:annotated_type, :annotated_id] }, if: lambda { |t| t.id.blank? }
+  validates :data, uniqueness: { scope: [:annotated_type, :annotated_id], message: I18n.t(:already_exists, default: 'already exists') }, if: lambda { |t| t.id.blank? }
 
   before_validation :normalize_tag, :store_full_tag
   after_save :add_update_elasticsearch_tag
