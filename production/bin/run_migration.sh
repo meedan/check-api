949aa2a0c8667f7cf88517bed1c90521fd6f8c1d
Benjamin Foote <git@bnf.net>
2017-02-17 15:55:32 -0800

    allow migration exit code to bubble up

--- production/bin/run_migration.sh
+++ production/bin/run_migration.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# start.sh
-# the Dockerfile CMD
+# run the migrations using the $DEPLOYUSER
+
+set -e
 
 LOGFILE=${DEPLOYDIR}/current/log/${RAILS_ENV}.log
 UPLOADS=${DEPLOYDIR}/shared/files/uploads
@@ -16,7 +17,11 @@ tail -f $LOGFILE &
 TAILPID=$!
 
 echo "running migrations"
-su ${DEPLOYUSER} -c "bundle exec rake db:migrate"
+# su to DEPLOYUSER and be sure to exit with the proper exit both inside and outside the migration
+su ${DEPLOYUSER} -c 'bundle exec rake db:migrate; exit $?'
+STATUS=$?
 
-echo "migrations complete"
+echo "migrations completed with exit $STATUS"
 kill $TAILPID
+
+exit $STATUS
