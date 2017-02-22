a69fca9e2a351c95d2e7c3132efdb7463e633a7a
Sawy <melsawy.be7eri@gmail.com>
2017-02-22 17:10:51 +0200

    Ticket #5160 - fix tests

--- lib/check_elastic_search.rb
+++ lib/check_elastic_search.rb
@@ -2,13 +2,14 @@ module CheckElasticSearch
 
   def update_media_search(keys, data = {})
     return if self.disable_es_callbacks
-    data = self.data if data.blank? and self.respond_to?(:data)
-    ElasticSearchWorker.perform_in(1.second, YAML::dump(self), YAML::dump(keys), YAML::dump(data), 'update_parent') unless data.blank?
+    ElasticSearchWorker.perform_in(1.second, YAML::dump(self), YAML::dump(keys), YAML::dump(data), 'update_parent')
   end
 
   def update_media_search_bg(keys, data)
     ms = get_elasticsearch_parent
     unless ms.nil?
+      # data = self.data if data.blank? and self.respond_to?(:data)
+      data = get_elasticsearch_data(data)
       options = {'last_activity_at' => Time.now.utc}
       keys.each{|k| options[k] = data[k] if ms.respond_to?("#{k}=") and !data[k].blank? }
       ms.update options
@@ -17,8 +18,7 @@ module CheckElasticSearch
 
   def add_update_media_search_child(child, keys, data = {})
     return if self.disable_es_callbacks
-    data = self.data if data.blank? and self.respond_to?(:data)
-    ElasticSearchWorker.perform_in(1.second, YAML::dump(self), YAML::dump(keys), YAML::dump(data), child) unless data.blank?
+    ElasticSearchWorker.perform_in(1.second, YAML::dump(self), YAML::dump(keys), YAML::dump(data), child)
   end
 
   def add_update_media_search_child_bg(child, keys, data)
@@ -38,6 +38,7 @@ module CheckElasticSearch
   end
 
   def store_elasticsearch_data(model, keys, data, options = {})
+    data = get_elasticsearch_data(data)
     keys.each do |k|
       model.send("#{k}=", data[k]) if model.respond_to?("#{k}=") and !data[k].blank?
     end
@@ -49,6 +50,10 @@ module CheckElasticSearch
     pm = self.annotated_id if pm.nil? and self.is_annotation? and self.annotated_type == 'ProjectMedia'
     sleep 1 if Rails.env == 'test'
     MediaSearch.search(query: { match: { annotated_id: pm } }).last unless pm.nil?
+  end
+
+  def get_elasticsearch_data(data)
+    (data.blank? and self.respond_to?(:data)) ? self.data : data
   end
 
 end
