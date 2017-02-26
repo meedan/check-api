7bab130377df7572f2c645bb2de247df851ec271
infojunkie <karim.ratib@gmail.com>
2017-02-26 12:35:39 -0800

    Remove hardcoded English words and update localizations #5349

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -9,10 +9,9 @@ class RegistrationMailer < ApplicationMailer
 
   def duplicate_email_detection(user, duplicate)
     @user = user
-    @provider = duplicate.provider? ? "#{duplicate.provider.camelcase}" : 'email'
-    @provider_prefix = duplicate.provider? ? 'a' : 'an'
-    @login_with = user.provider? ? "a #{user.provider.camelcase} account linked to" : 'an email-based account using'
-    mail(to: @user.email, subject: I18n.t(:mail_duplicate_email_exists, default: "Your Login Method for Check"))
+    @duplicate_provider = duplicate.provider? ? duplicate.provider.camelcase : I18n.t(:mail_duplicate_email_exists_email, default: 'email')
+    @user_provider = user.provider? ? user.provider.camelcase : I18n.t(:mail_duplicate_email_exists_email, default: 'email')
+    mail(to: @user.email, subject: I18n.t(:mail_duplicate_email_exists, default: "Your login method for Check"))
   end
 
 end
