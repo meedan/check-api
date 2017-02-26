45fda77e1c2ee752f3d4e2a2e0b200fc3156f73f
infojunkie <karim.ratib@gmail.com>
2017-02-26 13:02:56 -0800

    Fix email copy and remove default strings #5349

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -9,9 +9,9 @@ class RegistrationMailer < ApplicationMailer
 
   def duplicate_email_detection(user, duplicate)
     @user = user
-    @duplicate_provider = duplicate.provider? ? duplicate.provider.camelcase : I18n.t(:mail_duplicate_email_exists_email, default: 'email')
-    @user_provider = user.provider? ? user.provider.camelcase : I18n.t(:mail_duplicate_email_exists_email, default: 'email')
-    mail(to: @user.email, subject: I18n.t(:mail_duplicate_email_exists, default: "Your login method for Check"))
+    @duplicate_provider = duplicate.provider? ? duplicate.provider.camelcase : I18n.t(:mail_duplicate_email_exists_email)
+    @user_provider = user.provider? ? user.provider.camelcase : I18n.t(:mail_duplicate_email_exists_email)
+    mail(to: @user.email, subject: I18n.t(:mail_duplicate_email_exists))
   end
 
 end
