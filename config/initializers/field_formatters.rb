3be22cdcc245bd3cc656c7ee0dcc6f42caf3f85e
infojunkie <karim.ratib@gmail.com>
2017-04-26 13:56:35 -0700

    Complete refactoring + fix tests #5947

--- config/initializers/field_formatters.rb
+++ config/initializers/field_formatters.rb
@@ -10,13 +10,6 @@ DynamicAnnotation::Field.class_eval do
     end
   end
 
-  def response_value(field_value)
-    value = JSON.parse(field_value)
-    answer = value['selected'] || []
-    answer.insert(-1, value['other']) if !value['other'].blank?
-    answer.to_sentence(locale: I18n.locale)
-  end
-
   def field_formatter_name_response_single_choice
     response_value(self.value)
   end
@@ -24,5 +17,14 @@ DynamicAnnotation::Field.class_eval do
   def field_formatter_name_response_multiple_choice
     response_value(self.value)
   end
+
+  private
+
+    def response_value(field_value)
+      value = JSON.parse(field_value)
+      answer = value['selected'] || []
+      answer.insert(-1, value['other']) if !value['other'].blank?
+      answer.to_sentence(locale: I18n.locale)
+    end
 
 end
