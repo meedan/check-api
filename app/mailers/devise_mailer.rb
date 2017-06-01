4673d2c499ef19230c0cb057f27ae481678fc240
Daniela Soares Feitosa <danielafeitosa@gmail.com>
2017-06-01 16:47:20 -0300

    Ticket #6057: Replace `app_name`to `product_name`

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -5,13 +5,13 @@ class DeviseMailer < Devise::Mailer
   def confirmation_instructions(record, token, opts={})
     @host = CONFIG['checkdesk_base_url']
     @client_host = CONFIG['checkdesk_client']
-    opts[:subject] = I18n.t(:mail_account_confirmation, default: "%{app_name} account confirmation", app_name: CONFIG['app_name'])
+    opts[:subject] = I18n.t(:mail_account_confirmation, default: "%{product_name} account confirmation", product_name: CONFIG['product_name'])
     super
   end
 
   def reset_password_instructions(record, token, opts={})
     @host = CONFIG['checkdesk_base_url']
-    opts[:subject] = I18n.t(:reset_password_instructions, default: "%{app_name} reset password instructions", app_name: CONFIG['app_name'])
+    opts[:subject] = I18n.t(:reset_password_instructions, default: "%{product_name} reset password instructions", product_name: CONFIG['product_name'])
     super
   end
 end
