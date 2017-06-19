59d6e20b58c58ce301e8325f136c5988b2404a91
Sawy <melsawy.be7eri@gmail.com>
2017-06-19 22:09:04 +0200

    Ticket #5654 - fix tests

--- test/workers/machine_translation_worker_test.rb
+++ test/workers/machine_translation_worker_test.rb
@@ -5,6 +5,10 @@ class MachineTranslationWorkerTest < ActiveSupport::TestCase
     super
     require 'sidekiq/testing'
     Sidekiq::Testing.fake!
+    ft = DynamicAnnotation::FieldType.where(field_type: 'language').last || create_field_type(field_type: 'language', label: 'Language')
+    at = create_annotation_type annotation_type: 'language', label: 'Language'
+    create_field_instance annotation_type_object: at, name: 'language', label: 'Language', field_type_object: ft, optional: false
+
     ft = DynamicAnnotation::FieldType.where(field_type: 'json').last || create_field_type(field_type: 'json', label: 'JSON structure')
     at = create_annotation_type annotation_type: 'mt', label: 'Machine translation'
     create_field_instance annotation_type_object: at, name: 'mt_translations', label: 'Machine translations', field_type_object: ft, optional: false
@@ -14,14 +18,14 @@ class MachineTranslationWorkerTest < ActiveSupport::TestCase
     Sidekiq::Testing.fake!
     MachineTranslationWorker.drain
     assert_equal 0, MachineTranslationWorker.jobs.size
+    t = create_team
+    p = create_project team: t
+    p.settings = {:languages => ['ar']}; p.save!
     stub_configs({ 'alegre_host' => 'http://alegre', 'alegre_token' => 'test' }) do
       text = 'Testing'
       url = CONFIG['alegre_host'] + "/api/languages/identification?text=" + text
       response = '{"type":"language","data": [["EN", 1]]}'
       WebMock.stub_request(:get, url).with(:headers => {'X-Alegre-Token'=> CONFIG['alegre_token']}).to_return(body: response)
-      t = create_team
-      p = create_project team: t
-      p.settings = {:languages => ['ar']}; p.save!
       pm = create_project_media project: p, quote: text
       pm.update_mt = 1
       assert_equal 1, MachineTranslationWorker.jobs.size
