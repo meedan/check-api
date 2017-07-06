b8db1be240b924fc49c942fdd7de60f719012f8b
Sawy <melsawy.be7eri@gmail.com>
2017-07-07 01:04:52 +0200

    Apply Caio's comments

--- db/migrate/20170706201604_add_source_id_to_user.rb
+++ db/migrate/20170706201604_add_source_id_to_user.rb
@@ -3,8 +3,7 @@ class AddSourceIdToUser < ActiveRecord::Migration
     add_reference :users, :source, index: true, foreign_key: true
     User.find_each do |u|
       s = Source.where(user_id: u.id).first
-      u.source_id = s.id
-      u.save!
+      u.update_columns(source_id: s.id) unless s.nil?
     end
   end
 end
--- db/migrate/20170706214243_add_team_to_sources.rb
+++ db/migrate/20170706214243_add_team_to_sources.rb
@@ -2,9 +2,9 @@ class AddTeamToSources < ActiveRecord::Migration
   def change
     add_reference :sources, :team, index: true, foreign_key: true
     Source.find_each do |s|
-      t = s.user.teams.first
-      s.team = t
-      s.save!
+      u = s.user
+      t = u.teams.first unless u.nil?
+      s.update_columns(team_id: t.id) unless t.nil?
     end
   end
 end
