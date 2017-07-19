813d735c3c2b46f495618af18bde948e54963228
Caio SBA <caiosba@gmail.com>
2017-07-19 11:58:01 -0300

    Ticket #6172: Allow auto-refresh when source is added

--- app/graph/mutations/project_source_mutations.rb
+++ app/graph/mutations/project_source_mutations.rb
@@ -12,5 +12,5 @@ module ProjectSourceMutations
     id: '!id'
   }
 
-  Create, Update, Destroy = GraphqlCrudOperations.define_crud_operations('project_source', create_fields, update_fields)
+  Create, Update, Destroy = GraphqlCrudOperations.define_crud_operations('project_source', create_fields, update_fields, ['check_search_team', 'check_search_project'])
 end
