95186d94e952e9440f5fc7229867d515f9ea0dd9
Sawy <melsawy.be7eri@gmail.com>
2017-07-20 11:45:18 +0200

    Ticket #6111 - fix mapping for test env

--- db/migrate/20170720090144_add_es_mapping_for_account.rb
+++ db/migrate/20170720090144_add_es_mapping_for_account.rb
@@ -1,5 +1,6 @@
 class AddEsMappingForAccount < ActiveRecord::Migration
   def change
+    return if Rails.env === 'test'
     client = MediaSearch.gateway.client
     client.indices.put_mapping index: MediaSearch.index_name, type: 'account_search', body: AccountSearch.mappings.to_hash
   end
