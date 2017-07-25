dc2bb701a90aef89ea702a6d9ef1d61de9655e0b
Caio SBA <caiosba@gmail.com>
2017-07-25 17:36:39 -0300

    Ticket #6191: Skip default image

--- app/models/source.rb
+++ app/models/source.rb
@@ -44,7 +44,7 @@ class Source < ActiveRecord::Base
   end
 
   def image
-    return CONFIG['checkdesk_base_url'] + self.file.url unless self.file.nil?
+    return CONFIG['checkdesk_base_url'] + self.file.url if !self.file.nil? && self.file.url != '/images/source.png'
     self.avatar || (self.accounts.empty? ? '' : self.accounts.first.data['picture'].to_s)
   end
 
