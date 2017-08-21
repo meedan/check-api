5acafa88ccb0265b80017e588ad1c24395579184
infojunkie <karim.ratib@gmail.com>
2017-08-21 13:03:33 -0700

    Fix syntax error #6213

--- app/models/source.rb
+++ app/models/source.rb
@@ -23,7 +23,7 @@ class Source < ActiveRecord::Base
 
   notifies_pusher on: :update,
                   event: 'source_updated',
-                  data: proc { |s| s.to_json }
+                  data: proc { |s| s.to_json },
                   targets: proc { |s| [s, s.projects] }
 
   def user_id_callback(value, _mapping_ids = nil)
