be2f25db0f19f4e66fdaedf2ef21b282878b4686
Caio Almeida <caiosba@gmail.com>
2017-10-11 18:32:52 -0300

    Ticket #6209: Fixing parallel setup

--- scripts/setup-parallel-env.sh
+++ scripts/setup-parallel-env.sh
@@ -7,6 +7,7 @@ FAIL=0
 RAILS_ENV=test bundle exec rake db:create db:migrate &
 RAILS_ENV=test TEST_ENV_NUMBER=1 bundle exec rake db:create db:migrate &
 RAILS_ENV=test TEST_ENV_NUMBER=2 bundle exec rake db:create db:migrate &
+RAILS_ENV=test TEST_ENV_NUMBER=3 bundle exec rake db:create db:migrate &
 
 for job in `jobs -p`
 do
