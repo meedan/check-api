29feb314b9b4cca6dba28c1bc9e02bbb9dd4405c
Caio Almeida <caiosba@gmail.com>
2017-10-12 16:44:26 -0300

    Ticket #6209: Trying to send coverage to Code Climate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,19 @@ require 'webmock/minitest'
 require 'mocha/test_unit'
 require 'sample_data'
 require 'parallel_tests/test/runtime_logger'
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  nocov_token 'nocov'
+  merge_timeout 3600
+  command_name "Tests #{rand(100000)}"
+  add_filter do |file|
+    (!file.filename.match(/\/app\/controllers\/[^\/]+\.rb$/).nil? && file.filename.match(/application_controller\.rb$/).nil?) ||
+    !file.filename.match(/\/app\/controllers\/concerns\/[^\/]+_doc\.rb$/).nil? ||
+    !file.filename.match(/\/lib\/sample_data\.rb$/).nil?
+  end
+  coverage_dir 'coverage'
+end
 
 class ActionController::TestCase
   include Devise::Test::ControllerHelpers
