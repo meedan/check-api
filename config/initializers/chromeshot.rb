e733402e7aa74386e00849849a4275450a231dbd
Daniela Soares Feitosa <danielafeitosa@gmail.com>
2017-11-16 19:39:52 -0300

    Ticket #6114: Move chromeshot from initializer config to test coverage task

--- config/initializers/chromeshot.rb
+++ config/initializers/chromeshot.rb
@@ -1,6 +1,6 @@
 require 'chromeshot'
 
-puts "Starting Chromeshot on port #{CONFIG['chrome_debug_port']}"
-
-Chromeshot::Screenshot.setup_chromeshot(CONFIG['chrome_debug_port'])
-
+unless Rails.env.test?
+  puts "Starting Chromeshot on port #{CONFIG['chrome_debug_port']}"
+  Chromeshot::Screenshot.setup_chromeshot(CONFIG['chrome_debug_port'])
+end
