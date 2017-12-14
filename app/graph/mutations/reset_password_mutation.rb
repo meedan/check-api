092a0fe9bbb4fa3cc6151647cfefe30726a98499
infojunkie <karim.ratib@gmail.com>
2017-12-14 15:08:52 -0800

    Return token expiry time #6584

--- app/graph/mutations/reset_password_mutation.rb
+++ app/graph/mutations/reset_password_mutation.rb
@@ -4,15 +4,16 @@ ResetPasswordMutation = GraphQL::Relay::Mutation.define do
   input_field :email, !types.String
 
   return_field :success, types.Boolean
+  return_field :expiry, types.Int
 
   resolve -> (_root, inputs, _ctx) {
     user = User.where(email: inputs[:email]).last
     if user.nil?
-      raise ActiveRecord::RecordNotFound, I18n.t(:email_not_found, default: 'E-mail not found') 
+      raise ActiveRecord::RecordNotFound, I18n.t(:email_not_found, default: 'E-mail not found')
     else
       user.skip_check_ability = true
       user.send_reset_password_instructions
-      { success: true }
+      { success: true, expiry: User.reset_password_within.to_i }
     end
   }
 end
