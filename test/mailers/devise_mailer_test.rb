ce14e6ea1b204c346ef774e57d73746d3f120eef
infojunkie <karim.ratib@gmail.com>
2017-12-15 09:21:57 -0800

    Fix expiry time in email template #6584

--- test/mailers/devise_mailer_test.rb
+++ test/mailers/devise_mailer_test.rb
@@ -17,7 +17,7 @@ class DeviseMailerTest < ActionMailer::TestCase
   end
 
   test "should send reset password instructions" do
-    u = create_user name: 'Test User', provider: '', email: 'test@mail.com', reset_password_within: 6.hours
+    u = create_user name: 'Test User', provider: '', email: 'test@mail.com'
     email = DeviseMailer.reset_password_instructions(u, '12345')
 
     assert_emails 1 do
