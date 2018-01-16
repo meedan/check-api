7360c2fded16536dd69c29e13601df7673507300
Caio Almeida <caiosba@gmail.com>
2018-01-16 16:25:44 -0300

    Ticket #6600: Removing VideoVault from Keep bot, since now it has been moved to Pender, and refactoring the Keep bot and webhook to support any archiver

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -328,10 +328,6 @@ RailsAdmin.config do |config|
       configure :get_source_verification_statuses, :json do
         label 'Source verification statuses'
       end
-      configure :get_keep_enabled do
-        label 'Enable Keep archiving'
-        visible_only_for_admin
-      end
       configure :get_hide_names_in_embeds do
         label 'Hide names in embeds'
       end
@@ -403,16 +399,6 @@ RailsAdmin.config do |config|
         partial "json_editor"
         help "A list of custom verification statuses for reports that match your team's journalistic guidelines."
         visible_only_for_allowed_teams 'custom_statuses'
-      end
-      field :keep_enabled, :boolean do
-        label 'Enable Keep archiving'
-        formatted_value{ bindings[:object].get_keep_enabled }
-        help ''
-        visible_only_for_admin
-        visible_only_for_allowed_teams 'keep_integration'
-        hide do
-          bindings[:object].new_record?
-        end
       end
       field :hide_names_in_embeds, :boolean do
         label 'Hide names in embeds'
