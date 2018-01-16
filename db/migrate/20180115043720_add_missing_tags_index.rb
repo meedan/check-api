7e71bb4821900b9f3896469c4217c5e3afd1a645
Sawy <melsawy.be7eri@gmail.com>
2018-01-16 09:01:03 +0200

    Ticket #6626 - fix migration

--- db/migrate/20180115043720_add_missing_tags_index.rb
+++ db/migrate/20180115043720_add_missing_tags_index.rb
@@ -1,7 +1,7 @@
 class AddMissingTagsIndex < ActiveRecord::Migration
   def change
   	t_ids = Annotation.where(annotation_type: 'tag').map(&:id)
-  	ts_ids = TagSearch.search(query: { match_all: {  } }).results.map(&:_id)
+  	ts_ids = TagSearch.search(query: { match_all: {  } }, size: 10000).results.map(&:_id)
   	ids = t_ids - ts_ids.map(&:to_i) 
   	unless ids.blank?
   		Tag.where(id: ids).find_each do |t|
