4628d6a449dea1771e4b36b77f4015c251a1ab87
Caio Almeida <caiosba@gmail.com>
2018-01-25 12:26:42 -0300

    Ticket #6575: Fixing Code Climate issue

--- app/graph/types/user_type.rb
+++ app/graph/types/user_type.rb
@@ -52,7 +52,7 @@ UserType = GraphqlCrudOperations.define_default_type do
   end
 
   connection :assignments, -> { ProjectMediaType.connection_type } do
-    resolve ->(user, args, _ctx) {
+    resolve ->(user, _args, _ctx) {
       Annotation.project_media_assigned_to_user(user).order('id DESC')
     }
   end
