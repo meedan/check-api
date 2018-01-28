2ae35ce41fb232fbbce3f8242d967e15e6e76a8b
Caio Almeida <caiosba@gmail.com>
2018-01-28 02:08:07 -0300

    Ticket #6575: Fixing Code Climate issue

--- test/mailers/assignment_mailer_test.rb
+++ test/mailers/assignment_mailer_test.rb
@@ -2,16 +2,10 @@ require_relative '../test_helper'
 
 class AssignmentMailerTest < ActionMailer::TestCase
   test "should notify about report assignment" do
-    t = create_team
     u = create_user
-    u1 = create_user email: 'user1@mail.com'
-    u2 = create_user email: 'user2@mail.com'
-    create_team_user team: t, user: u1
-    create_team_user team: t, user: u2
-    p = create_project team: t
-    pm = create_project_media project: p
+    t = create_task
 
-    email = AssignmentMailer.notify(:assign_report, u, 'user1@mail.com', pm)
+    email = AssignmentMailer.notify(:assign_status, u, 'user1@mail.com', t.id)
 
     assert_emails 1 do
       email.deliver_now
