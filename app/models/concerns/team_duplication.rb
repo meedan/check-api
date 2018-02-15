a6b459d5d0331d459c7f65a15c03fc1f1a87ddff
Daniela Soares Feitosa <danielafeitosa@gmail.com>
2018-02-15 10:31:16 -0300

    Ticket #6599: Use deep_cloneable gem on duplication

--- app/models/concerns/team_duplication.rb
+++ app/models/concerns/team_duplication.rb
@@ -4,18 +4,31 @@ module TeamDuplication
   extend ActiveSupport::Concern
 
   included do
-    attr_accessor :project_mapping
+    attr_accessor :project_mapping, :source_mapping
 
     def self.duplicate(t)
-      team = t.dup
-      team.generate_slug
-      File.open(t.logo.path) { |f| team.logo = f }
-      team.copy_projects(t)
-      team.copy_contacts(t)
+      team = t.deep_clone include: [ { projects: [ :project_sources, :project_medias] }, :team_users, :contacts, :sources ] do |original, copy|
+        if copy.is_a? Team
+          copy.logo = original.logo
+          copy.generate_slug
+        end
+        if copy.is_a? Project
+          copy.lead_image = original.lead_image
+          copy.token = nil
+          @project_mapping ||= {}
+          @project_mapping[original.id] = copy
+        end
+        if copy.is_a? Source
+          copy.file = original.file
+          @source_mapping ||= {}
+          @source_mapping[original.id] = copy
+        end
+
+      end
       team.save!
 
-      team.update_team_checklist
-      team.copy_team_users(t)
+      team.update_team_checklist(@project_mapping)
+      team.update_project_sources(@source_mapping)
       team
     end
   end
@@ -23,42 +36,32 @@ module TeamDuplication
   def generate_slug
     i = 1
     loop do
-      slug = self.slug.concat("-copy-#{i}")
-      break unless Team.find_by(slug: slug)
-      i += 1
+      slug = self.slug + "-copy-#{i}"
+      if Team.find_by(slug: slug)
+        i += 1
+      else
+        self.slug = slug
+        break
+      end
     end
   end
 
-  def copy_projects(t)
-    self.project_mapping = {}
-    t.projects.each do |project|
-      p = project.dup
-      image = project.lead_image.path
-      File.open(image) { |f| p.lead_image = f } if image
-      p.token = nil
-      self.project_mapping[project.id] = p
-      self.projects << p
-    end
-  end
-
-  def update_team_checklist
+  def update_team_checklist(project_mapping)
     return if self.get_checklist.blank?
     self.get_checklist.each do |task|
-      task[:projects].map! { |p| self.project_mapping[p] ? self.project_mapping[p].id : p } if task[:projects]
+      task[:projects].map! { |p| project_mapping[p] ? project_mapping[p].id : p } if task[:projects]
     end
     self.save
   end
 
-  def copy_team_users(t)
-    t.team_users.each do |tu|
-      self.team_users << tu.dup
+  def update_project_sources(source_mapping)
+    self.projects.each do |project|
+      project.project_sources.each do |ps|
+        ps.source_id = source_mapping[ps.source_id] ? source_mapping[ps.source_id].id : ps.source_id
+        ps.save
+      end
     end
   end
 
-  def copy_contacts(t)
-    t.contacts.each do |c|
-      self.contacts << c.dup
-    end
-  end
 
 end
