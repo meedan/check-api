7841ebdaa8c143b2e54194bf617622f862e084c6
Caio Almeida <caiosba@gmail.com>
2018-02-16 21:08:43 -0300

    Ticket #6679: Remove Chrome dependency and request screenshot from Pender

--- app/models/bot/viber.rb
+++ app/models/bot/viber.rb
@@ -6,6 +6,9 @@ class Bot::Viber < ActiveRecord::Base
   end
 
   def text_to_image(m)
+    require 'open-uri'
+
+    # Prepare HTML
     av = ActionView::Base.new(Rails.root.join('app', 'views'))
     av.assign(m)
     content = av.render(template: 'viber/screenshot.html.erb', layout: nil)
@@ -13,10 +16,26 @@ class Bot::Viber < ActiveRecord::Base
     html_path = File.join(Rails.root, 'public', 'viber', filename + '.html')
     File.atomic_write(html_path) { |file| file.write(content) }
 
-    fetcher = Chromeshot::Screenshot.new debug_port: CONFIG['chrome_debug_port']
-    fetcher.take_screenshot!(url: CONFIG['checkdesk_base_url_private'] + '/viber/' + filename + '.html', output: File.join(Rails.root, 'public', 'viber', filename + '.jpg'))
+    # Request screenshot from Pender
+    url = CONFIG['checkdesk_base_url'] + '/viber/' + filename + '.html'
+    output = File.join(Rails.root, 'public', 'viber', filename)
+    params = { url: url }
+    result = PenderClient::Request.get_medias(CONFIG['pender_url_private'], params, CONFIG['pender_key'])
+    attempts = 0
+    while attempts < 20 && result['data']['screenshot_taken'].to_i == 0
+      sleep 15
+      attempts += 1
+      result = PenderClient::Request.get_medias(CONFIG['pender_url_private'], params, CONFIG['pender_key'])
+    end
 
+    # Save screenshot and remove HTML
+    screenshot = result['data']['screenshot'].gsub(CONFIG['pender_url'], CONFIG['pender_url_private'])
+    open(screenshot) do |f|
+      File.atomic_write("#{output}.png") { |file| file.write(f.read) }
+    end
+    system 'convert', "#{output}.png", "#{output}.jpg"
     FileUtils.rm_f html_path
+    FileUtils.rm_f "#{output}.png"
     filename
   end
 
