c07fde13d441d40b39bab4a8933251c860f9b858
Sawy <melsawy.be7eri@gmail.com>
2018-02-21 21:05:20 +0200

    Ticket #6649 - resendConfiramtion using UserID

--- app/graph/mutations/resend_confirmation_mutation.rb
+++ app/graph/mutations/resend_confirmation_mutation.rb
@@ -1,12 +1,12 @@
 ResendConfirmationMutation = GraphQL::Relay::Mutation.define do
   name 'ResendConfirmation'
 
-  input_field :email, !types.String
+  input_field :id, !types.Int
 
   return_field :success, types.Boolean
 
   resolve -> (_root, inputs, _ctx) {
-    user = User.where(email: inputs[:email]).last
+    user = User.where(id: inputs[:id]).last
     if user.nil?
       raise ActiveRecord::RecordNotFound, I18n.t(:email_not_found, default: 'E-mail not found')
     else
