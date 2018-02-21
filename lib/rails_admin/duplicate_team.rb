99a82964076feeae91866f530e1c29e554f2c27c
Daniela Soares Feitosa <danielafeitosa@gmail.com>
2018-02-21 18:19:36 -0300

    Ticket #6599: Add action to Rails Admin

---
+++ lib/rails_admin/duplicate_team.rb
@@ -0,0 +1,45 @@
+require 'rails_admin/config/actions'
+require 'rails_admin/config/actions/base'
+
+module RailsAdmin
+  module Config
+    module Actions
+      class DuplicateTeam < RailsAdmin::Config::Actions::Base
+        RailsAdmin::Config::Actions.register(self)
+        register_instance_option :link_icon do
+          'icon-retweet'
+        end
+        register_instance_option :member? do
+          true
+        end
+        register_instance_option :http_methods do
+          [:get, :put]
+        end
+        register_instance_option :pjax? do
+          false
+        end
+        register_instance_option :controller do
+          proc do
+            RailsAdmin::MainController.class_eval { respond_to :html, :js }
+            if request.get?
+              respond_with(@object)
+            elsif request.put?
+              RequestStore.store[:ability] = :admin
+              copy = Team.duplicate(@object)
+              if copy.valid?
+                respond_to do |format|
+                flash[:success] = t('admin.actions.duplicate_team.done')
+
+                  format.html { redirect_to_on_success }
+                end
+              else
+                handle_save_error
+              end
+            end
+
+          end
+        end
+      end
+    end
+  end
+end
