7b856f521c047b18660e4cb976ecad4fc8d6d155
Daniela Soares Feitosa <danielafeitosa@gmail.com>
2018-03-01 19:23:02 -0300

    Ticket #6599: Copy versions

--- app/models/concerns/team_duplication.rb
+++ app/models/concerns/team_duplication.rb
@@ -11,21 +11,28 @@ module TeamDuplication
       begin
         ActiveRecord::Base.transaction do
           team = t.deep_clone include: [ { projects: [ :project_sources, { project_medias: :versions } ] }, :team_users, :contacts, :sources ] do |original, copy|
-            @mapping[original.class_name.to_sym] ||= {}
-            @mapping[original.class_name.to_sym][original.id] = copy
+            self.set_mapping(original, copy)
             self.copy_image(original, copy)
+            self.versions_log_mapping(original, copy)
           end
           team.is_being_copied = true
           team.save!
           team.update_team_checklist(@mapping[:Project])
-          self.copy_annotations(@mapping)
+          self.copy_annotations
+          self.copy_versions(@mapping[:"PaperTrail::Version"])
           team
         end
       rescue StandardError => e
         Airbrake.notify(e) if Airbrake.configuration.api_key
-        Rails.logger.error "[Team Duplication] Could not duplicate team #{t.slug}: #{e.message}"
+        Rails.logger.error "[Team Duplication] Could not duplicate team #{t.slug}: #{e.message} #{e.backtrace.join("\n")}"
         nil
       end
+    end
+
+    def self.set_mapping(object, copy)
+      key = object.class_name.to_sym
+      @mapping[key] ||= {}
+      @mapping[key][object.id] = copy
     end
 
     def self.copy_image(original, copy)
@@ -36,18 +43,25 @@ module TeamDuplication
       end
     end
 
-    def self.copy_annotations(mapping)
+    def self.versions_log_mapping(original, copy)
+      if original.respond_to?(:get_versions_log)
+        original.get_versions_log.find_each do |log|
+          self.set_mapping(log, copy)
+        end
+      end
+    end
+
+    def self.copy_annotations
       [:ProjectMedia, :ProjectSource, :Source].each do |type|
-        next if mapping[type].blank?
-        mapping[type].each_pair do |original, copy|
+        next if @mapping[type].blank?
+        @mapping[type].each_pair do |original, copy|
           type.to_s.constantize.find(original).annotations.find_each do |a|
             a = a.load
             annotation = a.dup
             annotation.annotated = copy
             annotation.save!
-            mapping[a.class_name.to_sym] ||= {}
-            mapping[a.class_name.to_sym][a.id] = annotation.id
-            self.copy_annotation_fields(a, annotation, mapping[:Task])
+            self.set_mapping(a, annotation)
+            self.copy_annotation_fields(a, annotation, @mapping[:Task])
           end
         end
       end
@@ -57,9 +71,21 @@ module TeamDuplication
       original.get_fields.each do |f|
         field = f.dup
         field.annotation_id = copy.id
-        field.value = task_mapping[f.value.to_i].to_s if field.field_type == "task_reference"
+        field.value = task_mapping[f.value.to_i].id.to_s if field.field_type == "task_reference"
         field.save!
+        self.set_mapping(f, field)
       end
+    end
+
+    def self.copy_versions(versions_mapping)
+      PaperTrail::Version.skip_callback(:create, :after, :increment_project_association_annotations_count)
+      versions_mapping.each_pair do |original, copy|
+        log = PaperTrail::Version.find(original).dup
+        log.associated_id = copy.id
+        log.item_id = @mapping[log.item_type.to_sym][log.item_id.to_i].id.to_s
+        log.save!
+      end
+      PaperTrail::Version.set_callback(:create, :after, :increment_project_association_annotations_count)
     end
 
   end
