8fbc7e406fc38e9fca15cdce015f9dbf7ff91455
Caio Almeida <caiosba@gmail.com>
2018-03-01 17:41:28 -0300

    Ticket #6682: Fixing last Code Climate issue

--- app/models/project_media.rb
+++ app/models/project_media.rb
@@ -292,8 +292,7 @@ class ProjectMedia < ActiveRecord::Base
   def set_es_account_data
     data = {}
     a = self.media.account
-    embed = a.embed
-    self.overridden_embed_attributes.each{ |k| sk = k.to_s; data[sk] = embed[sk] unless embed[sk].nil? } unless embed.nil?
+    self.overridden_embed_attributes.each{ |k| sk = k.to_s; data[sk] = a.embed[sk] unless a.embed[sk].nil? } unless a.embed.nil?
     data["id"] = a.id unless data.blank?
     [data]
   end
