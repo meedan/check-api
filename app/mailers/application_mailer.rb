535bd7ce9c35e4e0e01bc2fb7e95f5fd63d911b2
Sawy <melsawy.be7eri@gmail.com>
2018-03-12 20:52:58 +0200

    Ticket #6651

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -7,7 +7,7 @@ class ApplicationMailer < ActionMailer::Base
   private
 
   def prevent_delivery
-    u = User.where(email: mail.to.first).last # TODO: Review this part for multiple emails
+    u = User.where(email: mail.to.first).last unless mail.to.blank? # TODO: Review this part for multiple emails
     mail.perform_deliveries = false if !u.nil? && u.get_send_email_notifications == "0"
   end
 end
