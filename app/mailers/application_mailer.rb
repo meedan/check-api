84092194e0082e357733ba5fe4aca0f05fa7e4bb
Sawy <melsawy.be7eri@gmail.com>
2018-03-13 08:58:50 +0200

    Ticket #6736

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,15 @@
 class ApplicationMailer < ActionMailer::Base
   default from: CONFIG['default_mail']
   layout 'mailer'
+
+  protected
+
+  def send_email_to_recipients(recipients, subject)
+    recipients = Bounce.remove_bounces(recipients)
+    unless recipients.empty?
+      Rails.logger.info "Sending e-mail to #{recipients}"
+      mail(to: recipients, subject: subject)
+    end
+  end
+  
 end
--- app/mailers/team_user_mailer.rb
+++ app/mailers/team_user_mailer.rb
@@ -25,13 +25,4 @@ class TeamUserMailer < ApplicationMailer
     end
   end
 
-  protected
-
-  def send_email_to_recipients(recipients, subject)
-    recipients = Bounce.remove_bounces(recipients)
-    unless recipients.empty?
-      Rails.logger.info "Sending e-mail to #{recipients}"
-      mail(to: recipients, subject: subject)
-    end
-  end
 end
