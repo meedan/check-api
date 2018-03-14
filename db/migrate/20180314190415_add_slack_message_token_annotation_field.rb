82d8727c754e633cc754d74090cd172b1416b822
Caio Almeida <caiosba@gmail.com>
2018-03-14 17:03:53 -0300

    Ticket #6730: Fixing migration

--- db/migrate/20180314190415_add_slack_message_token_annotation_field.rb
+++ db/migrate/20180314190415_add_slack_message_token_annotation_field.rb
@@ -1,4 +1,7 @@
 class AddSlackMessageTokenAnnotationField < ActiveRecord::Migration
+  require 'sample_data'
+  include SampleData
+
   def change
     ft = DynamicAnnotation::FieldType.where(field_type: 'text').last || create_field_type(field_type: 'text', label: 'Text')
     at = DynamicAnnotation::AnnotationType.where(annotation_type: 'slack_message').last
