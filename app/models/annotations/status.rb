31277b946ee1c0d2bb02819fd89be3acb9439116
Sawy <melsawy.be7eri@gmail.com>
2018-03-28 08:14:44 +0200

    Ticket #6559 - fix CC

--- app/models/annotations/status.rb
+++ app/models/annotations/status.rb
@@ -33,7 +33,7 @@ class Status < ActiveRecord::Base
   def self.validate_custom_statuses(team_id, statuses)
     keys = statuses[:statuses].collect{|s| s[:id]}
     project_medias = ProjectMedia.joins(:project).where({ 'projects.team_id' => team_id })
-    project_medias.collect{|pm| s = pm.last_status; {project_media: pm.id, url: pm.full_url, status: s} unless keys.include?s}.compact
+    project_medias.collect{|pm| s = pm.last_status; {project_media: pm.id, url: pm.full_url, status: s} unless keys.include?(s)}.compact
   end
 
   def store_previous_status
