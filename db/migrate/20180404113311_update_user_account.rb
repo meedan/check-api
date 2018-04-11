dc5bdd92f1e199118d3b58f4d3d1b17ed57de32e
Daniela Soares Feitosa <daniela@meedan.com>
2018-04-11 01:17:35 -0300

    Ticket #6591: Rename migration
    Also skip account update to avoid crash when account is nil

--- db/migrate/20180404113311_update_user_account.rb
+++ db/migrate/20180404113311_update_user_account.rb
@@ -2,8 +2,10 @@ class UpdateUserAccount < ActiveRecord::Migration
   def change
     User.where(provider: 'slack').find_each do |u|
       account = u.account
-      account.created_on_registration = true
-      account.refresh_embed_data
+      unless account.nil?
+        account.created_on_registration = true
+        account.refresh_embed_data
+      end
     end
   end
 end
