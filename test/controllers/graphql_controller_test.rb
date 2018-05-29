28dad4406a1a5ed6f84f45a349edbe8f462a158e
Caio Almeida <caiosba@gmail.com>
2018-05-28 22:49:49 -0300

    Ticket #6933: Fixing test (minor performance improvement)

--- test/controllers/graphql_controller_test.rb
+++ test/controllers/graphql_controller_test.rb
@@ -890,7 +890,7 @@ class GraphqlControllerTest < ActionController::TestCase
     query = "query { search(query: \"{}\") { medias(first: 10000) { edges { node { dbid, media { dbid } } } } } }"
 
     # This number should be always CONSTANT regardless the number of medias and annotations above
-    assert_queries (15) do
+    assert_queries (13) do
       post :create, query: query, team: 'team'
     end
 
