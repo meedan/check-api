7c7d0c523a81c7e8f4af71adf7483dbfe20d3740
Sawy <melsawy.be7eri@gmail.com>
2018-06-13 20:46:48 +0200

    Ticket #6947 - migrate verification_status only

--- db/migrate/20180613135905_migrate_versions_object_after_with_false_status.rb
+++ db/migrate/20180613135905_migrate_versions_object_after_with_false_status.rb
@@ -2,9 +2,11 @@ class MigrateVersionsObjectAfterWithFalseStatus < ActiveRecord::Migration
   def change
   	PaperTrail::Version.where("object_changes ILIKE ?", '%"value":[%,"false"]%').find_each do |v|
   		object_after = JSON.parse(v.object_after)
-  		object_after["value"] = "false"
-  		v.object_after = object_after.to_json
-  		v.save!
+  		if object_after["field_name"] == 'verification_status_status'
+  			object_after["value"] = "false"
+  			v.object_after = object_after.to_json
+  			v.save!
+  		end
   	end
   end
 end
