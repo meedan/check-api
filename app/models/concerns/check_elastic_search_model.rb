216ec8a7259a6f9c9ba4fae9d4b2f6a7220bbfe5
Sawy <melsawy.be7eri@gmail.com>
2018-06-20 12:17:03 +0200

    ES - reindex with zero downtime

--- app/models/concerns/check_elastic_search_model.rb
+++ app/models/concerns/check_elastic_search_model.rb
@@ -6,7 +6,7 @@ module CheckElasticSearchModel
     include ActiveModel::Validations::Callbacks
     include Elasticsearch::Persistence::Model
 
-    index_name CheckElasticSearchModel.get_index_name
+    index_name CheckElasticSearchModel.get_index_alias
 
     settings analysis: {
       char_filter: {
@@ -46,15 +46,30 @@ module CheckElasticSearchModel
     CONFIG['elasticsearch_index'].blank? ? [Rails.application.engine_name, Rails.env, 'annotations'].join('_') : CONFIG['elasticsearch_index']
   end
 
+  def self.get_index_alias
+     self.get_index_name + '_alias'
+  end
+
   def self.reindex_es_data
+    client = MediaSearch.gateway.client
     source_index = CheckElasticSearchModel.get_index_name
-    target_index = "#{source_index}_reindex"
-    # copy data to destination
+    target_index = "#{source_index}_#{Time.now.to_i}"
+    index_alias = CheckElasticSearchModel.get_index_alias
+    if client.indices.exists_alias? name: index_alias
+      alias_info = client.indices.get_alias name: index_alias
+      source_index = alias_info.keys.first
+    else
+      client.indices.put_alias index: source_index, name: index_alias
+    end
+      # copy data to destination
     MediaSearch.migrate_es_data(source_index, target_index)
-    sleep 20
-    # copy data from destination to original source
-    MediaSearch.migrate_es_data(target_index, source_index)
-    MediaSearch.delete_index target_index
+    client.indices.update_aliases body: {
+      actions: [
+        { remove: { index: source_index, alias: index_alias } },
+        { add:    { index: target_index, alias: index_alias } }
+      ]
+    }
+    MediaSearch.delete_index source_index
   end
 
   private
@@ -64,7 +79,8 @@ module CheckElasticSearchModel
   end
 
   module ClassMethods
-    def create_index(index_name = self.index_name)
+    def create_index(index_name = nil)
+      index_name = "#{CheckElasticSearchModel.get_index_name}_#{Time.now.to_i}" if index_name.nil?
       client = self.gateway.client
       settings = []
       mappings = []
@@ -75,6 +91,7 @@ module CheckElasticSearchModel
       settings = settings.reduce(:merge)
       mappings = mappings.reduce(:merge)
       client.indices.create index: index_name, body: { settings: settings.to_hash, mappings: mappings.to_hash }
+      client.indices.put_alias index: index_name, name: CheckElasticSearchModel.get_index_alias
     end
 
     def delete_index(index_name = self.index_name)
@@ -82,6 +99,10 @@ module CheckElasticSearchModel
       if client.indices.exists? index: index_name
         client.indices.delete index: index_name
       end
+      # index_alias = "#{self.index_name}"
+      # if client.indices.exists_alias? name: index_alias
+      #   client.indices.delete_alias index: '*', name: index_alias
+      # end
     end
 
     def migrate_es_data(source_index, target_index)
