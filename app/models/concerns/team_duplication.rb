f2fd2f9ea0543c6b3afbdeca7d037f3ad9f39af0
Caio Almeida <caiosba@gmail.com>
2018-07-09 19:33:34 -0300

    Revert "Fixing Code Climate issue"
    This reverts commit c085699d9ba4a99547e7bae745b3522b77d525a3.

--- app/models/concerns/team_duplication.rb
+++ app/models/concerns/team_duplication.rb
@@ -10,7 +10,6 @@ module TeamDuplication
       @mapping = {}
       @original_team = t
       @cloned_versions = []
-      team = nil
       begin
         ActiveRecord::Base.transaction do
           PaperTrail::Version.skip_callback(:create, :after, :increment_project_association_annotations_count)
@@ -32,12 +31,13 @@ module TeamDuplication
           self.update_cloned_versions(@cloned_versions)
           self.create_copy_version(@mapping[:ProjectMedia], user)
           PaperTrail::Version.set_callback(:create, :after, :increment_project_association_annotations_count)
+          team
         end
       rescue StandardError => e
         Airbrake.notify(e) if Airbrake.configuration.api_key
         Rails.logger.error "[Team Duplication] Could not duplicate team #{t.slug}: #{e.message} #{e.backtrace.join("\n")}"
+        nil
       end
-      team
     end
 
     def self.set_mapping(object, copy)
