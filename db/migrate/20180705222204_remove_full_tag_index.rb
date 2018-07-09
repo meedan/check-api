aa5a63c72def3240d3f11b685cb41b8ce2639e28
Sawy <melsawy.be7eri@gmail.com>
2018-07-09 20:08:46 +0200

    Ticket #66981 - remove full_tag field from ES

--- db/migrate/20180705222204_remove_full_tag_index.rb
+++ db/migrate/20180705222204_remove_full_tag_index.rb
@@ -1,5 +1,15 @@
 class RemoveFullTagIndex < ActiveRecord::Migration
   def change
+  	client = MediaSearch.gateway.client
+    options = {
+      index: CheckElasticSearchModel.get_index_name,
+      type: 'tag_search',
+      body: {
+        script: { inline: "ctx._source.remove('full_tag')" },
+        query: { bool: { must: [ { exists: { field: "full_tag" } } ] } }
+      }
+    }
+    client.update_by_query options
   	CheckElasticSearchModel.reindex_es_data
   end
 end
