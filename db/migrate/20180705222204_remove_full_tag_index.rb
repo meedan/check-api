1937303c48799ee92b92a3341a5bc85b26774abb
Sawy <melsawy.be7eri@gmail.com>
2018-07-10 14:10:44 +0200

    Ticket #6989 - normalize tags

--- db/migrate/20180705222204_remove_full_tag_index.rb
+++ db/migrate/20180705222204_remove_full_tag_index.rb
@@ -10,6 +10,23 @@ class RemoveFullTagIndex < ActiveRecord::Migration
       }
     }
     client.update_by_query options
+    sleep 5
   	CheckElasticSearchModel.reindex_es_data
+    sleep 5
+    # Remove full_tag field and trim spaces
+    Annotation.where(annotation_type: 'tag').find_each do |t|
+      t = t.load
+      data = t.data
+      data.delete(:full_tag)
+      trim_tag = data[:tag].strip!
+      if trim_tag.nil?
+        # just update columns
+        t.update_columns(data: data)
+      else
+        # re-save tag to update ES
+        t.data = data
+        t.save!
+      end
+    end
   end
 end
