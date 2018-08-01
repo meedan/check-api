5cd7d2f40a4dc772fc4a6e44437e78e1e335edd3
Caio Almeida <caiosba@gmail.com>
2018-08-01 19:22:48 -0300

    Ticket #7030: Adding automated test

--- app/workers/pg_hero_worker.rb
+++ app/workers/pg_hero_worker.rb
@@ -2,7 +2,7 @@ class PgHeroWorker
   include Sidekiq::Worker
   sidekiq_options :queue => :pg_hero, :retry => false
 
-  def perform(model, options, type)
+  def perform
     PgHero.capture_query_stats
     PgHeroWorker.perform_in(5.minutes)
   end
