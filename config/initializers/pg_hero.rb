d63c1e229da80008e33e41c039152366895e4ddd
Caio Almeida <caiosba@gmail.com>
2018-08-01 16:59:39 -0300

    Ticket #7030: Adding configuration key to toggle PGHero

--- config/initializers/pg_hero.rb
+++ config/initializers/pg_hero.rb
@@ -1,5 +1,7 @@
 # Start scheduler to save queries information every 5 minutes
-ENV['PGHERO_USERNAME'] = nil
-ENV['PGHERO_PASSWORD'] = nil
-count = Sidekiq::ScheduledSet.new.map(&:klass).select{ |klass| klass == 'PgHeroWorker' }.size
-PgHeroWorker.perform_in(5.minutes) if count == 0
+if CONFIG['pg_hero_enabled']
+  ENV['PGHERO_USERNAME'] = nil
+  ENV['PGHERO_PASSWORD'] = nil
+  count = Sidekiq::ScheduledSet.new.map(&:klass).select{ |klass| klass == 'PgHeroWorker' }.size
+  PgHeroWorker.perform_in(5.minutes) if count == 0
+end
