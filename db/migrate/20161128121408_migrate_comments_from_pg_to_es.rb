007506c0384756b2e4a7830d1e9f49ad02832007
Sawy <melsawy.be7eri@gmail.com>
2018-08-27 19:18:26 +0200

    Cleanup ES migrations

--- db/migrate/20161128121408_migrate_comments_from_pg_to_es.rb
+++
@@ -1,7 +0,0 @@
-class MigrateCommentsFromPgToEs < ActiveRecord::Migration
-  def change
-    Annotation.where(annotation_type: 'comment', annotated_type: 'Media').where.not(context_id: nil).each do |c|
-      c.add_update_media_search_child('comment_search', %w(text))
-    end
-  end
-end
