4f93015c7e7fcfb5559690c3958716937c3a8e47
Caio Almeida <caiosba@gmail.com>
2018-08-31 11:17:12 -0300

    Ticket #7059: Debugging migration

--- db/migrate/20180829194036_convert_task_answer_notes_to_comments.rb
+++ db/migrate/20180829194036_convert_task_answer_notes_to_comments.rb
@@ -1,7 +1,11 @@
 class ConvertTaskAnswerNotesToComments < ActiveRecord::Migration
   def change
     field_names = ['note_free_text', 'note_yes_no', 'note_single_choice', 'note_multiple_choice', 'note_datetime']
+    total = DynamicAnnotation::Field.where(field_name: field_names).count
+    i = 0
     DynamicAnnotation::Field.where(field_name: field_names).find_each do |answer_note|
+      i += 1
+      puts "Migrating task answer #{i}/#{total}"
       answer = answer_note.annotation
       unless answer.nil?
         taskref = answer.get_fields.select{ |f| f.field_type == 'task_reference' }.last
@@ -16,9 +20,9 @@ class ConvertTaskAnswerNotesToComments < ActiveRecord::Migration
             comment.created_at = answer.created_at
             comment.skip_check_ability = true
             comment.skip_notifications = true
-            comment.save
+            comment.save!
             User.current = nil
-            answer_note.destroy
+            answer_note.destroy!
           end
         end
       end
