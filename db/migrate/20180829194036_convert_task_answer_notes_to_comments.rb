6a8fcf193311ee32e77d4c71ef2e21ac3b29e7e3
Caio Almeida <caiosba@gmail.com>
2018-08-31 11:37:09 -0300

    Ticket #7059: Migrate comment even if item is archived

--- db/migrate/20180829194036_convert_task_answer_notes_to_comments.rb
+++ db/migrate/20180829194036_convert_task_answer_notes_to_comments.rb
@@ -6,27 +6,28 @@ class ConvertTaskAnswerNotesToComments < ActiveRecord::Migration
     DynamicAnnotation::Field.where(field_name: field_names).find_each do |answer_note|
       i += 1
       puts "Migrating task answer #{i}/#{total}"
-      next if answer_note.value.blank?
       answer = answer_note.annotation
-      unless answer.nil?
-        taskref = answer.get_fields.select{ |f| f.field_type == 'task_reference' }.last
-        unless taskref.nil?
-          task = Task.where(id: taskref.value.to_i).last
-          unless task.nil?
-            User.current = answer.annotator
-            comment = Comment.new
-            comment.text = answer_note.value
-            comment.annotated = task
-            comment.annotator = answer.annotator
-            comment.created_at = answer.created_at
-            comment.skip_check_ability = true
-            comment.skip_notifications = true
-            comment.save!
-            User.current = nil
-            answer_note.destroy!
+      if !answer_note.value.blank? && 
+        unless answer.nil?
+          taskref = answer.get_fields.select{ |f| f.field_type == 'task_reference' }.last
+          unless taskref.nil?
+            task = Task.where(id: taskref.value.to_i).last
+            unless task.nil?
+              User.current = answer.annotator
+              comment = Comment.new
+              comment.text = answer_note.value
+              comment.annotated = task
+              comment.annotator = answer.annotator
+              comment.created_at = answer.created_at
+              comment.skip_check_ability = true
+              comment.skip_notifications = true
+              comment.save(validate: false)
+              User.current = nil
+            end
           end
         end
       end
+      answer_note.destroy!
     end
   end
 end
