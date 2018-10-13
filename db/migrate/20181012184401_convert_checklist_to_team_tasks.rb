1a524d335aa2e3136b4bb5c10278bb7e6093eea0
Caio Almeida <caiosba@gmail.com>
2018-10-13 15:10:31 -0300

    Ticket #6761: Fixing migration

--- db/migrate/20181012184401_convert_checklist_to_team_tasks.rb
+++ db/migrate/20181012184401_convert_checklist_to_team_tasks.rb
@@ -3,7 +3,16 @@ class ConvertChecklistToTeamTasks < ActiveRecord::Migration
     Team.all.each do |team|
       next unless team.get_checklist.is_a?(Array)
       team.get_checklist.each do |task|
-        TeamTask.create!(task.merge({ team_id: team.id }))
+        task = task.with_indifferent_access
+        task['task_type'] = task['type']
+        task['project_ids'] = task['projects']
+        task['team_id'] = team.id
+        ['type', :type, 'projects', :projects].each{ |key| task.delete(key) }
+        tt = TeamTask.new
+        task.each do |k, v|
+          tt.send("#{k}=", v)
+        end
+        tt.save!
       end
     end
   end
