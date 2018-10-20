3248e125e75d38de8765cc817ae9321fa37f6bbe
Daniela Feitosa <danielafeitosa@gmail.com>
2018-10-20 18:30:13 -0300

    Feature/6769 sahara import (#334)
    Ticket #6769: Spreadsheet import fixes
    
    - Remove the hardcoded tasks answers
    - Remove notes for hardcoded task
    - Added validations of team, user and permission on Import Mutation
    - Updated Relay
    - Fix CC issues

--- app/models/concerns/team_import.rb
+++ app/models/concerns/team_import.rb
@@ -12,12 +12,10 @@ module TeamImport
       match ? match[1] : nil
     end
 
-    def import_spreadsheet_in_background(spreadsheet_url, user_id = nil)
-      team = Team.current
-      raise_error(I18n.t('team_import.team_not_present'), 'INVALID_VALUE') unless team
+    def import_spreadsheet_in_background(spreadsheet_url, team_id, user_id)
       spreadsheet_id = self.spreadsheet_id(spreadsheet_url)
       raise_error(I18n.t('team_import.invalid_google_spreadsheet_url', spreadsheet_url: spreadsheet_url), 'INVALID_VALUE') unless spreadsheet_id
-      TeamImportWorker.perform_async(team.id, spreadsheet_id, user_id)
+      TeamImportWorker.perform_async(team_id, spreadsheet_id, user_id)
     end
 
     def raise_error(message, code, args={})
@@ -32,6 +30,8 @@ module TeamImport
   end
 
   included do
+    TASKS_BEGIN = 9
+
     notifies_pusher on: :touch,
                     event: proc { |t| t.import_status },
                     targets: proc { |t| [t] },
@@ -40,18 +40,22 @@ module TeamImport
 
     attr_accessor :spreadsheet, :import_status
 
-    def import_spreadsheet(spreadsheet_id, email)
+    def import_spreadsheet(spreadsheet_id, user_id)
       @result = {}
+      user = User.find(user_id)
       self.spreadsheet = get_spreadsheet(spreadsheet_id)
       worksheet = self.spreadsheet.worksheets[0]
-      # self.update_import_status('start')
+      Rails.logger.info "[Team Import] Importing spreadsheet #{spreadsheet_id} to team #{self.slug} (requested by user #{user.login})"
+      self.update_import_status('start')
+      get_tasks_slugs(worksheet)
       for row in 2..worksheet.num_rows
         @result[row] = []
         import_row(worksheet, row)
         # self.update_import_status('row')
         update_worksheet(worksheet, row)
       end
-      # self.update_import_status('complete')
+      self.update_import_status('complete')
+      Rails.logger.info "[Team Import] Finished import of spreadsheet #{spreadsheet_id} to team #{self.slug}"
       @result
     end
 
@@ -59,11 +63,11 @@ module TeamImport
       begin
         @session = GoogleDrive::Session.from_service_account_key(CONFIG['google_credentials_path'])
         @session.spreadsheet_by_key(id)
-      rescue Signet::AuthorizationError => e
+      rescue Signet::AuthorizationError
         Team.raise_error(I18n.t('team_import.cannot_authenticate_with_the_credentials'), 'UNAUTHORIZED')
       rescue Google::Apis::ClientError => e
         Team.raise_error(I18n.t('team_import.not_found_google_spreadsheet_url'), 'INVALID_VALUE', {error_message: e.message}) unless self.spreadsheet
-      rescue Exception => e
+      rescue StandardError => e
         Team.raise_error(e.message, 'UNKNOWN', {error_message: e.message})
       end
     end
@@ -84,20 +88,20 @@ module TeamImport
       return if user_id.nil?
       projects.each do |project|
         begin
-          pm = create_item(project, data[:item], user_id, row)
+          pm = create_item(project, data[:item], user_id)
           @result[row] << pm.full_url
           add_note(pm, data[:note], data[:annotator], row)
           assign_to_user(pm, data[:assigned_to], row)
-          add_tags(pm, data[:tags], row)
+          add_tags(pm, data[:tags])
           add_tasks_answers(pm, worksheet, row)
-        rescue Exception => e
+        rescue StandardError => e
           @result[row] << e.message
         end
       end
     end
 
-    def create_item(project, item, user_id, row)
-      media = get_item(item, project, row)
+    def create_item(project, item, user_id)
+      media = get_item(item, project)
       pm = media[:project_media]
       if pm.nil?
         pm = ProjectMedia.create!({project_id: project, user_id: user_id}.merge(media[:params]))
@@ -115,7 +119,7 @@ module TeamImport
     def get_user(user, row, column = 'user')
       @result[row] << I18n.t("team_import.blank_#{column}") and return if user.blank?
       pattern = Regexp.new(CONFIG['checkdesk_client'] + "/check\/user\/([0-9]+)")
-      if match = pattern.match(user)
+      if (match = pattern.match(user))
         id = match[1].to_i
       else
         @result[row] << I18n.t("team_import.invalid_#{column}", user: user)
@@ -129,7 +133,7 @@ module TeamImport
       pattern = Regexp.new(CONFIG['checkdesk_client'] + "/#{self.slug}\/project\/([0-9]+)")
       ids = []
       projects.each do |p|
-        if match = pattern.match(p)
+        if (match = pattern.match(p))
           ids << match[1].to_i
         else
           @result[row] << I18n.t('team_import.invalid_project', project: p) if @result
@@ -138,7 +142,7 @@ module TeamImport
       ids
     end
 
-    def get_item(item, project, row)
+    def get_item(item, project)
       uri = URI.parse(URI.encode(item))
       params = uri.host.nil? ? {quote: item} : {url: item}
       media = Media.where(params).first
@@ -164,44 +168,36 @@ module TeamImport
       end
     end
 
-    def add_tags(pm, tags, row)
+    def add_tags(pm, tags)
       return if tags.blank?
       tags = tags.split(',').map { |t| t.strip }
       tags.each do |tag|
-        Tag.create!(tag: tag, annotator: pm.user, annotated: pm) # team tags already created
+        unless pm.annotations('tag').map(&:tag_text).include?(tag)
+          Tag.create!(tag: tag, annotator: pm.user, annotated: pm)
+        end
+      end
+    end
+
+    def get_tasks_slugs(worksheet)
+       @tasks = {}
+       for col in TASKS_BEGIN..worksheet.num_cols
+         task_question = worksheet[1, col]
+         unless task_question.blank?
+           @tasks[col] = Task.slug(task_question)
+         end
       end
     end
 
     def add_tasks_answers(pm, worksheet, row)
-      # "where_was_the_video_recorded": worksheet[row, 11],
-      pm.set_tasks_responses = {
-        "timestamp": worksheet[row, 9],
-        "what_does_the_video_document": worksheet[row, 10],
-        "when_was_the_video_recorded": worksheet[row, 12],
-        "what_human_rights_issue_s_does_this_video_document": worksheet[row, 13],
-        "if_the_video_includes_chants_or_protest_signs_what_do_they_say": worksheet[row, 14],
-        "if_the_video_includes_testimony_who_is_on_camera_and_what_do_they_say": worksheet[row, 15],
-        "if_there_is_additional_footage_of_the_same_event_please_copy_links_to_other_videos_or_photographs_below": worksheet[row, 16],
-        "if_there_online_reports_with_more_information_about_the_event_or_issue_please_copy_links_below": worksheet[row, 17],
-        "do_you_have_any_questions_uncertainties_or_anything_more_to_add_about_the_video": worksheet[row, 18],
-        "can_watching_western_sahara_can_contact_you_with_further_questions_if_so_please_enter_your_email_address_below": worksheet[row, 19],
-        "has_the_video_been_verified_and_contextualized": worksheet[row, 20]
-      }.with_indifferent_access
-      notes = { "has_the_video_been_verified_and_contextualized": [worksheet[row, 21],worksheet[row, 22]] }.with_indifferent_access
+      tasks_responses = {}
+      for col in TASKS_BEGIN..worksheet.num_cols
+        answer = worksheet[row, col]
+        tasks_responses[@tasks[col]] = answer unless answer.blank?
+      end
+      pm.set_tasks_responses = tasks_responses.with_indifferent_access
 
       User.current = pm.user
       pm.send(:respond_to_auto_tasks, pm.annotations('task'))
-
-      notes.each_pair do |slug, note|
-        task = pm.annotations('task').find {|t| t.slug == slug }
-        note.each do |text|
-          comment = Comment.new
-          comment.text = text
-          comment.annotated = task
-          comment.annotator = pm.user
-          comment.save
-        end
-      end
       User.current = nil
     end
 
