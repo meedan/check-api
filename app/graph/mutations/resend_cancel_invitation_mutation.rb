87b1b4f81ffd53c295ffc35b15377fdc7aadf495
Sawy <melsawy.be7eri@gmail.com>
2018-10-22 18:23:30 +0200

    Ticket #7060 - update invitation date after resend it

--- app/graph/mutations/resend_cancel_invitation_mutation.rb
+++ app/graph/mutations/resend_cancel_invitation_mutation.rb
@@ -18,6 +18,10 @@ ResendCancelInvitationMutation = GraphQL::Relay::Mutation.define do
       when 'resend'
         tu = user.team_users.where(team_id: Team.current.id).last
         user.send_invitation_mail(tu)
+        # update invitations dates
+        time = Time.now
+        tu.update_columns(created_at: time)
+        user.update_columns(invitation_created_at: time, invitation_sent_at: time) if user.invited_to_sign_up?
       end
       { success: true }
     end
