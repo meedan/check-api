e36eaa9291eb8f00acc2b9ad1ad7cf2c3998f175
Caio Almeida <caiosba@gmail.com>
2018-10-25 01:37:50 -0300

    Ticket #7130: Bulk-migrate task statuses

--- db/migrate/20181024185849_convert_task_statuses_to_workflow.rb
+++ db/migrate/20181024185849_convert_task_statuses_to_workflow.rb
@@ -1,18 +1,23 @@
 class ConvertTaskStatusesToWorkflow < ActiveRecord::Migration
   def change
     RequestStore.store[:skip_notifications] = true
+
+    annotations = []
     
+    puts 'Migrating task statuses...'
+    puts 'Loading annotations...'
+
     Task.where(annotation_type: 'task').find_each do |t|
-      begin
-        t.send(:create_first_task_status)
-        t.skip_check_ability = true
-        t.skip_notifications = true
-        t.skip_clear_cache = true
-        t.save!
-      rescue
-        puts "Skipping task with id #{t.id}"
-      end
+      annotation = Dynamic.new(annotation_type: 'task_status', annotated_type: 'Task', annotated_id: t.id)
+      annotation.fields.build(field_name: 'task_status_status', value: t.data[:status].downcase, annotation_type: 'task_status', field_type: 'select')
+      annotations << annotation
     end
+
+    puts 'Bulk-inserting annotations...'
+
+    Dynamic.import annotations, recursive: true, validate: false
+
+    puts 'Done!'
     
     RequestStore.store[:skip_notifications] = false
   end
