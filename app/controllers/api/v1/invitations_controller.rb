f0874a5918e58c98ba30e740e6629bf95d68cb52
Sawy <melsawy.be7eri@gmail.com>
2018-11-01 17:22:30 +0200

    Ticket #7060 - pass tests

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::InvitationsController < Devise::InvitationsController
   def edit
     invitation_token = params[:invitation_token]
     slug = params[:slug]
-    redirect_to "#{CONFIG['checkdesk_client']}/#{slug}"
     resource = User.accept_team_invitation(invitation_token, slug)
     path = resource.errors.empty? ? "/#{slug}" : '/check/user/invalid-invitation'
     redirect_to CONFIG['checkdesk_client'] + path
