2b73ef3893836474d65e65559dcbf7234d79eb39
Sawy <melsawy.be7eri@gmail.com>
2018-11-05 22:02:59 +0200

    Ticket #7060 - update relay.json and invitation mutations

--- app/graph/mutations/user_invitation_mutation.rb
+++ app/graph/mutations/user_invitation_mutation.rb
@@ -2,12 +2,15 @@ UserInvitationMutation = GraphQL::Relay::Mutation.define do
   name 'UserInvitation'
 
   input_field :invitation, types.String
+  
   input_field :members, !JsonStringType
 
   return_field :success, JsonStringType
 
+  return_field :team, TeamType
+
   resolve -> (_root, inputs, _ctx) {
     messages = User.send_user_invitation(inputs[:members], inputs[:invitation])
-    { success: messages }
+    { success: messages, team: Team.current }
   }
 end
