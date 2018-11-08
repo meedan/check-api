c4d1ad09d1ad7c9cabce39a3af7abaea0f5fe052
Caio Almeida <caiosba@gmail.com>
2018-11-08 19:31:54 -0300

    Ticket #7168: Fixing migration

--- db/migrate/20181108195808_turn_assignment_into_polymorphic.rb
+++ db/migrate/20181108195808_turn_assignment_into_polymorphic.rb
@@ -5,7 +5,7 @@ class TurnAssignmentIntoPolymorphic < ActiveRecord::Migration
     Assignment.find_each do |a|
       a.update_column(:assigned_type, 'Annotation')
     end
-    remove_index :assignments, 'index_assignments_on_assigned_id_and_user_id'
+    remove_index :assignments, name: 'index_assignments_on_assigned_id_and_user_id'
     add_index :assignments, :assigned_type
     add_index :assignments, [:assigned_id, :assigned_type]
     add_index :assignments, [:assigned_id, :assigned_type, :user_id], unique: true
