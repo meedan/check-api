ef1ea73a3c84b3c654bf18029d5a931130e862bd
Caio Almeida <caiosba@gmail.com>
2018-11-16 03:01:55 -0300

    Adding a migration to de-duplicate existing slugs

---
+++ db/migrate/20181115054433_de_duplicate_slugs.rb
@@ -0,0 +1,11 @@
+class DeDuplicateSlugs < ActiveRecord::Migration
+  def change
+    duplicates = Team.group(:slug).count.select{ |k, v| v > 1 }
+    duplicates.each do |slug, count|
+      Team.where(slug: slug).all.each_with_index do |t, index|
+        next if index == 0
+        t.update_column :slug, "#{t.slug}-#{index}"
+      end
+    end
+  end
+end
