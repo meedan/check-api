de025482a522f02e272064c2645b90a50700ef6b
Sawy <melsawy.be7eri@gmail.com>
2018-11-18 06:36:35 +0200

    Ticket #7060 - redirect user with invalid invitation to login page

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -5,7 +5,12 @@ class Api::V1::InvitationsController < Devise::InvitationsController
     invitation_token = params[:invitation_token]
     slug = params[:slug]
     resource = User.accept_team_invitation(invitation_token, slug)
-    path = resource.errors.empty? ? "/#{slug}" : '/check/user/invalid-invitation'
+    path = if resource.errors.empty?
+             "/#{slug}"
+           else
+             error_key = resource.errors.messages.keys[0].to_s
+             "?invitation_error=#{error_key}"
+           end
     redirect_to CONFIG['checkdesk_client'] + path
   end
 end
