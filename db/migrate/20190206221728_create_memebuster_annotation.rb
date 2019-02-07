203c5c03cc417d66ff960da70c63149986c92565
infojunkie <karim.ratib@gmail.com>
2019-02-06 16:25:47 -0800

    Memebuster annotation publishing timestamp #7262

--- db/migrate/20190206221728_create_memebuster_annotation.rb
+++ db/migrate/20190206221728_create_memebuster_annotation.rb
@@ -4,6 +4,7 @@ class CreateMemebusterAnnotation < ActiveRecord::Migration
   def change
     text = DynamicAnnotation::FieldType.where(field_type: 'text').last
     image = DynamicAnnotation::FieldType.where(field_type: 'image_path').last
+    datetime = DynamicAnnotation::FieldType.where(field_type: 'datetime').last
     at = create_annotation_type annotation_type: 'memebuster', label: 'Meme Generator Settings'
     create_field_instance annotation_type_object: at, name: 'memebuster_image', label: 'Image', field_type_object: image, optional: false
     create_field_instance annotation_type_object: at, name: 'memebuster_headline', label: 'Headline', field_type_object: text, optional: false
@@ -11,5 +12,6 @@ class CreateMemebusterAnnotation < ActiveRecord::Migration
     create_field_instance annotation_type_object: at, name: 'memebuster_status', label: 'Status', field_type_object: text, optional: false
     create_field_instance annotation_type_object: at, name: 'memebuster_overlay', label: 'Overlay Color', field_type_object: text, optional: false
     create_field_instance annotation_type_object: at, name: 'memebuster_operation', label: 'Operation', field_type_object: text, optional: false
+    create_field_instance annotation_type_object: at, name: 'memebuster_published_at', label: 'Published', field_type_object: datetime, optional: true
   end
 end
