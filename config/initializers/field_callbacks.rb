c2b7fa6508daf4f6bde0a46d3cbc1c3b7f736bf9
Caio Almeida <caiosba@gmail.com>
2019-02-15 00:13:56 -0300

    Ticket #7262: Make it possible to set memebuster_image path explicitly, instead of uploading a file

--- config/initializers/field_callbacks.rb
+++ config/initializers/field_callbacks.rb
@@ -4,6 +4,10 @@ Dynamic.class_eval do
   private
 
   def copy_memebuster_image_paths
+    unless self.set_fields.blank?
+      data = JSON.parse(self.set_fields)
+      return unless data['memebuster_image'].blank?
+    end
     urls = []
     self.file.each do |image|
       urls << CONFIG['checkdesk_base_url'] + image.url
