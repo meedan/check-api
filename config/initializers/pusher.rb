f99a52cd07944591d913c7bf17d438bf22cfeac3
infojunkie <karim.ratib@gmail.com>
2019-02-15 20:02:23 -0800

    Fix pusher

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -3,5 +3,6 @@ require 'pusher'
 Pusher.app_id = CONFIG['pusher_app_id']
 Pusher.key = CONFIG['pusher_key']
 Pusher.secret = CONFIG['pusher_secret']
+Pusher.cluster = CONFIG['pusher_cluster']
 Pusher.logger = Rails.logger
 Pusher.encrypted = true
