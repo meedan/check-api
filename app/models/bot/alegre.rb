7bf662f3c59b741ee78892ce0abb2b0bbf10d19d
infojunkie <karim.ratib@gmail.com>
2019-02-21 15:44:16 -0800

    Add similarity function to Alegre bot #7217

--- app/models/bot/alegre.rb
+++ app/models/bot/alegre.rb
@@ -6,9 +6,10 @@ class Bot::Alegre < ActiveRecord::Base
   def self.run(body)
     json = JSON.parse(body)
     pm = ProjectMedia.where(id: json['data']['dbid']).last
-    unless pm.nil?
-      Bot::Alegre.default.get_language_from_alegre(pm.text, pm)
-      pm.create_mt_annotation
+    unless pm.nil? or pm.text.blank? or CONFIG['alegre_host'].blank? or CONFIG['alegre_token'].blank?
+      Bot::Alegre.default.get_language_from_alegre(pm)
+      Bot::Alegre.default.create_empty_mt_annotation(pm)
+      Bot::Alegre.default.create_similarities_from_alegre(pm)
     end
   end
 
@@ -20,38 +21,29 @@ class Bot::Alegre < ActiveRecord::Base
     CONFIG['checkdesk_base_url'] + self.avatar.url
   end
 
-  def should_classify?(text)
-    !text.blank? && !CONFIG['alegre_host'].blank? && !CONFIG['alegre_token'].blank?
-  end
-
-  def get_language_from_alegre(text, target)
+  def get_language_from_alegre(target)
     lang = nil
-    if self.should_classify?(text)
-      begin
-        response = AlegreClient::Request.get_languages_identification(CONFIG['alegre_host'], { text: text }, CONFIG['alegre_token'])
-        lang = response['data'][0][0].split(',').first.downcase if response['type'] == 'language'
-      rescue
-        lang = nil
-      end
+    begin
+      response = AlegreClient::Request.get_languages_identification(CONFIG['alegre_host'], { text: target.text }, CONFIG['alegre_token'])
+      lang = response['data'][0][0].split(',').first.downcase if response['type'] == 'language'
+    rescue
     end
     self.save_language(target, lang) unless lang.nil?
     lang
   end
 
-  def get_source_language_for_alegre(text, target)
+  def get_source_language(target)
     field = self.get_dynamic_field_value(target, 'language', 'language')
-    field.nil? ? Bot::Alegre.default.get_language_from_alegre(text, target) : field.value
+    field.nil? ? Bot::Alegre.default.get_language_from_alegre(target) : field.value
   end
 
   def get_mt_from_alegre(target, author)
-    text = target.text
     translations = []
-    src_lang = self.get_source_language_for_alegre(text, target)
-    languages = target.project.languages
-    languages = languages - [src_lang]
+    src_lang = self.get_source_language(target)
+    languages = target.project.languages - [src_lang]
     languages.each do |lang|
       begin
-        response = AlegreClient::Request.get_mt(CONFIG['alegre_host'], { text: text, from: src_lang, to: lang }, CONFIG['alegre_token'])
+        response = AlegreClient::Request.get_mt(CONFIG['alegre_host'], { text: target.text, from: src_lang, to: lang }, CONFIG['alegre_token'])
         mt_text = response['data'] if response['type'] == 'mt' && !response['data'].blank?
       rescue
         mt_text = nil
@@ -60,6 +52,45 @@ class Bot::Alegre < ActiveRecord::Base
     end
     self.update_machine_translation(target, translations, author) unless translations.blank?
     translations
+  end
+
+  # Get existing claims which are similar to this new one, in the context of the same project.
+  # Find the "parent" claim among the set, and link this new one to it as a "child".
+  # If there's no existing parent, create the first relationship now.
+  # Then add this new claim to the similarity index.
+  def create_similarities_from_alegre(target)
+    return if target.media.type != 'Claim'
+
+    src_lang = self.get_source_language(target)
+    response = Bot::Alegre.request_api('POST', '/similarity/query', {
+      text: target.text,
+      language: src_lang,
+      context: {
+        project_id: target.project.id
+      }
+    })
+
+    if response['result'] and response['result'].length > 0 then
+      pm_ids = response['result'].collect{|r| r.dig('_source', 'context', 'project_media_id')}
+      source_ids = Relationship.where("source_id IN (:pm_ids)", { :pm_ids => pm_ids }).select(:source_id).distinct
+      parent_id = source_ids.length > 0 ? source_ids[0].source_id : pm_ids[0]
+
+      r = Relationship.new
+      r.skip_check_ability = true
+      r.relationship_type = { source: 'parent', target: 'child' }
+      r.source_id = parent_id
+      r.target_id = target.id
+      r.save!
+    end
+    response = Bot::Alegre.request_api('POST', '/similarity/', {
+      text: target.text,
+      language: src_lang,
+      context: {
+        team_id: target.project.team.id,
+        project_id: target.project.id,
+        project_media_id: target.id
+      }
+    })
   end
 
   def language_object(target, attr = nil)
@@ -71,8 +102,39 @@ class Bot::Alegre < ActiveRecord::Base
   def get_dynamic_field_value(target, annotation_type, field_type)
     DynamicAnnotation::Field.joins(:annotation).where('annotations.annotation_type' => annotation_type, 'annotations.annotated_type' => target.class.name, 'annotations.annotated_id' => target.id.to_s, field_type: field_type).first
   end
-  
+
+  def create_empty_mt_annotation(target)
+    src_lang = self.language_object(target, :value)
+    languages = target.project.get_languages
+    unless src_lang.blank? or languages.nil?
+      annotation = Dynamic.new
+      annotation.annotated = target
+      annotation.annotator = self
+      annotation.annotation_type = 'mt'
+      annotation.set_fields = { 'mt_translations': [].to_json }.to_json
+      annotation.skip_notifications = true
+      annotation.disable_es_callbacks = Rails.env.to_s == 'test'
+      annotation.save!
+      annotation.update_columns(annotator_id: self.id, annotator_type: 'Bot::Alegre')
+    end
+  end
+
   protected
+
+  def self.request_api(method, path, params = {})
+    uri = URI(CONFIG['alegre_host'] + path)
+    klass = 'Net::HTTP::' + method.capitalize
+    request = klass.constantize.new(uri.path, 'Content-Type' => 'application/json')
+    request.body = params.to_json
+    http = Net::HTTP.new(uri.hostname, uri.port)
+    http.use_ssl = uri.scheme == 'https'
+    begin
+      response = http.request(request)
+      JSON.parse(response.body)
+    rescue Exception => e
+      { 'type' => 'error', 'data' => { 'message' => e.message } }
+    end
+  end
 
   def save_language(target, lang)
     annotation = Dynamic.new
@@ -91,7 +153,7 @@ class Bot::Alegre < ActiveRecord::Base
     unless mt.nil?
       mt = mt.load
       User.current = author
-      mt.set_fields = {'mt_translations': translations.to_json}.to_json
+      mt.set_fields = { 'mt_translations': translations.to_json }.to_json
       mt.disable_es_callbacks = Rails.env.to_s == 'test'
       mt.save!
       User.current = nil
@@ -100,7 +162,7 @@ class Bot::Alegre < ActiveRecord::Base
       versions = mt_field.versions.to_a unless mt_field.nil?
       unless versions.blank?
         versions.pop
-        versions.each{|v| v.skip_check_ability = true; v.destroy}
+        versions.each{ |v| v.skip_check_ability = true; v.destroy }
       end
     end
   end
