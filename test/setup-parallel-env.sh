6e9fc8c885eccfc0cfc0c39cb561d3f94599a822
Caio Almeida <caiosba@gmail.com>
2019-02-22 19:27:15 -0300

    Trying to use 4 threads instead of 3 to run the tests in parallel on Travis

--- test/setup-parallel-env.sh
+++ test/setup-parallel-env.sh
@@ -8,6 +8,7 @@ RAILS_ENV=test bundle exec rake db:create db:migrate &
 RAILS_ENV=test TEST_ENV_NUMBER=1 bundle exec rake db:create db:migrate &
 RAILS_ENV=test TEST_ENV_NUMBER=2 bundle exec rake db:create db:migrate &
 RAILS_ENV=test TEST_ENV_NUMBER=3 bundle exec rake db:create db:migrate &
+RAILS_ENV=test TEST_ENV_NUMBER=4 bundle exec rake db:create db:migrate &
 
 for job in `jobs -p`
 do
