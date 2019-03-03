67d8464d82872b3f755f89d47f773111daf37651
infojunkie <karim.ratib@gmail.com>
2019-03-03 15:59:36 -0800

    Adjust deletion order

--- db/migrate/20190302041737_cleanup_task_references.rb
+++ db/migrate/20190302041737_cleanup_task_references.rb
@@ -1,7 +1,7 @@
 class CleanupTaskReferences < ActiveRecord::Migration
   def change
-    DynamicAnnotation::FieldInstance.where(:field_type => 'task_reference').destroy_all
     DynamicAnnotation::Field.where(:field_type => 'task_reference').destroy_all
+    DynamicAnnotation::FieldInstance.where(:field_type => 'task_reference').destroy_all
     DynamicAnnotation::FieldType.where(:field_type => 'task_reference').destroy_all
   end
 end
