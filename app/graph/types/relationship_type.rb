8e7b8decb4e4e0b3cc8fa8ea8ede5595acd49ddf
Caio Almeida <caiosba@gmail.com>
2019-03-26 21:13:22 -0300

    Ticket #7404: Relationship mutations

---
+++ app/graph/types/relationship_type.rb
@@ -0,0 +1,12 @@
+RelationshipType = GraphQL::ObjectType.define do
+  name 'Relationship'
+  description 'A relationship between two items'
+  interfaces [NodeIdentification.interface]
+  global_id_field :id
+
+  field :target_id, types.Int
+  field :source_id, types.Int
+
+  field :target, ProjectMediaType
+  field :source, ProjectMediaType
+end 
