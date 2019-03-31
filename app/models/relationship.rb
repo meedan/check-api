02554b3bf6523f7050ab18ddb1ffaf4ccb498f9c
Caio Almeida <caiosba@gmail.com>
2019-03-31 15:16:26 -0300

    Ticket #7404: Propagate relationship inversion

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -9,13 +9,15 @@ class Relationship < ActiveRecord::Base
   validate :relationship_type_is_valid
 
   after_create :increment_counters, :index_source
+  after_update :propagate_inversion, :reset_counters
   after_destroy :decrement_counters, :unindex_source
 
-  has_paper_trail on: [:create, :destroy], if: proc { |x| User.current.present? && !x.is_being_copied }
+  has_paper_trail on: [:create, :update, :destroy], if: proc { |x| User.current.present? && !x.is_being_copied }
 
-  notifies_pusher on: [:create, :destroy],
+  notifies_pusher on: [:save, :destroy],
                   event: 'relationship_change',
                   targets: proc { |r| r.source.nil? ? [] : [r.source.media] },
+                  bulk_targets: proc { |r| [r.source.media] },
                   if: proc { |r| !r.skip_notifications },
                   data: proc { |r| Relationship.where(id: r.id).last.nil? ? { source_id: r.source_id }.to_json : r.to_json }
 
@@ -29,7 +31,9 @@ class Relationship < ActiveRecord::Base
     query = inclusive ? query : query.where.not(target_id: self.target_id)
     pms = []
     query.collect do |r|
-      pm = r.target
+      t = r.target
+      next if t.inactive
+      pm = t
       pm.relationship = r
       pms << pm
     end
@@ -68,7 +72,7 @@ class Relationship < ActiveRecord::Base
     list = []
     targets.each do |key, value|
       id = [project_media.id, key].join('/')
-      medias = ProjectMedia.where(id: value).order('id DESC').limit(limit).collect{ |t| t.relationship = relationships_mapping[t.id] ; t }
+      medias = ProjectMedia.where(id: value, inactive: false).order('id DESC').limit(limit).collect{ |t| t.relationship = relationships_mapping[t.id] ; t }
       list << { type: key, targets: medias, id: id }.with_indifferent_access
     end
     list
@@ -130,6 +134,17 @@ class Relationship < ActiveRecord::Base
     self.update_counters(-1)
   end
 
+  def reset_counters
+    if (self.source_id_was && self.source_id_was != self.source_id) || (self.target_id_was && self.target_id_was != self.target_id)
+      previous = Relationship.new(source_id: self.source_id_was, target_id: self.target_id_was)
+      previous.update_counters(-1)
+      previous.send :unindex_source
+      current = Relationship.new(source_id: self.source_id, target_id: self.target_id)
+      current.update_counters(1)
+      current.send :index_source
+    end
+  end
+
   def index_source
     self.update_elasticsearch_doc(['relationship_sources'], { 'relationship_sources' => self.es_values }, self.target)
   end
@@ -138,5 +153,12 @@ class Relationship < ActiveRecord::Base
     value = self.es_values - [self.es_value]
     value = ['-'] if value.empty?
     self.update_elasticsearch_doc(['relationship_sources'], { 'relationship_sources' => value }, self.target)
+  end
+
+  def propagate_inversion
+    if self.source_id_was == self.target_id && self.target_id_was == self.source_id
+      ids = Relationship.where(source_id: self.target_id).map(&:graphql_id)
+      GraphqlCrudOperations.crud_operation('update', self, { source_id: self.source_id, ids: ids }, nil, [], {})
+    end
   end
 end
