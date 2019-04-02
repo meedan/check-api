2483eb4be8a769769e5f9713d0c465332278514c
Caio Almeida <caiosba@gmail.com>
2019-04-02 14:07:06 -0300

    Tickets #7462, #7460, #7459 and #7454: Fixes

--- lib/check_notifications/pusher.rb
+++ lib/check_notifications/pusher.rb
@@ -80,7 +80,7 @@ module CheckNotifications
         channels = targets.reject{ |t| t.blank? }.map(&:pusher_channel)
         data = '{}' if data == 'null'
 
-        Rails.env == 'test' ? self.request_pusher(channels, event, data, self.actor_session_id) : CheckNotifications::Pusher::Worker.perform_in(1.second, channels, event, data, self.actor_session_id)
+        Rails.env == 'test' ? self.request_pusher(channels, event, self.parse_data(data).to_json, self.actor_session_id) : CheckNotifications::Pusher::Worker.perform_in(1.second, channels, event, self.parse_data(data).to_json, self.actor_session_id)
         CheckNotifications::Pusher::Worker.perform_in(1.second, ['check-api-global-channel'], 'update', self.parse_data(data).merge({ pusherChannels: channels, pusherEvent: event }).to_json, self.actor_session_id)
       end
 
