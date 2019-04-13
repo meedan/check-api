1474bbb4ddb09eb3b3975a5c92b47cbc2ce727ce
infojunkie <karim.ratib@gmail.com>
2019-04-13 14:40:48 -0700

    Rake-task based migration for smooch sort #7484

--- db/migrate/20190413001453_add_smooch_annotations_index.rb
+++ db/migrate/20190413001453_add_smooch_annotations_index.rb
@@ -1,7 +1,8 @@
 class AddSmoochAnnotationsIndex < ActiveRecord::Migration
   def change
-    Dynamic.where(annotation_type: 'smooch').find_each do |d|
-      d.send(:add_elasticsearch_dynamic)
-    end
+    # No schema changes here, only need to call ES reindexing on all `smooch` annotations.
+    # Remember the last annotation we need to work on since once this code is deployed,
+    # all subsequent annotations will be properly indexed.
+    Rails.cache.write('check:migrate:add_smooch_annotations_index:last_id', Dynamic.where(annotation_type: 'smooch')&.last&.id || 0)
   end
 end
