914237e216b259c149bbcc1a5973450ce0b8df2f
infojunkie <karim.ratib@gmail.com>
2019-04-14 09:08:40 -0700

    Revert "Rake-task based migration for smooch sort #7484"
    This reverts commit 658b613efe3572f193e64fb3c75450ff2dabac01.

--- db/migrate/20190413001453_add_smooch_annotations_index.rb
+++ db/migrate/20190413001453_add_smooch_annotations_index.rb
@@ -1,8 +1,7 @@
 class AddSmoochAnnotationsIndex < ActiveRecord::Migration
   def change
-    # No schema changes here, only need to call ES reindexing on all `smooch` annotations.
-    # Remember the last annotation we need to work on since once this code is deployed,
-    # all subsequent annotations will be properly indexed.
-    Rails.cache.write('check:migrate:add_smooch_annotations_index:last_id', Dynamic.where(annotation_type: 'smooch')&.last&.id || 0)
+    Dynamic.where(annotation_type: 'smooch').find_each do |d|
+      d.send(:add_elasticsearch_dynamic)
+    end
   end
 end
