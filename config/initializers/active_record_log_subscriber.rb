df2c719a623fb46f884f3624f2288d38d95dd1bb
Caio Almeida <caiosba@gmail.com>
2019-04-16 02:03:47 -0300

    Ticket #7516: Improve mutation performance and show in development mode which line of code triggered a query

---
+++ config/initializers/active_record_log_subscriber.rb
@@ -0,0 +1,18 @@
+module LogQuerySource
+  def debug(*args, &block)
+    return unless super
+
+    backtrace = Rails.backtrace_cleaner.clean caller
+
+    relevant_caller_line = backtrace.detect do |caller_line|
+      !caller_line.include?('/initializers/')
+    end
+
+    if relevant_caller_line
+      logger.debug("  #{ relevant_caller_line.sub("#{ Rails.root }/", '') }")
+    end
+  end
+end
+if Rails.env.development?
+  ActiveRecord::LogSubscriber.send :prepend, LogQuerySource
+end
--- config/initializers/graphql_extensions.rb
+++ config/initializers/graphql_extensions.rb
@@ -5,7 +5,7 @@ module GraphQL
         relation = nil
         relation = PaperTrail::Version.where(id: child_node.id) if child_node.is_a?(PaperTrail::Version) && parent_node.is_a?(ProjectMedia)
         relation ||= child_node.is_annotation? ? parent_node.annotation_relation : parent_node.send(child_node.class_name.underscore.pluralize)
-        relation = child_node.class.where(id: child_node.id) unless relation.to_a.include?(child_node)
+        relation = child_node.class.where(id: child_node.id) if relation.where(id: child_node.id).count == 0
         parent_connection = GraphQL::Relay::RelationConnection.new(relation, {})
         self.new(child_node, parent_connection)
       end
