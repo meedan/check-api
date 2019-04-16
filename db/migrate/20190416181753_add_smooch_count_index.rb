d201eeb58c009a93ea1337da69d93975a35c829d
Daniela Feitosa <danielafeitosa@gmail.com>
2019-04-16 16:53:46 -0300

    Feature/7484 sort most requested (#400)
    Ticket #7484: Task to update smooch count on ES
    Ticket #7484: Update ES with `bulk` operation
    Ticket #7484: Add migration to store the last project media ID

---
+++ db/migrate/20190416181753_add_smooch_count_index.rb
@@ -0,0 +1,9 @@
+class AddSmoochCountIndex < ActiveRecord::Migration
+  def change
+    # No schema changes here, only need to call ES reindexing on all `smooch` annotations.
+    # Remember the last project media we need to work on since once this code is deployed,
+    # all subsequent annotations on new project medias will be properly indexed and
+    # all subsequent annotations on existent project medias will be considered when running the task
+    Rails.cache.write('check:migrate:add_smooch_annotations_index:last_id', ProjectMedia&.last&.id || 0)
+  end
+end
