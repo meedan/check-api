9b7e0ba256a3d048dac665975c325e244ee27dff
Daniela Feitosa <danielafeitosa@gmail.com>
2019-04-19 02:36:32 -0300

    Ticket #7526: Send project export link only to owners with member status (#403)
    The owners with emails notifications disabled don't receive a copy of download link

--- test/mailers/admin_mailer_test.rb
+++ test/mailers/admin_mailer_test.rb
@@ -3,11 +3,37 @@ require_relative '../test_helper'
 class AdminMailerTest < ActionMailer::TestCase
   test "should send download link" do
     p = create_project
-    email = AdminMailer.send_download_link(:csv, p, 'test@test.com', 'password')
+    requestor = create_user
+    email = AdminMailer.send_download_link(:csv, p, requestor.email, 'password')
     assert_emails 1 do
       email.deliver_now
     end
-    assert_equal ['test@test.com'], email.to
+    assert_equal [requestor.email], email.to
+  end
+
+  test "should include on download link cc only members with notifications enabled" do
+    t = create_team
+    p = create_project team: t
+    requestor = create_user
+    member = create_user
+    member_without_notifications = create_user
+    member_without_notifications.set_send_email_notifications = false; member_without_notifications.save!
+    requested = create_user
+    invited = create_user
+    banned = create_user
+    create_team_user team: t, user: requestor, role: 'owner', status: 'member'
+    create_team_user team: t, user: member, role: 'owner', status: 'member'
+    create_team_user team: t, user: member_without_notifications, role: 'owner', status: 'member'
+    create_team_user team: t, user: requested, role: 'owner', status: 'requested'
+    create_team_user team: t, user: invited, role: 'owner', status: 'invited'
+    create_team_user team: t, user: banned, role: 'owner', status: 'banned'
+
+    email = AdminMailer.send_download_link(:csv, p, requestor.email, 'password')
+    assert_emails 1 do
+      email.deliver_now
+    end
+    assert_equal [requestor.email], email.to
+    assert_equal [member.email], email.cc
   end
 
   test "should notify import completed" do
