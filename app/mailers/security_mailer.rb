3840af7d27b2ce5e384342c185b7d22e74b7f951
Sawy <melsawy.be7eri@gmail.com>
2019-04-30 20:28:47 +0200

    Ticket #7353 - set UTC for timezone and fix reset password link

--- app/mailers/security_mailer.rb
+++ app/mailers/security_mailer.rb
@@ -7,7 +7,7 @@ class SecurityMailer < ApplicationMailer
   	email = user.email
   	@user_agent = UserAgent.parse(activity.user_agent)
   	@location = "#{activity.city}, #{activity.country}"
-  	@timestamp = activity.created_at.strftime('%A, %d %B %Y %I:%M %p')
+  	@timestamp = activity.created_at.strftime('%A, %d %B %Y %I:%M %p %Z')
   	@ip = activity.ip
   	subject = I18n.t("mail_security.#{type}_subject",
       app_name: CONFIG['app_name'], browser: @user_agent.browser, platform: @user_agent.platform)
