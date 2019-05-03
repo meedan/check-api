59e0726ec7fadcc84d88f6a0746ef03840a74502
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2019-05-03 22:55:21 +0200

    Ticket #7353 - get full country name and localize it (#409)

--- app/mailers/security_mailer.rb
+++ app/mailers/security_mailer.rb
@@ -2,14 +2,18 @@ class SecurityMailer < ApplicationMailer
 	layout nil
 
   def notify(user, type, activity)
-    # Getting location
-    AuthTrail::GeocodeJob.perform_now(activity)
-    activity = activity.reload
+    address = []
+    Geocoder.configure(language: I18n.locale)
+    ip_result = Geocoder.search(activity.ip).first
+    unless ip_result.blank? || ip_result.data["loc"].blank?
+      loc_result = Geocoder.search(ip_result.data["loc"]).first
+      address = [loc_result.city, loc_result.country]
+    end
   	@user = user
   	@type = type
   	email = user.email
   	@user_agent = UserAgent.parse(activity.user_agent)
-  	@location = "#{activity.city}, #{activity.country}"
+    @location = address.compact.join(', ')
   	@timestamp = activity.created_at.strftime('%A, %d %B %Y %I:%M %p %Z')
   	@ip = activity.ip
     @platform = @user_agent.os.split.first
