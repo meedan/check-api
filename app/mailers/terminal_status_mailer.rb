2ed5a627194203cee14040b6ff28e28c7fa648b3
Sawy <melsawy.be7eri@gmail.com>
2019-05-19 17:24:47 +0200

    Ticket #7522 - add text version for media update status and apply css comments

--- app/mailers/terminal_status_mailer.rb
+++ app/mailers/terminal_status_mailer.rb
@@ -2,13 +2,15 @@ class TerminalStatusMailer < ApplicationMailer
 	layout nil
 
 	def notify(annotated, author, status)
+    verification_status = status.to_s
     project = annotated.project
 		team = project.team
 		recipients = team.recipients(author, ['editor', 'owner'])
     created_at = annotated.created_at
+    last_update = annotated.updated_at
     updated_at = status.updated_at
+    image_path = annotated.media.type == 'UploadedImage' ? annotated.media.image_path : ''
 		@info = {
-      username: '',
       team: team.name,
       project: project.title,
       project_url: project.url,
@@ -18,16 +20,19 @@ class TerminalStatusMailer < ApplicationMailer
       media_url: annotated.full_url,
       title: annotated.title,
       description: annotated.description,
-      image_path: annotated.media.image_path,
-      status: status.status,
+      image_path: image_path,
+      media_link: annotated.media.url,
+      status: verification_status,
       notes: annotated.get_versions_log_count,
       total_tasks: annotated.get_annotations('task').count,
       resolved_tasks: annotated.tasks_resolved_count,
       type: annotated.class.name.underscore,
+      media_type: annotated.media.type.downcase,
       created_at: created_at.strftime("%B #{created_at.day.ordinalize} %I:%M %p"),
-      updated_at: updated_at.strftime("%B #{updated_at.day.ordinalize} %I:%M %p")
+      updated_at: updated_at.strftime("%B #{updated_at.day.ordinalize} %I:%M %p"),
+      last_update: updated_at.strftime("%B #{last_update.day.ordinalize} %I:%M %p")
     }
-    subject = I18n.t('mails_notifications.media_status.subject', team: team.name, project: annotated.project.title, status: status.status)
+    subject = I18n.t('mails_notifications.media_status.subject', team: team.name, project: annotated.project.title, status: verification_status)
     self.send_email_to_recipients(recipients, subject, 'terminal_status') unless recipients.empty?
 	end
 
