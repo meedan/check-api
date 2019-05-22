c91ccd3912be3adb9d844a4dfdae7ded94649b3f
Daniela Soares Feitosa <daniela@meedan.com>
2019-05-22 15:01:08 -0300

    Ticket #7573: Remove recent_activity and recent_added keys from es doc
    Also:
    add a migration to store on cache the last ProjectMedia id
    add a task to remove the fields from existent media es docs

---
+++ db/migrate/20190522170933_remove_es_recent_fields.rb
@@ -0,0 +1,7 @@
+class RemoveEsRecentFields < ActiveRecord::Migration
+  def change
+    # Remember the last project media we need to work on since once this code is deployed,
+    # all subsequent new project medias will not include recent_activity and recent_added fields
+    Rails.cache.write('check:migrate:remove_es_recent_fields:last_id', ProjectMedia&.last&.id || 0)
+  end
+end
