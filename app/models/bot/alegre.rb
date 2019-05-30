3aaf8ea1ea859350eadddc0e248996fc1a83ffb4
infojunkie <karim.ratib@gmail.com>
2019-05-29 17:39:49 -0700

    Filter VFRAME queries by context and create relationships

--- app/models/bot/alegre.rb
+++ app/models/bot/alegre.rb
@@ -29,9 +29,9 @@ class Bot::Alegre < ActiveRecord::Base
     CONFIG['checkdesk_base_url'] + self.avatar.url
   end
 
-  def get_language(target)
-    lang = target.text.blank? || CONFIG['alegre_host'].blank? ? 'und' : self.get_language_from_alegre(target.text)
-    self.save_language(target, lang)
+  def get_language(pm)
+    lang = pm.text.blank? || CONFIG['alegre_host'].blank? ? 'und' : self.get_language_from_alegre(pm.text)
+    self.save_language(pm, lang)
     lang
   end
 
@@ -45,9 +45,9 @@ class Bot::Alegre < ActiveRecord::Base
     lang
   end
 
-  def save_language(target, lang)
+  def save_language(pm, lang)
     annotation = Dynamic.new
-    annotation.annotated = target
+    annotation.annotated = pm
     annotation.annotator = self
     annotation.annotation_type = 'language'
     annotation.disable_es_callbacks = Rails.env.to_s == 'test'
@@ -56,14 +56,52 @@ class Bot::Alegre < ActiveRecord::Base
     annotation
   end
 
-  def language_object(target, attr = nil)
-    field = self.get_dynamic_field_value(target, 'language', 'language')
+  def language_object(pm, attr = nil)
+    field = self.get_dynamic_field_value(pm, 'language', 'language')
     return nil if field.nil?
     attr.nil? ? field : field.send(attr)
   end
 
-  def get_dynamic_field_value(target, annotation_type, field_type)
-    DynamicAnnotation::Field.joins(:annotation).where('annotations.annotation_type' => annotation_type, 'annotations.annotated_type' => target.class.name, 'annotations.annotated_id' => target.id.to_s, field_type: field_type).first
+  def get_dynamic_field_value(pm, annotation_type, field_type)
+    DynamicAnnotation::Field.joins(:annotation).where('annotations.annotation_type' => annotation_type, 'annotations.annotated_type' => pm.class.name, 'annotations.annotated_id' => pm.id.to_s, field_type: field_type).first
+  end
+
+  def get_context(pm)
+    {
+      team_id: pm.project.team.id,
+      project_id: pm.project.id,
+      project_media_id: pm.id
+    }
+  end
+
+  def add_relationships(pm, pm_ids)
+    return if pm_ids.empty? || pm_ids.include?(pm.id)
+
+    # Take first match as being the best potential parent.
+    # Conditions to check for a valid parent in 2-level hierarchy:
+    # - If it's a child, get its parent.
+    # - If it's a parent, use it.
+    # - If it has no existing relationship, use it.
+    parent_id = pm_ids[0]
+    source_ids = Relationship.where(:target_id => parent_id).select(:source_id).distinct
+    if source_ids.length > 0 then
+      # Sanity check: if there are multiple parents, something is wrong in the dataset.
+      if source_ids.length > 1 then
+        Rails.logger.error("[Alegre Bot] Found multiple relationship parents for ProjectMedia #{parent_id}")
+      end
+      # Take the first source as the parent.
+      parent_id = source_ids[0].source_id
+    end
+
+    # Better be safe than sorry.
+    return if parent_id == pm.id
+
+    r = Relationship.new
+    r.skip_check_ability = true
+    r.relationship_type = { source: 'parent', target: 'child' }
+    r.source_id = parent_id
+    r.target_id = pm.id
+    r.save!
   end
 
   def get_image_similarities(pm)
@@ -71,40 +109,48 @@ class Bot::Alegre < ActiveRecord::Base
 
     require 'net/http/post/multipart'
 
+    # Send image to VFRAME to get matches.
     url = URI.parse(CONFIG['vframe_host'] + '/api/v1/match')
+    response = { 'results' => [] }
     Net::HTTP.start(url.host, url.port) do |http|
       req = Net::HTTP::Post::Multipart.new(url, {
         'url' => CONFIG['checkdesk_base_url_private'] + pm.media.file.url,
-        'context' => {
-          team_id: pm.project.team.id,
-          project_id: pm.project.id,
-          project_media_id: pm.id
-        }.to_json
+        'context' => self.get_context(pm).to_json,
+        'filter' => { project_id: pm.project.id }.to_json
       })
       http.use_ssl = (url.scheme == 'https')
-      result = http.request(req).body.to_json
+
+      begin
+        response = JSON.parse(http.request(req).body)
+      rescue StandardError => e
+        Rails.logger.error("[Alegre Bot] Bad response from VFRAME: #{e.message}")
+        Airbrake.notify(e) if Airbrake.configuration.api_key
+      end
     end
+    print response.inspect
+    pm_ids = response['results'].collect{|r| r.dig('context', 'project_media_id')}
+    self.add_relationships(pm, pm_ids)
   end
 
-  # def get_source_language(target)
-  #   field = self.get_dynamic_field_value(target, 'language', 'language')
-  #   field.nil? ? Bot::Alegre.default.get_language_from_alegre(target) : field.value
+  # def get_source_language(pm)
+  #   field = self.get_dynamic_field_value(pm, 'language', 'language')
+  #   field.nil? ? Bot::Alegre.default.get_language_from_alegre(pm) : field.value
   # end
 
-  # def get_mt_from_alegre(target, author)
+  # def get_mt_from_alegre(pm, author)
   #   translations = []
-  #   src_lang = self.get_source_language(target)
-  #   languages = target.project.languages - [src_lang]
+  #   src_lang = self.get_source_language(pm)
+  #   languages = pm.project.languages - [src_lang]
   #   languages.each do |lang|
   #     begin
-  #       response = AlegreClient::Request.get_mt(CONFIG['alegre_host'], { text: target.text, from: src_lang, to: lang }, CONFIG['alegre_token'])
+  #       response = AlegreClient::Request.get_mt(CONFIG['alegre_host'], { text: pm.text, from: src_lang, to: lang }, CONFIG['alegre_token'])
   #       mt_text = response['data'] if response['type'] == 'mt' && !response['data'].blank?
   #     rescue
   #       mt_text = nil
   #     end
   #     translations << { lang: lang, text: mt_text } unless mt_text.nil?
   #   end
-  #   self.update_machine_translation(target, translations, author) unless translations.blank?
+  #   self.update_machine_translation(pm, translations, author) unless translations.blank?
   #   translations
   # end
 
@@ -112,21 +158,21 @@ class Bot::Alegre < ActiveRecord::Base
   # Find the "parent" claim among the set, and link this new one to it as a "child".
   # If there's no existing parent, create the first relationship now.
   # Then add this new claim to the similarity index.
-  # def create_similarities_from_alegre(target)
-  #   return if target.media.type != 'Claim'
+  # def create_similarities_from_alegre(pm)
+  #   return if pm.media.type != 'Claim'
 
-  #   src_lang = self.get_source_language(target)
+  #   src_lang = self.get_source_language(pm)
   #   response = Bot::Alegre.request_api('POST', '/similarity/query', {
-  #     text: target.text,
+  #     text: pm.text,
   #     language: src_lang,
   #     context: {
-  #       project_id: target.project.id
+  #       project_id: pm.project.id
   #     }
   #   })
 
   #   if response['result'] and response['result'].length > 0 then
   #     pm_ids = response['result'].collect{|r| r.dig('_source', 'context', 'project_media_id')}
-  #     return if pm_ids.include?(target.id)
+  #     return if pm_ids.include?(pm.id)
 
   #     # Take first match as being the best potential parent.
   #     # Conditions to check for a valid parent in 2-level hierarchy:
@@ -134,7 +180,7 @@ class Bot::Alegre < ActiveRecord::Base
   #     # - If it's a parent, use it.
   #     # - If it has no existing relationship, use it.
   #     parent_id = pm_ids[0]
-  #     source_ids = Relationship.where(:target_id => parent_id).select(:source_id).distinct
+  #     source_ids = Relationship.where(:pm_id => parent_id).select(:source_id).distinct
   #     if source_ids.length > 0 then
   #       # Sanity check: if there are multiple parents, something is wrong in the dataset.
   #       if source_ids.length > 1 then
@@ -145,33 +191,33 @@ class Bot::Alegre < ActiveRecord::Base
   #     end
 
   #     # Better be safe than sorry.
-  #     return if parent_id == target.id
+  #     return if parent_id == pm.id
 
   #     r = Relationship.new
   #     r.skip_check_ability = true
-  #     r.relationship_type = { source: 'parent', target: 'child' }
+  #     r.relationship_type = { source: 'parent', pm: 'child' }
   #     r.source_id = parent_id
-  #     r.target_id = target.id
+  #     r.pm_id = pm.id
   #     r.save!
   #   end
 
   #   Bot::Alegre.request_api('POST', '/similarity/', {
-  #     text: target.text,
+  #     text: pm.text,
   #     language: src_lang,
   #     context: {
-  #       team_id: target.project.team.id,
-  #       project_id: target.project.id,
-  #       project_media_id: target.id
+  #       team_id: pm.project.team.id,
+  #       project_id: pm.project.id,
+  #       project_media_id: pm.id
   #     }
   #   })
   # end
 
-  # def create_empty_mt_annotation(target)
-  #   src_lang = self.language_object(target, :value)
-  #   languages = target.project.get_languages
+  # def create_empty_mt_annotation(pm)
+  #   src_lang = self.language_object(pm, :value)
+  #   languages = pm.project.get_languages
   #   unless src_lang.blank? or languages.nil?
   #     annotation = Dynamic.new
-  #     annotation.annotated = target
+  #     annotation.annotated = pm
   #     annotation.annotator = self
   #     annotation.annotation_type = 'mt'
   #     annotation.set_fields = { 'mt_translations': [].to_json }.to_json
@@ -196,8 +242,8 @@ class Bot::Alegre < ActiveRecord::Base
   #   end
   # end
 
-  # def update_machine_translation(target, translations, author)
-  #   mt = target.annotations.where(annotation_type: 'mt').last
+  # def update_machine_translation(pm, translations, author)
+  #   mt = pm.annotations.where(annotation_type: 'mt').last
   #   unless mt.nil?
   #     mt = mt.load
   #     User.current = author
@@ -206,7 +252,7 @@ class Bot::Alegre < ActiveRecord::Base
   #     mt.save!
   #     User.current = nil
   #     # Delete old versions
-  #     mt_field = self.get_dynamic_field_value(target, 'mt', 'json')
+  #     mt_field = self.get_dynamic_field_value(pm, 'mt', 'json')
   #     versions = mt_field.versions.to_a unless mt_field.nil?
   #     unless versions.blank?
   #       versions.pop
