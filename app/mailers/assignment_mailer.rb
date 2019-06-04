3265e17e4e42bf52db612321d28c436b05dde6d3
Sawy <melsawy.be7eri@gmail.com>
2019-06-04 18:19:55 +0200

    Ticket #7500 - add text version

--- app/mailers/assignment_mailer.rb
+++ app/mailers/assignment_mailer.rb
@@ -9,11 +9,23 @@ class AssignmentMailer < ApplicationMailer
     if assigned.is_a?(Annotation)
       annotation = assigned
       project_media = annotation.annotated
-      task = annotation.load.label if annotation.annotation_type == 'task'
-      title = project_media.title
       url = project_media.full_url
+      title, media_title = project_media.title
+      description = project_media.description
       project = project_media.project
       team = project.team
+      media_title = project_media.title
+      if annotation.annotation_type == 'task'
+        task = annotation.load
+        title = task.label
+        description = task.description
+      end
+      # add more info related to media 
+      image_path = project_media.media.type == 'UploadedImage' ? project_media.media.image_path : ''
+      media_link = project_media.media.url
+      updated_at = project_media.updated_at.strftime("%B #{project_media.updated_at.day.ordinalize} %I:%M %p")
+      total_tasks = project_media.get_annotations('task').count
+      resolved_tasks =  project_media.tasks_resolved_count
     elsif assigned.is_a?(Project)
       project = assigned
       title = project.title
@@ -29,23 +41,29 @@ class AssignmentMailer < ApplicationMailer
       profile_image = author.profile_image
     end
     # map verification_status and translation_status to one event called "item"
-    event_key = event.gsub("verification_status", "item").gsub("translation_status", "item")
-
+    event_key = event.gsub("verification_status", "media").gsub("translation_status", "media")
+    model = event_key.partition('_').last
     info = {
       event_key: event_key,
       event_type: event.partition('_').first,
-      partial_template: event_key.partition('_').last,
+      model: model,
       author: author_name,
       author_id: author_id,
       team: team.name,
       project: project.title,
       title: title,
+      media_title: media_title,
       url: url,
       profile_image: profile_image,
       role: role,
       created_at: created_at.strftime("%B #{created_at.day.ordinalize} %I:%M %p"),
+      image_path: image_path,
+      media_link: media_link,
+      updated_at: updated_at,
+      total_tasks: total_tasks,
+      resolved_tasks: resolved_tasks,
       button: I18n.t("slack.fields.view_button", {
-        type: I18n.t("activerecord.models.project"), app: CONFIG['app_name']
+        type: I18n.t("activerecord.models.#{model}"), app: CONFIG['app_name']
       }),
       project_url: project.url,
       description: description,
