b0e2ad5e01d2a8bae2964011cd83995373e2129b
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2019-06-12 23:07:20 +0200

    Feature/7500 team user email templates (#424)
    * Ticket #7500 - apply templates for team user mailers
    
    * Ticket #7500 - fix tests and modify template text

--- app/mailers/team_user_mailer.rb
+++ app/mailers/team_user_mailer.rb
@@ -1,35 +1,43 @@
 class TeamUserMailer < ApplicationMailer
   layout nil
 
-  def self.request_to_join_notification(team, requestor, origin)
+  def self.send_notification(options)
+    options = YAML::load(options)
+    team = options[:team]
+    requestor = options[:user]
     if team && requestor
+      info = {
+        team: team,
+        requestor: requestor,
+        url: "#{CONFIG['checkdesk_client']}/#{team.slug}",
+      }
+      subject = I18n.t("mails_notifications.request_to_join.subject", team: team.name)
       recipients = team.recipients(requestor)
       recipients = Bounce.remove_bounces(recipients)
       recipients.each do |recipient|
-        self.delay.request_to_join(recipient, team, requestor, origin)
+        request_to_join(recipient, info, subject).deliver_now
       end
     end
   end
 
-  def request_to_join(recipient, team, requestor, origin)
-    @team = team
-    @requestor = requestor
-    @url = origin.blank? ? '' : URI.join(origin, "/#{@team.slug}")
-    @handle = requestor.handle
-    subject = I18n.t(:mail_request_to_join, user: requestor.name, team: team.name, app_name: CONFIG['app_name'])
+  def request_to_join(recipient, info, subject)
+    self.set_template_var(info, recipient)
     mail(to: recipient, subject: subject)
   end
 
-  def request_to_join_processed(team, requestor, accepted, origin)
+  def request_to_join_processed(team, requestor, accepted)
     if team && requestor && !requestor.email.blank?
-      @team = team
-      @requestor = requestor
-      @accepted = accepted
-      @url = origin.blank? ? '' : URI.join(origin, "/#{@team.slug}")
+      request_status = accepted ? 'approved' : 'rejected'
+      info = {
+        team: team,
+        requestor: requestor,
+        url: "#{CONFIG['checkdesk_client']}/#{team.slug}",
+        request_status: request_status
+      }
+      self.set_template_var(info, requestor.email)
       Rails.logger.info "Sending e-mail to #{requestor.email}"
-      status = accepted ? I18n.t(:approved) : I18n.t(:rejected)
-      self.send_email_to_recipients(requestor.email,
-      I18n.t(:mail_request_to_join_processed, team: team.name, status: status, app_name: CONFIG['app_name']))
+      subject = I18n.t("mails_notifications.request_to_join.#{request_status}_subject", team: team.name)
+      self.send_email_to_recipients(requestor.email, subject)
     end
   end
 
