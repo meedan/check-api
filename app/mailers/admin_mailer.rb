155c38e3935db022df993039c4114745a5919574
Sawy <melsawy.be7eri@gmail.com>
2019-06-16 21:16:17 +0200

    Ticket #7500 - add templates for admin mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -9,13 +9,13 @@ class AdminMailer < ApplicationMailer
       @project = obj.title
       @link = link
       @days = CONFIG['export_download_expiration_days'] || 7
-      @app = CONFIG['app_name']
       @password = password
       @type = type
       recipients = obj.team.owners('owner', ['member']).where.not(email: [nil, '', email]).to_a
       recipients = recipients.delete_if {|u| u.get_send_email_notifications == false }.map(&:email)
       recipients = Bounce.remove_bounces(recipients)
-      mail(to: email, cc: recipients, subject: I18n.t("project_export_email_title_#{type}".to_sym))
+      subject = I18n.t("mails_notifications.admin_mailer.project_export_#{type}_subject", project: @project)
+      mail(to: email, cc: recipients, subject: subject)
     end
   end
 
@@ -23,8 +23,7 @@ class AdminMailer < ApplicationMailer
     return if email.blank?
     Rails.logger.info "[Team Import] Sending e-mail to #{email} to inform that the data import of #{worksheet_url} has completed"
     @worksheet_url = worksheet_url
-    @app_name = CONFIG['app_name']
-    mail(to: email, subject: I18n.t(:team_import_completed_subject))
+    mail(to: email, subject: I18n.t("mails_notifications.admin_mailer.team_import_subject"))
   end
 
   def send_team_download_link(slug, link, email, password)
@@ -32,7 +31,6 @@ class AdminMailer < ApplicationMailer
     Rails.logger.info "Sending e-mail to #{email} with download link #{@link} related to team #{slug}"
     @team = slug
     @days = CONFIG['export_download_expiration_days'] || 7
-    @app = CONFIG['app_name']
     @password = password
     mail(to: email, subject: I18n.t("mails_notifications.admin_mailer.team_download_subject", team: @team))
   end
