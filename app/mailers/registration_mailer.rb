24413567227a685945173cec680312a887a4a019
Sawy <melsawy.be7eri@gmail.com>
2019-06-17 13:47:33 +0200

    Ticket #7500 - update registeration templates

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -5,15 +5,15 @@ class RegistrationMailer < ApplicationMailer
     @user = user
     @user.password ||= password
     @url = CONFIG['checkdesk_client']
-    mail(to: @user.email, subject: I18n.t(:mail_new_account, app_name: CONFIG['app_name'])) unless @user.email.blank?
+    mail(to: @user.email, subject: I18n.t("mails_notifications.register.subject", app_name: CONFIG['app_name'])) unless @user.email.blank?
   end
 
   def duplicate_email_detection(user, provider)
     @user = user
-    @duplicate_provider =  provider.blank? ? I18n.t(:mail_duplicate_email_exists_email) : provider
-    @user_provider = user.encrypted_password? ? I18n.t(:mail_duplicate_email_exists_email) : user.get_user_provider(user.email)
-    @mail_body = (provider.blank? && user.encrypted_password?) ? :mail_duplicate_email_exists_body_both_emails : :mail_duplicate_email_exists_body
-    mail(to: @user.email, subject: I18n.t(:mail_duplicate_email_exists, app_name: CONFIG['app_name'])) unless @user.email.blank?
+    @duplicate_provider =  provider.blank? ? I18n.t("mails_notifications.duplicated.email") : provider
+    @user_provider = user.encrypted_password? ? I18n.t("mails_notifications.duplicated.email") : user.get_user_provider(user.email)
+    @body_key = (provider.blank? && user.encrypted_password?) ? "both_emails" : "one_email"
+    mail(to: @user.email, subject: I18n.t("mails_notifications.duplicated.subject", app_name: CONFIG['app_name'])) unless @user.email.blank?
   end
 
 end
