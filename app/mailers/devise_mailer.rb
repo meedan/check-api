927672ae927b01bccf7becf6a7d3725de4bcb37a
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2019-06-21 01:52:36 +0200

    Fix/7500 template direction (#430)
    * Ticket #7500 - add direction based on lang code
    
    * Ticket #7614 - localize date
    
    * Ticket #7614 - localize DeviseMailer
    
    * Fix reset password link
    
    * Ticket #7500 - fix tests

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -5,6 +5,7 @@ class DeviseMailer < Devise::Mailer
   def confirmation_instructions(record, token, opts={})
     @host = CONFIG['checkdesk_base_url']
     @client_host = CONFIG['checkdesk_client']
+    @direction = ApplicationMailer.set_template_direction
     opts[:subject] = I18n.t(:mail_account_confirmation, app_name: CONFIG['app_name'])
     super
   end
@@ -12,6 +13,7 @@ class DeviseMailer < Devise::Mailer
   def reset_password_instructions(record, token, opts={})
     @host = CONFIG['checkdesk_base_url']
     @title = I18n.t("mails_notifications.reset_password.title")
+    @direction = ApplicationMailer.set_template_direction
     opts[:subject] = I18n.t('devise.mailer.reset_password_instructions.subject', app_name: CONFIG['app_name'])
     super
   end
@@ -27,6 +29,7 @@ class DeviseMailer < Devise::Mailer
     @invited_type = @invited_text.blank? ? 'default' : 'custom'
     @due_at = opts[:due_at]
     @title = I18n.t("mails_notifications.invitation.title")
+    @direction = ApplicationMailer.set_template_direction
     opts[:subject] = I18n.t(:'devise.mailer.invitation_instructions.subject', user: @invited_by, team: @team.name)
     super
   end
