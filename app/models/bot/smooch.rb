2317c0cb6ac355bf3ea1db37bf5fae65e765d00c
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2019-06-24 10:57:58 +0200

    Create a new module for check i18n (#431)

--- app/models/bot/smooch.rb
+++ app/models/bot/smooch.rb
@@ -1,6 +1,9 @@
 require 'digest'
 
 class Bot::Smooch
+
+  include CheckI18n
+
   ::Workflow::VerificationStatus.class_eval do
     check_workflow from: :any, to: :any, actions: :replicate_status_to_children
     check_workflow from: :any, to: :terminal, actions: :reply_to_smooch_users
@@ -210,14 +213,6 @@ class Bot::Smooch
     bot = TeamBot.where(identifier: 'smooch').last
     tbi = TeamBotInstallation.where(team_id: pm.project.team_id, team_bot_id: bot.id).last
     !tbi.nil? && tbi.settings.with_indifferent_access[:smooch_disabled].blank?
-  end
-
-  def self.convert_numbers(str)
-    return nil if str.blank?
-    altzeros = [0x0030, 0x0660, 0x06F0, 0x07C0, 0x0966, 0x09E6, 0x0A66, 0x0AE6, 0x0B66, 0x0BE6, 0x0C66, 0x0CE6, 0x0D66, 0x0DE6, 0x0E50, 0x0ED0, 0x0F20, 0x1040, 0x1090, 0x17E0, 0x1810, 0x1946, 0x19D0, 0x1A80, 0x1A90, 0x1B50, 0x1BB0, 0x1C40, 0x1C50, 0xA620, 0xA8D0, 0xA900, 0xA9D0, 0xA9F0, 0xAA50, 0xABF0, 0xFF10]
-    digits = altzeros.flat_map { |z| ((z.chr(Encoding::UTF_8))..((z+9).chr(Encoding::UTF_8))).to_a }.join('')
-    replacements = "0123456789" * altzeros.size
-    str.tr(digits, replacements).to_i
   end
 
   def self.get_installation(key, value)
