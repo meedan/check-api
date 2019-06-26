910bcf195b97190d54abac9696e9ebeb1cf2afce
Caio Almeida <caiosba@gmail.com>
2019-06-26 00:27:46 -0300

    Ticket #7618: Separate configuration key for CORS headers

--- config/application.rb
+++ config/application.rb
@@ -52,7 +52,7 @@ module Check
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins(/^(#{cfg['checkdesk_client']}|(moz|chrome)-extension:)|file:/)
+        origins(/^(#{cfg['allowed_origins']}|(moz|chrome)-extension:)|file:/)
         resource '*',
         headers: [cfg['authorization_header'], 'Content-Type', 'Accept', 'X-Requested-With', 'Origin', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Credentials', 'X-Check-Client', 'X-Check-Team', 'X-API-Key', 'X-Timezone'],
         methods: [:get, :post, :put, :delete, :options]
