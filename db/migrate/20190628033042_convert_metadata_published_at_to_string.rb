865146e67fc5e7f26fca7a4de1e6d3bd1b0e0173
Daniela Soares Feitosa <daniela@meedan.com>
2019-06-28 14:59:35 -0300

    Ticket #7619: Keep original `published_at` format coming from Pender
    Also add task to convert the `published_at` info on metadata annotations from Integer to Time format

---
+++ db/migrate/20190628033042_convert_metadata_published_at_to_string.rb
@@ -0,0 +1,7 @@
+class ConvertMetadataPublishedAtToString < ActiveRecord::Migration
+  def change
+    # Remember the last metadata annotation we need to verify on since this code is deployed,
+    # all subsequent `published_at` info on metadata annotation will be String.
+    Rails.cache.write('check:migrate:convert_metadata_published_at_to_string:last_id', DynamicAnnotation::Field.where(field_name: 'metadata_value')&.last&.id || 0)
+  end
+end
