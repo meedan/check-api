7d4c87174f93c468e2104076f64b9e70eb358195
Caio Almeida <caiosba@gmail.com>
2019-10-22 21:28:36 -0300

    Expose more fields on GraphQL type "Tag"

--- app/graph/types/tag_type.rb
+++ app/graph/types/tag_type.rb
@@ -1,1 +1,4 @@
-TagType = GraphqlCrudOperations.define_annotation_type('tag', { tag: 'str', tag_text: 'str' })
+TagType = GraphqlCrudOperations.define_annotation_type('tag', { tag: 'str', tag_text: 'str', fragment: 'str' }) do
+  field :tag_text_object, TagTextType
+  field :parsed_fragment, JsonStringType
+end
