f828daaf35ba11a5315402a7cf61f626e926df2e
Sawy <melsawy.be7eri@gmail.com>
2019-11-04 21:20:12 +0200

    Ticket #7771 - fix CC

--- app/graph/mutations/project_media_mutations.rb
+++ app/graph/mutations/project_media_mutations.rb
@@ -1,27 +1,28 @@
 module ProjectMediaMutations
-  create_fields = {
+  fields = {
     media_id: 'int',
     project_id: '!int',
-    media_type: 'str',
+    related_to_id: 'int'
+  }
+
+  create_fields = fields.merge({
     url: 'str',
     quote: 'str',
     quote_attributions: 'str',
     set_annotation: 'str',
     set_tasks_responses: 'json',
-    related_to_id: 'int'
-  }
+    media_type: 'str'
+  })
 
-  update_fields = {
-    media_id: 'int',
+  update_fields = fields.merge({
     project_id: 'int',
     previous_project_id: 'int',
     copy_to_project_id: 'int',
     refresh_media: 'int',
     update_mt: 'int',
     archived: 'int',
-    metadata: 'str',
-    related_to_id: 'int'
-  }
+    metadata: 'str'
+  })
 
   Create, Update, Destroy = GraphqlCrudOperations.define_crud_operations('project_media', create_fields, update_fields, ['project', 'project_was', 'check_search_team', 'check_search_trash', 'check_search_project', 'check_search_project_was', 'relationships_target', 'relationships_source', 'related_to'])
 end
