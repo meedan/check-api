398c4a3bc50f3b64ae8ac40d084809cdd15644f1
Caio Almeida <caiosba@gmail.com>
2019-11-11 01:25:59 -0300

    Make sure that CheckS3 constant is loaded

--- config/initializers/plugins.rb
+++ config/initializers/plugins.rb
@@ -1,2 +1,2 @@
 # Initialize the plugin by just calling the classes here
-CcDeville && Bot::Viber && Bot::Keep && Bot::BridgeReader && Workflow::Workflow.workflows
+CcDeville && Bot::Viber && Bot::Keep && Bot::BridgeReader && Workflow::Workflow.workflows && CheckS3
