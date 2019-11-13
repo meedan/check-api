fefd50ad6a257e0002a2a1c365a388edf6d8fe19
Aaron Huslage <aaron@meedan.com>
2019-11-13 12:51:03 -0500

    Doing it right this time. Thanks @caiosba

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ credentials = {
   region:                CONFIG['storage']['bucket_region'],
   host:                  URI(CONFIG['storage']['endpoint']).host,
   endpoint:              CONFIG['storage']['endpoint'],
-  path_style:            CONFIG['storage']['path_style'] || true
+  path_style:            CONFIG['storage']['path_style'].nil? ? true : CONFIG['storage']['path_style']
 }
 
 bucket_name = CONFIG['storage']['bucket']
