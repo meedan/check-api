e7dc84005bbb063eecb04c1fcac91b55ca5fdfed
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2019-11-14 21:28:29 +0200

    Ticket #7784 - store original ip based on  header (#464)
    * Ticket #7784 - store original ip based on  header
    
    * Ticket #7784 - fix start_with for nil

--- test/models/login_activity_test.rb
+++ test/models/login_activity_test.rb
@@ -105,5 +105,18 @@ class LoginActivityTest < ActiveSupport::TestCase
     end
   end
 
+  test "should store original ip" do
+    user = create_user
+    ip = random_ip
+    RequestStore.stubs(:[]).with(:request).returns(OpenStruct.new({ headers: { 'X-Forwarded-For' => ip } }))
+    la = create_login_activity user: user, success: true
+    assert_equal ip, la.ip
+    # security notification should based on original ip
+    assert_no_difference 'ActionMailer::Base.deliveries.size' do
+      create_login_activity user: user, success: true
+    end
+    RequestStore.unstub(:[])
+  end
+
   
 end
