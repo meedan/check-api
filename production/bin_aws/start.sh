3fd3def1f9c9df3a290cf7d7ed6c03abd48b7ddf
hfikry92 <hfikry92@gmail.com>
2019-12-05 10:35:46 +1100

    cloning configurator at run time

--- production/bin_aws/start.sh
+++ production/bin_aws/start.sh
@@ -18,6 +18,23 @@ function config_replace() {
     fi
 }
 
+#since GITHUB_TOKEN environment variable is a json object, we need parse the value
+#This function is here due to a limitation by "secrets manager"
+function getParsedGithubToken(){
+    
+  echo $GITHUB_TOKEN | python -c 'import sys, json; print(json.load(sys.stdin)["GITHUB_TOKEN"])'
+}
+if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
+	echo "GITHUB_TOKEN, DEPLOY_ENV and APP must be in the environment.   Exiting."
+	exit 1
+fi
+
+$GITHUB_TOKEN_PARSED = $(getParsedGithubToken)
+
+if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN_PARSED}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
+d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
+
+
 # sed in environmental variables
 for ENV in $( env | cut -d= -f1); do
     config_replace "$ENV" "${!ENV}" /etc/nginx/sites-available/${APP}
@@ -54,11 +71,8 @@ if [ -n "${PRIMARY}" ]; then
     /opt/bin/run_migration.sh
 fi
 
-# compile assets in the background, particularly the admin interface
-su ${DEPLOYUSER} -c "nice bundle exec rake assets:precompile" &
-
 echo "tailing ${LOGFILE}"
-tail -f $LOGFILE &
+tail -f ${LOGFILE} &
 
 echo "compiling assets"
 su ${DEPLOYUSER} -c "bundle exec rake assets:precompile"
