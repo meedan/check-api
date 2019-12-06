d81ff5e203ba0d6366ab0a8d7e6657b978851719
hfikry92 <hfikry92@gmail.com>
2019-12-06 14:42:35 +1100

    fixing var name

--- production/bin_aws/start.sh
+++ production/bin_aws/start.sh
@@ -18,12 +18,12 @@ function config_replace() {
     fi
 }
 
-if [[ -z ${GH_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
-	echo "GH_TOKEN, DEPLOY_ENV and APP must be in the environment. Exiting."
+if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} || -z ${RAILS_ENV+x}  ]]; then
+	echo "GITHUB_TOKEN, DEPLOY_ENV, APP and RAILS_ENV must be in the environment. Exiting."
 	exit 1
 fi
 
-if [ ! -d "configurator" ]; then git clone https://${GH_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
+if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
 d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
 
 
