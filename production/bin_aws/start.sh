d0aab36400c8c644640b8bb1c074951904d0a27e
hfikry92 <hfikry92@gmail.com>
2019-12-11 10:38:50 +1100

    Documenting the github token parser function

--- production/bin_aws/start.sh
+++ production/bin_aws/start.sh
@@ -17,6 +17,8 @@ function config_replace() {
 	sed -i'.bak' -e ${CMD} ${FILE}
     fi
 }
+#since GITHUB_TOKEN environment variable is a json object, we need parse the value
+#This function is here dure to a limitation by "secrets manager" 
 function getParsedGithubToken(){
     
   echo $GITHUB_TOKEN | python -c 'import sys, json; print(json.load(sys.stdin)["GITHUB_TOKEN"])'
