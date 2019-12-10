d55aed5733584f398774778912b412f7192535a4
Sawy <melsawy.be7eri@gmail.com>
2019-12-10 19:17:24 +0200

    Ticket #7871 - Configuration ES to connect to Elastic Cloud

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,6 +1,10 @@
 require 'typhoeus'
 require 'typhoeus/adapters/faraday'
+cloud_id = CONFIG['elasticsearch_cloud_id'].to_s
+user = CONFIG['elasticsearch_cloud_id'].to_s
+password = CONFIG['elasticsearch_password'].to_s
 host = CONFIG['elasticsearch_host'].to_s + ':' + CONFIG['elasticsearch_port'].to_s
-Elasticsearch::Persistence.client = Elasticsearch::Client.new(log: !!CONFIG['elasticsearch_log'], host: host) do |f|
+
+Elasticsearch::Persistence.client = Elasticsearch::Client.new(log: !!CONFIG['elasticsearch_log'], cloud_id: cloud_id, user: user, password: password, host: host) do |f|
   f.adapter :typhoeus
 end
