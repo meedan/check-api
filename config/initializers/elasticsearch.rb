331fc89cd59b073d18710c4d10ac3ccdb5b25cbd
Sawy <melsawy.be7eri@gmail.com>
2019-12-11 19:47:48 +0200

    Ticket #7871 - depend on host not cloud_id

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,10 +1,9 @@
 require 'typhoeus'
 require 'typhoeus/adapters/faraday'
 cloud_id = CONFIG['elasticsearch_cloud_id'].to_s
-user = CONFIG['elasticsearch_cloud_id'].to_s
+user = CONFIG['elasticsearch_user'].to_s
 password = CONFIG['elasticsearch_password'].to_s
 host = CONFIG['elasticsearch_host'].to_s + ':' + CONFIG['elasticsearch_port'].to_s
-
-Elasticsearch::Persistence.client = Elasticsearch::Client.new(log: !!CONFIG['elasticsearch_log'], cloud_id: cloud_id, user: user, password: password, host: host) do |f|
+Elasticsearch::Persistence.client = Elasticsearch::Client.new(log: !!CONFIG['elasticsearch_log'], user: user, password: password, host: host) do |f|
   f.adapter :typhoeus
 end
