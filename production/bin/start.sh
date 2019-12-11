20db8268604c268f0655580fa7cd4a6d96fe7c49
hfikry92 <hfikry92@gmail.com>
2019-12-11 11:15:56 +1100

    fixing spelling mistake

--- production/bin/start.sh
+++ production/bin/start.sh
@@ -18,11 +18,12 @@ function config_replace() {
     fi
 }
 #since GITHUB_TOKEN environment variable is a json object, we need parse the value
-#This function is here due to a limitation by "secrets manager" 
+#This function is here due to a limitation by "secrets manager"
 function getParsedGithubToken(){
     
   echo $GITHUB_TOKEN | python -c 'import sys, json; print(json.load(sys.stdin)["GITHUB_TOKEN"])'
 }
+
 if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
 	echo "GITHUB_TOKEN, DEPLOY_ENV and APP must be in the environment.   Exiting."
 	exit 1
