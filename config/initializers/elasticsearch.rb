d4d239873446c32008d0999f83ccfd6093199518
Sawy <melsawy.be7eri@gmail.com>
2019-12-11 20:45:02 +0200

    Ticket #7871 - remove cloud_id variable

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,6 +1,5 @@
 require 'typhoeus'
 require 'typhoeus/adapters/faraday'
-cloud_id = CONFIG['elasticsearch_cloud_id'].to_s
 user = CONFIG['elasticsearch_user'].to_s
 password = CONFIG['elasticsearch_password'].to_s
 host = CONFIG['elasticsearch_host'].to_s + ':' + CONFIG['elasticsearch_port'].to_s
