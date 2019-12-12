1b0484a060f71567037810f7b6b1daa14656fcfd
hfikry92 <hfikry92@gmail.com>
2019-12-12 15:19:17 +1100

    cloning configurator in the startup script with simple validation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} || -z ${RAILS_ENV+x}  ]]; then
-	echo "GITHUB_TOKEN, DEPLOY_ENV, APP and RAILS_ENV must be in the environment. Exiting."
+#since GITHUB_TOKEN environment variable is a json object, we need parse the value
+#This function is here due to a limitation by "secrets manager"
+function getParsedGithubToken(){
+    
+    
+  echo $GITHUB_TOKEN | python -c 'import sys, json; print(json.load(sys.stdin)["GITHUB_TOKEN"])'
+}
+if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
+	echo "GITHUB_TOKEN, DEPLOY_ENV, and APP must be in the environment. Exiting."
 	exit 1
 fi
-if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
-d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
 
+$GITHUB_TOKEN_PARSED = $(getParsedGithubToken)
+
+if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN_PARSED}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
+d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
 
 
 # Wait for Elasticsearch
