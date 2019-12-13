68ca66862b557647c956589d5cccf9b55a0609eb
Aaron Huslage <aaron@meedan.com>
2019-12-13 12:45:22 -0500

    Sorry for the pollution - Better bash formatting

--- production/bin_aws/start.sh
+++ production/bin_aws/start.sh
@@ -22,7 +22,7 @@ function config_replace() {
 #This function is here due to a limitation by "secrets manager"
 function getParsedGithubToken(){
     
-  return echo $GITHUB_TOKEN | jq -r .GITHUB_TOKEN
+  echo $GITHUB_TOKEN | jq -r .GITHUB_TOKEN
 
 }
 
@@ -31,7 +31,7 @@ if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
 	exit 1
 fi
 
-GITHUB_TOKEN_PARSED = $(getParsedGithubToken)
+GITHUB_TOKEN_PARSED=$(getParsedGithubToken)
 
 if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN_PARSED}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
 d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
