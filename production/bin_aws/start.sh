806a013714963f65202077693414f394b06850be
Aaron Huslage <aaron@meedan.com>
2019-12-13 12:30:21 -0500

    Apply config at runtime for `check-api-background` task

--- production/bin_aws/start.sh
+++ production/bin_aws/start.sh
@@ -31,7 +31,7 @@ if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
 	exit 1
 fi
 
-$GITHUB_TOKEN_PARSED = $(getParsedGithubToken)
+GITHUB_TOKEN_PARSED = $(getParsedGithubToken)
 
 if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN_PARSED}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
 d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
