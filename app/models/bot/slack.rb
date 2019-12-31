ef04829668a092da2d8736cbec71ac3935ec0e5a
Caio Almeida <caiosba@gmail.com>
2019-12-31 18:44:04 -0300

    Ticket #7881: Fixing exception

--- app/models/bot/slack.rb
+++ app/models/bot/slack.rb
@@ -115,7 +115,7 @@ class Bot::Slack < BotUser
   def get_team(model, project)
     t = model.team if model.respond_to?(:team)
     t ||= project.team unless project.nil?
-    t = Team.find(model.get_team.last) if t.nil? && model.is_annotation?
+    t = Team.where(id: model.get_team.last.to_i).last if t.nil? && model.is_annotation?
     t
   end
 
