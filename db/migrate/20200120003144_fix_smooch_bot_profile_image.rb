e1541136edce9f7791909866170745a43d8066f6
Caio Almeida <caiosba@gmail.com>
2020-01-19 21:35:40 -0300

    Ticket #7954: Fixing path to Smooch bot profile image

---
+++ db/migrate/20200120003144_fix_smooch_bot_profile_image.rb
@@ -0,0 +1,6 @@
+class FixSmoochBotProfileImage < ActiveRecord::Migration
+  def change
+    source = Source.where(name: 'Smooch').last
+    source.update_column :avatar, CONFIG['checkdesk_base_url'] + '/smooch.png'
+  end
+end
