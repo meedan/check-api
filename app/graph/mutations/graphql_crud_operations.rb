fd4313d40704165b4c2e291a6db9fe503dc4dff2
Sawy <melsawy.be7eri@gmail.com>
2020-02-02 08:49:55 +0200

    Ticket #8019 - update es with inactive field changes

--- app/graph/mutations/graphql_crud_operations.rb
+++ app/graph/mutations/graphql_crud_operations.rb
@@ -107,7 +107,9 @@ class GraphqlCrudOperations
       objs[klass.constantize] ||= []
       objs[klass.constantize] << id
     end
-    objs.each { |klass, ids| klass.where(id: ids).update_all(inactive: inactive) if klass.column_names.include?('inactive') }
+    objs.each do |klass, ids|
+      klass.where(id: ids).find_each { |obj| obj.update_attributes(inactive: inactive) } if klass.column_names.include?('inactive')
+    end
   end
 
   def self.crud_operation(operation, obj, inputs, ctx, parents, returns = {})
