329dcd05c950597269ade836984d5e7e163cda76
Aaron Huslage <aaron@meedan.com>
2020-02-06 17:19:22 -0500

    Fixing GITHUB_TOKEN in background image and logfile tail in api image

--- production/bin/start_background.sh
+++ production/bin/start_background.sh
@@ -2,24 +2,8 @@
 
 # start_background.sh
 
-#since GITHUB_TOKEN environment variable is a json object, we need parse the value
-#This function is here due to a limitation by "secrets manager"
-function getParsedGithubToken(){
-    
-  echo $GITHUB_TOKEN | jq -r .GITHUB_TOKEN
-
-}
-
-if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
-	echo "GITHUB_TOKEN, DEPLOY_ENV and APP must be in the environment.   Exiting."
-	exit 1
-fi
-
-GITHUB_TOKEN_PARSED=$(getParsedGithubToken)
-
-if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN_PARSED}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
+if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
 d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
-
 
 LOGFILE=${DEPLOYDIR}/log/sidekiq.log
 
@@ -38,12 +22,12 @@ for d in ${PERSIST_DIRS}; do
 done
 
 echo "create the temporary directory"
-su ${DEPLOYUSER} -c "mkdir -p /app/current/tmp"
+mkdir -p /app/current/tmp
 
 echo "starting static files server"
-su ${DEPLOYUSER} -c "bundle exec ruby bin/static-files-server &"
+bundle exec ruby bin/static-files-server &
 
 echo "starting sidekiq"
-su ${DEPLOYUSER} -c "bundle exec sidekiq -L ${LOGFILE} &"
+bundle exec sidekiq -L ${LOGFILE} &
 
 tail -f ${LOGFILE}
