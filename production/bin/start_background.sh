5071d2279485a3ee067302d25a3ec37e0bc63769
Aaron Huslage <aaron@meedan.com>
2020-02-06 17:44:08 -0500

    More script and dockerfile fixes

--- production/bin/start_background.sh
+++ production/bin/start_background.sh
@@ -2,27 +2,17 @@
 
 # start_background.sh
 
+if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
+	echo "GITHUB_TOKEN, DEPLOY_ENV and APP must be in the environment.   Exiting."
+	exit 1
+fi
+
 if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
 d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
 
 LOGFILE=${DEPLOYDIR}/log/sidekiq.log
 
-echo "setting permissions for ${LOGFILE}"
 touch ${LOGFILE}
-chown ${DEPLOYUSER}:www-data ${LOGFILE}
-chmod 775 ${LOGFILE}
-
-echo PERSIST_DIRS $PERSIST_DIRS
-for d in ${PERSIST_DIRS}; do
-    d=/app/shared/files/${d}
-    if [ ! -e "${d}" ]; then
-        echo "creating directory ${d}"
-        mkdir -p ${d}
-    fi
-done
-
-echo "create the temporary directory"
-mkdir -p /app/current/tmp
 
 echo "starting static files server"
 bundle exec ruby bin/static-files-server &
