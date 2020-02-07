cf3306f5cebb6a9bda6094e87b82cb681cd62f62
Aaron Huslage <aaron@meedan.com>
2020-02-07 13:15:39 -0500

    quiet git

--- production/bin/start_background.sh
+++ production/bin/start_background.sh
@@ -7,7 +7,7 @@ if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
 	exit 1
 fi
 
-if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
+if [ ! -d "configurator" ]; then git clone -q https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
 d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
 
 echo "starting static files server"
