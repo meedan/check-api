e3c6a37e1a33cb39f3ae34fd2ec605539bb0c036
Aaron Huslage <aaron@meedan.com>
2020-02-07 13:11:55 -0500

    More of the sidekiqs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,7 @@
 file = File.join(Rails.root, 'config', "sidekiq-#{Rails.env}.yml")
 file = File.join(Rails.root, 'config', 'sidekiq.yml') unless File.exist?(file)
 require File.join(Rails.root, 'lib', 'middleware_sidekiq_server_retry')
+require 'airbrake/sidekiq'
 
 Airbrake.add_filter(Airbrake::Sidekiq::RetryableJobsFilter.new)
 
@@ -16,9 +17,7 @@ if File.exist?(file)
 
   Sidekiq.configure_server do |config|
     config.redis = redis_config
-    config.error_handlers << Proc.new do |e, context|
-      Airbrake.notify(e, context) if Airbrake.configured? && !e.is_a?(Elasticsearch::Transport::Transport::Errors::Conflict)
-    end
+
     config.server_middleware do |chain|
       chain.add ::Middleware::Sidekiq::Server::Retry
     end
