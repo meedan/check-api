83a665fed393c51596af1ae07585593eeea93552
Caio Almeida <caiosba@gmail.com>
2020-03-08 03:55:32 -0300

    Ticket #8115: Display coverage results on Travis and adding missing tests - now we are back to 100% coverage

--- test/sum-upload-coverage.sh
+++ test/sum-upload-coverage.sh
@@ -3,5 +3,7 @@
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ $TRAVIS_TEST_RESULT == 0 ]]
 then
   aws s3 cp --recursive s3://check-api-travis/codeclimate/$TRAVIS_REPO_SLUG/$TRAVIS_BUILD_NUMBER/ ../coverage
-  ./cc-test-reporter sum-coverage --output - --parts 2 ../coverage/codeclimate.* | sed 's/\/home\/travis\/build\/meedan\/check-api\///g' | ./cc-test-reporter upload-coverage --input -
+  ./cc-test-reporter sum-coverage --output - --parts 2 ../coverage/codeclimate.* | sed 's/\/home\/travis\/build\/meedan\/check-api\///g' > ../coverage/codeclimate.json
+  cat ../coverage/codeclimate.json | ./cc-test-reporter upload-coverage --input -
+  ruby report-coverage.rb ../coverage/codeclimate.json
 fi
