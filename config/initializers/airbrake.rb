8b3cfe8cdb4f1fd1daee178aa51e41f305462879
infojunkie <karim.ratib@gmail.com>
2020-03-29 16:54:34 -0700

    Fix initializer

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -2,7 +2,7 @@ unless CONFIG['airbrake']['host'].blank?
   Airbrake.configure do |config|
     config.project_key = CONFIG['airbrake']['project_key']
     config.project_id = 1
-    config.host    = `https://#{CONFIG['airbrake']['host']}:#{CONFIG['airbrake']['port']}`
+    config.host = "https://#{CONFIG['airbrake']['host']}:#{CONFIG['airbrake']['port']}"
     config.ignore_environments = %w(development test)
     config.environment = CONFIG['airbrake']['environment']
   end
