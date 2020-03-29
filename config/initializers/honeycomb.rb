6d035587cc19c3e1ff4e5643215aae39a0587196
infojunkie <karim.ratib@gmail.com>
2020-03-29 12:36:26 -0700

    Config updates and cleanup #8162

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -1,14 +1,16 @@
-Honeycomb.configure do |config|
-  config.write_key = CONFIG['honeycomb_key']
-  config.dataset = CONFIG['honeycomb_dataset']
-  config.notification_events = %w[
-    sql.active_record
-    render_template.action_view
-    render_partial.action_view
-    render_collection.action_view
-    process_action.action_controller
-    send_file.action_controller
-    send_data.action_controller
-    deliver.action_mailer
-  ].freeze
+unless CONFIG['honeycomb_key'].blank?
+  Honeycomb.configure do |config|
+    config.write_key = CONFIG['honeycomb_key']
+    config.dataset = CONFIG['honeycomb_dataset']
+    config.notification_events = %w[
+      sql.active_record
+      render_template.action_view
+      render_partial.action_view
+      render_collection.action_view
+      process_action.action_controller
+      send_file.action_controller
+      send_data.action_controller
+      deliver.action_mailer
+    ].freeze
+  end
 end
