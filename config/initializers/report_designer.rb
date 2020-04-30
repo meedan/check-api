48c31735f5316635738dc9fca79b931b59f34942
Caio Almeida <caiosba@gmail.com>
2020-04-30 11:25:13 -0300

    Ticket #8231: Generate visual card using Narcissus service instead of PhantomJS

--- config/initializers/report_designer.rb
+++ config/initializers/report_designer.rb
@@ -1,5 +1,3 @@
-require 'webshot'
-
 Dynamic.class_eval do
   def report_design_introduction(data)
     if self.annotation_type == 'report_design'
@@ -21,16 +19,8 @@ Dynamic.class_eval do
     end
   end
 
-  def report_design_image_filename
-    "#{self.id}.png" if self.annotation_type == 'report_design'
-  end
-
-  def report_design_image_filepath
-    "report_design/#{self.report_design_image_filename}" if self.annotation_type == 'report_design'
-  end
-
   def report_design_image_url
-    self.annotation_type == 'report_design' ? CheckS3.public_url(self.report_design_image_filepath) : nil
+    self.annotation_type == 'report_design' ? Dynamic.find(self.id).get_field_value('visual_card_url') : nil
   end
 
   def adjust_report_design_image_url(url)
@@ -38,44 +28,49 @@ Dynamic.class_eval do
     (ENV['RAILS_ENV'] == 'development' && url =~ /^#{CONFIG['storage']['asset_host']}/) ? url.gsub(CONFIG['storage']['asset_host'], "#{CONFIG['storage']['endpoint']}/#{CONFIG['storage']['bucket']}") : url
   end
 
-  def report_image_generate_png(force = false)
+  def report_image_generate_png
     if self.annotation_type == 'report_design'
-      filename = self.report_design_image_filename
-      filepath = 'report_design/' + filename
+      team = self.annotated&.project&.team
+      return if team.nil?
 
-      if !CheckS3.exist?(filepath) || force
-        team = self.annotated&.project&.team
-        return if team.nil?
-        FileUtils.mkdir_p(File.join(Rails.root, 'public', 'report_design'))
-        template = team.get_report_design_image_template
-        doc = Nokogiri::HTML(template)
-        title = doc.at_css('#title')
-        title.content = self.get_field_value('headline')
-        status = doc.at_css('#status')
-        status.content = self.get_field_value('status_label')
-        description = doc.at_css('#description')
-        description.content = self.get_field_value('description')
-        url = doc.at_css('#url')
-        url.content = self.get_field_value('url')
-        avatar = self.adjust_report_design_image_url(team.avatar)
-        image = self.adjust_report_design_image_url(self.get_field_value('image'))
+      # Get the template and generate the HTML
+      FileUtils.mkdir_p(File.join(Rails.root, 'public', 'report_design'))
+      template = team.get_report_design_image_template
+      text = {
+        title: self.get_field_value('headline'),
+        status: self.get_field_value('status_label'),
+        description: self.get_field_value('description')
+      }
+      language = ::Bot::Alegre.get_language_from_alegre(text.values.map(&:to_s).join("\n"))
+      doc = Nokogiri::HTML(template)
+      body = doc.at_css('body')
+      body['class'] = language.to_s
+      html = doc.at_css('html')
+      html['lang'] = language.to_s
+      title = doc.at_css('#title')
+      title.content = text[:title]
+      status = doc.at_css('#status')
+      status.content = text[:status]
+      description = doc.at_css('#description')
+      description.content = text[:description]
+      url = doc.at_css('#url')
+      url.content = self.get_field_value('url')
+      avatar = self.adjust_report_design_image_url(team.avatar)
+      image = self.adjust_report_design_image_url(self.get_field_value('image'))
+      temp_name = 'temp-' + SecureRandom.hex(16) + '-' + self.id.to_s
+      temp = File.join(Rails.root, 'public', 'report_design', temp_name)
+      output = File.open("#{temp}.html", 'w+')
+      output.puts doc.to_s.gsub(/#CCCCCC/, self.get_field_value('theme_color')).gsub('%IMAGE_URL%', image).gsub('%AVATAR_URL%', avatar)
+      output.close
 
-        temp_name = 'temp-' + SecureRandom.hex(16) + self.id.to_s
-        temp = File.join(Rails.root, 'public', 'report_design', temp_name)
-        output = File.open("#{temp}.html", 'w+')
-        output.puts doc.to_s.gsub(/#CCCCCC/, self.get_field_value('theme_color')).gsub('%IMAGE_URL%', image).gsub('%AVATAR_URL%', avatar)
-        output.close
+      # Convert the HTML to PNG
+      temp_url = "#{CONFIG['checkdesk_base_url']}/report_design/#{temp_name}.html"
+      screenshot = JSON.parse(Net::HTTP.get_response(URI("#{CONFIG['screenshot_service_url']}/?url=#{temp_url}&selector=%23frame")).body)['url']
+      raise "Unexpected response from screenshot service: #{screenshot}" unless screenshot =~ /^http/
+      self.set_fields = self.data.merge({ visual_card_url: screenshot }).to_json
+      self.save!
 
-        screenshot = Webshot::Screenshot.instance
-        screenshot.capture "#{CONFIG['checkdesk_base_url_private']}/report_design/#{temp_name}.html", "#{temp}.png", width: 500, height: 500
-
-        CheckS3.write(filepath, 'image/png', File.read("#{temp}.png"))
-
-        FileUtils.rm_f "#{temp}.html"
-        FileUtils.rm_f "#{temp}.png"
-      end
-
-      CheckS3.public_url(filepath)
+      FileUtils.rm_f "#{temp}.html"
     end
   end
 
