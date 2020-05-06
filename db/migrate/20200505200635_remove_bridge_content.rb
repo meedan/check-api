c43879f89d1ccc654ac2aca186abf3da781543a2
infojunkie <karim.ratib@gmail.com>
2020-05-05 23:35:17 -0700

    Migration to remove Bridge content #8219

---
+++ db/migrate/20200505200635_remove_bridge_content.rb
@@ -0,0 +1,17 @@
+class Bot::Viber < BotUser
+end
+class Bot::Facebook < BotUser
+end
+class Bot::Twitter < BotUser
+end
+class Bot::BridgeReader < BotUser
+end
+
+class RemoveBridgeContent < ActiveRecord::Migration
+  def change
+    bot_types = ['Bot::Viber', 'Bot::Facebook', 'Bot::Twitter', 'Bot::BridgeReader']
+    User.where('type in (?)', bot_types).destroy_all
+    bot_names = ['Viber Bot', 'Facebook Bot', 'Twitter Bot', 'Bridge Reader Bot']
+    Source.where('name in (?)', bot_names).destroy_all
+  end
+end
