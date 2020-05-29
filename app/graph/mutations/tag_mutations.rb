c2b178b1953ef4e27f5fc597781d497907e2c6e0
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2020-05-29 14:33:04 -0300

    Merge develop

--- app/graph/mutations/tag_mutations.rb
+++ app/graph/mutations/tag_mutations.rb
@@ -2,5 +2,5 @@ module TagMutations
   create_fields = GraphqlCrudOperations.define_annotation_mutation_fields.merge({ tag: '!str' })
   update_fields = GraphqlCrudOperations.define_annotation_mutation_fields.merge({ tag: 'str' })
 
-  Create, Update, Destroy, BulkCreate = GraphqlCrudOperations.define_crud_operations('tag', create_fields, update_fields, ['source', 'project_media', 'project_source', 'team'], true)
+  Create, Update, Destroy, BulkCreate = GraphqlCrudOperations.define_crud_operations('tag', create_fields, update_fields, ['source', 'project_media', 'project_source', 'team', 'tag_text_object'], true)
 end
