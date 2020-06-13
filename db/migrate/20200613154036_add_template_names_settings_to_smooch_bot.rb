8bcac12eea02117f510271f994ceff93262e6a5e
Caio Almeida <caiosba@gmail.com>
2020-06-13 16:51:00 -0300

    Ticket #8361: Adding setting to Smooch Bot that allows the template names to be customized

---
+++ db/migrate/20200613154036_add_template_names_settings_to_smooch_bot.rb
@@ -0,0 +1,29 @@
+class AddTemplateNamesSettingsToSmoochBot < ActiveRecord::Migration
+  def change
+    tb = BotUser.where(login: 'smooch').last
+    unless tb.nil?
+      settings = tb.get_settings.clone || []
+      # Reorder existing template settings
+      ['smooch_template_namespace', 'smooch_template_locales'].each do |name|
+        i = settings.index{ |s| s.with_indifferent_access[:name] == name }
+        setting = settings[i].clone
+        settings.delete_at(i)
+        settings << setting
+      end
+      # Add new template settings
+      ['fact_check_report', 'fact_check_report_image_only', 'fact_check_report_text_only', 'fact_check_status',
+       'fact_check_report_updated', 'fact_check_report_updated_image_only', 'fact_check_report_updated_text_only',
+       'more_information_needed', 'more_information_needed_image_only', 'more_information_needed_text_only'
+      ].each do |name|
+        settings << {
+          name: "smooch_template_name_for_#{name}",
+          label: "Template name for template '#{name}'",
+          type: 'string',
+          default: name
+        }
+      end
+      tb.set_settings(settings)
+      tb.save!
+    end
+  end
+end
