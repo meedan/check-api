b864ea94c9e976e053aa37b248497166d8f9774a
Sawy <melsawy.be7eri@gmail.com>
2020-08-14 03:20:02 +0200

    Ticket #8412 - fix indentation

--- db/migrate/20200813175255_set_team_fieldsets.rb
+++ db/migrate/20200813175255_set_team_fieldsets.rb
@@ -1,7 +1,7 @@
 class SetTeamFieldsets < ActiveRecord::Migration
   def change
-  	RequestStore.store[:skip_notifications] = true
-  	fieldsets = [
+    RequestStore.store[:skip_notifications] = true
+    fieldsets = [
       {
         identifier: 'tasks',
         singular: 'task',
@@ -14,17 +14,17 @@ class SetTeamFieldsets < ActiveRecord::Migration
       }.with_indifferent_access
     ]
     # Set fieldsets setting to be the default for all teams
-  	Team.find_each do |t|
-  		settings = t.settings || {}
-  		settings[:fieldsets] = fieldsets
-  		t.update_column(:settings, settings)
-  	end
-  	# Set fieldset = "tasks" for all TeamTasks
-  	TeamTask.update_all(fieldset: 'tasks')
-  	# Set fieldset: "tasks" for all Tasks
-  	Task.where(annotation_type: 'task').find_in_batches(:batch_size => 5000) do |tasks|
-  		new_tasks = []
-  		tasks.each do |t|
+    Team.find_each do |t|
+      settings = t.settings || {}
+      settings[:fieldsets] = fieldsets
+      t.update_column(:settings, settings)
+    end
+    # Set fieldset = "tasks" for all TeamTasks
+    TeamTask.update_all(fieldset: 'tasks')
+    # Set fieldset: "tasks" for all Tasks
+    Task.where(annotation_type: 'task').find_in_batches(:batch_size => 5000) do |tasks|
+      new_tasks = []
+      tasks.each do |t|
         print "."
         t.data["fieldset"] = 'tasks'
         new_tasks <<  t
@@ -33,7 +33,7 @@ class SetTeamFieldsets < ActiveRecord::Migration
       Task.where(id: tasks.map(&:id)).delete_all
       # Import new tasks with old ids to keep versions
       Task.import(new_tasks, recursive: false, validate: false)
-  	end
-  	RequestStore.store[:skip_notifications] = false
+    end
+    RequestStore.store[:skip_notifications] = false
   end
 end
