75762dab5ece6f52c80fdec8bb4ca8415316fde6
Aaron Huslage <aaron@meedan.com>
2020-08-21 14:58:59 -0400

    Add quotes for safety

--- production/bin/run_command.sh
+++ production/bin/run_command.sh
@@ -14,7 +14,7 @@ fi
 if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
 d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
 
-command=$@
+command="$@"
 
 echo "Running Command ($command)"
 # su to DEPLOYUSER and be sure to exit with the proper exit both inside and outside the migration
