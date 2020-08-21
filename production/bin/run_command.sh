29ad2eb039b2df1826db4a5eee5ae72293cd0ab9
Aaron Huslage <aaron@meedan.com>
2020-08-21 15:27:43 -0400

    Simplify for simplicity

--- production/bin/run_command.sh
+++ production/bin/run_command.sh
@@ -4,8 +4,6 @@
 
 set -e
 
-DEPLOY_ENV=$1
-
 if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} ]]; then
 	echo "GITHUB_TOKEN, DEPLOY_ENV  must be in the environment. Exiting."
 	exit 1
@@ -14,13 +12,4 @@ fi
 if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
 d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
 
-command="$@"
-
-echo "Running Command ($command)"
-# su to DEPLOYUSER and be sure to exit with the proper exit both inside and outside the migration
-$($command)
-STATUS=$?
-
-echo "migrations completed with exit $STATUS"
-
-exit $STATUS
+$@
