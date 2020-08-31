3d368993e3cdf066a3c19517ea1bbf056569958e
Caio Almeida <caiosba@gmail.com>
2020-08-31 02:10:02 -0300

    Ticket #8560: Adding new fields to report designer: custom date and report text headline

---
+++ db/migrate/20200830233935_add_new_fields_to_report_designer.rb
@@ -0,0 +1,14 @@
+class AddNewFieldsToReportDesigner < ActiveRecord::Migration
+  def change
+    at = DynamicAnnotation::AnnotationType.where(annotation_type: 'report_design').last
+    unless at.nil?
+      json_schema = at.json_schema.clone.with_indifferent_access
+      json_schema['properties']['options']['items']['properties'].merge!({
+        title: { type: 'string', default: '' },
+        date: { type: 'string', default: '' }
+      })
+      at.json_schema = json_schema
+      at.save!
+    end
+  end
+end
