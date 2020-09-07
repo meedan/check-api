36244ff8bdce8cf42a87bff6e7f8c9735f92062a
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2020-09-07 22:40:32 +0200

    Feature/8631 store all user messages from smooch bot (#627)
    * Ticket #8631 - Store all user messages from Smooch Bot
    
    * Ticket #8631- add tests and fix CC
    
    * Ticket #8631 - fix tests && CC

--- app/workers/smooch_worker.rb
+++ app/workers/smooch_worker.rb
@@ -6,10 +6,11 @@ class SmoochWorker
 
   sidekiq_retry_in { |_count, _exception| 20 }
 
-  def perform(json_message, type, app_id)
+  def perform(json_message, type, app_id, request_type, annotated)
+    annotated = YAML.load(annotated)
     User.current = BotUser.where(login: 'smooch').last
     benchmark.send("smooch_save_#{type}_message") do
-      Bot::Smooch.save_message(json_message, app_id, User.current)
+      Bot::Smooch.save_message(json_message, app_id, User.current, request_type, annotated)
     end
     benchmark.finish
     User.current = nil
