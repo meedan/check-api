c5b881c65451ef26d6d08ac52ff2862d8dc791b2
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2020-09-15 16:37:50 -0300

    #8081 Query count changed because of smooch bot detection

--- test/controllers/graphql_controller_3_test.rb
+++ test/controllers/graphql_controller_3_test.rb
@@ -33,7 +33,7 @@ class GraphqlController3Test < ActionController::TestCase
     query = "query { search(query: \"{}\") { medias(first: 10000) { edges { node { dbid, media { dbid } } } } } }"
 
     # This number should be always CONSTANT regardless the number of medias and annotations above
-    assert_queries (16) do
+    assert_queries (18) do
       post :create, query: query, team: 'team'
     end
 
@@ -464,7 +464,7 @@ class GraphqlController3Test < ActionController::TestCase
       }}
     '
 
-    assert_queries 18, '=' do
+    assert_queries 20, '=' do
       post :create, query: query, team: 'team'
     end
 
