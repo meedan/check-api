1938c9b8e23818b75aee94fecd8e635000810c77
Caio Almeida <caiosba@gmail.com>
2020-10-05 22:17:09 -0300

    Ticket #8689: Set Slack settings for project through GraphQL mutation and get Slack settings for project from GraphQL type

--- app/models/project.rb
+++ app/models/project.rb
@@ -238,6 +238,10 @@ class Project < ActiveRecord::Base
     team.inactive
   end
 
+  def slack_events=(events_json)
+    self.set_slack_events = JSON.parse(events_json)
+  end
+
   def self.bulk_update_medias_count(pids)
     pids_count = Hash[pids.product([0])] # Initialize all projects as zero
     ProjectMediaProject
