f52fcc151381ba8a4c2e90586d1ad7e2db93c126
Devin Gaffney <itsme@devingaffney.com>
2020-10-16 16:02:10 -0400

    alter migration to set alegre bot to default

--- db/migrate/202010161522429_add_default_to_bots.rb
+++ db/migrate/202010161522429_add_default_to_bots.rb
@@ -1,5 +1,8 @@
 class AddDefaultToBots < ActiveRecord::Migration
   def change
     add_column :users, :default, :boolean, default: false
+    tb = BotUser.where(login: 'alegre').last
+    tb.default = true
+    tb.save!
   end
 end
