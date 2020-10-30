9181eb209f4e34469f40c0a7a71c766946bff3bf
Caio Almeida <caiosba@gmail.com>
2020-10-30 00:15:28 -0300

    Ticket #8790: Resource content should not be mandatory

--- app/models/bot_resource.rb
+++ app/models/bot_resource.rb
@@ -1,7 +1,7 @@
 class BotResource < ActiveRecord::Base
   include Versioned
 
-  validates_presence_of :uuid, :title, :content, :team_id
+  validates_presence_of :uuid, :title, :team_id
 
   belongs_to :team
 end
