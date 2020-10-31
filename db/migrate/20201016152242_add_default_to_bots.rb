4e81fbb379406befd4f88ac7798618da023fc48f
Caio Almeida <caiosba@gmail.com>
2020-10-31 12:05:35 -0300

    Fixing migration

--- db/migrate/20201016152242_add_default_to_bots.rb
+++ db/migrate/20201016152242_add_default_to_bots.rb
@@ -1,6 +1,6 @@
 class AddDefaultToBots < ActiveRecord::Migration
   def change
-    add_column :users, :default, :boolean, default: false
+    add_column(:users, :default, :boolean, default: false) unless column_exists?(:users, :default)
     tb = BotUser.where(login: 'alegre').last
     tb.default = true
     tb.save!
