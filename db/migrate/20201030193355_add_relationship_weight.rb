16ae00af6f239dfd5382ed35fe66a1cc97f3ab37
Devin Gaffney <itsme@devingaffney.com>
2020-11-02 12:12:36 -0500

    update migration remove non-null constraint

--- db/migrate/20201030193355_add_relationship_weight.rb
+++ db/migrate/20201030193355_add_relationship_weight.rb
@@ -1,5 +1,5 @@
 class AddRelationshipWeight < ActiveRecord::Migration
   def change
-    add_column :relationships, :weight, :float, default: 0, null: false
+    add_column :relationships, :weight, :float, default: 0
   end
 end
