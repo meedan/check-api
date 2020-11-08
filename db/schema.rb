984c9a3106e1eb9b6e1e4db5ae32e25cbe70a849
Caio Almeida <caiosba@gmail.com>
2020-11-08 19:00:32 -0300

    Ticket #8809: Exposing bot "default" field on GraphQL

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201102115356) do
+ActiveRecord::Schema.define(version: 202010161522429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
