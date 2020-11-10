75a5c6f8b9c3291e6c2dd2ff296c822a726a42fc
Devin Gaffney <itsme@devingaffney.com>
2020-11-10 16:15:58 -0500

    try to fix ability test which broke for some reason?

--- test/models/ability_test.rb
+++ test/models/ability_test.rb
@@ -2094,7 +2094,6 @@ class AbilityTest < ActiveSupport::TestCase
       end
       pmids = pmids.sort
       query = "SELECT COUNT(*) FROM \"project_medias\" WHERE \"project_medias\".\"team_id\" = $1 AND \"project_medias\".\"id\" IN (#{pmids[0]}, #{pmids[1]}, #{pmids[2]})"
-      binding.pry
       assert_equal query, queries.first
     end
   end
