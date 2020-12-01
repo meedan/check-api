f893de15ba508a5d2749dd9104e526bb5090b5f8
Sawy <melsawy.be7eri@gmail.com>
2020-12-01 21:50:32 +0200

    Ticket #8852 - fix migration

--- db/migrate/20201124120741_convert_archived_flag_to_integer.rb
+++ db/migrate/20201124120741_convert_archived_flag_to_integer.rb
@@ -1,24 +1,11 @@
 class ConvertArchivedFlagToInteger < ActiveRecord::Migration
   def change
-    # Team
-    if Team.column_for_attribute('archived').type != :integer
-      change_column :teams, :archived, :boolean, default: nil
-      change_column :teams, :archived, :integer, using: 'archived::integer', default: 0
-    end
-    # Project
-    if Project.column_for_attribute('archived').type != :integer
-      change_column :projects, :archived, :boolean, default: nil
-      change_column :projects, :archived, :integer, using: 'archived::integer', default: 0
-    end
-    # ProjectMedia
-    if ProjectMedia.column_for_attribute('archived').type != :integer
-      change_column :project_medias, :archived, :boolean, default: nil
-      change_column :project_medias, :archived, :integer, using: 'archived::integer', default: 0
-    end
-    # Source
-    if Source.column_for_attribute('archived').type != :integer
-      change_column :sources, :archived, :boolean, default: nil
-      change_column :sources, :archived, :integer, using: 'archived::integer', default: 0
+    { teams: Team, projects: Project, project_medias: ProjectMedia, sources: Source }.each do |table, model|
+      column = model.column_for_attribute('archived')
+      unless column.nil? || column.type == :integer
+        change_column table, :archived, :boolean, default: nil
+        change_column table, :archived, :integer, using: 'archived::integer', default: 0
+      end
     end
     # update archived logs with 0/1 insted of true/false
     PaperTrail::Version.where("object_changes = ?", '{"archived":[true,false]}').update_all(object_changes: "{\"archived\":[1,0]}")
