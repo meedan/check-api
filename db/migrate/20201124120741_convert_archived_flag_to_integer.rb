3eb76d683c51732c4abc79df0b219b674da2b7c5
Sawy <melsawy.be7eri@gmail.com>
2020-12-01 17:10:29 +0200

    Ticket #8852 - migrate archived logs

--- db/migrate/20201124120741_convert_archived_flag_to_integer.rb
+++ db/migrate/20201124120741_convert_archived_flag_to_integer.rb
@@ -1,25 +1,27 @@
 class ConvertArchivedFlagToInteger < ActiveRecord::Migration
   def change
-    # TODO: Sawy fix later
-    # # Team
-    # if Team.column_for_attribute('archived').type != :integer
-    #   change_column :teams, :archived, :boolean, default: nil
-    #   change_column :teams, :archived, :integer, using: 'archived::integer', default: 0
-    # end
-    # # Project
-    # if Project.column_for_attribute('archived').type != :integer
-    #   change_column :projects, :archived, :boolean, default: nil
-    #   change_column :projects, :archived, :integer, using: 'archived::integer', default: 0
-    # end
-    # # ProjectMedia
-    # if ProjectMedia.column_for_attribute('archived').type != :integer
-    #   change_column :project_medias, :archived, :boolean, default: nil
-    #   change_column :project_medias, :archived, :integer, using: 'archived::integer', default: 0
-    # end
-    # # Source
-    # if Source.column_for_attribute('archived').type != :integer
-    #   change_column :sources, :archived, :boolean, default: nil
-    #   change_column :sources, :archived, :integer, using: 'archived::integer', default: 0
-    # end
+    # Team
+    if Team.column_for_attribute('archived').type != :integer
+      change_column :teams, :archived, :boolean, default: nil
+      change_column :teams, :archived, :integer, using: 'archived::integer', default: 0
+    end
+    # Project
+    if Project.column_for_attribute('archived').type != :integer
+      change_column :projects, :archived, :boolean, default: nil
+      change_column :projects, :archived, :integer, using: 'archived::integer', default: 0
+    end
+    # ProjectMedia
+    if ProjectMedia.column_for_attribute('archived').type != :integer
+      change_column :project_medias, :archived, :boolean, default: nil
+      change_column :project_medias, :archived, :integer, using: 'archived::integer', default: 0
+    end
+    # Source
+    if Source.column_for_attribute('archived').type != :integer
+      change_column :sources, :archived, :boolean, default: nil
+      change_column :sources, :archived, :integer, using: 'archived::integer', default: 0
+    end
+    # update archived logs with 0/1 insted of true/false
+    PaperTrail::Version.where("object_changes = ?", '{"archived":[true,false]}').update_all(object_changes: "{\"archived\":[1,0]}")
+    PaperTrail::Version.where("object_changes = ?", '{"archived":[false,true]}').update_all(object_changes: "{\"archived\":[0,1]}")
   end
 end
