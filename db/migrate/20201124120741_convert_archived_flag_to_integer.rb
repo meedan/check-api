c89f1264fcbd1699d81a60a2ea958c4885137d87
Sawy <melsawy.be7eri@gmail.com>
2020-12-02 12:12:06 +0200

    Ticket #8852 - add a rake task to migrate archived logs

--- db/migrate/20201124120741_convert_archived_flag_to_integer.rb
+++ db/migrate/20201124120741_convert_archived_flag_to_integer.rb
@@ -7,8 +7,5 @@ class ConvertArchivedFlagToInteger < ActiveRecord::Migration
         change_column table, :archived, :integer, using: 'archived::integer', default: 0
       end
     end
-    # update archived logs with 0/1 insted of true/false
-    PaperTrail::Version.where("object_changes = ?", '{"archived":[true,false]}').update_all(object_changes: "{\"archived\":[1,0]}")
-    PaperTrail::Version.where("object_changes = ?", '{"archived":[false,true]}').update_all(object_changes: "{\"archived\":[0,1]}")
   end
 end
