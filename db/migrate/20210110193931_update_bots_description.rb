fc04f6fbb77bbb4e58154afc9378eb0b4ae909fc
Caio Almeida <caiosba@gmail.com>
2021-01-10 16:50:03 -0300

    Ticket CHECK-101: Adding a migration to update the description of some bots

---
+++ db/migrate/20210110193931_update_bots_description.rb
@@ -0,0 +1,14 @@
+class UpdateBotsDescription < ActiveRecord::Migration
+  def change
+    b = BotUser.smooch_user
+    unless b.nil?
+      b.set_description = 'Connect Check with Facebook Messenger, WhatsApp and Twitter to create tiplines.'
+      b.save!
+    end
+    b = BotUser.keep_user
+    unless b.nil?
+      b.set_description = 'Archive links to third-party archiving services.'
+      b.save!
+    end
+  end
+end
