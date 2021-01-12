2622c1670e5f9e08c301bce10bc0d0a64d976379
Adam Hooper <adam@adamhooper.com>
2021-01-12 15:11:26 -0500

    Upload to Workbench TUS URLs (#690)
    Also, retry uploads on failure and skip uploads when the retries fail.
    
    TO DEPLOY, be sure to change the CSV to match the new schema. Instead
    of `workflow_id` and `step_id`, there is now just one column,
    `workbench_step_url`. To migrate your CSV, apply the formula:
    
    workbench_step_url = "https://app.workbenchdata.com/api/v1/workflows/" & workflow_id & "/steps/" & step_id & "/files"
    
    For instance, now a valid CSV looks like:
    
        team_slug,workbench_step_url,api_token
        my-team,https://app.workbenchdata.com/api/v1/workflows/123456/steps/step-abcdef/files,SoMe-Tok3n
        my-team,https://app.workbenchdata.com/api/v1/workflows/123456/steps/step-bcdefg/files,4n0th3r-70k_n

--- lib/tasks/data/push_team_to_workbench.rake
+++ lib/tasks/data/push_team_to_workbench.rake
@@ -4,17 +4,16 @@ require_relative './workbench_upload'
 namespace :check do
   namespace :data do
     desc "Upload workspace data to Workbench Check module"
-    task :push_team_to_workbench, [:team_slug, :workflow_id, :step_id, :api_token] => :environment do |task, args|
-      raise "Usage: #{task.to_s}[team_slug,workflow_id,step_id,api_token]" unless (
-        args.team_slug && args.workflow_id && args.step_id && args.api_token
+    task :push_team_to_workbench, [:team_slug, :workbench_step_url, :api_token] => :environment do |task, args|
+      raise "Usage: #{task.to_s}[team_slug,workbench_step_url,api_token]" unless (
+        args.team_slug && args.workbench_step_url && args.api_token
       )
 
       Tempfile.open("#{args.team_slug}-sqlite3-lz4") do |f|
         f.close
         PgExport::export_team_to_sqlite_lz4_file(args.team_slug, f.path)
         WorkbenchUpload::upload_file_to_workbench(
-          workflow_id: args.workflow_id,
-          step_id: args.step_id,
+          workbench_step_url: args.workbench_step_url,
           api_token: args.api_token,
           path: f.path,
           filename: "#{args.team_slug}-#{Date.today.iso8601}.sqlite3.lz4"
--- lib/tasks/data/workbench_upload.rb
+++ lib/tasks/data/workbench_upload.rb
@@ -1,13 +1,27 @@
 # :nocov:
-require 'aws-sdk-s3'
 require 'typhoeus'
 
+N_RETRIES = 3
+
 module WorkbenchUpload
-  def self.upload_file_to_workbench(workflow_id:, step_id:, api_token:, path:, filename:)
-    s3_config = get_s3_config_from_workbench(workflow_id, step_id, api_token)
-    puts "Uploading to s3://#{s3_config['bucket']}/#{s3_config['key']}"
-    upload_file_to_s3(path, s3_config)
-    finish_workbench_upload(s3_config['finishUrl'], api_token, filename)
+  def self.upload_file_to_workbench(step_files_url:, api_token:, path:, filename:)
+    attempt = 0
+    begin
+      tus_upload_url = get_tus_upload_url_from_workbench(
+        step_files_url: step_files_url,
+        api_token: api_token,
+        filename: filename,
+        size: File.size(path)
+      )
+      tus_upload(path, tus_upload_url)
+    rescue Typhoeus::Errors::TyphoeusError
+      attempt += 1
+      if attempt < N_RETRIES
+        retry
+      else
+        raise
+      end
+    end
   end
 
   protected
@@ -15,55 +29,41 @@ module WorkbenchUpload
   def self.raise_on_http_problem(response)
     if not response.success?
       if response.timed_out?
-        raise 'HTTP request timed out'
+        raise Typhoeus::Errors::TyphoeusError, 'HTTP request timed out'
       elsif response.code == 0
-        raise "HTTP request did not complete: #{response.return_message}"
+        raise Typhoeus::Errors::TyphoeusError, "HTTP request did not complete: #{response.return_message}"
       else
-        puts response.inspect
-        raise "HTTP #{response.status_message}"
+        raise Typhoeus::Errors::TyphoeusError, "HTTP #{response.status_message}: #{response.inspect}"
       end
     end
   end
 
-  def self.get_s3_config_from_workbench(workflow_id, step_id, api_token)
-    url = "https://app.workbenchdata.com/api/v1/workflows/#{workflow_id}/steps/#{step_id}/uploads"
-    response = Typhoeus.post(url, headers: {Authorization: "Bearer #{api_token}"})
-    raise_on_http_problem(response)
-    JSON.parse(response.response_body)
-  end
-
-  def self.upload_file_to_s3(path, s3_config)
-    s3_client = Aws::S3::Client.new(
-      # Commented out: aws-sdk v3 format
-      # endpoint: s3_config['endpoint'],
-      # force_path_style: true,
-      # credentials: Aws::Credentials(
-      #   s3_config['credentials']['accessKeyId'],
-      #   s3_config['credentials']['secretAccessKey'],
-      #   s3_config['credentials']['sessionToken'],
-      # )
-      # aws-sdk v1 format:
-      endpoint: s3_config['endpoint'],
-      force_path_style: true,
-      access_key_id: s3_config['credentials']['accessKeyId'],
-      secret_access_key: s3_config['credentials']['secretAccessKey'],
-      session_token: s3_config['credentials']['sessionToken'],
-    )
-    s3_resource = Aws::S3::Resource.new(client: s3_client)
-    s3_resource.bucket(s3_config['bucket']).object(s3_config['key']).upload_file(path)
-  end
-
-  def self.finish_workbench_upload(finish_url, api_token, filename)
+  def self.get_tus_upload_url_from_workbench(step_files_url:, api_token:, filename:, size:)
+    puts "POST #{step_files_url}"
     response = Typhoeus.post(
-      finish_url,
-      body: JSON.dump({'filename': filename}),
+      step_files_url,
       headers: {
         Authorization: "Bearer #{api_token}",
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json"
+      },
+      body: JSON.dump({ filename: filename, size: size })
     )
     raise_on_http_problem(response)
-    response
+    JSON.parse(response.response_body)['tusUploadUrl']
+  end
+
+  def self.tus_upload(path, url)
+    puts "PATCH #{url} (contents: #{path})"
+    response = Typhoeus.patch(
+      url,
+      headers: {
+        "Content-Type": "application/offset+octet-stream",
+        "Tus-Resumable": "1.0.0",
+        "Upload-Offset": "0"
+      },
+      body: File.open(path, "rb").read
+    )
+    raise_on_http_problem(response)
   end
 end
 # :nocov:
