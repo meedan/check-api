02370ec4f11704a500d280a8483150561b0a18f0
Caio Almeida <caiosba@gmail.com>
2021-01-28 19:30:22 -0300

    Removing whitespaces

--- app/graph/types/root_level_type.rb
+++ app/graph/types/root_level_type.rb
@@ -5,19 +5,19 @@ RootLevelType = GraphQL::ObjectType.define do
   interfaces [NodeIdentification.interface]
 
   global_id_field :id
-  
+
   field :current_user, UserType do
     resolve -> (_object, _args, _ctx) {
       User.current
-    } 
+    }
   end
 
   field :current_team, TeamType do
     resolve -> (_object, _args, _ctx) {
       Team.current
-    } 
+    }
   end
-  
+
   connection :team_bots_approved, BotUserType.connection_type do
     resolve -> (_object, _args, _ctx) {
       BotUser.all.select{ |b| b.get_approved }
