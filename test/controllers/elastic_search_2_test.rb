d01486f8ea260cd7eb649c01510b498251dedd5d
Devin Gaffney <itsme@devingaffney.com>
2021-02-03 17:39:45 -0500

    CHECK-176 alter stubbing

--- test/controllers/elastic_search_2_test.rb
+++ test/controllers/elastic_search_2_test.rb
@@ -3,9 +3,6 @@ require_relative '../test_helper'
 class ElasticSearch2Test < ActionController::TestCase
   def setup
     super
-    stub_configs({ 'alegre_host' => 'http://alegre', 'alegre_token' => 'test' })
-    WebMock.stub_request(:post, 'http://alegre/text/similarity/').to_return(body: {success: true}.to_json)
-    WebMock.stub_request(:delete, 'http://alegre/text/similarity/').to_return(body: {"_index"=>"alegre_similarity", "_type"=>"_doc", "_id"=>"Y2hlY2stcHJvamVjdF9tZWRpYS0xOTUwLWRlc2NyaXB0aW9u", "_version"=>3, "result"=>"deleted", "_shards"=>{"total"=>2, "successful"=>1, "failed"=>0}, "_seq_no"=>39, "_primary_term"=>176}.to_json)
     setup_elasticsearch
   end
   
