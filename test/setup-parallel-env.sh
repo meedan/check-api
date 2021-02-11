9c70a3e017ed5dec4a09d0f8b64eb289ff83fa5e
danielevalverde <danierzevalverde@gmail.com>
2021-02-11 13:41:10 -0300

    Ticket CHECK-253: add dependencies in docker-compose.yml and fix build

--- test/setup-parallel-env.sh
+++ test/setup-parallel-env.sh
@@ -11,9 +11,6 @@ TEST_ENV_NUMBER=3 bundle exec rake db:create db:migrate &
 TEST_ENV_NUMBER=4 bundle exec rake db:create db:migrate &
 TEST_ENV_NUMBER=5 bundle exec rake db:create db:migrate &
 
-touch tmp/parallel_runtime_test.log
-chmod +w tmp/parallel_runtime_test.log
-
 for job in `jobs -p`
 do
   echo $job
