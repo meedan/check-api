b7449bb277bdd4596db94095d361dfe7982c21b1
Caio Almeida <caiosba@gmail.com>
2021-02-11 12:49:37 -0300

    Ticket CHECK-251: Fixing Facebook permissions for different API version

--- app/controllers/concerns/facebook_authentication.rb
+++ app/controllers/concerns/facebook_authentication.rb
@@ -2,7 +2,10 @@ module FacebookAuthentication
   extend ActiveSupport::Concern
 
   def setup_facebook
-    request.env['omniauth.strategy'].options[:scope] = 'manage_pages,pages_messaging' if params[:context] == 'smooch'
+    # pages_manage_metadata is for Facebook API > 7
+    # manage_pages is for Facebook API < 7
+    # An error will be displayed for Facebook users that are admins of the Facebook app, but should be transparent for other users
+    request.env['omniauth.strategy'].options[:scope] = 'pages_manage_metadata,manage_pages,pages_messaging' if params[:context] == 'smooch'
   end
 
   # OAuth callback
