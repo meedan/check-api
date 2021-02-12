01ced51cfb6a4fa27fa7a93b208960a4876655aa
Caio Almeida <caiosba@gmail.com>
2021-02-12 00:42:59 -0300

    Ticket CHECK-251: Fixing Facebook authentication for tipline integration

--- app/controllers/concerns/facebook_authentication.rb
+++ app/controllers/concerns/facebook_authentication.rb
@@ -6,9 +6,14 @@ module FacebookAuthentication
     # manage_pages is for Facebook API < 7
     # An error will be displayed for Facebook users that are admins of the Facebook app, but should be transparent for other users
     request.env['omniauth.strategy'].options[:scope] = 'pages_manage_metadata,manage_pages,pages_messaging' if params[:context] == 'smooch'
-    prefix = params[:context].to_s == 'smooch' ? 'smooch_' : ''
+    prefix = facebook_context == 'smooch' ? 'smooch_' : ''
     request.env['omniauth.strategy'].options[:client_id] = CheckConfig.get("#{prefix}facebook_app_id")
     request.env['omniauth.strategy'].options[:client_secret] = CheckConfig.get("#{prefix}facebook_app_secret")
+  end
+
+  def facebook_context
+    return params[:context] if params[:context]
+    session['omniauth.params'] ? session['omniauth.params']['context'] : nil
   end
 
   # OAuth callback
