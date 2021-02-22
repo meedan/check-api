5e608daac39f3ced4140896a5eaa6562eb95544e
danielevalverde <danierzevalverde@gmail.com>
2021-02-22 11:23:46 -0300

    Ticket CHECK-251: Fix script

--- test/unit-tests.sh
+++ test/unit-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export PATTERN='controllers' && cd test && rm -rf $PATTERN && echo 'Running tests:' && ls && cd - && bundle exec rake parallel:test[5] && cd test && ./format-coverage.sh && cd -
+export PATTERN='controllers' && cd test && rm -rf $PATTERN && echo 'Running tests:' && ls && cd - && bundle exec rake parallel:test[5]
