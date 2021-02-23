b87bd83a04731024adf78099849a1a1bc3ef9b55
danielevalverde <danierzevalverde@gmail.com>
2021-02-23 16:19:52 -0300

    Ticket CHECK-253: fix script path and variable name

--- test/format-coverage.sh
+++ test/format-coverage.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 pip install --user awscli
-echo "$TRAVIS_BUILD_STAGE_NAME"
 
-if [ "$TRAVIS_BUILD_STAGE_NAME" == "unit-tests" ] ; then
+echo "$TRAVIS_JOB_NAME"
+
+if [ "$TRAVIS_JOB_NAME" == "unit-tests" ] ; then
   PATTERN='controllers';
 else
   PATTERN='models mailers integration workers lib'
@@ -12,6 +13,6 @@ fi
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
   name=$(echo $PATTERN | sed 's/ /-/g')
-  ./cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$name.json ../coverage/.resultset.json
+  ./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$name.json ../coverage/.resultset.json
   aws s3 cp ../coverage/codeclimate.$name.json s3://check-api-travis/codeclimate/$TRAVIS_REPO_SLUG/$TRAVIS_BUILD_NUMBER/codeclimate.$name.json
 fi
