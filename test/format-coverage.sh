15393570aed91d3eb1aee096b527144e305ef9f1
danielevalverde <danierzevalverde@gmail.com>
2021-02-24 20:00:20 -0300

    Ticket CHECK-253: replace 'APP' by check and remove handling with sed command on 'PATTERN'

--- test/format-coverage.sh
+++ test/format-coverage.sh
@@ -5,12 +5,11 @@ pip install --user awscli
 if [ "$TRAVIS_JOB_NAME" == "unit-tests" ] ; then
   PATTERN='controllers';
 else
-  PATTERN='models mailers integration workers lib'
+  PATTERN='models-mailers-integration-workers-lib'
 fi
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
-  name=$(echo $PATTERN | sed 's/ /-/g')
-  ./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$name.json ../coverage/.resultset.json
-  aws s3 cp ../coverage/codeclimate.$name.json s3://check-api-travis/codeclimate/$TRAVIS_REPO_SLUG/$TRAVIS_BUILD_NUMBER/codeclimate.$name.json
+  ./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$PATTERN.json ../coverage/.resultset.json
+  aws s3 cp ../coverage/codeclimate.$PATTERN.json s3://check-api-travis/codeclimate/$TRAVIS_REPO_SLUG/$TRAVIS_BUILD_NUMBER/codeclimate.$PATTERN.json
 fi
