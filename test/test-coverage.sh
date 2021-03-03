442ce6ec2cdaabaabf3da26a1c033f7e87d3f253
Daniele Valverde <34126648+danielevalverde@users.noreply.github.com>
2021-03-03 16:09:04 -0300

    Ticket CHECK-253 run tests on docker (#725)
    * Ticket CHECK-253: Run tests on docker on Travis

---
+++ test/test-coverage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get install -y golang-go golang-glide
+printf '#!/bin/bash\ngo run /tmp/go/src/github.com/codeclimate/test-reporter/main.go $@\n' > test/cc-test-reporter && chmod +x test/cc-test-reporter
+export GOPATH=/tmp/go
+mkdir -p /tmp/go/src/github.com/codeclimate
+git clone https://github.com/codeclimate/test-reporter /tmp/go/src/github.com/codeclimate/test-reporter
+./test/format-coverage.sh 
+echo 'Combining and uploading coverage...' && cd test && ./sum-upload-coverage.sh && cd -
+echo 'Parallel tests runtime log' && cat tmp/parallel_runtime_test.log
