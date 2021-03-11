f4cb643a88a6ba2875fe35f010c869b523b49a3d
Sawy <melsawy.be7eri@gmail.com>
2021-03-11 07:33:45 +0200

    Ticket CHECK-59: log social login error for debugging

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -41,7 +41,7 @@ Devise.setup do |config|
   config.omniauth :twitter, setup: true
   config.omniauth :facebook, CheckConfig.get('facebook_app_id'), CheckConfig.get('facebook_app_secret'), scope: 'email,public_profile', info_fields: 'name,email,picture', setup: true
   config.omniauth :slack, CheckConfig.get('slack_app_id'), CheckConfig.get('slack_app_secret'), scope: 'identify,users:read'
-  google_auth_config = { access_type: 'offline', approval_prompt: '' }
+  google_auth_config = { access_type: 'offline', approval_prompt: '',  provider_ignores_state: true }
   google_auth_config[:redirect_uri] = CheckConfig.get('google_auth_redirect_uri') unless CheckConfig.get('google_auth_redirect_uri').blank?
   config.omniauth :google_oauth2, CheckConfig.get('google_client_id'), CheckConfig.get('google_client_secret'), google_auth_config
   config.skip_session_storage = [:http_auth, :token_auth]
