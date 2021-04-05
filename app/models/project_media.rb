806844eb9018fc9283120f22aceeb9dab4c82fbf
Caio Almeida <caiosba@gmail.com>
2021-04-05 13:55:20 -0300

    Ticket CHECK-423: Fixing race condition for similar item to inherit status from the main item

--- app/models/project_media.rb
+++ app/models/project_media.rb
@@ -18,12 +18,13 @@ class ProjectMedia < ActiveRecord::Base
   validate :source_belong_to_team, unless: proc { |pm| pm.source_id.blank? || pm.is_being_copied }
 
   before_validation :set_team_id, on: :create
-  after_create :create_project_media_project, :set_quote_metadata, :create_annotation, :notify_team_bots_create, :create_metrics_annotation
+  after_create :create_project_media_project, :set_quote_metadata, :create_annotation, :create_metrics_annotation
   after_create :send_slack_notification, :create_auto_tasks_for_team_item, if: proc { |pm| pm.add_to_project_id.nil? }
   after_create :create_relationship
   after_commit :apply_rules_and_actions_on_create, on: [:create]
   after_commit :create_relationship, on: [:update]
   after_commit :set_quote_metadata, on: [:create]
+  after_commit :notify_team_bots_create, on: [:create]
   after_update :archive_or_restore_related_medias_if_needed, :notify_team_bots_update
   after_update :apply_rules_and_actions_on_update, if: proc { |pm| pm.changes.keys.include?('read') }
   after_destroy :destroy_related_medias
