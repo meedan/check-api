e3d0a9600d86a44ac9d5acbb1a8f30f1f567eb93
Caio Almeida <caiosba@gmail.com>
2021-04-08 19:09:47 -0300

    Ticket CHECK-196: Changing default message for Smooch Bot query state and making sure that as soon as Smooch Bot is installed it contains all minimum settings to be actionable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210402222705) do
+ActiveRecord::Schema.define(version: 20210408210408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
