b4cad6602c54f27c43957315852ee33a25ff5caa
Caio Almeida <caiosba@gmail.com>
2021-04-09 18:17:22 -0300

    Ticket CHECK-445: Allow profile URLs to be created as media items

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,7 +3,6 @@ class Link < Media
 
   validates :url, presence: true, on: :create
   validate :validate_pender_result_and_retry, on: :create
-  validate :pender_result_is_a_media, on: :create
   validate :url_is_unique, on: :create
 
   after_create :set_pender_result_as_annotation, :set_account
@@ -59,10 +58,6 @@ class Link < Media
       self.account = Account.create_for_source(self.pender_data['author_url'], nil, false, false, self.pender_data[:pender_key])
       self.save!
     end
-  end
-
-  def pender_result_is_a_media
-    errors.add(:base, 'Sorry, this is not a media') if !self.pender_data.nil? && self.pender_data['type'] != 'item'
   end
 
   def url_is_unique
