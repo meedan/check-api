01e7d9b1d115fcff05956432c14784acfe387870
Darius Kazemi <darius.kazemi@gmail.com>
2021-04-15 08:37:05 -0700

    Configurating JSONAPI to return record count (#753)
    This appears on the top level of every query response in a `meta` object, per the JSONAPI spec.

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -3,4 +3,6 @@ JSONAPI.configure do |config|
   config.default_paginator = :offset
   config.default_page_size = 10
   config.maximum_page_size = 50
+  config.top_level_meta_include_record_count = true
+  config.top_level_meta_record_count_key = :record_count
 end
