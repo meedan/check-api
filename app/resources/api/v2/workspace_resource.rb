c27b31a56c2c1d3d4f33860576916f4fb7361d4f
Caio Almeida <caiosba@gmail.com>
2021-05-04 15:47:21 -0300

    Ticket CHECK-353: Filter workspaces by whether they have Smooch Bot installed or not

--- app/resources/api/v2/workspace_resource.rb
+++ app/resources/api/v2/workspace_resource.rb
@@ -8,6 +8,16 @@ module Api
       def self.records(options = {})
         self.workspaces(options)
       end
+
+      filter :is_tipline_installed, apply: ->(records, value, _options) {
+        bot = BotUser.smooch_user
+        return records if bot.nil?
+        if value && value[0]
+          records.joins("INNER JOIN team_users tu2 ON tu2.team_id = teams.id AND tu2.user_id = #{bot.id}")
+        else
+          records.joins("LEFT OUTER JOIN team_users tu2 ON tu2.team_id = teams.id AND tu2.user_id = #{bot.id}").where('tu2.team_id' => nil)
+        end
+      }
     end
   end
 end
