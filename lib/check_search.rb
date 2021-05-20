f22b62fbcb0974559e600e4146cff8d13416307b
Sawy <melsawy.be7eri@gmail.com>
2021-05-20 23:34:06 +0200

    CHECK-432: fix CC

--- lib/check_search.rb
+++ lib/check_search.rb
@@ -114,15 +114,14 @@ class CheckSearch
   end
 
   def get_pg_results
-    sort_key = SORT_MAPPING.keys.include?(@options['sort'].to_s) ? SORT_MAPPING[@options['sort'].to_s] : @options['sort'].to_s
-    sort = { sort_key => @options['sort_type'].to_s.downcase.to_sym }
+    sort = { SORT_MAPPING[@options['sort'].to_s] => @options['sort_type'].to_s.downcase.to_sym }
     relation = get_pg_results_for_media
     @options['id'] ? relation.where(id: @options['id']) : relation.order(sort).limit(@options['eslimit'].to_i).offset(@options['esoffset'].to_i)
   end
 
   def item_navigation_offset
     return -1 unless @options['es_id']
-    sort_key = SORT_MAPPING.keys.include?(@options['sort'].to_s) ? SORT_MAPPING[@options['sort'].to_s] : @options['sort'].to_s
+    sort_key = SORT_MAPPING[@options['sort'].to_s]
     sort_type = @options['sort_type'].to_s.downcase.to_sym
     pm = ProjectMedia.where(id: @options['id']).last
     return -1 if pm.nil?
