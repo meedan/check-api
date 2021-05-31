c4b9454f9621785f6aba97a5e045b4cf4cbc9bcc
Caio Almeida <caiosba@gmail.com>
2021-05-31 20:16:58 -0300

    Ticket CHECK-614: Workspaces API should filter by similarity feature enabled

--- app/resources/api/v2/workspace_resource.rb
+++ app/resources/api/v2/workspace_resource.rb
@@ -5,19 +5,26 @@ module Api
       attributes :name, :slug
       paginator :none
 
+      filter :is_tipline_installed, apply: ->(records, value, _options) {
+        self.has_bot_installed(records, value, BotUser.smooch_user)
+      }
+
+      filter :is_similarity_feature_enabled, apply: ->(records, value, _options) {
+        self.has_bot_installed(records, value, BotUser.alegre_user)
+      }
+
       def self.records(options = {})
         self.workspaces(options)
       end
 
-      filter :is_tipline_installed, apply: ->(records, value, _options) {
-        bot = BotUser.smooch_user
+      def self.has_bot_installed(records, value, bot = nil)
         return records if bot.nil?
         if value && value[0]
           records.joins("INNER JOIN team_users tu2 ON tu2.team_id = teams.id AND tu2.user_id = #{bot.id}")
         else
           records.joins("LEFT OUTER JOIN team_users tu2 ON tu2.team_id = teams.id AND tu2.user_id = #{bot.id}").where('tu2.team_id' => nil)
         end
-      }
+      end
     end
   end
 end
