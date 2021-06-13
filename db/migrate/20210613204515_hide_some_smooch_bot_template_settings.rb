e2eeb0d69c8b121708557e826a44ff8ff9ecdfae
Caio Almeida <caiosba@gmail.com>
2021-06-13 19:18:05 -0300

    Ticket CHECK-676: Hide deprecated tipline template settings fields

---
+++ db/migrate/20210613204515_hide_some_smooch_bot_template_settings.rb
@@ -0,0 +1,16 @@
+class HideSomeSmoochBotTemplateSettings < ActiveRecord::Migration
+  def change
+    tb = BotUser.smooch_user
+    fields_to_hide = ['smooch_template_name_for_fact_check_report', 'smooch_template_name_for_fact_check_report_updated', 'smooch_template_name_for_more_information_needed', 'smooch_template_name_for_more_information_needed_image_only']
+    unless tb.nil?
+      settings = tb.get_settings.clone || []
+      settings.each_with_index do |s, i|
+        if fields_to_hide.include?(s.with_indifferent_access[:name])
+          settings[i][:type] = 'hidden'
+        end
+      end
+      tb.set_settings(settings)
+      tb.save!
+    end
+  end
+end
