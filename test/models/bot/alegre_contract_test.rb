8b0113cbf9794082ca8e48989c06cfa818bb3957
Caio Almeida <caiosba@gmail.com>
2021-06-21 00:37:20 -0300

    Ticket CHECK-671: Print log/alegre_mock_service.log in test output

--- test/models/bot/alegre_contract_test.rb
+++ test/models/bot/alegre_contract_test.rb
@@ -23,6 +23,12 @@ class Bot::AlegreContractTest < ActiveSupport::TestCase
       )
   end
 
+  def teardown
+    puts '$ cat log/alegre_mock_service.log'
+    path = File.join(Rails.root, 'log', 'alegre_mock_service.log')
+    puts `cat #{path}`
+  end
+
   test 'returns language' do
     stub_configs({ 'alegre_host' => 'http://localhost:5000' }) do
       assert_equal 'en', Bot::Alegre.get_language_from_alegre('This is a test')
