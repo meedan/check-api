9ee067b8b0f5729848232c31b19c6c8bd4fe9e8c
Daniela Soares Feitosa <daniela@meedan.com>
2021-06-30 15:22:12 -0700

    GraphQL deprecation warning
    DEPRECATION WARNING: GraphQL::Schema.define will be removed in GraphQL-Ruby 2.0; use a class-based definition instead. See https://graphql-ruby.org/schema/class_based_api.html.

--- config/initializers/relay_on_rails.rb
+++ config/initializers/relay_on_rails.rb
@@ -1,6 +1,6 @@
 require 'apollo/tracing'
 
-RelayOnRailsSchema = GraphQL::Schema.define do
+class RelayOnRailsSchema < GraphQL::Schema
   query QueryType
   mutation MutationType
   use GraphQL::Batch
@@ -19,7 +19,7 @@ RelayOnRailsSchema = GraphQL::Schema.define do
   #   }
   # end
 
-  resolve_type -> (_type, object, _ctx) do
+  def self.resolve_type(_type, object, _ctx)
     klass = (object.respond_to?(:type) && object.type) ? object.type : object.class_name
     klass = 'Task' if Task.task_types.include?(klass)
     klass = 'User' if object.class.name == 'User'
@@ -30,7 +30,7 @@ RelayOnRailsSchema = GraphQL::Schema.define do
     CheckGraphql.id_from_object(obj, type, ctx)
   }
 
-  object_from_id -> (id, ctx) do
+  def self.object_from_id(id, ctx)
     CheckGraphql.object_from_id(id, ctx)
   end
 
