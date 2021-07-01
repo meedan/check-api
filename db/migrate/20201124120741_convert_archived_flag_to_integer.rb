07805b2ec847efbb652ac20b2f923cd9776dfa92
Daniela Soares Feitosa <daniela@meedan.com>
2021-07-01 14:35:42 -0700

    Fix migration: ActiveRecord::StatementInvalid: PG::DatatypeMismatch
    ActiveRecord::StatementInvalid: PG::DatatypeMismatch: ERROR:  column "archived" cannot be cast automatically to type boolean

--- db/migrate/20201124120741_convert_archived_flag_to_integer.rb
+++ db/migrate/20201124120741_convert_archived_flag_to_integer.rb
@@ -3,7 +3,7 @@ class ConvertArchivedFlagToInteger < ActiveRecord::Migration[4.2]
     { teams: Team, projects: Project, project_medias: ProjectMedia, sources: Source }.each do |table, model|
       column = model.column_for_attribute('archived')
       unless column.nil? || column.type == :integer
-        change_column table, :archived, :boolean, default: nil
+        change_column table, :archived, :boolean, using: 'archived::boolean', default: nil
         change_column table, :archived, :integer, using: 'archived::integer', default: 0
       end
     end
