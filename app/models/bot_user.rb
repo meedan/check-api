42bfba7980710801199279c87234ed3a449ed6b2
Daniela Soares Feitosa <daniela@meedan.com>
2021-07-01 13:06:48 -0700

    Revert "DEPRECATION WARNING: #table_exists?"
    This reverts commit 9cf9fdded882d5f7297494d2fa79da418ffb4b9b.

--- app/models/bot_user.rb
+++ app/models/bot_user.rb
@@ -3,7 +3,7 @@ class BotUser < User
   include CheckPusher
 
   EVENTS = ['create_project_media', 'update_project_media', 'create_source', 'update_source', 'update_annotation_own', 'publish_report']
-  if ApplicationRecord.data_source_exists?(:dynamic_annotation_annotation_types)
+  if ApplicationRecord.connection.table_exists?(:dynamic_annotation_annotation_types)
     annotation_types = DynamicAnnotation::AnnotationType.all.map(&:annotation_type) + ['comment', 'tag', 'task', 'geolocation']
     annotation_types.each do |type|
       EVENTS << "create_annotation_#{type}"
