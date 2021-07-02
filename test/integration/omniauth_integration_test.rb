e55a85d39ae593188a439f5bac6a1485cb54860d
Daniela Soares Feitosa <daniela@meedan.com>
2021-07-02 15:53:02 -0700

    Ticket CHECK-601: Fix integration tests adding `params` on requests

--- test/integration/omniauth_integration_test.rb
+++ test/integration/omniauth_integration_test.rb
@@ -2,7 +2,7 @@ require_relative '../test_helper'
 
 class OmniauthIntegrationTest < ActionDispatch::IntegrationTest
   test "should close in case of failure" do
-    get '/api/users/auth/slack/callback', error: 'access_denied'
+    get '/api/users/auth/slack/callback', params: { error: 'access_denied' }
     assert_redirected_to '/close.html'
   end
 end
