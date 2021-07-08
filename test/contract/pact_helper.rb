f49627349b79df5291aec1fbba0786d20aa9bfa3
Daniele Valverde <34126648+danielevalverde@users.noreply.github.com>
2021-07-08 18:16:52 -0300

    Test/671 contract test (#817)
    * Ticket CHECK-671: add pact gem and contract tests

---
+++ test/contract/pact_helper.rb
@@ -0,0 +1,10 @@
+require_relative '../test_helper'
+require 'pact/consumer/minitest'
+
+Pact.service_consumer "Check API" do
+  has_pact_with "Alegre" do
+    mock_service :alegre do
+      port 5000
+    end
+  end
+end
