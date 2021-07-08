5ea76679d4739ecd43354e48cd8dd50a7867bc5d
Martin Peck <martin@meedan.com>
2021-07-08 15:40:36 -0700

    Purge configurator with fire. So that when SSM omits honeycomb there is no default config for it either.

--- production/bin/start.sh
+++ production/bin/start.sh
@@ -6,16 +6,10 @@
 LOGFILE=${DEPLOYDIR}/log/${RAILS_ENV}.log
 SERVER_PORT=3300
 
-#since GITHUB_TOKEN environment variable is a json object, we need parse the value
-#This function is here due to a limitation by "secrets manager"
-
-if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
-	echo "GITHUB_TOKEN, DEPLOY_ENV and APP must be in the environment.   Exiting."
+if [[ -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
+	echo "DEPLOY_ENV and APP must be in the environment.   Exiting."
 	exit 1
 fi
-
-if [ ! -d "configurator" ]; then git clone -q https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
-d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
 
 /app/current/vendor/bundle/ruby/2.4.0/gems/apollo-tracing-1.5.0/bin/engineproxy_linux_amd64 --config config/apollo-engine-proxy.json &
 
