033a27db161518075f8d48e60b60f87b10f8e2ca
Doci Mou <22457019+dmou@users.noreply.github.com>
2021-07-13 11:55:04 -0500

    wait for ES cluster to be responsive before starting tests

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo 'Waiting for ElasticSearch cluster to be healthy...'
+curl -XGET 'http://localhost:9200/_cluster/health?wait_for_status=yellow&timeout=120s'
 cd test && rm -rf $PATTERN && echo 'Running tests:' && ls && cd - && bundle exec rake parallel:test[5]
