39422834f3f6e10bc98afd63f52062081490dad5
Doci Mou <22457019+dmou@users.noreply.github.com>
2021-07-13 12:37:51 -0500

    set job name for unit tests

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -2,4 +2,10 @@
 
 echo 'Waiting for ElasticSearch cluster to be healthy...'
 curl -XGET 'http://localhost:9200/_cluster/health?wait_for_status=yellow&timeout=120s'
+
+if [ "$GITHUB_JOB_NAME" == "unit-tests" ] ; then
+  PATTERN='controllers';
+elif [ "$GITHUB_JOB_NAME" == "functional-tests" ] ; then
+  PATTERN='models mailers integration workers lib'
+fi
 cd test && rm -rf $PATTERN && echo 'Running tests:' && ls && cd - && bundle exec rake parallel:test[5]
