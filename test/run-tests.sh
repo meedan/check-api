cc50cb5b66ec659a66c429302ceea408c1b69a86
Doci Mou <22457019+dmou@users.noreply.github.com>
2021-07-13 23:08:21 -0500

    remove second check for ES status

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo 'Waiting for ElasticSearch cluster to be healthy...'
-until curl -I -f --silent  -XGET 'http://localhost:9200/_cluster/health?wait_for_status=yellow&timeout=120s'; do printf '.'; sleep 1; done
-
 if [ "$GITHUB_JOB_NAME" == "unit-tests" ] ; then
   PATTERN='controllers';
 elif [ "$GITHUB_JOB_NAME" == "functional-tests" ] ; then
