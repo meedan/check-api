68bbcacf997e1e2310fe67aa17ec4870ef4658be
Sawy <melsawy.be7eri@gmail.com>
2021-09-20 11:22:16 +0200

    CHECK-872: fix tests

--- lib/pender_data.rb
+++ lib/pender_data.rb
@@ -20,6 +20,7 @@ module PenderData
         self.pender_error_code = result['data']['code']
         self.retry_pender_or_fail(force, retry_on_error, result)
       else
+        self.pender_error = false
         self.pender_data = result['data'].to_h.merge(pender_key: pender_key)
         self.url = self.get_url_from_result(result)
       end
