bf513577b5d1f87b400d1ee6321284c199e730a7
Sawy <melsawy.be7eri@gmail.com>
2021-11-14 10:09:37 +0200

    CHECK-87: add a custom field for flags

--- db/migrate/20211114080408_update_flag_dynamic_annotation_schema.rb
+++ db/migrate/20211114080408_update_flag_dynamic_annotation_schema.rb
@@ -4,8 +4,12 @@ class UpdateFlagDynamicAnnotationSchema < ActiveRecord::Migration[5.2]
     json_schema = at.json_schema
     # add show cover field
     json_schema['properties'][:show_cover] = { type: 'boolean' }
+    # adjust min and max for flags
+    ["racy", "spam", "adult", "spoof", "medical", "violence"].each do |key|
+      json_schema['properties']['flags']['properties'][key] = { type: 'integer', minimum: 0, maximum: 7 }
+    end
     # add custom flag to existing ones
-    # json_schema['properties']['flags']['properties']['custom']
+    json_schema['properties']['flags']['properties']['custom'] = { type: 'object' }
     at.json_schema = json_schema
     at.save!
   end
