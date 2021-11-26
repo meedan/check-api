b9f66d3401eeee76fc74dc058227ad491379d173
Caio Almeida <caiosba@gmail.com>
2021-11-26 06:26:47 -0300

    Ticket CHECK-1162: Pass one more placeholder to the tipline newsletter template

--- app/workers/tipline_newsletter_worker.rb
+++ app/workers/tipline_newsletter_worker.rb
@@ -13,7 +13,7 @@ class TiplineNewsletterWorker
               content = Bot::Smooch.build_newsletter_content(newsletter, language, team_id)
               fallback = I18n.t(:smooch_bot_message_newsletter_fallback, { platform: ts.platform, locale: language, team: tbi.team.name, date: date, content: content, unsubscribe: I18n.t(:unsubscribe, { locale: language }) })
               Bot::Smooch.get_installation { |i| i.id == tbi.id }
-              message = Bot::Smooch.format_template_message(tbi.get_newsletter_template_name, [tbi.team.name, date, content], nil, fallback, language)
+              message = Bot::Smooch.format_template_message(tbi.get_newsletter_template_name, [ts.platform, tbi.team.name, date, content], nil, fallback, language)
               Bot::Smooch.send_message_to_user(ts.uid, message)
             end
           end
