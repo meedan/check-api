4baffbf6c40da01da4ffe945e2178bc02f1a75d5
Sawy <melsawy.be7eri@gmail.com>
2021-12-15 18:25:29 +0200

    CHECK-1150: export is_default for project

--- app/graph/mutations/project_mutations.rb
+++ app/graph/mutations/project_mutations.rb
@@ -13,7 +13,8 @@ module ProjectMutations
     assignment_message: 'str',
     project_group_id: 'int',
     previous_project_group_id: 'int',
-    privacy: 'int'
+    privacy: 'int',
+    is_default: 'bool'
   }
 
   Create, Update, Destroy = GraphqlCrudOperations.define_crud_operations('project', create_fields, update_fields, ['team', 'check_search_team', 'project_group', 'project_group_was'])
