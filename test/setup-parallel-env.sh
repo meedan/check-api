2429320fdf8a11c347409d79b0363e02c9a12869
Caio Almeida <caiosba@gmail.com>
2021-12-20 17:09:37 -0300

    Create ElasticSearch indexes for parallel test execution

--- test/setup-parallel-env.sh
+++ test/setup-parallel-env.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-echo 'Setting up parallel databases'
+echo 'Setting up parallel test environment...'
+
+echo 'Setting up parallel ElasticSearch indexes...'
+
+bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.create!(index, true) ; puts "Created ElasticSearch index named #{index}"'
+TEST_ENV_NUMBER=1 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.create!(index, true) ; puts "Created ElasticSearch index named #{index}"'
+TEST_ENV_NUMBER=2 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.create!(index, true) ; puts "Created ElasticSearch index named #{index}"'
+TEST_ENV_NUMBER=3 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.create!(index, true) ; puts "Created ElasticSearch index named #{index}"'
+TEST_ENV_NUMBER=4 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.create!(index, true) ; puts "Created ElasticSearch index named #{index}"'
+TEST_ENV_NUMBER=5 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.create!(index, true) ; puts "Created ElasticSearch index named #{index}"'
+sleep 10
+
+echo 'Setting up parallel databases...'
 
 FAIL=0
 
