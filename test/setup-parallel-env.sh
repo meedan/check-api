eb7ed0fc04924f3045610e69b4e09482415b1526
Caio Almeida <caiosba@gmail.com>
2021-12-21 14:28:23 -0300

    Create ElasticSearch indexes for parallel test execution

--- test/setup-parallel-env.sh
+++ test/setup-parallel-env.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
+export RAILS_ENV=test
+
 echo 'Setting up parallel test environment...'
 
 echo 'Setting up parallel ElasticSearch indexes...'
 
-bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.create!(index, true) ; puts "Created ElasticSearch index named #{index}"'
-TEST_ENV_NUMBER=1 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.create(index, true) ; puts "Created ElasticSearch index named #{index}"'
-TEST_ENV_NUMBER=2 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.create(index, true) ; puts "Created ElasticSearch index named #{index}"'
-TEST_ENV_NUMBER=3 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.create(index, true) ; puts "Created ElasticSearch index named #{index}"'
-TEST_ENV_NUMBER=4 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.create(index, true) ; puts "Created ElasticSearch index named #{index}"'
-TEST_ENV_NUMBER=5 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.create(index, true) ; puts "Created ElasticSearch index named #{index}"'
+TEST_ENV_NUMBER=0 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.delete_index(index) ; MediaSearch.create_index(index, true) ; puts "Created ElasticSearch index named #{index}"'
+TEST_ENV_NUMBER=1 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.delete_index(index) ; MediaSearch.create_index(index, true) ; puts "Created ElasticSearch index named #{index}"'
+TEST_ENV_NUMBER=2 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.delete_index(index) ; MediaSearch.create_index(index, true) ; puts "Created ElasticSearch index named #{index}"'
+TEST_ENV_NUMBER=3 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.delete_index(index) ; MediaSearch.create_index(index, true) ; puts "Created ElasticSearch index named #{index}"'
+TEST_ENV_NUMBER=4 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.delete_index(index) ; MediaSearch.create_index(index, true) ; puts "Created ElasticSearch index named #{index}"'
+TEST_ENV_NUMBER=5 bundle exec rails runner 'index = CheckConfig.get("elasticsearch_index") ; MediaSearch.delete_index(index) ; MediaSearch.create_index(index, true) ; puts "Created ElasticSearch index named #{index}"'
 sleep 10
 
 echo 'Setting up parallel databases...'
 
 FAIL=0
 
-bundle exec rake db:create db:migrate &
-TEST_ENV_NUMBER=1 bundle exec rake db:create db:migrate &
-TEST_ENV_NUMBER=2 bundle exec rake db:create db:migrate &
-TEST_ENV_NUMBER=3 bundle exec rake db:create db:migrate &
-TEST_ENV_NUMBER=4 bundle exec rake db:create db:migrate &
-TEST_ENV_NUMBER=5 bundle exec rake db:create db:migrate &
+TEST_ENV_NUMBER=0 bundle exec rake db:drop db:create db:migrate &
+TEST_ENV_NUMBER=1 bundle exec rake db:drop db:create db:migrate &
+TEST_ENV_NUMBER=2 bundle exec rake db:drop db:create db:migrate &
+TEST_ENV_NUMBER=3 bundle exec rake db:drop db:create db:migrate &
+TEST_ENV_NUMBER=4 bundle exec rake db:drop db:create db:migrate &
+TEST_ENV_NUMBER=5 bundle exec rake db:drop db:create db:migrate &
 
 for job in `jobs -p`
 do
