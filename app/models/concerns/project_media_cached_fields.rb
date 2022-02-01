dc54d881cd8b384962a51b9424f1bc53a2b9bd7d
Caio Almeida <caiosba@gmail.com>
2022-02-01 12:57:00 -0300

    Ticket CHECK-1316: Item title comes from claim and fact-check now

--- app/models/concerns/project_media_cached_fields.rb
+++ app/models/concerns/project_media_cached_fields.rb
@@ -9,24 +9,18 @@ module ProjectMediaCachedFields
   end
 
   module ClassMethods
-    def analysis_update(field)
+    def claim_or_fact_check_update
       [
         {
-          model: DynamicAnnotation::Field,
-          if: proc { |f| f.field_name == field && f.annotation.annotation_type == 'verification_status' && !f.value.blank? },
-          affected_ids: proc { |f| [f.annotation.annotated_id] },
+          model: ClaimDescription,
+          affected_ids: proc { |cd| [cd.project_media] },
           events: {
-            save: proc { |_pm, f| f.value }
+            save: :recalculate
           }
         },
         {
-          model: DynamicAnnotation::Field,
-          if: proc { |f| f.field_name == 'metadata_value' },
-          affected_ids: proc { |f|
-            if ['Media', 'Link'].include?(f.annotation.annotated_type)
-              ProjectMedia.where(media_id: f.annotation.annotated_id).map(&:id)
-            end
-          },
+          model: FactCheck,
+          affected_ids: proc { |fc| fc.claim_description.project_media },
           events: {
             save: :recalculate
           }
@@ -140,12 +134,12 @@ module ProjectMediaCachedFields
       ]
 
     cached_field :description,
-      recalculate: proc { |pm| pm.has_analysis_description? ? pm.analysis_description : pm.original_description },
-      update_on: analysis_update('content')
+      recalculate: proc { |pm| pm.fact_check_summary || pm.claim_description_content || pm.original_description },
+      update_on: claim_or_fact_check_update
 
     cached_field :title,
-      recalculate: proc { |pm| pm.has_analysis_title? ? pm.analysis_title : pm.original_title },
-      update_on: analysis_update('title')
+      recalculate: proc { |pm| pm.fact_check_title || pm.claim_description_content || pm.original_title },
+      update_on: claim_or_fact_check_update
 
     cached_field :status,
       recalculate: proc { |pm| pm.last_verification_status },
