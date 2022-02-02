9b77daeff65de1255fc2db97f6a43835b077af95
Caio Almeida <caiosba@gmail.com>
2022-02-02 00:12:30 -0300

    Tickets CHECK-1352 and CHECK-1353: Index claims and fact-checks in Alegre and in ElasticSearch

--- lib/check_search.rb
+++ lib/check_search.rb
@@ -385,7 +385,7 @@ class CheckSearch
   def build_keyword_conditions_media_fields
     es_fields = []
     conditions = []
-    %w(title description quote analysis_title analysis_description url extracted_text).each do |f|
+    %w(title description quote analysis_title analysis_description url extracted_text claim_description_content fact_check_title fact_check_summary).each do |f|
       es_fields << f if should_include_keyword_field?(f)
     end
     conditions << { simple_query_string: { query: @options["keyword"], fields: es_fields, default_operator: "AND" } } unless es_fields.blank?
