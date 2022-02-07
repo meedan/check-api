900b691ba8fde46a14d701281bea3d3b81e26800
Sawy <melsawy.be7eri@gmail.com>
2022-02-07 07:29:27 +0200

    CHECK-1377: remove column exists check

--- db/migrate/20160613183535_create_team_users.rb
+++ db/migrate/20160613183535_create_team_users.rb
@@ -4,6 +4,10 @@ class CreateTeamUsers < ActiveRecord::Migration[4.2]
       t.belongs_to :team, index: true
       t.belongs_to :user, index: true
       t.string :type, index: true
+      t.integer :invited_by_id
+      t.string :invitation_token
+      t.string :raw_invitation_token
+      t.datetime :invitation_accepted_at
       t.timestamps null: false
     end
   end
