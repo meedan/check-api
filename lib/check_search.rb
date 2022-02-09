6db89e6bc57f5708f4bf95011c8d82a24605035d
Sawy <melsawy.be7eri@gmail.com>
2022-02-09 14:06:57 +0200

    CHECK-1302: allow user to search date filter by less_than option

--- lib/check_search.rb
+++ lib/check_search.rb
@@ -637,8 +637,27 @@ class CheckSearch
   def format_times_search_range_filter(values, timezone)
     return if values.blank?
     tz = (!timezone.blank? && ActiveSupport::TimeZone[timezone]) ? timezone : 'UTC'
-    from = format_time_with_timezone(values.dig('start_time'), tz)
-    to = format_time_with_timezone(values.dig('end_time'), tz)
+    # condition_type = 'less_than', 'is_between'
+    condition_type = values.dig('condition') || 'is_between'
+    if condition_type == 'less_than'
+      period = values.dig('period').to_i
+      from_date = case values.dig('period_type').downcase
+                  when 'd'
+                    Time.now - period.day
+                  when 'w'
+                    Time.now - period.week
+                  when 'm'
+                    Time.now - period.month
+                  when 'y'
+                    Time.now - period.year
+                  end
+      from = from_date.blank? ? nil : format_time_with_timezone(from_date.to_s, tz)
+      to = nil
+    else
+      from = format_time_with_timezone(values.dig('start_time'), tz)
+      to = format_time_with_timezone(values.dig('end_time'), tz)
+    end
+
     return if from.blank? && to.blank?
     from ||= DateTime.new
     to ||= DateTime.now.in_time_zone(tz)
