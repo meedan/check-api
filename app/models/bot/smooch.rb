6f81adb389a85604f846e2adb104ba4f7a7b64ba
Caio Almeida <caiosba@gmail.com>
2022-02-11 19:32:23 -0300

    Ticket CHECK-925: Attach main menu to outgoing text report

--- app/models/bot/smooch.rb
+++ app/models/bot/smooch.rb
@@ -904,7 +904,8 @@ class Bot::Smooch < BotUser
         sleep 3
       end
       if report.report_design_field_value('use_text_message', lang)
-        last_smooch_response = self.send_message_to_user(uid, report.report_design_text(lang))
+        workflow = self.get_workflow(lang)
+        last_smooch_response = self.send_final_message_to_user(uid, report.report_design_text(lang), workflow, lang)
         Rails.logger.info "[Smooch Bot] Sent text report to user #{uid} for item with ID #{pm.id}, response was: #{last_smooch_response.to_json}"
       end
       self.save_smooch_response(last_smooch_response, parent, data['received'], fallback_template, lang)
