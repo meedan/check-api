8df7fca75699768d134f36c27b5c4fafb1e08a89
Caio Almeida <caiosba@gmail.com>
2022-02-11 11:24:49 -0300

    Ticket CHECK-1342: Fixing tests

--- db/migrate/20220211034158_add_first_added_at_and_last_added_at_to_clusters.rb
+++ db/migrate/20220211034158_add_first_added_at_and_last_added_at_to_clusters.rb
@@ -1,7 +1,7 @@
 class AddFirstAddedAtAndLastAddedAtToClusters < ActiveRecord::Migration[5.2]
   def change
-    # add_column :clusters, :first_item_at, :datetime
-    # add_column :clusters, :last_item_at, :datetime
+    add_column :clusters, :first_item_at, :datetime
+    add_column :clusters, :last_item_at, :datetime
     Cluster.reset_column_information
     Cluster.find_each do |c|
       c.project_medias.find_each do |pm|
