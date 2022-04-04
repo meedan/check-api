ca2440a2fcc09a81c149f2ed0956eedbe1afe6a0
Martin Peck <51542678+sonoransun@users.noreply.github.com>
2022-04-03 20:32:10 -0700

    Cut honeycomb sample rate by half again, as we are exceeding daily burst protection limits. (#1036)
    Co-authored-by: Martin Peck <martin@meedan.com>

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -3,7 +3,7 @@ class CustomSampler
 
   def self.sample(fields)
     if ['http_request', 'sql.active_record'].include?(fields['name']) && should_sample(1, fields['trace.trace_id'])
-      return [true, 128]
+      return [true, 256]
     end
     return [false, 0]
   end
