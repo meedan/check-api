2dd8b8f4c96d830f6efd4a27901d6231b96ef982
Caio Almeida <caiosba@gmail.com>
2022-04-11 10:32:10 -0300

    Ticket CHECK-1650: Tipline bot v2 with buttons support for Telegram, Viber and Messenger

--- test/models/bot/smooch_6_test.rb
+++ test/models/bot/smooch_6_test.rb
@@ -357,4 +357,10 @@ class Bot::Smooch6Test < ActiveSupport::TestCase
 
     send_message_to_smooch_bot('Read now', @uid, { 'quotedMessage' => { 'content' => { '_id' => id } } })
   end
+
+  test "should show main menu as buttons for non-WhatsApp platforms on tipline bot v2" do
+    send_message_to_smooch_bot('Hello', @uid, { 'source' => { 'type' => 'telegram' } })
+    send_message_to_smooch_bot('1', @uid, { 'source' => { 'type' => 'telegram' } })
+    assert_state 'main'
+  end
 end
