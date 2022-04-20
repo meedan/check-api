3f0a3150eb97c7c07c4eb17abc96a12a2dbbe9d2
Caio Almeida <caiosba@gmail.com>
2022-04-20 17:50:40 -0300

    Some changes as per product review of the feeds API feature
    Requested changes for the feeds resource were:
    
    * Remove attributes report_title and report_summary
    * Add attributes claim_context, claim_tags and fact_check_published_on
    * Add an "after" (date) filter, exposed in Swagger UI as well
    * Rename a few other attributes
    
    References: CHECK-1748.

--- app/resources/api/v2/feed_resource.rb
+++ app/resources/api/v2/feed_resource.rb
@@ -4,24 +4,28 @@ module Api
       model_name 'ProjectMedia'
 
       attribute :claim, delegate: :claim_description_content
-      attribute :title, delegate: :fact_check_title
-      attribute :summary, delegate: :fact_check_summary
-      attribute :url, delegate: :published_url
-      attribute :report_title, delegate: :report_text_title
-      attribute :report_summary, delegate: :report_text_content
+      attribute :claim_context, delegate: :claim_description_context
+      attribute :claim_tags, delegate: :tags_as_sentence
+      attribute :fact_check_title
+      attribute :fact_check_summary
+      attribute :fact_check_published_on
+      attribute :fact_check_rating, delegate: :status
+      attribute :published_article_url, delegate: :published_url
       attribute :organization, delegate: :team_name
-      attribute :rating, delegate: :status
 
       filter :type, default: 'text', apply: ->(records, _value, _options) { records }
       filter :query, apply: ->(records, _value, _options) { records }
+      filter :after, apply: ->(records, _value, _options) { records }
 
       def self.records(options = {})
         team_ids = self.workspaces(options).map(&:id)
         filters = options[:filters] || {}
         query = filters.dig(:query, 0)
         type = filters.dig(:type, 0)
+        after = filters.dig(:after, 0)
+        after = Time.parse(after) unless after.blank?
         return ProjectMedia.none if team_ids.blank? || query.blank?
-        results = Bot::Smooch.search_for_similar_published_fact_checks(type, CGI.unescape(query), team_ids)
+        results = Bot::Smooch.search_for_similar_published_fact_checks(type, CGI.unescape(query), team_ids, after)
         ProjectMedia.where(id: results.map(&:id))
       end
 
