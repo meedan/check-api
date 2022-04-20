7bddf2f1aada847e29ec5721c62d5f63c58853c7
Caio Almeida <caiosba@gmail.com>
2022-04-19 21:47:01 -0300

    Swagger UI updated for API v2

--- app/resources/api/v2/feed_resource.rb
+++ app/resources/api/v2/feed_resource.rb
@@ -3,7 +3,15 @@ module Api
     class FeedResource < BaseResource
       model_name 'ProjectMedia'
 
-      attributes :title
+      attribute :claim, delegate: :claim_description_content
+      attribute :title, delegate: :fact_check_title
+      attribute :summary, delegate: :fact_check_summary
+      attribute :url, delegate: :published_url
+      attribute :report_title, delegate: :report_text_title
+      attribute :report_summary, delegate: :report_text_content
+      attribute :organization, delegate: :team_name
+      attribute :rating, delegate: :status
+
       filter :type, default: 'text', apply: ->(records, _value, _options) { records }
       filter :query, apply: ->(records, _value, _options) { records }
 
@@ -13,7 +21,7 @@ module Api
         query = filters.dig(:query, 0)
         type = filters.dig(:type, 0)
         return ProjectMedia.none if team_ids.blank? || query.blank?
-        results = Bot::Smooch.search_for_similar_published_fact_checks(type, query, team_ids)
+        results = Bot::Smooch.search_for_similar_published_fact_checks(type, CGI.unescape(query), team_ids)
         ProjectMedia.where(id: results.map(&:id))
       end
 
