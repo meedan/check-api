272d07fdc9dfcd74856379147906feca7af30949
Caio Almeida <caiosba@gmail.com>
2022-05-11 14:10:21 -0300

    Adding more logging to tipline newsletter delivery.
    Adding more log messages to tipline newsletter delivery, so we have more information when debugging why some user didn't receive the newsletter.
    
    Reference: CHECK-1831.

--- app/workers/tipline_newsletter_worker.rb
+++ app/workers/tipline_newsletter_worker.rb
@@ -7,15 +7,24 @@ class TiplineNewsletterWorker
       tbi.settings['smooch_workflows'].to_a.each do |workflow|
         if workflow['smooch_workflow_language'] == language
           newsletter = workflow['smooch_newsletter']
+          Rails.logger.info "[Smooch Bot] [Newsletter] [Team ##{team_id}] [Language #{language}] [#{Time.now}] Preparing newsletter to be sent..."
           if !newsletter.nil? && Bot::Smooch.newsletter_content_changed?(newsletter, language, team_id)
             date = I18n.l(Time.now.to_date, locale: language.to_s.tr('_', '-'), format: :long)
             TiplineSubscription.where(language: language, team_id: team_id).each do |ts|
-              introduction = newsletter['smooch_newsletter_introduction'].to_s.gsub('{date}', date).gsub('{channel}', ts.platform)
-              content = Bot::Smooch.build_newsletter_content(newsletter, language, team_id).gsub('{date}', date).gsub('{channel}', ts.platform)
-              Bot::Smooch.get_installation { |i| i.id == tbi.id }
-              response = Bot::Smooch.send_final_message_to_user(ts.uid, content, workflow, language)
-              Bot::Smooch.save_smooch_response(response, nil, nil, 'newsletter', language, { introduction: introduction })
+              Rails.logger.info "[Smooch Bot] [Newsletter] [Team ##{team_id}] [Language #{language}] [#{Time.now}] Sending newsletter to subscriber ##{ts.id}..."
+              begin
+                introduction = newsletter['smooch_newsletter_introduction'].to_s.gsub('{date}', date).gsub('{channel}', ts.platform)
+                content = Bot::Smooch.build_newsletter_content(newsletter, language, team_id).gsub('{date}', date).gsub('{channel}', ts.platform)
+                Bot::Smooch.get_installation { |i| i.id == tbi.id }
+                response = Bot::Smooch.send_final_message_to_user(ts.uid, content, workflow, language)
+                Bot::Smooch.save_smooch_response(response, nil, nil, 'newsletter', language, { introduction: introduction })
+                Rails.logger.info "[Smooch Bot] [Newsletter] [Team ##{team_id}] [Language #{language}] [#{Time.now}] Newsletter sent to subscriber ##{ts.id}, response: #{response.inspect}"
+              rescue StandardError => e
+                Rails.logger.info "[Smooch Bot] [Newsletter] [Team ##{team_id}] [Language #{language}] [#{Time.now}] Could not send newsletter to subscriber ##{ts.id}: #{e.message}"
+              end
             end
+          else
+            Rails.logger.info "[Smooch Bot] [Newsletter] [Team ##{team_id}] [Language #{language}] [#{Time.now}] Not sending newsletter because content hasn't changed since the last delivery"
           end
         end
       end
