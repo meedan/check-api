6d13a8b1ff9ca3389832eec130bb7991f949b54b
Caio Almeida <caiosba@gmail.com>
2022-05-16 13:32:09 -0300

    Fixing test, adding missing tests and fixing a couple Code Climate issues.
    Reference: CHECK-1831.

--- app/workers/tipline_newsletter_worker.rb
+++ app/workers/tipline_newsletter_worker.rb
@@ -3,31 +3,40 @@ class TiplineNewsletterWorker
 
   def perform(team_id, language)
     tbi = TeamBotInstallation.where(user_id: BotUser.smooch_user&.id.to_i, team_id: team_id.to_i).last
+    count = 0
     unless tbi.nil?
       tbi.settings['smooch_workflows'].to_a.each do |workflow|
         if workflow['smooch_workflow_language'] == language
           newsletter = workflow['smooch_newsletter']
-          Rails.logger.info "[Smooch Bot] [Newsletter] [Team ##{team_id}] [Language #{language}] [#{Time.now}] Preparing newsletter to be sent..."
+          log team_id, language, 'Preparing newsletter to be sent...'
           if !newsletter.nil? && Bot::Smooch.newsletter_content_changed?(newsletter, language, team_id)
             date = I18n.l(Time.now.to_date, locale: language.to_s.tr('_', '-'), format: :long)
             TiplineSubscription.where(language: language, team_id: team_id).each do |ts|
-              Rails.logger.info "[Smooch Bot] [Newsletter] [Team ##{team_id}] [Language #{language}] [#{Time.now}] Sending newsletter to subscriber ##{ts.id}..."
+              log team_id, language, "Sending newsletter to subscriber ##{ts.id}..."
               begin
                 introduction = newsletter['smooch_newsletter_introduction'].to_s.gsub('{date}', date).gsub('{channel}', ts.platform)
                 content = Bot::Smooch.build_newsletter_content(newsletter, language, team_id).gsub('{date}', date).gsub('{channel}', ts.platform)
                 Bot::Smooch.get_installation { |i| i.id == tbi.id }
                 response = Bot::Smooch.send_final_message_to_user(ts.uid, content, workflow, language)
                 Bot::Smooch.save_smooch_response(response, nil, nil, 'newsletter', language, { introduction: introduction })
-                Rails.logger.info "[Smooch Bot] [Newsletter] [Team ##{team_id}] [Language #{language}] [#{Time.now}] Newsletter sent to subscriber ##{ts.id}, response: #{response.inspect}"
+                log team_id, language, "Newsletter sent to subscriber ##{ts.id}, response: #{response.inspect}"
+                count += 1
               rescue StandardError => e
-                Rails.logger.info "[Smooch Bot] [Newsletter] [Team ##{team_id}] [Language #{language}] [#{Time.now}] Could not send newsletter to subscriber ##{ts.id}: #{e.message}"
+                log team_id, language, "Could not send newsletter to subscriber ##{ts.id}: #{e.message}"
               end
             end
           else
-            Rails.logger.info "[Smooch Bot] [Newsletter] [Team ##{team_id}] [Language #{language}] [#{Time.now}] Not sending newsletter because content hasn't changed since the last delivery"
+            log team_id, language, 'Not sending newsletter because content has not changed since the last delivery'
           end
         end
       end
     end
+    count
+  end
+
+  private
+
+  def log(team_id, language, message)
+    Rails.logger.info "[Smooch Bot] [Newsletter] [Team ##{team_id}] [Language #{language}] [#{Time.now}] #{message}"
   end
 end
--- test/workers/tipline_newsletter_worker_test.rb
+++ test/workers/tipline_newsletter_worker_test.rb
@@ -5,15 +5,28 @@ class TiplineNewsletterWorkerTest < ActiveSupport::TestCase
     super
     require 'sidekiq/testing'
     Sidekiq::Testing.inline!
-  end
-
-  test "should send newsletter" do
     setup_smooch_bot(true)
     create_tipline_subscription team_id: @team.id
     rss = '<rss version="1"><channel><title>x</title><link>x</link><description>x</description><item><title>x</title><link>x</link></item></channel></rss>'
     WebMock.stub_request(:get, 'http://test.com/feed.rss').to_return(status: 200, body: rss)
+  end
+
+  test "should send newsletter" do
     assert_nothing_raised do
       TiplineNewsletterWorker.perform_async(@team.id, 'en')
     end
+  end
+
+  test "should not crash if error happens when sending newsletter to some subscriber" do
+    Bot::Smooch.stubs(:send_final_message_to_user).raises(StandardError)
+    assert_nothing_raised do
+      TiplineNewsletterWorker.perform_async(@team.id, 'en')
+    end
+    Bot::Smooch.unstub(:send_final_message_to_user)
+  end
+
+  test "should skip sending newsletter if content hasn't changed" do
+    assert_equal 1, TiplineNewsletterWorker.new.perform(@team.id, 'en')
+    assert_equal 0, TiplineNewsletterWorker.new.perform(@team.id, 'en')
   end
 end
