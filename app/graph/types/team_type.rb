33b0513571dd0c806dde3e6b0b7e7fe151181a7d
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2022-05-18 09:10:40 +0200

    CHECK-1774: remove tasks (#1088)

--- app/graph/types/team_type.rb
+++ app/graph/types/team_type.rb
@@ -42,7 +42,6 @@ TeamType = GraphqlCrudOperations.define_default_type do
   field :get_fieldsets, JsonStringType
   field :list_columns, JsonStringType
   field :get_data_report_url, types.String
-  field :get_tasks_enabled, types.Boolean
   field :url, types.String
   field :get_tipline_inbox_filters, JsonStringType
   field :get_suggested_matches_filters, JsonStringType
