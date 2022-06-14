2edc54e881dba3e4791531c6cf6edc493f246bdd
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2022-06-14 07:28:22 +0200

    CHECK-1781: should archive target before destory relation (#1122)

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -295,7 +295,7 @@ class RelationshipTest < ActiveSupport::TestCase
     assert r.is_confirmed?
   end
 
-  test "should detach to specific list" do
+  test "should archive detach to specific list" do
     t = create_team
     p = create_project team: t
     p2 = create_project team: t
@@ -304,8 +304,10 @@ class RelationshipTest < ActiveSupport::TestCase
     r = create_relationship source_id: pm_s.id, target_id: pm_t.id, relationship_type: Relationship.confirmed_type
     assert_equal p.id, pm_t.project_id
     r.add_to_project_id = p2.id
+    r.archive_target = CheckArchivedFlags::FlagCodes::SPAM
     r.destroy
     assert_equal p2.id, pm_t.reload.project_id
+    assert_equal CheckArchivedFlags::FlagCodes::SPAM, pm_t.reload.archived
 
   end
 
