9335134ad394b352815f935fc25a04cbbc855333
Caio Almeida <caiosba@gmail.com>
2022-06-19 10:09:23 -0300

    Revert "Revert "Revert "Avoid duplicate Fetch import"""
    This reverts commit deeb8abc85d9f484d52ada59b2dd2e413863a594.

--- db/migrate/20220610222339_add_unique_index_for_fetch_external_id_field.rb
+++
@@ -1,5 +0,0 @@
-class AddUniqueIndexForFetchExternalIdField < ActiveRecord::Migration[5.2]
-  def change
-    execute %{CREATE UNIQUE INDEX fetch_unique_id ON dynamic_annotation_fields (value) WHERE field_name = 'external_id' AND value <> '' AND value <> '""'}
-  end
-end
