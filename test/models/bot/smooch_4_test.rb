c110a3245445a0cbb93a39ca168b200889a43302
Devin Gaffney <itsme@devingaffney.com>
2022-07-26 13:22:16 -0700

    CHECK-2121 Add stub to tests

--- test/models/bot/smooch_4_test.rb
+++ test/models/bot/smooch_4_test.rb
@@ -5,10 +5,12 @@ class Bot::Smooch4Test < ActiveSupport::TestCase
   def setup
     super
     setup_smooch_bot
+    Bot::Alegre.stubs(:request_api).returns({ success: true })
   end
 
   def teardown
     super
+    Bot::Alegre.unstub(:request_api)
     CONFIG.unstub(:[])
     Bot::Smooch.unstub(:get_language)
   end
