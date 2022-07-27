604b164bad93269b6720aad303e98277729a446b
Devin Gaffney <itsme@devingaffney.com>
2022-07-27 08:37:07 -0700

    CHECK-2121 more stubs per test results

--- test/workers/report_designer_worker_test.rb
+++ test/workers/report_designer_worker_test.rb
@@ -11,10 +11,12 @@ class ReportDesignerWorkerTest < ActiveSupport::TestCase
     Sidekiq::Testing.inline!
     r = publish_report
     Bot::Smooch.stubs(:send_report_to_users).once
+    Bot::Alegre.stubs(:request_api).returns({ success: true })
     assert_nothing_raised do
       ReportDesignerWorker.perform_async(r.id, 'publish')
     end
     Bot::Smooch.unstub(:send_report_to_users)
+    Bot::Alegre.unstub(:request_api)
   end
 
   test "should have an interval between retries" do
@@ -23,8 +25,10 @@ class ReportDesignerWorkerTest < ActiveSupport::TestCase
 
   test "should save error after many retries" do
     r = publish_report
+    Bot::Alegre.stubs(:request_api).returns({ success: true })
     assert r.get_field_value('last_error').blank?
     ReportDesignerWorker.retries_exhausted_callback({ 'args' => [r.id], 'error_message' => 'Test' }, StandardError.new)
     assert_match /Test/, r.reload.get_field_value('last_error')
+    Bot::Alegre.unstub(:request_api)
   end
 end
