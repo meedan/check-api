87ef135478b8d33062803ec0979f10d12eee9631
Martin Peck <martin@meedan.com>
2022-07-30 11:53:11 -0700

    Also setup puma for background task.

--- production/bin/start_background.sh
+++ production/bin/start_background.sh
@@ -10,6 +10,18 @@ fi
 # Create configuration from SSM and ENV settings
 bash /opt/bin/create_config.sh
 
+# Setup puma server
+mkdir -p ${PWD}/tmp/pids
+mkdir -p ${PWD}/log
+puma="${PWD}/tmp/puma-${RAILS_ENV}.rb"
+cp config/puma.rb ${puma}
+cat << EOF >> ${puma}
+pidfile '/app/current/tmp/pids/server-${RAILS_ENV}.pid'
+environment '${RAILS_ENV}'
+workers 2
+worker_timeout 120
+EOF
+
 echo "starting static files server in background"
 bundle exec ruby bin/static-files-server &
 
