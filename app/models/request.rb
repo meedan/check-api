ec4d66ca0a2365dbb48a314d3c110613d6b1d8cd
Caio <caiosba@gmail.com>
2022-09-06 20:03:10 -0300

    Fixing request clustering and validation.
    * Cluster requests related to the same media
    * Validate that the `request_type` is valid
    
    Fixes CHECK-2253.

--- app/models/request.rb
+++ app/models/request.rb
@@ -8,6 +8,8 @@ class Request < ApplicationRecord
   after_commit :send_to_alegre, on: :create
   after_commit :update_fields, on: :update
 
+  validates_inclusion_of :request_type, in: ['audio', 'video', 'image', 'text']
+
   def similarity_threshold
     0.85 # FIXME: Adjust this value for text and image (eventually it can be a feed setting)
   end
@@ -21,15 +23,17 @@ class Request < ApplicationRecord
     similar_request_id = nil
     context = { feed_id: self.feed_id }
     threshold = self.similarity_threshold
-    if media.type == 'Claim' && ::Bot::Alegre.get_number_of_words(media.quote) > 3
-      params = { text: media.quote, threshold: threshold, context: context }
-      similar_request_id = ::Bot::Alegre.request_api('get', '/text/similarity/', params).dig('result').to_a.collect{ |result| result.dig('_source', 'context', 'request_id').to_i }.find{ |id| id != 0 && id != self.id }
-    elsif ['UploadedImage', 'UploadedAudio', 'UploadedVideo'].include?(media.type)
-      type = media.type.gsub(/^Uploaded/, '').downcase
-      params = { url: media.file.file.public_url, threshold: threshold, context: context }
-      similar_request_id = ::Bot::Alegre.request_api('get', "/#{type}/similarity/", params).dig('result').to_a.collect{ |result| result.dig('context').to_a.collect{ |c| c['request_id'].to_i } }.flatten.find{ |id| id != 0 && id != self.id }
-    elsif media.type == 'Link'
-      similar_request_id = Request.where(media_id: media.id, feed_id: self.feed_id).where.not(id: self.id).order('id ASC').first
+    # First try to find an identical media
+    similar_request_id = Request.where(media_id: media.id, feed_id: self.feed_id).where.not(id: self.id).order('id ASC').first
+    if similar_request_id.nil?
+      if media.type == 'Claim' && ::Bot::Alegre.get_number_of_words(media.quote) > 3
+        params = { text: media.quote, threshold: threshold, context: context }
+        similar_request_id = ::Bot::Alegre.request_api('get', '/text/similarity/', params).dig('result').to_a.collect{ |result| result.dig('_source', 'context', 'request_id').to_i }.find{ |id| id != 0 && id != self.id }
+      elsif ['UploadedImage', 'UploadedAudio', 'UploadedVideo'].include?(media.type)
+        type = media.type.gsub(/^Uploaded/, '').downcase
+        params = { url: media.file.file.public_url, threshold: threshold, context: context }
+        similar_request_id = ::Bot::Alegre.request_api('get', "/#{type}/similarity/", params).dig('result').to_a.collect{ |result| result.dig('context').to_a.collect{ |c| c['request_id'].to_i } }.flatten.find{ |id| id != 0 && id != self.id }
+      end
     end
     unless similar_request_id.blank?
       similar_request = Request.where(id: similar_request_id, feed_id: self.feed_id).last
