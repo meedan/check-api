02c4216921216c2f24a8b8b84bd82deb63a24a82
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2022-09-15 14:55:22 +0200

    CHECK-2310: add fact check published at column (#1211)
    * CHECK-2310: add fact check published at column
    
    * CHECK-2310: re-use fact_check_published_on method
    
    * CHECK-2310: use timestamp for published at date

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -2356,7 +2356,7 @@ class TeamTest < ActiveSupport::TestCase
     t = create_team
     2.times { create_team_task(team_id: t.id, fieldset: 'metadata', associated_type: 'ProjectMedia') }
     2.times { create_team_task(team_id: t.id, fieldset: 'metadata', associated_type: 'Source') }
-    assert_equal 22, t.list_columns.size
+    assert_equal 23, t.list_columns.size
   end
 
   test "should match rule by title with spaces" do
