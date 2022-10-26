6fa8cf4d4d81f1779202bcad422f9e226b2e4d63
Caio <caiosba@gmail.com>
2022-10-26 12:26:02 -0300

    Revert "Adding a rake task to output fact-checks published and requested for a set of workspaces in a time frame."
    This reverts commit 962e2e6c967ccbdcc3561f98edad61fdbe161e78.

--- app/graph/types/feed_type.rb
+++ app/graph/types/feed_type.rb
@@ -18,21 +18,9 @@ FeedType = GraphqlCrudOperations.define_default_type do
     argument :offset, types.Int
     argument :sort, types.String
     argument :sort_type, types.String
-    # Filters
-    argument :medias_count_min, types.Int
-    argument :medias_count_max, types.Int
 
     resolve ->(feed, args, _ctx) {
       request_id = (args['request_id'].to_i == 0 ? nil : args['request_id'].to_i)
-
-      query = Request.where(request_id: request_id, feed_id: feed.id)
-      query = query.or(Request.where(id: request_id, feed_id: feed.id)) unless request_id.nil?
-
-      # Filters
-      query = query.where('medias_count >= ?', args['medias_count_min'].to_i) unless args['medias_count_min'].blank?
-      query = query.where('medias_count <= ?', args['medias_count_max'].to_i) unless args['medias_count_max'].blank?
-
-      # Sort
       sort = {
         'requests' => 'requests_count',
         'medias' => 'medias_count',
@@ -43,8 +31,9 @@ FeedType = GraphqlCrudOperations.define_default_type do
         'fact_checks' => 'project_medias_count'
       }[args['sort'].to_s] || 'last_submitted_at'
       sort_type = args['sort_type'].to_s.downcase == 'asc' ? 'ASC' : 'DESC'
+      query = Request.where(request_id: request_id, feed_id: feed.id)
       query = query.joins(:media) if sort == 'medias.type'
-
+      query = query.or(Request.where(id: request_id, feed_id: feed.id)) unless request_id.nil?
       query.order(sort => sort_type).offset(args['offset'].to_i)
     }
   end
