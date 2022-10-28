aad14346eaa23ebb2ed3f6705020c38860d420dd
Caio Almeida <caiosba@gmail.com>
2022-10-27 23:10:17 -0300

    Feed filters
    * All GraphQL connections have a `totalCount` field now
    * Adding some filters to the `feed.requests` connection, as GraphQL arguments
    * Refactored feed search logic by moving it to the model
    * Refactored search by date logic as a helper used by both the `Feed` model and the `CheckSearch` library
    
    Reference: CHECK-2518.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,4 +1,6 @@
 class Feed < ApplicationRecord
+  include SearchHelper
+
   check_settings
 
   has_many :requests
@@ -54,6 +56,43 @@ class Feed < ApplicationRecord
   def item_belongs_to_feed?(pm)
     items = self.project_media_ids(pm.team_id)
     items.include?(pm.id)
+  end
+
+  def search(args = {})
+    request_id = (args['request_id'].to_i == 0 ? nil : args['request_id'].to_i)
+
+    query = Request.where(request_id: request_id, feed_id: self.id)
+    query = query.or(Request.where(id: request_id, feed_id: self.id)) unless request_id.nil?
+
+    # Filters
+    {
+      'medias_count_min' => 'medias_count >= ?',
+      'medias_count_max' => 'medias_count <= ?',
+      'requests_count_min' => 'requests_count >= ?',
+      'requests_count_max' => 'requests_count <= ?'
+    }.each do |key, condition|
+      query = query.where(condition, args[key].to_i) unless args[key].blank?
+    end
+    query = query.where('requests.created_at' => Range.new(*format_times_search_range_filter(JSON.parse(args['request_created_at']), nil))) unless args['request_created_at'].blank?
+    query = query.where('requests.content ILIKE ?', "%#{args['keyword']}%") unless args['keyword'].blank?
+    query = query.joins(:project_media_requests) if args['fact_checked_by'].to_s == 'ANY'
+    query = query.joins(:project_medias).where('project_medias.team_id' => Team.current&.id&.to_i) if args['fact_checked_by'].to_s == 'MINE'
+    query = query.left_joins(:project_media_requests).where(project_media_requests: { id: nil }) if args['fact_checked_by'].to_s == 'NONE'
+
+    # Sort
+    sort = {
+      'requests' => 'requests_count',
+      'medias' => 'medias_count',
+      'last_submitted' => 'last_submitted_at',
+      'subscriptions' => 'subscriptions_count',
+      'media_type' => 'medias.type',
+      'fact_checked_by' => 'fact_checked_by_count',
+      'fact_checks' => 'project_medias_count'
+    }[args['sort'].to_s] || 'last_submitted_at'
+    sort_type = args['sort_type'].to_s.downcase == 'asc' ? 'ASC' : 'DESC'
+    query = query.joins(:media) if sort == 'medias.type'
+
+    query.order(sort => sort_type).offset(args['offset'].to_i).distinct('requests.id')
   end
 
   # This takes some time to run because it involves external HTTP requests and writes to the database:
--- app/models/feed_team.rb
+++ app/models/feed_team.rb
@@ -4,6 +4,11 @@ class FeedTeam < ApplicationRecord
   belongs_to :team
   belongs_to :feed
 
+  def requests_filters=(filters)
+    filters = filters.is_a?(String) ? JSON.parse(filters) : filters
+    self.send(:set_requests_filters, filters)
+  end
+
   def sharing_enabled?
     self.shared
   end
