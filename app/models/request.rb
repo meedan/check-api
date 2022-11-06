180a2cf3fd3d1502ba2ee133dcfb4824a380a92f
computermacgyver <computermacgyver>
2022-11-06 16:46:21 +0000

    add missing self. name to new similarity_models_and_thresholds method

--- app/models/request.rb
+++ app/models/request.rb
@@ -36,7 +36,7 @@ class Request < ApplicationRecord
     similar_request_id = Request.where(media_id: media.id, feed_id: self.feed_id).where.not(id: self.id).order('id ASC').first
     if similar_request_id.nil?
       if media.type == 'Claim' && ::Bot::Alegre.get_number_of_words(media.quote) > 1
-        params = { text: media.quote, models: similarity_models_and_thresholds.keys(), per_model_threshold: similarity_models_and_thresholds, context: context }
+        params = { text: media.quote, models: self.similarity_models_and_thresholds.keys(), per_model_threshold: self.similarity_models_and_thresholds, context: context }
         similar_request_id = ::Bot::Alegre.request_api('get', '/text/similarity/', params)&.dig('result').to_a.collect{ |result| result&.dig('_source', 'context', 'request_id').to_i }.find{ |id| id != 0 && id != self.id }
       elsif ['UploadedImage', 'UploadedAudio', 'UploadedVideo'].include?(media.type)
         type = media.type.gsub(/^Uploaded/, '').downcase
@@ -172,7 +172,7 @@ class Request < ApplicationRecord
       params = {
         doc_id: doc_id,
         text: text,
-        models: similarity_models_and_thresholds.keys(),
+        models: self.similarity_models_and_thresholds.keys(),
         context: context
       }
       ::Bot::Alegre.request_api('post', '/text/similarity/', params)
