a1975f1ba33efd0533733b9f7e6378cc574874ab
Caio <caiosba@gmail.com>
2022-11-14 13:52:21 -0300

    Fixing flaky coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,7 +12,8 @@ unless ARGV.include?('-n')
       (!file.filename.match(/\/app\/controllers\/[^\/]+\.rb$/).nil? && file.filename.match(/application_controller\.rb$/).nil?) ||
       !file.filename.match(/\/app\/controllers\/concerns\/[^\/]+_doc\.rb$/).nil? ||
       !file.filename.match(/\/lib\/sample_data\.rb$/).nil? ||
-      !file.filename.match(/\/app\/graph\/types\/mutation_type\.rb$/).nil?
+      !file.filename.match(/\/app\/graph\/types\/mutation_type\.rb$/).nil? ||
+      !file.filename.match(/\/app\/graphql\/types\/mutation_type\.rb$/).nil?
     end
     coverage_dir 'coverage'
   end
