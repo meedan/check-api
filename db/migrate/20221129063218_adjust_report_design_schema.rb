ae46d7df0b7f99f55de027a386100dffa05d6b24
Sawy <melsawy.be7eri@gmail.com>
2022-12-01 11:08:24 +0200

    CHECK-1900: add rake task to migrate report_design annotations

--- db/migrate/20221129063218_adjust_report_design_schema.rb
+++ db/migrate/20221129063218_adjust_report_design_schema.rb
@@ -1,30 +1,12 @@
 class AdjustReportDesignSchema < ActiveRecord::Migration[5.2]
   def change
-    RequestStore.store[:skip_rules] = true
-
     at = DynamicAnnotation::AnnotationType.where(annotation_type: 'report_design').last
     unless at.nil?
       json_schema = at.json_schema.clone.with_indifferent_access
       options_schema = json_schema[:properties][:options][:items]
       json_schema[:properties][:options] = options_schema
-      
       at.json_schema = json_schema
       at.save!
-
-      # We need to do this in the migration, otherwise the app can be inconsistent
-
-      n = Dynamic.where(annotation_type: 'report_design').count
-      i = 0
-      Dynamic.where(annotation_type: 'report_design').find_each do |report|
-        i += 1
-        puts "[#{Time.now}] (#{i}/#{n}) Updating report with ID #{report.id}..."
-        data = report.data.with_indifferent_access
-        data[:options] = data[:options].first
-        report.data = data
-        report.save!
-      end
     end
-
-    RequestStore.store[:skip_rules] = false
   end
 end
