9c6c4feffef560823335e92f63478a59a46fe7c2
danielevalverde <danierzevalverde@gmail.com>
2022-12-20 17:52:35 -0300

    try to fix issue 'detected dubious ownership in repository at /app'

--- test/test-coverage.sh
+++ test/test-coverage.sh
@@ -3,7 +3,9 @@
 apt-get install -y golang-go golang-glide
 printf '#!/bin/bash\ngo run /tmp/go/src/github.com/codeclimate/test-reporter/main.go $@\n' > test/cc-test-reporter && chmod +x test/cc-test-reporter
 export GOPATH=/tmp/go
+git config --global --add safe.directory /app
 mkdir -p /tmp/go/src/github.com/codeclimate
+# git config --global --add safe.directory '*'
 git clone -b 0.10.3 https://github.com/codeclimate/test-reporter /tmp/go/src/github.com/codeclimate/test-reporter
 ./test/format-coverage.sh 
 echo 'Combining and uploading coverage...' && cd test && ./sum-upload-coverage.sh && cd -
