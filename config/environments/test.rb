251811df40ccdb71eb38cfe077dd65f8657d5698
Caio <caiosba@gmail.com>
2022-12-22 21:08:45 -0300

    Changing how tipline newsletter statistics are calculated.
    * From now on, number of new newsletter subscriptions should be calculated from `versions`, otherwise users that subscribed but later on unsubscribed wouldn't be included
    * Upgrading PaperTrail gem to the latest version for Rails 5 (13.0)
    * Save a version when a `TiplineSubscription` is created
    * Highlight code that triggered database queries in test environment logs
    * Do not delete versions when a `TiplineSubscription` instance is deleted from the database
    
    Fixes CHECK-2859.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,7 @@ Rails.application.configure do
 
   # https://guides.rubyonrails.org/caching_with_rails.html#cache-stores
   config.cache_store = :file_store, "#{Rails.root}/tmp/cache#{ENV['TEST_ENV_NUMBER']}"
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
 end
