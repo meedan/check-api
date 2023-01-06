5aca09d063927b0e4a93b98ce4ab63926a6010b7
Caio <caiosba@gmail.com>
2023-01-05 22:50:04 -0300

    Fixing Code Climate issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_29_063218) do
+ActiveRecord::Schema.define(version: 2023_01_03_125636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
