f2836dd3d4740c01bfc13350f5fca1c8205c5e5a
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2023-02-02 16:30:11 -0300

    Use language names as defined in CLDR data (#1388)
    Don't programatically capitalize language names. Use them as defined in CLDR data. (/data/ ... / languages.yml)
    
    Fixes CV2-2670

--- test/lib/check_cldr_test.rb
+++ test/lib/check_cldr_test.rb
@@ -6,11 +6,11 @@ class CheckCldrTest < ActiveSupport::TestCase
   end
 
   test "should return localized language name" do
-    assert_equal 'Francês', CheckCldr.language_code_to_name('fr', 'pt')
+    assert_equal 'francês', CheckCldr.language_code_to_name('fr', 'pt')
     assert_equal 'xx', CheckCldr.language_code_to_name('xx', 'pt')
     assert_equal 'भोजपुरी', CheckCldr.language_code_to_name('bho', 'bho')
     I18n.expects(:locale).returns(:es)
-    assert_equal 'Francés', CheckCldr.language_code_to_name('fr')
+    assert_equal 'francés', CheckCldr.language_code_to_name('fr')
     I18n.unstub(:locale)
   end
 
