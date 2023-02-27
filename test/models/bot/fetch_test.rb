e4d1ba41df71bc2b61e6c1a50114122caeed5ca1
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2023-02-27 21:29:12 +0200

    CV2-2798: fix cached fields for imported items (#1417)

--- test/models/bot/fetch_test.rb
+++ test/models/bot/fetch_test.rb
@@ -218,4 +218,15 @@ class Bot::FetchTest < ActiveSupport::TestCase
     fc = cd.fact_check
     assert_equal 'fr', fc.language
   end
+
+  test "cache item title for imported items" do
+    RequestStore.store[:skip_cached_field_update] = false
+    text = "Earth isn't flat"
+    Bot::Fetch::Import.delay(retry: 0).import_claim_reviews(@installation.id)
+    pm = ProjectMedia.last
+    assert_equal text, pm.title
+    assert_equal text, pm.fact_check_title
+    assert_equal text, pm.title(true)
+    assert_equal text, pm.fact_check_title(true)
+  end
 end
