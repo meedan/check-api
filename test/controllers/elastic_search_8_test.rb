0e24f434ea02077aefd3f292512c3d226afb249a
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2023-03-09 11:51:11 +0200

    CV2-2699: improve automated tests (#1427)

--- test/controllers/elastic_search_8_test.rb
+++ test/controllers/elastic_search_8_test.rb
@@ -6,52 +6,52 @@ class ElasticSearch8Test < ActionController::TestCase
     setup_elasticsearch
   end
 
-  # FIXME : These three tests are taking 15 minutes to run. we should refactor to reduce the runtime
-  # Reference: CV2-2699
-
-  [:linked_items_count, :suggestions_count, :demand].each do |field|
-    test "should filter by #{field} numeric range" do
-      RequestStore.store[:skip_cached_field_update] = false
-      p = create_project
+  test "should filter by linked_items_count, suggestions_count and demand numeric range" do
+    RequestStore.store[:skip_cached_field_update] = false
+    p = create_project
+    [:linked_items_count, :suggestions_count, :demand].each do |field|
       query = { projects: [p.id], "#{field}": { max: 5 } }
       query[field][:min] = 0
       result = CheckSearch.new(query.to_json, nil, p.team_id)
       assert_equal 0, result.medias.count
-      pm1 = create_project_media project: p, quote: 'Test A', disable_es_callbacks: false
-      pm2 = create_project_media project: p, quote: 'Test B', disable_es_callbacks: false
-      pm3 = create_project_media project: p, quote: 'Test C', disable_es_callbacks: false
+    end
+    pm1 = create_project_media project: p, quote: 'Test A', disable_es_callbacks: false
+    pm2 = create_project_media project: p, quote: 'Test B', disable_es_callbacks: false
+    pm3 = create_project_media project: p, quote: 'Test C', disable_es_callbacks: false
 
-      # Add linked items
-      t_pm2 = create_project_media project: p
-      t_pm3 = create_project_media project: p
-      t2_pm3 = create_project_media project: p
-      create_relationship source_id: pm2.id, target_id: t_pm2.id, relationship_type: Relationship.confirmed_type
-      create_relationship source_id: pm3.id, target_id: t_pm3.id, relationship_type: Relationship.confirmed_type
-      create_relationship source_id: pm3.id, target_id: t2_pm3.id, relationship_type: Relationship.confirmed_type
+    # Add linked items
+    t_pm2 = create_project_media project: p
+    t_pm3 = create_project_media project: p
+    t2_pm3 = create_project_media project: p
+    create_relationship source_id: pm2.id, target_id: t_pm2.id, relationship_type: Relationship.confirmed_type
+    create_relationship source_id: pm3.id, target_id: t_pm3.id, relationship_type: Relationship.confirmed_type
+    create_relationship source_id: pm3.id, target_id: t2_pm3.id, relationship_type: Relationship.confirmed_type
 
-      # Add suggested items
-      t_pm2 = create_project_media project: p, quote: 'Test D', disable_es_callbacks: false
-      t_pm3 = create_project_media project: p, quote: 'Test E', disable_es_callbacks: false
-      t2_pm3 = create_project_media project: p, quote: 'Test F', disable_es_callbacks: false
-      create_relationship source_id: pm2.id, target_id: t_pm2.id, relationship_type: Relationship.suggested_type
-      create_relationship source_id: pm3.id, target_id: t_pm3.id, relationship_type: Relationship.suggested_type
-      create_relationship source_id: pm3.id, target_id: t2_pm3.id, relationship_type: Relationship.suggested_type
+    # Add suggested items
+    t_pm2 = create_project_media project: p, quote: 'Test D', disable_es_callbacks: false
+    t_pm3 = create_project_media project: p, quote: 'Test E', disable_es_callbacks: false
+    t2_pm3 = create_project_media project: p, quote: 'Test F', disable_es_callbacks: false
+    create_relationship source_id: pm2.id, target_id: t_pm2.id, relationship_type: Relationship.suggested_type
+    create_relationship source_id: pm3.id, target_id: t_pm3.id, relationship_type: Relationship.suggested_type
+    create_relationship source_id: pm3.id, target_id: t2_pm3.id, relationship_type: Relationship.suggested_type
 
-      # Add requests
-      create_annotation_type_and_fields('Smooch', { 'Data' => ['JSON', false] })
-      create_dynamic_annotation annotation_type: 'smooch', annotated: pm2
-      2.times { create_dynamic_annotation(annotation_type: 'smooch', annotated: pm3) }
+    # Add requests
+    create_annotation_type_and_fields('Smooch', { 'Data' => ['JSON', false] })
+    create_dynamic_annotation annotation_type: 'smooch', annotated: pm2
+    2.times { create_dynamic_annotation(annotation_type: 'smooch', annotated: pm3) }
 
-      sleep 2
+    sleep 2
 
-      min_mapping = {
-        "0": [pm1.id, pm2.id, pm3.id, t_pm2.id, t_pm3.id, t2_pm3.id],
-        "1": [pm2.id, pm3.id],
-        "2": [pm3.id],
-        "3": [],
-      }
+    min_mapping = {
+      "0": [pm1.id, pm2.id, pm3.id, t_pm2.id, t_pm3.id, t2_pm3.id],
+      "1": [pm2.id, pm3.id],
+      "2": [pm3.id],
+      "3": [],
+    }
 
-      # query with numeric range only
+    # query with numeric range only
+    [:linked_items_count, :suggestions_count, :demand].each do |field|
+      query = { projects: [p.id], "#{field}": { max: 5 } }
       min_mapping.each do |min, items|
         query[field][:min] = min.to_s
         result = CheckSearch.new(query.to_json, nil, p.team_id)
