a3565d83d55c91a4c9265edaf61e7b442f2457a1
Sawy <melsawy.be7eri@gmail.com>
2023-03-20 20:08:52 +0200

    CV2-2812: fix CC

--- lib/check_cached_fields.rb
+++ lib/check_cached_fields.rb
@@ -70,7 +70,7 @@ module CheckCachedFields
         value = target.send(update_index, value) if update_index.is_a?(Symbol) && target.respond_to?(update_index)
         field_name = options[:es_field_name] || name
         es_options = { keys: [field_name], data: { field_name => value } }
-        # es_options[:pm_id] = target.id if target.class.name == 'ProjectMedia'
+        es_options[:pm_id] = target.id if target.class.name == 'ProjectMedia'
         model = { klass: target.class.name, id: target.id }
         ElasticSearchWorker.new.perform(YAML::dump(model), YAML::dump(es_options), 'update_doc')
       end
