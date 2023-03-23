b0b5be1191cc3792f31a7582a3d812a3c28fdda8
manu vasconcelos <manu@meedan.com>
2023-03-23 17:59:23 -0300

    Populate Check API's `seeds.rb` file
    Right now I'm only creating a user, team and medias.
    
    I was using the methods from sample data before, but for
    what we need we might want to go with just create itself.
    Those methods were creating an account with a fake url and that
    couldn't be validated by pender. If we just create the a media
    instance with a working url it gets the account from there.
    
    For all of that to work we need real urls for the media.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+include SampleData
+require "faker"
+
+
+p "...Seeding..."
+p "Making Team..."
+team = create_team(name: Faker::Company.name)
+
+p "Making User..."
+user_name = Faker::Name.first_name.downcase
+user_password = Faker::Internet.password(min_length: 8)
+
+user = create_user(name: user_name, login: user_name, password: user_password, password_confirmation: user_password, email: Faker::Internet.safe_email(name: user_name), is_admin: true)
+
+p "Making Project..."
+project = create_project(title: team.name, team_id: team.id, user: user, description: '') 
+
+p "Making Team User..."
+create_team_user(team: team, user: user, role: "admin")
+
+p "Making Medias..."
+link_media_link = 'https://meedan.com/post/exploring-the-use-of-offline-games-for-media-literacy-and-misinformation-education'
+
+claim_media = create_claim_media(user_id: user.id, quote: Faker::Quotes::Shakespeare.hamlet_quote)
+link_media = Link.create!(user_id: user.id, url: link_media_link)
+audio_media = UploadedAudio.create!(user_id: user.id, file: File.open(File.join(Rails.root, 'test', 'data', 'rails.mp3')))
+image_media = UploadedImage.create!(user_id: user.id, file: File.open(File.join(Rails.root, 'test', 'data', 'rails.png')))
+video_media = UploadedVideo.create!(user_id: user.id, file: File.open(File.join(Rails.root, 'test', 'data', 'rails.mp4')))
+
+[claim_media, link_media, audio_media, image_media, video_media].each { |claim_type| create_project_media(user_id: user.id, project: project, team: team, media: claim_type) }
+
+p "Created — user: #{user_name} — email: #{user.email} — password : #{user_password}"
+
