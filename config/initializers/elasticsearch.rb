111f74ed86b82790c225f49b8dcebc0f6e7b3d99
Martin Peck <martin@meedan.com>
2023-03-29 19:06:50 -0700

    Use common URL syntax for ElasticSearch initialization within ENV.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,10 +1,7 @@
 require 'typhoeus'
 require 'typhoeus/adapters/faraday'
 
-user = CheckConfig.get('elasticsearch_user').to_s
-password = CheckConfig.get('elasticsearch_password').to_s
-host = CheckConfig.get('elasticsearch_host').to_s + ':' + CheckConfig.get('elasticsearch_port').to_s
-client = Elasticsearch::Client.new(log: (CheckConfig.get('elasticsearch_log').to_i == 1), user: user, password: password, host: host) do |f|
+client = Elasticsearch::Client.new(log: (CheckConfig.get('elasticsearch_log').to_i == 1)) do |f|
   f.adapter :typhoeus
 end
 
