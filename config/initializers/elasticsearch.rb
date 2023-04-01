4386578636edb6e78ebf744f842cd0938a42ce5d
Martin Peck <martin@meedan.com>
2023-04-01 14:04:18 -0700

    Support legacy parameterized ElasticSearch configuration.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,8 +1,18 @@
 require 'typhoeus'
 require 'typhoeus/adapters/faraday'
 
-client = Elasticsearch::Client.new(log: (CheckConfig.get('elasticsearch_log').to_i == 1)) do |f|
-  f.adapter :typhoeus
+if ENV['ELASTICSEARCH_URL'].is_set?
+  # NOTE: We may want to initialize explicitly with url: set for future compatibility.
+  client = Elasticsearch::Client.new(log: (CheckConfig.get('elasticsearch_log').to_i == 1)) do |f|
+    f.adapter :typhoeus
+  end
+else
+  user = CheckConfig.get('elasticsearch_user').to_s
+  password = CheckConfig.get('elasticsearch_password').to_s
+  host = CheckConfig.get('elasticsearch_host').to_s + ':' + CheckConfig.get('elasticsearch_port').to_s
+  client = Elasticsearch::Client.new(log: (CheckConfig.get('elasticsearch_log').to_i == 1), user: user, password: password, host: host) do |f|
+    f.adapter :typhoeus
+  end
 end
 
 $repository = MediaSearch.new(client: client)
