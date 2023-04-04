f936834aa7c8b1f5aa645b79fc0a4970788414b3
manu vasconcelos <manu@meedan.com>
2023-04-04 10:57:09 -0300

    change create_project_media to ProjectMedia.create!
    create_project_media was creating a bunch of extra users, will
    look into it

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ audio_media = UploadedAudio.create!(user_id: user.id, file: File.open(File.join(
 image_media = UploadedImage.create!(user_id: user.id, file: File.open(File.join(Rails.root, 'test', 'data', 'rails.png')))
 video_media = UploadedVideo.create!(user_id: user.id, file: File.open(File.join(Rails.root, 'test', 'data', 'rails.mp4')))
 
-[claim_media, link_media, audio_media, image_media, video_media].each { |claim_type| create_project_media(user_id: user.id, project: project, team: team, media: claim_type) }
+[claim_media, link_media, audio_media, image_media, video_media].each { |claim_type| ProjectMedia.create!(user_id: user.id, project: project, team: team, media: claim_type) }
 
 p 'Making claim description and fact_check...'
 project_media = ProjectMedia.create!(project: project, team: team, media: Blank.create!)
@@ -37,16 +37,19 @@ claim_description = ClaimDescription.create!(description: Faker::Lorem.paragraph
 fact_check_media = create_fact_check(summary: Faker::Company.catch_phrase, url: fact_check_link, title: Faker::Company.name, user: user, claim_description: claim_description)
 
 p 'Making Relationship...'
+
+# ['Garlic can help you fight covid', 'Garlic can help you fight covid', 'Tea with garlic is a covid treatment', 'Tea with garlic is a covid treatment', Faker::Quotes::Shakespeare.hamlet_quote]
+
 # claims
 claim_media_1 = create_claim_media(user_id: user.id, quote: 'Garlic can help you fight covid')
 claim_media_2 = create_claim_media(user_id: user.id, quote: 'Garlic can help you fight covid')
 claim_media_3 = create_claim_media(user_id: user.id, quote: 'Tea with garlic is a covid treatment')
 claim_media_4 = create_claim_media(user_id: user.id, quote: 'Tea is a great covid treatment')
 
-project_media_1 = create_project_media(user_id: user.id, project: project, team: team, media: claim_media_1) 
-project_media_2 = create_project_media(user_id: user.id, project: project, team: team, media: claim_media_2) 
-project_media_3 = create_project_media(user_id: user.id, project: project, team: team, media: claim_media_3) 
-project_media_4 = create_project_media(user_id: user.id, project: project, team: team, media: claim_media_4) 
+project_media_1 = ProjectMedia.create!(user_id: user.id, project: project, team: team, media: claim_media_1) 
+project_media_2 = ProjectMedia.create!(user_id: user.id, project: project, team: team, media: claim_media_2) 
+project_media_3 = ProjectMedia.create!(user_id: user.id, project: project, team: team, media: claim_media_3) 
+project_media_4 = ProjectMedia.create!(user_id: user.id, project: project, team: team, media: claim_media_4) 
 
 create_relationship(source_id: project_media_1.id, target_id: project_media_2.id, relationship_type: Relationship.confirmed_type)
 create_relationship(source_id: project_media_3.id, target_id: project_media_4.id, relationship_type: Relationship.suggested_type)
@@ -55,8 +58,8 @@ create_relationship(source_id: project_media_3.id, target_id: project_media_4.id
 image_media_1 = UploadedImage.create!(user_id: user.id, file: File.open(File.join(Rails.root, 'test', 'data', 'rails.png')))
 image_media_2 = UploadedImage.create!(user_id: user.id, file: File.open(File.join(Rails.root, 'test', 'data', 'rails.png')))
 
-project_media_5 = create_project_media(user_id: user.id, project: project, team: team, media: image_media_1) 
-project_media_6 = create_project_media(user_id: user.id, project: project, team: team, media: image_media_2) 
+project_media_5 = ProjectMedia.create!(user_id: user.id, project: project, team: team, media: image_media_1) 
+project_media_6 = ProjectMedia.create!(user_id: user.id, project: project, team: team, media: image_media_2) 
 
 create_relationship(source_id: project_media_5.id, target_id: project_media_6.id, relationship_type: Relationship.confirmed_type)
 
@@ -64,8 +67,8 @@ create_relationship(source_id: project_media_5.id, target_id: project_media_6.id
 audio_media_1 = UploadedAudio.create!(user_id: user.id, file: File.open(File.join(Rails.root, 'test', 'data', 'rails.mp3')))
 audio_media_2 = UploadedAudio.create!(user_id: user.id, file: File.open(File.join(Rails.root, 'test', 'data', 'rails.mp3')))
 
-project_media_7 = create_project_media(user_id: user.id, project: project, team: team, media: audio_media_1) 
-project_media_8 = create_project_media(user_id: user.id, project: project, team: team, media: audio_media_2) 
+project_media_7 = ProjectMedia.create!(user_id: user.id, project: project, team: team, media: audio_media_1) 
+project_media_8 = ProjectMedia.create!(user_id: user.id, project: project, team: team, media: audio_media_2) 
 
 create_relationship(source_id: project_media_7.id, target_id: project_media_8.id)
 
