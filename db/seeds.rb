9198f6928fafd75068b9e042815ae6e7a95e63b4
manu vasconcelos <manu@meedan.com>
2023-04-11 10:56:15 -0300

    create tipline requests with different degrees of completeness
    same as we did before with media:
    - medias with tipline requests that have both claim_description and fact_check
    - medias with tipline requests that have only claim_description
    - medias with tipline requests that don't have either

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,33 +8,45 @@ data = {
     user_name: Faker::Name.first_name.downcase,
     user_password: Faker::Internet.password(min_length: 8),
     link_media_links: [
-        'https://meedan.com/post/q-a-with-watching-western-sahara-bringing-one-of-the-worlds-most-invisible-crises-to-the-spotlight-through-citizen-journalism-and-cinema', 
-        'https://meedan.com/post/q-a-with-meedan-partner-digital-rights-foundation-on-digital-rights-and-gender-violence-in-south-asia', 
-        'https://meedan.com/post/meedan-supports-hyperlocal-efforts-to-tackle-climate-misinformation',
-        'https://meedan.com/post/users-are-moving-to-the-decentralized-web-after-musks-twitter-takeover-heres-what-that-means-for-verification',
-        'https://meedan.com/post/five-content-moderation-takeaways-from-elon-musks-twitter-takeover',
-        'https://meedan.com/post/meedan-supports-spanish-language-fact-checking-on-whatsapp-ahead-of-u-s-midterms',
-        'https://meedan.com/post/save-alaa-abdel-fatah',
-        'https://meedan.com/post/announcing-the-2021-meedan-annual-report',
-        'https://meedan.com/post/meedan-and-national-democratic-institute-release-recommendations-for-ending-online-violence-against-women-in-politics',
-        'https://meedan.com/post/q-a-with-cite-a-zimbabwean-innovation-hub-tracking-promises-by-politicians'
+        # 'https://meedan.com/post/q-a-with-watching-western-sahara-bringing-one-of-the-worlds-most-invisible-crises-to-the-spotlight-through-citizen-journalism-and-cinema', 
+        # 'https://meedan.com/post/q-a-with-meedan-partner-digital-rights-foundation-on-digital-rights-and-gender-violence-in-south-asia', 
+        # 'https://meedan.com/post/meedan-supports-hyperlocal-efforts-to-tackle-climate-misinformation',
+        # 'https://meedan.com/post/users-are-moving-to-the-decentralized-web-after-musks-twitter-takeover-heres-what-that-means-for-verification',
+        # 'https://meedan.com/post/five-content-moderation-takeaways-from-elon-musks-twitter-takeover',
+        # 'https://meedan.com/post/meedan-supports-spanish-language-fact-checking-on-whatsapp-ahead-of-u-s-midterms',
+        # 'https://meedan.com/post/save-alaa-abdel-fatah',
+        # 'https://meedan.com/post/announcing-the-2021-meedan-annual-report',
+        # 'https://meedan.com/post/meedan-and-national-democratic-institute-release-recommendations-for-ending-online-violence-against-women-in-politics',
+        # 'https://meedan.com/post/q-a-with-cite-a-zimbabwean-innovation-hub-tracking-promises-by-politicians',
+        'https://meedan.com/post/addressing-misinformation-across-countries-a-pioneering-collaboration-between-taiwan-factcheck-center-vera-files',
+        'https://meedan.com/post/entre-becos-a-women-led-hyperlocal-newsletter-from-the-peripheries-of-brazil',
+        'https://meedan.com/post/check-global-launches-independent-media-response-fund-tackles-on-climate-misinformation',
+        'https://meedan.com/post/chambal-media',
+        'https://meedan.com/post/application-process-for-the-check-global-independent-media-response-fund',
+        'https://meedan.com/post/fact-checkers-and-their-mental-health-research-work-in-progress',
+        'https://meedan.com/post/meedan-stands-with-rappler-in-the-fight-against-disinformation',
+        'https://meedan.com/post/2022-french-elections-meedan-software-supported-agence-france-presse',
+        'https://meedan.com/post/how-to-write-longform-git-commits-for-better-software-development',
+        'https://meedan.com/post/welcome-smriti-singh-our-research-intern',
+
     ],
     audios: ['e-item.mp3', 'rails.mp3', 'with_cover.mp3', 'with_cover.ogg', 'with_cover.wav'],
     images: ['large-image.jpg', 'maçã.png', 'rails-photo.jpg', 'rails.png', 'rails2.png', 'ruby-big.png', 'ruby-small.png'],
     videos: ['d-item.mp4', 'rails.mp4'],
     fact_check_links: [
-        'https://meedan.com/post/addressing-global-challenges-through-regional-interventions', 
-        'https://meedan.com/post/exploring-the-use-of-offline-games-for-media-literacy-and-misinformation-education', 
-        'https://meedan.com/post/op-ed-heres-what-were-considering-in-the-lead-up-to-the-supreme-courts-decisions-on-the-future-of-the-internet',
-        'https://meedan.com/post/meedan-impact-story-using-ai-to-investigate-weapons-trafficking-and-human-rights-violations',
-        'https://meedan.com/post/meedan-partner-fatabyyano-launches-tipline-for-earthquake-crisis-response',
+        # 'https://meedan.com/post/addressing-global-challenges-through-regional-interventions', 
+        # 'https://meedan.com/post/exploring-the-use-of-offline-games-for-media-literacy-and-misinformation-education', 
+        # 'https://meedan.com/post/op-ed-heres-what-were-considering-in-the-lead-up-to-the-supreme-courts-decisions-on-the-future-of-the-internet',
+        # 'https://meedan.com/post/meedan-impact-story-using-ai-to-investigate-weapons-trafficking-and-human-rights-violations',
+        # 'https://meedan.com/post/meedan-partner-fatabyyano-launches-tipline-for-earthquake-crisis-response',
+        'https://meedan.com/post/welcome-haramoun-hamieh-our-program-manager-for-nawa',
+        'https://meedan.com/post/strengthening-fact-checking-with-media-literacy-technology-and-collaboration',
+        'https://meedan.com/post/highlights-from-the-work-of-meedans-partners-on-international-fact-checking',
+        'https://meedan.com/post/what-is-gendered-health-misinformation-and-why-is-it-an-equity-problem-worth',
+        'https://meedan.com/post/the-case-for-a-public-health-approach-to-moderate-health-misinformation',
     ],
     quotes: ['Garlic can help you fight covid', 'Tea with garlic is a covid treatment', 'If you have covid you should eat garlic', 'Are you allergic to garlic?', 'Vampires can\'t eat garlic']
 }
-
-def open_file(file)
-    File.open(File.join(Rails.root, 'test', 'data', file))
-end
 
 p '...Seeding...'
 p 'Making Team / Workspace...'
@@ -50,12 +62,16 @@ p 'Making Team User...'
 create_team_user(team: team, user: user, role: 'admin')
 
 p 'Making Medias...'
+def open_file(file)
+    File.open(File.join(Rails.root, 'test', 'data', file))
+end
+
 def create_project_medias(user, project, team, n_medias = 9)
     Media.last(n_medias).each { |media| ProjectMedia.create!(user_id: user.id, project: project, team: team, media: media) }
 end
 
 def add_claim_descriptions_and_fact_checks(user,n_project_medias = 6, n_claim_descriptions = 3)
-    ProjectMedia.last(n_project_medias).each { |project_media| ClaimDescription.create!(description: Faker::Lorem.paragraph(sentence_count: 10), context: Faker::Lorem.sentence, user: user, project_media: project_media) }
+    ProjectMedia.last(n_project_medias).each { |project_media| ClaimDescription.create!(description: Faker::Company.catch_phrase, context: Faker::Lorem.sentence, user: user, project_media: project_media) }
     ClaimDescription.last(n_claim_descriptions).each { |claim_description| FactCheck.create!(summary: Faker::Company.catch_phrase, title: Faker::Company.name, user: user, claim_description: claim_description) }
 end
 
@@ -68,6 +84,11 @@ p 'Making Medias and Project Medias: Links...'
 data[:link_media_links].each { |link_media_link| Link.create!(user_id: user.id, url: link_media_link) }
 create_project_medias(user, project, team)
 add_claim_descriptions_and_fact_checks(user)
+
+# if I want to try to add the link to the claim description for links, this might help:
+# uri = URI.parse(Link.last.url)
+# path = uri.path
+# path.remove('/post/').underscore.humanize
 
 p 'Making Medias and Project Medias: Audios...'
 data[:audios].each { |audio| UploadedAudio.create!(user_id: user.id, file: open_file(audio)) }
@@ -100,7 +121,7 @@ def create_blank(project, team)
 end
 
 def create_claim_description(user, project, team)
-    ClaimDescription.create!(description: Faker::Lorem.paragraph(sentence_count: 10), context: Faker::Lorem.sentence, user: user, project_media: create_blank(project, team))
+    ClaimDescription.create!(description: Faker::Company.catch_phrase, context: Faker::Lorem.sentence, user: user, project_media: create_blank(project, team))
 end
 
 data[:fact_check_links].each { |fact_check_link| create_fact_check(fact_check_attributes(fact_check_link, user, project, team)) }
@@ -126,7 +147,7 @@ project_medias_for_audio = []
 Relationship.create!(source_id: project_medias_for_audio[0].id, target_id: project_medias_for_audio[1].id, relationship_type: Relationship.confirmed_type)
 
 p 'Making Tipline requests...'
-10.times do
+9.times do
     claim_media = Claim.create!(user_id: user.id, quote: Faker::Lorem.paragraph(sentence_count: 10))
     project_media = ProjectMedia.create!(project: project, team: team, media: claim_media)
 
@@ -210,6 +231,9 @@ p 'Making Tipline requests...'
     a.set_fields = fields.to_json
     a.save!
 end
+
+add_claim_descriptions_and_fact_checks(user)
+
 
 # ##########
 p "Created — user: #{data[:user_name]} — email: #{user.email} — password : #{data[:user_password]}"
