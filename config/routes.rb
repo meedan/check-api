187c0c32fbf70905339c434f4f36fc6920110e4d
Caio Almeida <117518+caiosba@users.noreply.github.com>
2023-06-24 18:31:08 -0300

    Deleting access control to Sidekiq UI from the application side
    This must be controled by a different layer, for example, DNS, load balancer, etc.
    
    Reference: DEVOPS-417.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/api/graphql'
-
-  authenticate :api_user, -> (user) { user.is_admin } do
-    mount Sidekiq::Web => '/sidekiq'
-  end
+  mount Sidekiq::Web => '/sidekiq'
 
   namespace :api, defaults: { format: 'json' } do
     # Call v2 API by passing header: 'Accept: application/vnd.api+json'
