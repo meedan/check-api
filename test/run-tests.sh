4712ec1f5737a9d86ee9cc8ba0c32c96b0425b73
Christa Hartsock <christa@meedan.com>
2023-06-26 14:10:47 -0700

    Update CI test jobs to not re-run certain tests
    Previously we were accidentally re-running a directory or two across
    test jobs, when we mean to make them unique. This updates so that we only
    run tests once, and also attempts to clarify the usage of the arguments
    passed into our job task. Admittedly, it's still a little confusing.
    
    CV2-3094

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd test && rm -rf $PATTERN && echo 'Running tests:' && ls && cd - && RUBYOPT='-W0' bundle exec rake parallel:test[6]
+cd test && rm -rf $EXCLUDE_DIRECTORIES && echo 'Running tests:' && ls && cd - && RUBYOPT='-W0' bundle exec rake parallel:test[6]
