a9cbf7da286021d5422c5e8da8e160e57cdc042c
Christa Hartsock <christa@meedan.com>
2023-06-29 14:16:35 -0700

    Extract shared behavior for task and annotations fields

--- app/graph/types/inclusions/task_and_annotation_fields.rb
+++ app/graph/types/inclusions/task_and_annotation_fields.rb
@@ -1,59 +1,52 @@
-module TaskAndAnnotationFields
-  # Inject field definitions into class
-  def self.included(base)
-    base.class_eval do
-      # .field_annotations
-      field :annotations,
-            "AnnotationUnion.connection_type",
-            null: true do
+module Types::Inclusions
+  module TaskAndAnnotationFields
+    extend ActiveSupport::Concern
+
+    included do
+      field :annotations, ::AnnotationUnion.connection_type, null: true do
         argument :annotation_type, GraphQL::Types::String, required: true, camelize: false
       end
 
-      # .field_annotations_count
+      def annotations(**args)
+        object.get_annotations(args[:annotation_type].split(",").map(&:strip))
+      end
+
       field :annotations_count, GraphQL::Types::Int, null: true do
         argument :annotation_type, GraphQL::Types::String, required: true, camelize: false
       end
 
-      # .field_tasks
-      field :tasks, "TaskType.connection_type", null: true do
+      def annotations_count(**args)
+        object.get_annotations(args[:annotation_type].split(",").map(&:strip)).count
+      end
+
+      field :tasks, ::TaskType.connection_type, null: true do
         argument :fieldset, GraphQL::Types::String, required: false
       end
+
+      def tasks(**args)
+        tasks =
+          ::Task.where(
+            annotation_type: "task",
+            annotated_type: object.class.name,
+            annotated_id: object.id
+          )
+        tasks = tasks.from_fieldset(args[:fieldset]) unless args["fieldset"].blank?
+        # Order tasks by order field
+        ids = tasks.to_a.sort_by { |task| task.order ||= 0 }.map(&:id)
+        values = []
+        ids.each_with_index { |id, i| values << "(#{id}, #{i})" }
+        return tasks if values.empty?
+        joins =
+          ActiveRecord::Base.send(
+            :sanitize_sql_array,
+            [
+              "JOIN (VALUES %s) AS x(value, order_number) ON %s.id = x.value",
+              values.join(", "),
+              "annotations"
+            ]
+          )
+        tasks.joins(joins).order("x.order_number")
+      end
     end
-  end
-
-  # For field: :tasks, imported by .field_tasks
-  def tasks(**args)
-    tasks =
-      Task.where(
-        annotation_type: "task",
-        annotated_type: object.class.name,
-        annotated_id: object.id
-      )
-    tasks = tasks.from_fieldset(args[:fieldset]) unless args["fieldset"].blank?
-    # Order tasks by order field
-    ids = tasks.to_a.sort_by { |task| task.order ||= 0 }.map(&:id)
-    values = []
-    ids.each_with_index { |id, i| values << "(#{id}, #{i})" }
-    return tasks if values.empty?
-    joins =
-      ActiveRecord::Base.send(
-        :sanitize_sql_array,
-        [
-          "JOIN (VALUES %s) AS x(value, order_number) ON %s.id = x.value",
-          values.join(", "),
-          "annotations"
-        ]
-      )
-    tasks.joins(joins).order("x.order_number")
-  end
-
-  # For field: :annotations, imported by .field_annotations
-  def annotations(**args)
-    object.get_annotations(args[:annotation_type].split(",").map(&:strip))
-  end
-
-  # For field: :annotations_count, imported by .field_annotations_count
-  def annotations_count(**args)
-    object.get_annotations(args[:annotation_type].split(",").map(&:strip)).count
   end
 end
