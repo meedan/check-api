a8e46e5dcce0be214636e4623530fbfa7b333696
Caio <117518+caiosba@users.noreply.github.com>
2023-07-07 16:18:03 -0300

    Ticket CV2-3371: Do not re-fetch articles for the same newsletter object in memory

--- app/models/tipline_newsletter.rb
+++ app/models/tipline_newsletter.rb
@@ -134,6 +134,7 @@ class TiplineNewsletter < ApplicationRecord
   end
 
   def articles
+    return @articles unless @articles.to_a.empty?
     articles = []
     if self.content_type == 'static'
       [:first_article, :second_article, :third_article].each do |article|
@@ -142,13 +143,14 @@ class TiplineNewsletter < ApplicationRecord
     elsif self.content_type == 'rss' && !self.rss_feed_url.blank?
       articles = RssFeed.new(self.rss_feed_url).get_articles(self.number_of_articles)
     end
-    articles.reject{ |article| article.blank? }.first(self.number_of_articles).collect do |article|
+    @articles = articles.reject{ |article| article.blank? }.first(self.number_of_articles).collect do |article|
       if self.team.get_shorten_outgoing_urls || self.content_type == 'rss'
         UrlRewriter.shorten_and_utmize_urls(article, self.team.get_outgoing_urls_utm_code, self)
       else
         article
       end
     end
+    @articles
   end
 
   def body
