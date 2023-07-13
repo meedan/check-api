25bc637a8df28571765b9e7fbe63de1442a3082a
Caio <117518+caiosba@users.noreply.github.com>
2023-07-13 09:58:34 -0300

    Moving line outside the block, fixing array truncation, manual linting and adding comment

--- app/models/concerns/smooch_menus.rb
+++ app/models/concerns/smooch_menus.rb
@@ -108,8 +108,8 @@ module SmoochMenus
               payload: row[:id],
             }
           end
-        actions = actions[..13] if 'LINE'==self.request_platform && actions.length>13
         end
+        actions = actions[..12] if 'LINE' == self.request_platform && actions.length > 13 # LINE supports maximum 13 options
         extra = { actions: actions }
         fallback = [text]
       end
