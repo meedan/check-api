e548d103a97f82a975a2c2a412e5cb982338a1e4
Caio <117518+caiosba@users.noreply.github.com>
2023-07-17 23:30:53 -0300

    Fixing tests

--- app/controllers/api/v1/webhooks_controller.rb
+++ app/controllers/api/v1/webhooks_controller.rb
@@ -13,7 +13,7 @@ module Api
           render_error('Bot not found', 'ID_NOT_FOUND', 404) and return
         end
         bot = bot_name_to_class[params[:name].to_sym]
-        if bot.should_ignore_request?(request)
+        if bot.respond_to?(:should_ignore_request?) && bot.should_ignore_request?(request)
           render_success('ignored') and return
         end
         unless bot.valid_request?(request)
