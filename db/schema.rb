f14ca7b4f4a0274ae56a29b0b623abb9a63c71f8
Christa Hartsock <christa@meedan.com>
2023-07-20 08:06:27 -0700

    Make GraphQL schema generation deterministic and changes trackable (#1585)
    Currently when running `bin/rails lapis:graphql:schema` from a fresh
    database, it includes mutations from ArchiveIs despite the fact that we no
    longer support it and the annotation has been removed from QA and Live. This
    is because that removal was done manually in those environments in a Rails console--
    but we need to add a migation so that our local data also does not have that
    annotation and as a result doesn't add ArchiveIs to the GraphQL schema.
    
    Additionally, this adds a GraphQL IDL file generation to our schema generation
    task, so that we can more easily track changes to the schema via pull request.
    I'll use this in our GraphQL update.
    
    More on that here: https://graphql-ruby.org/testing/schema_structure.html#maintain-a-graphql-schema-dump
    
    The changes are easier to track via IDL than JSON.
    
    The IDL file is written to lib so that it is not autoloaded by the application,
    and not accidentally made public (though this would probably be fine).
    
    CV2-3094

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_18_012453) do
+ActiveRecord::Schema.define(version: 2023_07_19_171626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
