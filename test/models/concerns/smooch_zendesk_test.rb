665241d89fcdee55e5a28d6a53a2721041fb6b7c
Caio <117518+caiosba@users.noreply.github.com>
2023-07-25 10:41:08 -0300

    The "body" of a response of an API call to Smooch with an empty message should be an object, not a string.
    In the method used to send a message to a tipline user on Smooch we return a custom object when the message is blank, since it's not valid. But that message must follow the same format as a response from Smooch API. So, the `body` must return an object, not a string.
    
    Fixes: CV2-3516.

--- test/models/concerns/smooch_zendesk_test.rb
+++ test/models/concerns/smooch_zendesk_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require_relative '../../test_helper'
 
 class FakeSmoochZendesk
   include SmoochZendesk
@@ -27,5 +27,10 @@ class SmoochZendeskTest < ActiveSupport::TestCase
     CheckSentry.expects(:notify).with(mock_error, has_entries(smooch_app_id: 'app-id', uid: 'phone-12345:123456', smooch_body: anything, errors: errors))
 
     FakeSmoochZendesk.zendesk_send_message_to_user('phone-12345:123456', 'fake text')
+  end
+
+  test "should return nil when it tries to get the ID of a message that was not sent because was blank" do
+    response = FakeSmoochZendesk.zendesk_send_message_to_user('abc123', '')
+    assert_nil Bot::Smooch.get_id_from_send_response(response)
   end
 end
