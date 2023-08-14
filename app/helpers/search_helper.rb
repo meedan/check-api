a98482d5784f5646e4f7267c4e404afb92c5f40f
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2023-08-14 20:21:40 +0300

    Cv2 3397 add option more than in date range (#1617)
    * CV2-3397: add more_than date condition
    
    * CV2-3397: add tests

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -8,11 +8,13 @@ module SearchHelper
   end
 
   def get_from_and_to_values(values, tz)
-    # condition_type = 'less_than', 'is_between'
+    # condition_type = 'less_than', 'more_than', 'is_between'
     condition_type = values.dig('condition') || 'is_between'
-    if condition_type == 'less_than'
+    from = nil
+    to = nil
+    if ['less_than', 'more_than'].include?(condition_type)
       period = values.dig('period').to_i
-      from_date = case values.dig('period_type').downcase
+      period_date = case values.dig('period_type').downcase
                   when 'd'
                     Time.now - period.day
                   when 'w'
@@ -22,8 +24,12 @@ module SearchHelper
                   when 'y'
                     Time.now - period.year
                   end
-      from = from_date.blank? ? nil : format_time_with_timezone(from_date.to_s, tz)
-      to = nil
+      condition_date = period_date.blank? ? nil : format_time_with_timezone(period_date.to_s, tz)
+      if condition_type == 'less_than'
+        from = condition_date
+      else
+        to = condition_date
+      end
     else
       from = format_time_with_timezone(values.dig('start_time'), tz)
       to = format_time_with_timezone(values.dig('end_time'), tz)
