14020c38e602780acfe3b913cd00beebc1bd66fc
Caio <117518+caiosba@users.noreply.github.com>
2023-08-23 14:54:22 -0300

    Fixing typo in migration

--- db/migrate/20230823171135_rename_bot_resource_to_tipline_resource.rb
+++ db/migrate/20230823171135_rename_bot_resource_to_tipline_resource.rb
@@ -1,5 +1,5 @@
 class RenameBotResourceToTiplineResource < ActiveRecord::Migration[6.1]
   def change
-    rename_table :bot_resources, :tipline_resource
+    rename_table :bot_resources, :tipline_resources
   end
 end
