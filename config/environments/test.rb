cb538226e386345ea642339d29793aa97142ebd4
Caio <117518+caiosba@users.noreply.github.com>
2023-08-25 16:56:03 -0300

    Enable PaperTrail for the tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
   # Disable PaperTrail by default on tests
   # https://github.com/paper-trail-gem/paper_trail#7-testing
   config.after_initialize do
-    PaperTrail.enabled = ENV['PAPERTRAIL_ENABLED'] || false
+    PaperTrail.enabled = true # ENV['PAPERTRAIL_ENABLED'] || false
   end
 end
