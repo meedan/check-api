29a4395a8e4d42f9d5d3cdd29e6eed33f0dd4793
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2023-08-25 10:34:06 +0300

    CV2-3469: set media slug for all items (#1627)
    * CV2-3469: set media slug for all items
    
    * CV2-3469: add unit test

--- test/models/project_media_5_test.rb
+++ test/models/project_media_5_test.rb
@@ -483,9 +483,6 @@ class ProjectMedia5Test < ActiveSupport::TestCase
   end
 
   test "should set automatic title for links" do
-    m = create_uploaded_image file: 'rails.png'
-    v = create_uploaded_video file: 'rails.mp4'
-    a = create_uploaded_audio file: 'rails.mp3'
     bot = create_team_bot name: 'Smooch', login: 'smooch', set_approved: true
     u = create_user
     team = create_team slug: 'workspace-slug'
@@ -555,6 +552,7 @@ class ProjectMedia5Test < ActiveSupport::TestCase
     with_current_user_and_team(u, team) do
       pm_youtube = create_project_media team: team, media: l_youtube
       assert_equal "youtube", pm_youtube.title
+      assert_equal "youtube-#{team.slug}-#{pm_youtube.id}", pm_youtube.media_slug
       pm_twitter = create_project_media team: team, media: l_twitter
       assert_equal "twitter", pm_twitter.title
       pm_facebook = create_project_media team: team, media: l_facebook
@@ -563,10 +561,12 @@ class ProjectMedia5Test < ActiveSupport::TestCase
       assert_equal "instagram", pm_instagram.title
       pm_tiktok = create_project_media team: team, media: l_tiktok
       assert_equal "tiktok", pm_tiktok.title
+      assert_equal "tiktok-#{team.slug}-#{pm_tiktok.id}", pm_tiktok.media_slug
       pm_telegram = create_project_media team: team, media: l_telegram
       assert_equal "telegram", pm_telegram.title
       pm_weblink = create_project_media team: team, media: l_weblink
       assert_equal "weblink", pm_weblink.title
+      assert_equal "weblink-#{team.slug}-#{pm_weblink.id}", pm_weblink.media_slug
     end
   end
 
@@ -593,7 +593,7 @@ class ProjectMedia5Test < ActiveSupport::TestCase
       # verify media_slug field
       cd = create_claim_description project_media: pm, description: 'description_text'
       assert_equal pm.get_title, cd.description
-      assert_empty pm.media_slug
+      assert_equal "text-#{team.slug}-#{pm.id}", pm.media_slug
     end
   end
 
