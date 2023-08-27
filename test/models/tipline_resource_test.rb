364031334bde1034233d72d5611f8f4bb546a3df
Caio <117518+caiosba@users.noreply.github.com>
2023-08-27 18:53:28 -0300

    Adding test

--- test/models/tipline_resource_test.rb
+++ test/models/tipline_resource_test.rb
@@ -34,15 +34,20 @@ class TiplineResourceTest < ActiveSupport::TestCase
 
   test "should belong to team" do
     t = create_team
-    br1 = create_tipline_resource team: t
-    br2 = create_tipline_resource team: t
+    tr1 = create_tipline_resource team: t
+    tr2 = create_tipline_resource team: t
     create_tipline_resource
-    assert_equal t, br1.team
-    assert_equal t, br2.team
-    assert_equal [br1, br2].sort, t.tipline_resources.sort
+    assert_equal t, tr1.team
+    assert_equal t, tr2.team
+    assert_equal [tr1, tr2].sort, t.tipline_resources.sort
   end
 
   test "should define a content name" do
     assert_equal 'resource', TiplineResource.content_name
+  end
+
+  test "should have static content" do
+    tr = create_tipline_resource title: 'Foo', content: 'Bar', content_type: 'static'
+    assert_equal "*Foo*\n\nBar", tr.format_as_tipline_message
   end
 end
