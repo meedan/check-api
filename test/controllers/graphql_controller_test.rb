2eb0a2c2e4e641845caf85df308760194dfac683
Caio <117518+caiosba@users.noreply.github.com>
2023-09-21 22:58:07 -0300

    Fixing tests

--- test/controllers/graphql_controller_test.rb
+++ test/controllers/graphql_controller_test.rb
@@ -132,7 +132,7 @@ class GraphqlControllerTest < ActionController::TestCase
     authenticate_with_user(u)
     pm = create_project_media
     create_flag annotated: pm
-    query = "query GetById { project_media(ids: \"#{pm.id},nil,#{pm.team_id}\") { source { id }, flags(first: 10) { edges { node { id } } }, annotation(annotation_type: \"flag\") { project_media { id } }, annotations(annotation_type: \"flag\") { edges { node { ... on Flag { id } } } } } }"
+    query = "query GetById { project_media(ids: \"#{pm.id},nil,#{pm.team_id}\") { source { id }, flags(first: 10) { edges { node { id } } }, annotation(annotation_type: \"flag\") { permissions, medias(first: 1) { edges { node { id } } } project_media { id } }, annotations(annotation_type: \"flag\") { edges { node { ... on Flag { id } } } } } }"
     post :create, params: { query: query, team: pm.team.slug }
     assert_response :success
   end
@@ -920,6 +920,7 @@ class GraphqlControllerTest < ActionController::TestCase
     assert_nothing_raised do
       class TestType < BaseObject
         implements BaseInterface
+        field :test, ::BaseEnum.connection_type, null: false
       end
     end
   end
