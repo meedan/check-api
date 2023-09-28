5375af2c4f3469712be5e285f8747852fe0b77e9
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2023-09-28 21:54:54 +0300

    List `TiplineMessage`'s by UID
    Add GraphQL type for `TiplineMessage` and list tipline messages by UID in `TeamType`.
    
    Reference: CV2-3681.

---
+++ app/graph/types/tipline_message_type.rb
@@ -0,0 +1,22 @@
+class TiplineMessageType < DefaultObject
+  description "TiplineMessage type"
+
+  implements GraphQL::Types::Relay::Node
+
+  field :dbid, GraphQL::Types::Int, null: true
+  field :event, GraphQL::Types::String, null: true
+  field :direction, GraphQL::Types::String, null: true
+  field :language, GraphQL::Types::String, null: true
+  field :platform, GraphQL::Types::String, null: true
+  field :uid, GraphQL::Types::String, null: true
+  field :external_id, GraphQL::Types::String, null: true
+  field :payload, JsonStringType, null: true
+  field :team_id, GraphQL::Types::Int, null: true
+  field :state, GraphQL::Types::String, null: true
+  field :team, TeamType, null: true
+  field :sent_at, GraphQL::Types::String, null: true, camelize: false
+
+  def sent_at
+    object.sent_at.to_i.to_s
+  end
+end
