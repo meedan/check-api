d53b01590e8020bf198e936c5120482936f71be2
Caio <117518+caiosba@users.noreply.github.com>
2023-10-17 12:20:11 -0300

    Block tipline users when they send more than X messages in 24 hours.
    "X" is defined by a configuration key but has a default value.
    
    Reference: CV2-3860.

--- test/models/tipline_message_test.rb
+++ test/models/tipline_message_test.rb
@@ -223,4 +223,26 @@ class TiplineMessageTest < ActiveSupport::TestCase
     assert_equal url, create_tipline_message(direction: 'incoming', payload: incoming_payload).media_url
     assert_equal url, create_tipline_message(direction: 'outgoing', payload: outgoing_payload).media_url
   end
+
+  test "should block user when rate limit is reached" do
+    uid = random_string
+    assert !Bot::Smooch.user_blocked?(uid)
+    stub_configs({ 'tipline_user_max_messages_per_day' => 2 }) do
+      # User sent a message
+      create_tipline_message uid: uid, state: 'received'
+      assert !Bot::Smooch.user_blocked?(uid)
+      # User sent a message
+      create_tipline_message uid: uid, state: 'received'
+      assert !Bot::Smooch.user_blocked?(uid)
+      # Another user sent a message
+      create_tipline_message state: 'received'
+      assert !Bot::Smooch.user_blocked?(uid)
+      # User received a message
+      create_tipline_message uid: uid, state: 'delivered'
+      assert !Bot::Smooch.user_blocked?(uid)
+      # User sent a message and is now over rate limit, so should be blocked
+      create_tipline_message uid: uid, state: 'received'
+      assert Bot::Smooch.user_blocked?(uid)
+    end
+  end
 end
