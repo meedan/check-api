cd4a816a0497b4901f1b7827d9e16ae1a0cd8cac
Sawy <melsawy.be7eri@gmail.com>
2023-10-24 18:57:01 +0300

    CV2-3798: adjust template logo

--- app/mailers/feed_invitation_mailer.rb
+++ app/mailers/feed_invitation_mailer.rb
@@ -6,7 +6,7 @@ class FeedInvitationMailer < ApplicationMailer
     @user = record.user
     @feed = record.feed
     @direction = ApplicationMailer.set_template_direction
-    @due_at = record.created_at + 1.month
+    @due_at = record.created_at + CheckConfig.get('feed_invitation_due_to', 30).to_i.days
     Rails.logger.info "Sending a feed invitation e-mail to #{@recipient}"
     subject = I18n.t("mails_notifications.feed_invitation.subject", user: @user.name, feed: @feed.name)
     mail(to: @recipient, email_type: 'feed_invitation', subject: subject)
