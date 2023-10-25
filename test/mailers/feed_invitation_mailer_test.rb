be25e601121f3134bc7d1181317bb09d2e0cc46c
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2023-10-25 19:39:25 +0300

    CV2-3798 shared feed invite emails (#1704)
    * CV2-3798: send feed invitation mail
    
    * CV2-3798: adjust mail template and fix tests
    
    * CV2-3798: fix check logo for other templates

---
+++ test/mailers/feed_invitation_mailer_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class FeedInvitationMailerTest < ActionMailer::TestCase
+  test "should notify about feed invitation" do
+    fi = create_feed_invitation
+    email = FeedInvitationMailer.notify(fi)
+    assert_emails 1 do
+      email.deliver_now
+    end
+    assert_equal [fi.email], email.to
+  end
+end
