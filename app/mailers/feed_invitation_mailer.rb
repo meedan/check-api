f1167b5a38fad178aabc56483081f24f7bfbb4e5
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2023-10-26 17:14:53 +0300

    CV2-3798: update mail logo for all mails templates (#1708)

--- app/mailers/feed_invitation_mailer.rb
+++ app/mailers/feed_invitation_mailer.rb
@@ -5,11 +5,8 @@ class FeedInvitationMailer < ApplicationMailer
     @recipient = record.email
     @user = record.user
     @feed = record.feed
-    @direction = ApplicationMailer.set_template_direction
     @due_at = record.created_at + CheckConfig.get('feed_invitation_due_to', 30).to_i.days
     subject = I18n.t("mails_notifications.feed_invitation.subject", user: @user.name, feed: @feed.name)
-    attachments.inline['check_logo.png'] = File.read("#{Rails.root}/public/images/check.svg")
-    @logo_url = attachments['check_logo.png'].url
     Rails.logger.info "Sending a feed invitation e-mail to #{@recipient}"
     mail(to: @recipient, email_type: 'feed_invitation', subject: subject)
   end
