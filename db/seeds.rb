3c4f651c8a07bd085eca5f58b88b2e1e1b96f279
manu vasconcelos <manu@meedan.com>
2023-11-06 13:21:53 -0300

    move tipline user and data creation to a method
    we are going to have to use this multiple times, so this will make it
    easier

--- db/seeds.rb
+++ db/seeds.rb
@@ -75,6 +75,86 @@ def create_claim_description(user, project, team)
   ClaimDescription.create!(description: Faker::Company.catch_phrase, context: Faker::Lorem.sentence, user: user, project_media: create_blank(project, team))
 end
 
+def create_tipline_user_and_data(project_media, team)
+  tipline_user_name = Faker::Name.first_name.downcase
+  tipline_user_surname = Faker::Name.last_name
+  tipline_text = Faker::Lorem.paragraph(sentence_count: 10)
+  phone = [ Faker::PhoneNumber.phone_number, Faker::PhoneNumber.cell_phone, Faker::PhoneNumber.cell_phone_in_e164, Faker::PhoneNumber.phone_number_with_country_code, Faker::PhoneNumber.cell_phone_with_country_code].sample
+  uid = random_string
+
+  # Tipline user
+  smooch_user_data = {
+    'id': uid,
+    'raw': {
+      '_id': uid,
+      'givenName': tipline_user_name,
+      'surname': tipline_user_surname,
+      'signedUpAt': Time.now.to_s,
+      'properties': {},
+      'conversationStarted': true,
+      'clients': [
+        {
+          'id': random_string,
+          'status': 'active',
+          'externalId': phone,
+          'active': true,
+          'lastSeen': Time.now.to_s,
+          'platform': 'whatsapp',
+          'integrationId': random_string,
+          'displayName': phone,
+          'raw': {
+            'profile': {
+              'name': tipline_user_name
+            },
+            'from': phone
+          }
+        }
+      ],
+      'pendingClients': []
+    },
+    'identifier': random_string,
+    'app_name': random_string
+  }
+
+  fields = {
+    smooch_user_id: uid,
+    smooch_user_app_id: random_string,
+    smooch_user_data: smooch_user_data.to_json
+  }
+
+  Dynamic.create!(annotation_type: 'smooch_user', annotated: team, annotator: BotUser.smooch_user, set_fields: fields.to_json)
+
+  # Tipline request
+  smooch_data = {
+    'role': 'appUser',
+    'source': {
+      'type': 'whatsapp',
+      'id': random_string,
+      'integrationId': random_string,
+      'originalMessageId': random_string,
+      'originalMessageTimestamp': Time.now.to_i
+    },
+    'authorId': uid,
+    'name': tipline_user_name,
+    '_id': random_string,
+    'type': 'text',
+    'received': Time.now.to_f,
+    'text': tipline_text,
+    'language': 'en',
+    'mediaUrl': nil,
+    'mediaSize': 0,
+    'archived': 3,
+    'app_id': random_string
+  }
+
+  fields = {
+    smooch_request_type: 'default_requests',
+    smooch_data: smooch_data.to_json
+  }
+
+  Dynamic.create!(annotation_type: 'smooch', annotated: project_media, annotator: BotUser.smooch_user, set_fields: fields.to_json)
+end
+
 puts "If you want to create a new user: press 1 then enter"
 puts "If you want to add more data to an existing user: press 2 then enter"
 print ">> "
@@ -170,83 +250,7 @@ ActiveRecord::Base.transaction do
     project_media = ProjectMedia.create!(project: project, team: team, media: claim_media, channel: { main: CheckChannels::ChannelCodes::WHATSAPP })
 
     10.times do
-      tipline_user_name = Faker::Name.first_name.downcase
-      tipline_user_surname = Faker::Name.last_name
-      tipline_text = Faker::Lorem.paragraph(sentence_count: 10)
-      phone = [ Faker::PhoneNumber.phone_number, Faker::PhoneNumber.cell_phone, Faker::PhoneNumber.cell_phone_in_e164, Faker::PhoneNumber.phone_number_with_country_code, Faker::PhoneNumber.cell_phone_with_country_code].sample
-      uid = random_string
-
-      # Tipline user
-      smooch_user_data = {
-        'id': uid,
-        'raw': {
-          '_id': uid,
-          'givenName': tipline_user_name,
-          'surname': tipline_user_surname,
-          'signedUpAt': Time.now.to_s,
-          'properties': {},
-          'conversationStarted': true,
-          'clients': [
-            {
-              'id': random_string,
-              'status': 'active',
-              'externalId': phone,
-              'active': true,
-              'lastSeen': Time.now.to_s,
-              'platform': 'whatsapp',
-              'integrationId': random_string,
-              'displayName': phone,
-              'raw': {
-                'profile': {
-                  'name': tipline_user_name
-                },
-                'from': phone
-              }
-            }
-          ],
-          'pendingClients': []
-        },
-        'identifier': random_string,
-        'app_name': random_string
-      }
-
-      fields = {
-        smooch_user_id: uid,
-        smooch_user_app_id: random_string,
-        smooch_user_data: smooch_user_data.to_json
-      }
-
-      Dynamic.create!(annotation_type: 'smooch_user', annotated: team, annotator: BotUser.smooch_user, set_fields: fields.to_json)
-
-      # Tipline request
-      smooch_data = {
-        'role': 'appUser',
-        'source': {
-          'type': 'whatsapp',
-          'id': random_string,
-          'integrationId': random_string,
-          'originalMessageId': random_string,
-          'originalMessageTimestamp': Time.now.to_i
-        },
-        'authorId': uid,
-        'name': tipline_user_name,
-        '_id': random_string,
-        'type': 'text',
-        'received': Time.now.to_f,
-        'text': tipline_text,
-        'language': 'en',
-        'mediaUrl': nil,
-        'mediaSize': 0,
-        'archived': 3,
-        'app_id': random_string
-      }
-
-      fields = {
-        smooch_request_type: 'default_requests',
-        smooch_data: smooch_data.to_json
-      }
-
-      Dynamic.create!(annotation_type: 'smooch', annotated: project_media, annotator: BotUser.smooch_user, set_fields: fields.to_json)
+      create_tipline_user_and_data(project_media, team)
     end
   end
 
