688f2bc30c3c4510fd4bf23bd73f95a83f6c8b8b
manu vasconcelos <manu@meedan.com>
2023-11-06 15:17:20 -0300

    add begin/rescue when creating links with requests
    we don't want the whole script to fail if Pender is not running

--- db/seeds.rb
+++ db/seeds.rb
@@ -262,15 +262,19 @@ ActiveRecord::Base.transaction do
   end
 
   puts 'Making Tipline requests for links...'
-  tipline_pm_links_arr = []
-  data[:link_media_links][0..5].each do |link_media_link| 
-    link_media = Link.create!(user_id: user.id, url: link_media_link+"?timestamp=#{Time.now.to_f}") 
-    project_media = create_tipline_project_media(project, team, link_media)
-    tipline_pm_links_arr.push(project_media)
-  end
+  begin
+    tipline_pm_links_arr = []
+    data[:link_media_links][0..5].each do |link_media_link| 
+      link_media = Link.create!(user_id: user.id, url: link_media_link+"?timestamp=#{Time.now.to_f}") 
+      project_media = create_tipline_project_media(project, team, link_media)
+      tipline_pm_links_arr.push(project_media)
+    end
 
-  tipline_pm_links_arr[0..2].each {|pm| create_tipline_user_and_data(pm, team)}
-  tipline_pm_links_arr[3..5].each {|pm| 15.times {create_tipline_user_and_data(pm, team)}}
+    tipline_pm_links_arr[0..2].each {|pm| create_tipline_user_and_data(pm, team)}
+    tipline_pm_links_arr[3..5].each {|pm| 15.times {create_tipline_user_and_data(pm, team)}}
+  rescue
+    puts "Couldn't create Links. Other medias will still be created. \nIn order to create Links make sure Pender is running."
+  end
 
   add_claim_descriptions_and_fact_checks(user)
 
