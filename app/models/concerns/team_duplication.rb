7770a061b22bc3f3a7399277c8ddf181c8983d31
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2023-11-09 08:51:12 +0200

    CV2-3940: fix default language for duplicated team (#1720)

--- app/models/concerns/team_duplication.rb
+++ app/models/concerns/team_duplication.rb
@@ -45,6 +45,7 @@ module TeamDuplication
       new_list_columns = old_team.get_list_columns.to_a.collect{|lc| lc.include?("task_value_") ? "task_value_#{team_task_map[lc.split("_").last.to_i]}" : lc}
       new_team.set_list_columns = new_list_columns unless new_list_columns.blank?
       new_team.set_languages = old_team.get_languages
+      new_team.set_language = old_team.get_language
       new_team
     end
 
