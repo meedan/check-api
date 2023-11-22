01b81cc14e2c5afe0704a866369b37e37f22d4d1
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2023-11-22 15:40:41 +0200

    CV2-4028: fix url strip (#1737)

--- config/initializers/field_validators.rb
+++ config/initializers/field_validators.rb
@@ -16,7 +16,8 @@ DynamicAnnotation::Field.class_eval do
     errormsg = I18n.t(:url_invalid_value)
     urls = self.value
     urls.each do |item|
-      url = URI.parse(item['url'].strip!)
+      item['url'] = item['url'].strip
+      url = URI.parse(item['url'])
       errors.add(:base, errormsg + ' ' + url.to_s) unless url.is_a?(URI::HTTP) && !url.host.nil?
     end
   end
