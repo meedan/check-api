22c523ba920b354c96b454d1fb1b0c5ca572b3bd
Sawy <melsawy.be7eri@gmail.com>
2023-12-05 07:12:15 +0200

    CV2-2648: test coverage

--- test/models/bot/smooch_8_test.rb
+++ test/models/bot/smooch_8_test.rb
@@ -23,4 +23,19 @@ class Bot::Smooch8Test < ActiveSupport::TestCase
       end
     end
   end
+
+  test "should rescue when raise error on tipline request creation" do
+    TiplineRequest.any_instance.stubs(:save!).raises(ActiveRecord::RecordNotUnique)
+    t = create_team
+    pm = create_project_media team: t
+    u = create_user
+    create_team_user team: t, user: u, role: 'admin'
+    with_current_user_and_team(u, t) do
+      fields = { 'smooch_message_id' => random_string, 'smooch_data' => { authorId: random_string, language: 'en' } }
+      assert_no_difference 'TiplineRequest.count' do
+        Bot::Smooch.create_tipline_requests(pm, nil, fields, true)
+      end
+    end
+    TiplineRequest.any_instance.unstub(:save!)
+  end
 end
