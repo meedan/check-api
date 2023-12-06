e0f79302f0af97afbb2a636b170629338459b16a
Caio <117518+caiosba@users.noreply.github.com>
2023-12-06 00:28:35 -0300

    Should not allow feed to be deleted if it still has collaborators.
    Fixes CV2-3952.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -4,7 +4,7 @@ class Feed < ApplicationRecord
   check_settings
 
   has_many :requests
-  has_many :feed_teams, dependent: :destroy
+  has_many :feed_teams, dependent: :restrict_with_error
   has_many :teams, through: :feed_teams
   has_many :feed_invitations, dependent: :destroy
   belongs_to :user, optional: true
