bf1bd445e790b6631a0f2c7bea9265f3bd5bb05c
Sawy <melsawy.be7eri@gmail.com>
2024-01-15 20:37:26 +0200

    CV2-4072: remove TeamUser from UserType

--- app/graph/types/user_type.rb
+++ app/graph/types/user_type.rb
@@ -10,42 +10,10 @@ class UserType < DefaultObject
   field :last_active_at, GraphQL::Types::Int, null: true
   field :is_bot, GraphQL::Types::Boolean, null: true
   field :is_active, GraphQL::Types::Boolean, null: true
-  field :number_of_teams, GraphQL::Types::Int, null: true
-  field :accepted_terms, GraphQL::Types::Boolean, null: true
-  field :current_team_id, GraphQL::Types::Int, null: true
 
   field :source, SourceType, null: true
 
   def source
     Source.find(object.source_id)
-  end
-
-  field :current_team, TeamType, null: true
-
-  def current_team
-    User.current == object ? object.current_team : nil
-  end
-
-  field :team_user, TeamUserType, null: true do
-    argument :team_slug, GraphQL::Types::String, required: true, camelize: false
-  end
-
-  def team_user(team_slug:)
-    tu = TeamUser
-      .joins(:team)
-      .where("teams.slug" => team_slug, :user_id => object.id)
-      .last
-    tu.nil? ? nil : TeamUser.find_if_can(tu.id, context[:ability])
-  end
-
-  field :team_users, TeamUserType.connection_type, null: true do
-    argument :status, GraphQL::Types::String, required: false
-  end
-
-  def team_users(status: nil)
-    return TeamUser.none unless object == User.current
-    team_users = object.team_users
-    team_users = team_users.where(status: status) if status
-    team_users
   end
 end
