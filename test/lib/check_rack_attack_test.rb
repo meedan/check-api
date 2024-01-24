4b03a986f7ba5fe014711d7dc039b29dc98a0e33
Jay Joshua <7008757+jayjay-w@users.noreply.github.com>
2024-01-24 19:03:05 +0100

    Reduce the number of api calls when testing rate limiting
    Reduce the number of actual api calls to 2 per test when testing
    rate limiting.

--- test/lib/check_rack_attack_test.rb
+++ test/lib/check_rack_attack_test.rb
@@ -6,26 +6,27 @@ class ThrottlingTest < ActionDispatch::IntegrationTest
   end
 
   test "should throttle excessive requests to /api/graphql" do
-    limit = 100
+    stub_configs({ 'api_rate_limit' => 2 }) do
+      2.times do
+        post api_graphql_path
+        assert_response :unauthorized
+      end
 
-    limit.times do
       post api_graphql_path
-      assert_response :unauthorized
+      assert_response :too_many_requests
     end
-
-    get api_graphql_path
-    assert_response :too_many_requests
   end
 
   test "should throttle excessive requests to /api/users/sign_in" do
-    limit = 10
-    user_params = { api_user: { email: 'user@example.com', password: 'password' } }
+    stub_configs({ 'login_rate_limit' => 2 }) do
+      user_params = { api_user: { email: 'user@example.com', password: 'password' } }
 
-    limit.times do
+      2.times do
+        post api_user_session_path, params: user_params, as: :json
+      end
+
       post api_user_session_path, params: user_params, as: :json
+      assert_response :too_many_requests
     end
-
-    post api_user_session_path, params: user_params, as: :json
-    assert_response :too_many_requests
   end
 end
