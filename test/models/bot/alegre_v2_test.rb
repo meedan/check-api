6060da7f984723e3a89b73fc4c0f702e8d86ac03
Devin Gaffney <itsme@devingaffney.com>
2024-01-31 07:22:17 -0800

    CV2-4233 add retry looper for alegre requests that intermittently fail (#1786)
    * CV2-4233 add retry looper for alegre requests that intermittently fail
    
    * CV2-4233 fix fixture
    
    * update test with right params
    
    * change blah to proper type
    
    * update function based on it being a hash that's actually being passed around
    
    * refactors from PR
    
    * add stub
    
    * fix typo

--- test/models/bot/alegre_v2_test.rb
+++ test/models/bot/alegre_v2_test.rb
@@ -283,6 +283,14 @@ class Bot::AlegreTest < ActiveSupport::TestCase
     assert_equal JSON.parse(Bot::Alegre.get_sync(pm1).to_json), JSON.parse(response.to_json)
   end
 
+  test "should safe_get_sync" do
+    pm1 = create_project_media team: @team, media: create_uploaded_audio
+    WebMock.stub_request(:post, "#{CheckConfig.get('alegre_host')}/similarity/sync/audio").to_return(body: '{}')
+    expected = {}
+    actual = Bot::Alegre.safe_get_sync(pm1, "audio", {})
+    assert_equal expected, actual
+  end
+
   test "should run delete request" do
     pm1 = create_project_media team: @team, media: create_uploaded_audio
     response = {"requested"=>
