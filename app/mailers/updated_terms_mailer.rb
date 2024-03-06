6b0477fe14d5d87bad56732baf770e8ac303e247
Sawy <melsawy.be7eri@gmail.com>
2024-03-06 20:44:20 +0200

    CV2-2357: get tos from config

--- app/mailers/updated_terms_mailer.rb
+++ app/mailers/updated_terms_mailer.rb
@@ -3,7 +3,7 @@ class UpdatedTermsMailer < ApplicationMailer
 
   def notify(recipient, name)
     @name = name
-    @accept_terms_url = "https://meedan.com/legal/terms-of-service"
+    @accept_terms_url = CheckConfig.get('tos_url')
     subject = I18n.t("mails_notifications.updated_terms.subject")
     Rails.logger.info "Sending ToS e-mail to #{recipient}"
     mail(to: recipient, email_type: 'updated_terms', subject: subject)
