870640c8c59561e3e7abe9e5d9f075bd13c0b773
manu vasconcelos <manu@meedan.com>
2024-03-08 16:39:46 -0300

    remove create_report
    This method was causing an issue and is not needed anymore.
    We can simply substitute it by fc.publish_report = true
    
    Issue:
    When trying to import some Fact Checks, using the FetchBot,
    with language set to pt_BR, the language was still being set as en.
    
    That was happening because inside import_claim_review after it runs
    set_claim_and_fact_check, it runs create_report.
    (https://github.com/meedan/check-api/blob/4a44ab093fbae68e25114c4cef0af537daec9f2b/app/models/bot/fetch.rb#L187-L199 )
    
    Inside create_report it sets the language to the workspace’s
    default language. So, we might want to add a language as pt_BR,
    but if the default language of the workspace is en, that gets overwritten.
    (https://github.com/meedan/check-api/blob/4a44ab093fbae68e25114c4cef0af537daec9f2b/app/models/bot/fetch.rb#L338)

--- app/models/bot/fetch.rb
+++ app/models/bot/fetch.rb
@@ -196,7 +196,6 @@ class Bot::Fetch < BotUser
             self.set_status(claim_review, pm, status_fallback, status_mapping)
             self.set_analysis(claim_review, pm)
             self.set_claim_and_fact_check(claim_review, pm, user, team)
-            self.create_report(claim_review, pm, team, user, auto_publish_reports)
             self.create_tags(claim_review, pm, user)
           end
         end
@@ -261,8 +260,8 @@ class Bot::Fetch < BotUser
       fc.url = claim_review['url'].to_s
       fc.summary = self.get_summary(claim_review).to_s
       fc.user = user
-      fc.skip_report_update = true
       fc.language = fc_language
+      fc.publish_report = true
       fc.save!
       User.current = current_user
     end
@@ -321,48 +320,6 @@ class Bot::Fetch < BotUser
 
     def self.parse_text(text)
       CGI.unescapeHTML(ActionView::Base.full_sanitizer.sanitize(text.to_s))
-    end
-
-    def self.create_report(claim_review, pm, team, user, auto_publish_reports)
-      report = Dynamic.new
-      report.annotation_type = 'report_design'
-      report.annotated = pm
-      report.annotator = user
-      tmp_file_path = begin self.get_image_file(claim_review['image'].to_s) rescue nil end
-      if tmp_file_path
-        File.open(tmp_file_path) do |f|
-          report.file = [f]
-        end
-      end
-      date = claim_review['datePublished'].blank? ? Time.now : Time.parse(claim_review['datePublished'])
-      language = team.default_language
-      title = self.get_title(claim_review).truncate(140)
-      summary = self.parse_text(claim_review['text']).truncate(620)
-      fields = {
-        state: auto_publish_reports ? 'published' : 'paused',
-        options: {
-          language: language,
-          status_label: pm.status_i18n(pm.reload.last_verification_status, { locale: language }),
-          description: summary,
-          title: title,
-          published_article_url: claim_review['url'],
-          headline: title,
-          use_visual_card: false,
-          image: '',
-          use_introduction: !!report.report_design_team_setting_value('use_introduction', language),
-          introduction: report.report_design_team_setting_value('introduction', language).to_s,
-          theme_color: pm.reload.last_status_color,
-          url: '',
-          use_text_message: true,
-          text: summary,
-          date: report.report_design_date(date.to_date, language)
-        }
-      }
-      report.set_fields = fields.to_json
-      report.action = 'save'
-      report.skip_check_ability = true
-      report.save!
-      FileUtils.rm_f(tmp_file_path) if tmp_file_path
     end
   end
 end
