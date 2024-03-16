3723ff9d32cdaa33f7a890ed7a6ec7c7d1f8efa6
Sawy <melsawy.be7eri@gmail.com>
2024-03-16 17:56:06 +0200

    CV2-4128: fix CC

--- lib/check_cluster_center.rb
+++ lib/check_cluster_center.rb
@@ -1,5 +1,4 @@
 class CheckClusterCenter
-
   def self.replace_or_keep_cluster_center(current_center, new_pm)
     return new_pm.id if current_center.nil?
     # Define passed variable to contain items that passed check
@@ -8,32 +7,30 @@ class CheckClusterCenter
     # passed contain one item (length == 1) return the id
 
     # Check #1: Fact-check
-    passed = self.fact_check_condition(current_center, new_pm)
+    passed = self.fact_check_check(current_center, new_pm)
     return passed.first if passed.length == 1
     # Check #2: Has a claim?
     passed = ClaimDescription.where(project_media_id: [current_center.id, new_pm.id])
     return passed.first.project_media_id if passed.length == 1
     # Check #3: Most number of requests
-    passed = self.requests_count_condition(current_center, new_pm)
+    passed = self.requests_count_check(current_center, new_pm)
     return passed.first if passed.length == 1
     # Check #4: Most recently Last updated
-    passed = self.most_recent_condition(current_center, new_pm)
+    passed = self.most_recent_check(current_center, new_pm)
     return passed.first if passed.length == 1
     # Check #5: Alphabetical team name
     first_team = [current_center.team_name, new_pm.team_name].sort.first
     current_center.team_name == first_team ? current_center.id : new_pm.id
   end
 
-  private
-
-  def self.fact_check_condition(current_center, new_pm)
+  def self.fact_check_check(current_center, new_pm)
     passed = []
     passed << current_center.id if current_center.report_status == 'published'
     passed << new_pm.id if new_pm.report_status == 'published'
     passed
   end
 
-  def self.requests_count_condition(current_center, new_pm)
+  def self.requests_count_check(current_center, new_pm)
     passed = []
     if (current_center.requests_count != 0 || new_pm.requests_count != 0) && current_center.requests_count != new_pm.requests_count
       id = current_center.requests_count > new_pm.requests_count ? current_center.id : new_pm.id
@@ -42,7 +39,7 @@ class CheckClusterCenter
     passed
   end
 
-  def self.most_recent_condition(current_center, new_pm)
+  def self.most_recent_check(current_center, new_pm)
     passed = []
     if current_center.updated_at != new_pm.updated_at
       id = current_center.updated_at > new_pm.updated_at ? current_center.id : new_pm.id
