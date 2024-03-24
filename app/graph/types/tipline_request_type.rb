e1a5f50fb2c3fc8b6b5e720cc39395aef3278634
Caio <117518+caiosba@users.noreply.github.com>
2024-03-23 23:13:22 -0300

    Privacy control for tipline requests for feed

--- app/graph/types/tipline_request_type.rb
+++ app/graph/types/tipline_request_type.rb
@@ -6,9 +6,6 @@ class TiplineRequestType < DefaultObject
   field :dbid, GraphQL::Types::Int, null: true
   field :associated_id, GraphQL::Types::Int, null: true
   field :associated_type, GraphQL::Types::String, null: true
-  field :smooch_data, JsonStringType, null: true
-  field :smooch_user_slack_channel_url, GraphQL::Types::String, null: true
-  field :smooch_user_external_identifier, GraphQL::Types::String, null: true
   field :smooch_report_received_at, GraphQL::Types::Int, null: true
   field :smooch_report_update_received_at, GraphQL::Types::Int, null: true
   field :smooch_user_request_language, GraphQL::Types::String, null: true
@@ -16,4 +13,26 @@ class TiplineRequestType < DefaultObject
   field :smooch_report_correction_sent_at, GraphQL::Types::Int, null: true
   field :smooch_request_type, GraphQL::Types::String, null: true
   field :associated_graphql_id, GraphQL::Types::String, null: true
+
+  field :smooch_user_external_identifier, GraphQL::Types::String, null: true
+
+  def smooch_user_external_identifier
+    ability = context[:ability] || Ability.new
+    # Mask the user identifier when this request is displayed in a feed context
+    ability.can?(:read, object) ? object.smooch_user_external_identifier : random_smooch_id
+  end
+
+  field :smooch_data, JsonStringType, null: true
+
+  def smooch_data
+    ability = context[:ability] || Ability.new
+    # Mask user information when this request is displayed in a feed context
+    ability.can?(:read, object) ? object.smooch_data : object.smooch_data.to_h.merge({ 'name' => random_smooch_id, 'authorId' => random_smooch_id })
+  end
+
+  private
+
+  def random_smooch_id
+    SecureRandom.hex.first(5)
+  end
 end
