87612cc639049d744fd2c04b3e77725135c8120a
Sawy <melsawy.be7eri@gmail.com>
2024-04-08 08:41:05 +0200

    CV2-4468: rescue Net::SMTPFatalError

--- app/mailers/updated_terms_mailer.rb
+++ app/mailers/updated_terms_mailer.rb
@@ -6,6 +6,10 @@ class UpdatedTermsMailer < ApplicationMailer
     @accept_terms_url = CheckConfig.get('tos_url')
     subject = I18n.t("mails_notifications.updated_terms.subject")
     Rails.logger.info "Sending ToS e-mail to #{recipient}"
-    begin mail(to: recipient, email_type: 'updated_terms', subject: subject) rescue nil end
+    begin
+      mail(to: recipient, email_type: 'updated_terms', subject: subject)
+    rescue Net::SMTPFatalError
+      nil
+    end
   end
 end
