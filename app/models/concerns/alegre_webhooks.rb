74c0e108b8532694e6e1ea040a38f931f4fca181
Devin Gaffney <itsme@devingaffney.com>
2024-05-01 13:44:50 -0700

    fix broken tests to accommodate for body based sends

--- app/models/concerns/alegre_webhooks.rb
+++ app/models/concerns/alegre_webhooks.rb
@@ -21,20 +21,20 @@ module AlegreWebhooks
     def webhook(request)
       key = nil
       body = parse_body(request)
+      redis = Redis.new(REDIS_CONFIG)
       begin
-        doc_id = request.params.dig('data', 'requested', 'id')
+        doc_id = body.dig('data', 'requested', 'id')
         # search for doc_id on completed full-circuit callbacks
-        doc_id = request.params.dig('data', 'item', 'id') if doc_id.nil?
+        doc_id = body.dig('data', 'item', 'id') if doc_id.nil?
         # search for doc_id on completed short-circuit callbacks (i.e. items already known to Alegre but added context TODO make these the same structure)
-        doc_id = request.params.dig('data', 'item', 'raw', 'doc_id') if doc_id.nil?
+        doc_id = body.dig('data', 'item', 'raw', 'doc_id') if doc_id.nil?
         CheckSentry.notify(AlegreCallbackError.new("Tracing Webhook NOT AN ERROR"), params: {doc_id: doc_id, is_not_a_bug_is_a_temporary_log_to_sentry: true, alegre_response: request.params, body: body })
         raise 'Unexpected params format' if doc_id.blank?
         if is_from_alegre_search_result_callback(body)
           Bot::Alegre.process_alegre_callback(body)
         else
-          redis = Redis.new(REDIS_CONFIG)
           key = "alegre:webhook:#{doc_id}"
-          redis.lpush(key, request.params.to_json)
+          redis.lpush(key, body.to_json)
         end
       rescue StandardError => e
         CheckSentry.notify(AlegreCallbackError.new(e.message), params: { alegre_response: request.params })
