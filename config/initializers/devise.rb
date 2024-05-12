9c23289f11ca0ff110a253f805819e8ad203e001
Jay Joshua <7008757+jayjay-w@users.noreply.github.com>
2024-05-12 13:09:25 +0200

    Disable devise user locking
    Currently, we block users with repeated incorrect sign-in attempts. However, this introduces a scenario where an attacker might target a specific user and lead to their account being locked. To prevent this, we are disabling account lock-out in devise.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -49,13 +49,6 @@ Devise.setup do |config|
   end
   config.mailer = 'DeviseMailer'
   config.invite_for = 1.month
-
-  # Account lockout
-  config.lock_strategy = :failed_attempts
-  config.unlock_strategy = :time
-  config.unlock_keys = [ :time ]
-  config.maximum_attempts = CheckConfig.get('devise_maximum_attempts', 5) * 2
-  config.unlock_in = CheckConfig.get('devise_unlock_accounts_after', 1).hour
 end
 
 AuthTrail.geocode = false
