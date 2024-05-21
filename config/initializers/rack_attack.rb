96d786ad4c1582bc2f54e1aa1a72df4f60714e60
Jay Joshua <7008757+jayjay-w@users.noreply.github.com>
2024-05-21 14:07:38 +0200

    Excessive login attempts: Block IP not user (#1890)
    * Disable devise user locking
    
    Currently, we block users with repeated incorrect sign-in attempts. However, this introduces a scenario where an attacker might target a specific user and lead to their account being locked. To prevent this, we are disabling account lock-out in devise.
    
    * Change rack-attack configuration to block offending login requests instead of throttling them
    
    * Fix rack-attack tests
    
    * Fix failing specs
    
    * Update blocklist comment to be more clear
    
    Update code comment to make it clear we are adding IP to blocklist and not actually blocking it.

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,21 +1,32 @@
 class Rack::Attack
-  # Throttle login attempts by IP address
-  throttle('logins/ip', limit: proc { CheckConfig.get('login_rate_limit', 10, :integer) }, period: 60.seconds) do |req|
+  # Throttle all graphql requests by IP address
+  throttle('api/graphql', limit:  proc { CheckConfig.get('api_rate_limit', 100, :integer) }, period: 60.seconds) do |req|
+    req.ip if req.path == '/api/graphql'
+  end
+
+  # Blocklist IP addresses that are permanently blocked
+  blocklist('block aggressive IPs') do |req|
+    Rails.cache.fetch("block:#{req.ip}") { false }
+  end
+
+  # Track excessive login attempts for permanent blocking
+  track('track excessive logins/ip') do |req|
     if req.path == '/api/users/sign_in' && req.post?
+      # Increment the counter for the IP and check if it should be blocked
+      count = Rails.cache.increment("track:#{req.ip}") || 0
+      Rails.cache.write("track:#{req.ip}", count, expires_in: 1.hour)
+
+      # Add IP to blocklist if count exceeds the threshold
+      if count >= CheckConfig.get('login_block_limit', 100, :integer)
+        Rails.cache.write("block:#{req.ip}", true)  # No expiration
+      end
+
       req.ip
     end
   end
 
-  # Throttle login attempts by email address
-  throttle('logins/email', limit: proc { CheckConfig.get('login_rate_limit', 10, :integer) }, period: 60.seconds) do |req|
-    if req.path == '/api/users/sign_in' && req.post?
-      # Return the email if present, nil otherwise
-      req.params['user']['email'].presence if req.params['user']
-    end
-  end
-
-  # Throttle all graphql requests by IP address
-  throttle('api/graphql', limit:  proc { CheckConfig.get('api_rate_limit', 100, :integer) }, period: 60.seconds) do |req|
-    req.ip if req.path == '/api/graphql'
+  # Response to blocked requests
+  self.blocklisted_response = lambda do |env|
+    [ 403, {}, ['Blocked - Your IP has been permanently blocked due to suspicious activity.']]
   end
 end
