5a567c785d5b9ff163d0cf08cbdd4e7d91703964
Jay Joshua <7008757+jayjay-w@users.noreply.github.com>
2024-05-25 00:03:48 +0200

    Change the way RackAttack blocks excessive login requests
    Here we are changing the way RackAttach blocks excessive login requests
     - Use redis directly instead of Rails.Cache
     - Change deprecated responder method call

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,32 +1,35 @@
 class Rack::Attack
+  redis = Redis.new(REDIS_CONFIG)
   # Throttle all graphql requests by IP address
-  throttle('api/graphql', limit:  proc { CheckConfig.get('api_rate_limit', 100, :integer) }, period: 60.seconds) do |req|
+  
+  throttle('api/graphql', limit: proc { CheckConfig.get('api_rate_limit', 100, :integer) }, period: 60.seconds) do |req|
     req.ip if req.path == '/api/graphql'
   end
 
   # Blocklist IP addresses that are permanently blocked
   blocklist('block aggressive IPs') do |req|
-    Rails.cache.fetch("block:#{req.ip}") { false }
+    redis.get("block:#{req.ip}") == "true"
   end
 
   # Track excessive login attempts for permanent blocking
   track('track excessive logins/ip') do |req|
     if req.path == '/api/users/sign_in' && req.post?
+      ip = req.ip
       # Increment the counter for the IP and check if it should be blocked
-      count = Rails.cache.increment("track:#{req.ip}") || 0
-      Rails.cache.write("track:#{req.ip}", count, expires_in: 1.hour)
+      count = redis.incr("track:#{ip}")
+      redis.expire("track:#{ip}", 3600) # Set the expiration time to 1 hour
 
       # Add IP to blocklist if count exceeds the threshold
-      if count >= CheckConfig.get('login_block_limit', 100, :integer)
-        Rails.cache.write("block:#{req.ip}", true)  # No expiration
+      if count.to_i >= CheckConfig.get('login_block_limit', 100, :integer)
+        redis.set("block:#{ip}", true)  # No expiration
       end
 
-      req.ip
+      ip
     end
   end
 
   # Response to blocked requests
-  self.blocklisted_response = lambda do |env|
-    [ 403, {}, ['Blocked - Your IP has been permanently blocked due to suspicious activity.']]
+  self.blocklisted_responder = lambda do |req|
+    [403, {}, ['Blocked - Your IP has been permanently blocked due to suspicious activity.']]
   end
 end
