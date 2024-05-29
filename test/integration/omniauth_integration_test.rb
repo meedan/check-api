e70eee6da2d4bbf32845f434ff29c865d96947af
Jay Joshua <7008757+jayjay-w@users.noreply.github.com>
2024-05-29 04:13:29 +0300

    Fix flaky test in OmniauthIntegrationTest (#1902)
    Since we added rate limiting for login requests, we need to flush redis before the tests run.

--- test/integration/omniauth_integration_test.rb
+++ test/integration/omniauth_integration_test.rb
@@ -1,6 +1,11 @@
 require_relative '../test_helper'
 
 class OmniauthIntegrationTest < ActionDispatch::IntegrationTest
+  setup do
+    redis = Redis.new(REDIS_CONFIG)
+    redis.flushdb
+  end
+
   test "should close in case of failure" do
     get '/api/users/auth/slack/callback', params: { error: 'access_denied' }
     assert_redirected_to '/close.html'
