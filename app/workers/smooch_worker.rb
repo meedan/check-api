fe6a0f903a723596933f7557361a9f74a9e42f77
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2024-06-04 18:49:55 +0300

    CV2-4666: Sentry issue (#1907)
    * CV2-4666: check existing smooch_message_id before creating new one and pass id to SmoochWorker instead of object
    
    * CV2-4666: fix tests and apply PR comments

--- app/workers/smooch_worker.rb
+++ app/workers/smooch_worker.rb
@@ -6,11 +6,16 @@ class SmoochWorker
 
   sidekiq_retry_in { |_count, _exception| 20 }
 
-  def perform(json_message, type, app_id, request_type, annotated)
-    annotated = YAML.load(annotated)
+  def perform(message_json, type, app_id, request_type, associated_id = nil, associated_class = nil)
     User.current = BotUser.smooch_user
+    # This is a temp code for existing jobs and should remove it in next deployment
+    annotated = begin YAML.load(associated_id) rescue nil end
+    unless annotated.nil?
+      associated_id = annotated.id
+      associated_class = annotated.class.name
+    end
     benchmark.send("smooch_save_#{type}_message") do
-      Bot::Smooch.save_message(json_message, app_id, User.current, request_type, annotated)
+      Bot::Smooch.save_message(message_json, app_id, User.current, request_type, associated_id, associated_class)
     end
     benchmark.finish
     User.current = nil
