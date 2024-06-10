cfb502f07c84d6d997a5ffe69aafc01268014ffd
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2024-06-10 10:34:00 +0300

    CV2- 4715 no method error undefined method user agent for nil class (#1919)
    * CV2-4441: fix sentry issue
    
    * CV2-4715: fix test coverage

--- app/mailers/security_mailer.rb
+++ app/mailers/security_mailer.rb
@@ -4,9 +4,10 @@ class SecurityMailer < ApplicationMailer
   def notify(user_id, type, activity_id)
     user = User.find_by_id(user_id)
     activity = LoginActivity.find_by_id(activity_id)
+    return if user.nil? || activity.nil?
     address = []
     Geocoder.configure(language: I18n.locale)
-    ip_result = Geocoder.search(activity&.ip)&.first
+    ip_result = Geocoder.search(activity.ip)&.first
     unless ip_result.blank? || ip_result.data["loc"].blank?
       loc_result = Geocoder.search(ip_result.data["loc"]).first
       address = [loc_result.city, loc_result.country] unless loc_result.nil?
@@ -14,13 +15,14 @@ class SecurityMailer < ApplicationMailer
     @user = user
     @type = type
     email = user.email
-    @user_agent = UserAgent.parse(activity.user_agent)
+    user_agent = UserAgent.parse(activity.user_agent)
     @location = address.compact.join(', ')
     @timestamp = activity.created_at
     @ip = activity.ip
-    @platform = begin @user_agent.os.split.first rescue 'Unknown' end
+    @platform = begin user_agent.os.split.first rescue 'Unknown' end
+    @browser = begin user_agent.browser rescue 'Unknown' end
     subject = I18n.t("mail_security.#{type}_subject",
-      app_name: CheckConfig.get('app_name'), browser: @user_agent.browser, platform: @platform)
+      app_name: CheckConfig.get('app_name'), browser: @browser, platform: @platform)
     mail(to: email, subject: subject)
   end
 
@@ -29,5 +31,4 @@ class SecurityMailer < ApplicationMailer
     attachments.inline['signup.png'] = File.read('public/images/signup.png')
     mail(to: @user.email, subject: subject)
   end
-
 end
