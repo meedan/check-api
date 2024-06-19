fe9d7032f5bcc7c121922cde47483f215ddcb50f
Skye Bender-deMoll <skye@meedan.org>
2024-06-19 16:21:15 +0100

    [CV2-4007] testing _checkdesk_session cookie permissions allowing access to all checkmedia subdomians

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_checkdesk_session'
+# allow subdomains to aceess the session cookie
+# TODO: do we need to set seperate cookies for qa and live so the wrong ones don't get posted?
+Rails.application.config.session_store :cookie_store, :key => '_checkdesk_session', :domain => '.checkmedia.org'
