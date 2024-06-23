f7ec3763aa9f8e5298fae290a899b24035f4740b
Caio <117518+caiosba@users.noreply.github.com>
2024-06-23 08:59:05 -0300

    Merging migrations [skip ci]

--- db/migrate/20240619131714_add_team_id_to_claim_descriptions.rb
+++ db/migrate/20240619131714_add_team_id_to_claim_descriptions.rb
@@ -1,5 +1,6 @@
 class AddTeamIdToClaimDescriptions < ActiveRecord::Migration[6.1]
   def change
     add_reference :claim_descriptions, :team, index: true
+    change_column_null :claim_descriptions, :project_media_id, true
   end
 end
--- db/migrate/20240619141452_allow_null_project_medias_to_claims.rb
+++
@@ -1,5 +0,0 @@
-class AllowNullProjectMediasToClaims < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null :claim_descriptions, :project_media_id, true
-  end
-end
