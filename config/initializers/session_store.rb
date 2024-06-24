fdead1f493915f63cd89230e9bbf8e724eccc432
Skye Bender-deMoll <skye@meedan.org>
2024-06-24 16:03:53 +0100

    try to tell rails to set with . instead of explicit domain which was breaking integration tests

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # allow subdomains to aceess the session cookie
+# :domain => :all tells Rails to put a dot in front of the cookie domain (which is whatever host your browser has
+# browsed to), such that the cookie applies to all subdomains.
 # TODO: do we need to set seperate cookies for qa and live so the wrong ones don't get posted?
-Rails.application.config.session_store :cookie_store, :key => '_checkdesk_session', :domain => '.checkmedia.org'
+Rails.application.config.session_store :cookie_store, :key => '_checkdesk_session', :domain => :all
