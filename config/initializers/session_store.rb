c7da502de8b39b0c79e289df3da305fe2253b8cf
Jay Joshua <7008757+jayjay-w@users.noreply.github.com>
2024-06-25 11:31:13 +0200

    Set default domains for test and development environments
    When running tests, there is no valid domain set, so our omniauth tests were failing.
    
    This attempts to fix that by using a separate `checkmedia.org` domain for both dev
    and test environments.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,4 +4,10 @@
 # :domain => :all tells Rails to put a dot in front of the cookie domain (which is whatever host your browser has
 # browsed to), such that the cookie applies to all subdomains.
 # TODO: do we need to set seperate cookies for qa and live so the wrong ones don't get posted?
-Rails.application.config.session_store :cookie_store, :key => '_checkdesk_session', :domain => :all
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, key: '_checkdesk_session', domain: '.checkmedia.org'
+elsif Rails.env.development?
+  Rails.application.config.session_store :cookie_store, key: '_checkdesk_session', domain: 'localhost'
+else
+  Rails.application.config.session_store :cookie_store, key: '_checkdesk_session'
+end
