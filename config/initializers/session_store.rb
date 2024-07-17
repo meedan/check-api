012934f97756d89053a6775be696a73aa20eccaf
Jay Joshua <7008757+jayjay-w@users.noreply.github.com>
2024-07-17 16:49:47 +0200

    Revert "[CV2-4007] update _checkdesk_session cookie permissions to entire dom…"
    This reverts commit 06fc0a060b9429d475f15a88670338bfe3eb05ed.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-# Retrieve the session key and domain based on the environment using CheckConfig.
-cookie_key = CheckConfig.get('session_store_key', '_checkdesk_session')
-domain_setting = CheckConfig.get('session_store_domain', Rails.env.development? ? 'localhost' : '.checkmedia.org')
-
-# Configure the session store with the dynamically obtained session key and domain.
-Rails.application.config.session_store :cookie_store, key: cookie_key, domain: domain_setting
+Rails.application.config.session_store :cookie_store, key: '_checkdesk_session'
