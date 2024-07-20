62893bba682e4c51c7e75e3b91eaaac8b3afb312
chinelo-obitube <chinelo@ChinelOtubesMac.lan>
2024-07-21 00:04:08 +0100

    rewrite coverage

--- test/format-coverage.sh
+++ test/format-coverage.sh
@@ -1,18 +1,18 @@
-# #!/bin/bash
+#!/bin/bash
 
 # apt-get install -y awscli
 
 # if [ "$GITHUB_PULL_REQUEST" == "false" ]
 # then
-#   ./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$GITHUB_JOB_NAME.json ../coverage/.resultset.json
-#   aws s3 cp ../coverage/codeclimate.$GITHUB_JOB_NAME.json s3://check-api-travis/codeclimate/$GITHUB_REPO_SLUG/$GITHUB_BUILD_NUMBER/codeclimate.$GITHUB_JOB_NAME.json
+  ./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$GITHUB_JOB_NAME.json ../coverage/.resultset.json
+  aws s3 cp ../coverage/codeclimate.$GITHUB_JOB_NAME.json s3://check-api-travis/codeclimate/$GITHUB_REPO_SLUG/$GITHUB_BUILD_NUMBER/codeclimate.$GITHUB_JOB_NAME.json
+
+# #!/bin/bash
+
+# apt-get install -y awscli
+
+# if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+# then
+#   ./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$TRAVIS_JOB_NAME.json ../coverage/.resultset.json
+#   aws s3 cp ../coverage/codeclimate.$TRAVIS_JOB_NAME.json s3://check-api-travis/codeclimate/$TRAVIS_REPO_SLUG/$TRAVIS_BUILD_NUMBER/codeclimate.$TRAVIS_JOB_NAME.json
 # fi
-#!/bin/bash
-
-apt-get install -y awscli
-
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
-then
-  ./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$TRAVIS_JOB_NAME.json ../coverage/.resultset.json
-  aws s3 cp ../coverage/codeclimate.$TRAVIS_JOB_NAME.json s3://check-api-travis/codeclimate/$TRAVIS_REPO_SLUG/$TRAVIS_BUILD_NUMBER/codeclimate.$TRAVIS_JOB_NAME.json
-fi
