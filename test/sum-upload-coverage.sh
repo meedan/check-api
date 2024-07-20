21aad878e4723f769c6271686f317727f207c306
chinelo-obitube <chinelo@ChinelOtubesMac.lan>
2024-07-20 23:21:10 +0100

    rewrite coverage

--- test/sum-upload-coverage.sh
+++ test/sum-upload-coverage.sh
@@ -1,9 +1,31 @@
+# #!/bin/bash
+
+# if [[ "$GITHUB_PULL_REQUEST" == "false" ]] && [[ $GITHUB_TEST_RESULT == 0 ]]
+# then
+#   rm -rf ../coverage/*
+#   aws s3 cp --recursive s3://check-api-travis/codeclimate/$GITHUB_REPO_SLUG/$GITHUB_BUILD_NUMBER/ ../coverage
+#   if [[ $(ls ../coverage/codeclimate.* | wc -l) -eq 3 ]]
+#   then
+#     # Make sure we are not dealing with a file that is still being uploaded
+#     previous_size=0
+#     size=$(du -s ../coverage/ | cut -f1)
+#     while [ $size -gt $previous_size ]
+#     do
+#       previous_size=$size
+#       sleep 5
+#       size=$(du -s ../coverage/ | cut -f1)
+#     done
+#     ./cc-test-reporter sum-coverage --output - --parts 3 ../coverage/codeclimate.* | sed 's/\/home\/runner\/work\/check-api\///g' > ../coverage/codeclimate.json
+#     cat ../coverage/codeclimate.json | ./cc-test-reporter upload-coverage --input -
+#     ./cc-test-reporter show-coverage ../coverage/codeclimate.json
+#   fi
+# fi
 #!/bin/bash
 
-if [[ "$GITHUB_PULL_REQUEST" == "false" ]] && [[ $GITHUB_TEST_RESULT == 0 ]]
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ $TRAVIS_TEST_RESULT == 0 ]]
 then
   rm -rf ../coverage/*
-  aws s3 cp --recursive s3://check-api-travis/codeclimate/$GITHUB_REPO_SLUG/$GITHUB_BUILD_NUMBER/ ../coverage
+  aws s3 cp --recursive s3://check-api-travis/codeclimate/$TRAVIS_REPO_SLUG/$TRAVIS_BUILD_NUMBER/ ../coverage
   if [[ $(ls ../coverage/codeclimate.* | wc -l) -eq 3 ]]
   then
     # Make sure we are not dealing with a file that is still being uploaded
@@ -15,7 +37,7 @@ then
       sleep 5
       size=$(du -s ../coverage/ | cut -f1)
     done
-    ./cc-test-reporter sum-coverage --output - --parts 3 ../coverage/codeclimate.* | sed 's/\/home\/runner\/work\/check-api\///g' > ../coverage/codeclimate.json
+    ./cc-test-reporter sum-coverage --output - --parts 3 ../coverage/codeclimate.* | sed 's/\/home\/travis\/build\/meedan\/check-api\///g' > ../coverage/codeclimate.json
     cat ../coverage/codeclimate.json | ./cc-test-reporter upload-coverage --input -
     ./cc-test-reporter show-coverage ../coverage/codeclimate.json
   fi
