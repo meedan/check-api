43622a3a2bc2fdd425c1b32e8e94b593733efd87
chinelo-obitube <chinelo@ChinelOtubesMac.lan>
2024-07-20 13:42:05 +0100

    change path

--- test/format-coverage.sh
+++ test/format-coverage.sh
@@ -2,7 +2,7 @@
 
 apt-get install -y awscli
 
-# if [ "$GITHUB_PULL_REQUEST" == "false" ]
+# if [ "$GITHUB_PULL_REQUEST" == "true" ]
 # then
   ./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$GITHUB_JOB_NAME.json ../coverage/.resultset.json
   aws s3 cp ../coverage/codeclimate.$GITHUB_JOB_NAME.json s3://check-api-travis/codeclimate/$GITHUB_REPO_SLUG/$GITHUB_BUILD_NUMBER/codeclimate.$GITHUB_JOB_NAME.json
