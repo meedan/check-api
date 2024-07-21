f3266b30948035d6fe3c888c91c0895db72fd43d
chinelo-obitube <chinelo@ChinelOtubesMac.lan>
2024-07-21 01:53:54 +0100

    rewrite coverage

--- test/format-coverage.sh
+++ test/format-coverage.sh
@@ -4,8 +4,8 @@
 
 # if [ "$GITHUB_PULL_REQUEST" == "false" ]
 # then
-  ./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$GITHUB_JOB_NAME.json ../coverage/.resultset.json
-  aws s3 cp ../coverage/codeclimate.$GITHUB_JOB_NAME.json s3://check-api-travis/codeclimate/$GITHUB_REPO_SLUG/$GITHUB_BUILD_NUMBER/codeclimate.$GITHUB_JOB_NAME.json
+./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$GITHUB_JOB_NAME.json ../coverage/.resultset.json
+aws s3 cp ../coverage/codeclimate.$GITHUB_JOB_NAME.json s3://check-api-travis/codeclimate/$GITHUB_REPO_SLUG/$GITHUB_BUILD_NUMBER/codeclimate.$GITHUB_JOB_NAME.json
 
 # #!/bin/bash
 
