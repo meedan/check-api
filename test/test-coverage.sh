ac793395649aec4f3698cfe1af5e93ec23eb4695
chinelo-obitube <chinelo@ChinelOtubesMac.lan>
2024-07-21 02:21:49 +0100

    rewrite coverage

--- test/test-coverage.sh
+++ test/test-coverage.sh
@@ -6,5 +6,10 @@ export GOPATH=/tmp/go
 git config --global --add safe.directory /app
 mkdir -p /tmp/go/src/github.com/codeclimate
 git clone -b 0.10.3 https://github.com/codeclimate/test-reporter /tmp/go/src/github.com/codeclimate/test-reporter
-./test/format-coverage.sh 
+cd /tmp/go/src/github.com/codeclimate/test-reporter
+go mod init github.com/codeclimate/test-reporter
+go mod tidy
+cd -
+./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$GITHUB_JOB_NAME.json ../coverage/.resultset.json
+aws s3 cp ../coverage/codeclimate.$GITHUB_JOB_NAME.json s3://check-api-travis/codeclimate/$GITHUB_REPO_SLUG/$GITHUB_BUILD_NUMBER/codeclimate.$GITHUB_JOB_NAME.json
 echo 'Combining and uploading coverage...'
