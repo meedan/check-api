270929bcc4c51450ec6ec0854f897838cc205635
chinelo-obitube <chinelo@ChinelOtubesMac.lan>
2024-07-21 11:44:20 +0100

    rewrite coverage

--- test/format-coverage.sh
+++ test/format-coverage.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# apt-get install -y awscli
+apt-get install -y awscli
 
-# if [ "$GITHUB_PULL_REQUEST" == "false" ]
-# then
+if [ "$GITHUB_PULL_REQUEST" != "pull_request" ]
+then
 ./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$GITHUB_JOB_NAME.json ../coverage/.resultset.json
 aws s3 cp ../coverage/codeclimate.$GITHUB_JOB_NAME.json s3://check-api-travis/codeclimate/$GITHUB_REPO_SLUG/$GITHUB_BUILD_NUMBER/codeclimate.$GITHUB_JOB_NAME.json
+fi
 
 # #!/bin/bash
 
