5a57004dfdbcc79fe1c47be949e3d664f26005e8
chinelo-obitube <chinelo@ChinelOtubesMac.lan>
2024-07-21 01:32:45 +0100

    rewrite coverage

--- test/test-coverage.sh
+++ test/test-coverage.sh
@@ -7,5 +7,4 @@ git config --global --add safe.directory /app
 mkdir -p /tmp/go/src/github.com/codeclimate
 git clone -b 0.10.3 https://github.com/codeclimate/test-reporter /tmp/go/src/github.com/codeclimate/test-reporter
 ./test/format-coverage.sh 
-echo 'Combining and uploading coverage...' && cd test && ./sum-upload-coverage.sh && cd -
-echo 'Parallel tests runtime log' && cat tmp/parallel_runtime_test.log
+echo 'Combining and uploading coverage...'
