a6ff075cba612f59b3d77a4658373cfcad1c44c1
chinelo-obitube <chinelo@ChinelOtubesMac.lan>
2024-07-21 02:57:39 +0100

    rewrite coverage

--- test/test-coverage.sh
+++ test/test-coverage.sh
@@ -9,6 +9,7 @@ git clone -b 0.10.3 https://github.com/codeclimate/test-reporter /tmp/go/src/git
 cd /tmp/go/src/github.com/codeclimate/test-reporter
 go mod init github.com/codeclimate/test-reporter
 go mod tidy
+apt-get install -y awscli
 cd -
 ./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$GITHUB_JOB_NAME.json ../coverage/.resultset.json
 aws s3 cp ../coverage/codeclimate.$GITHUB_JOB_NAME.json s3://check-api-travis/codeclimate/$GITHUB_REPO_SLUG/$GITHUB_BUILD_NUMBER/codeclimate.$GITHUB_JOB_NAME.json
