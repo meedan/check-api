294fbd992d750e1e0e1dfeb9933008426a544c17
chinelo-obitube <chinelo@ChinelOtubesMac.lan>
2024-07-21 03:05:33 +0100

    rewrite coverage

--- test/test-coverage.sh
+++ test/test-coverage.sh
@@ -1,21 +1,3 @@
-# #!/bin/bash
-
-# apt-get install -y golang-go golang-glide
-# printf '#!/bin/bash\ngo run /tmp/go/src/github.com/codeclimate/test-reporter/main.go $@\n' > test/cc-test-reporter && chmod +x test/cc-test-reporter
-# export GOPATH=/tmp/go
-# git config --global --add safe.directory /app
-# mkdir -p /tmp/go/src/github.com/codeclimate
-# git clone -b 0.10.3 https://github.com/codeclimate/test-reporter /tmp/go/src/github.com/codeclimate/test-reporter
-# cd /tmp/go/src/github.com/codeclimate/test-reporter
-# go mod init github.com/codeclimate/test-reporter
-# go mod tidy
-# apt-get install -y awscli
-# cd -
-# ./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$GITHUB_JOB_NAME.json ../coverage/.resultset.json
-# aws s3 cp ../coverage/codeclimate.$GITHUB_JOB_NAME.json s3://check-api-travis/codeclimate/$GITHUB_REPO_SLUG/$GITHUB_BUILD_NUMBER/codeclimate.$GITHUB_JOB_NAME.json
-# echo 'Combining and uploading coverage...'
-
-
 #!/bin/bash
 
 apt-get install -y golang-go golang-glide
