fb82962f6076e418bbf41aacba88f233269209fd
Chinelo Obitube <74656858+chinelo-obitube@users.noreply.github.com>
2024-07-22 23:53:53 +0100

    CV2-4794:Add coverage results to upload to s3 bucket on push event (#1965)
    * CV2-4794:Add coverage results to upload to s3 bucket on push event
    
    * change the variable name from GITHUB_PULL_REQUEST to GITHUB_EVENT to match action
    
    * fix syntax error
    
    * fix fmt error
    
    ---------
    
    Co-authored-by: chinelo-obitube <chinelo@ChinelOtubesMac.lan>

--- test/format-coverage.sh
+++ test/format-coverage.sh
@@ -2,7 +2,7 @@
 
 apt-get install -y awscli
 
-if [ "$GITHUB_PULL_REQUEST" == "pull_request" ]
+if [[ "$GITHUB_EVENT" == "pull_request" || "$GITHUB_EVENT" == "push" ]]
 then
 ./test/cc-test-reporter format-coverage -t simplecov --output ../coverage/codeclimate.$GITHUB_JOB_NAME.json ../coverage/.resultset.json
 aws s3 cp ../coverage/codeclimate.$GITHUB_JOB_NAME.json s3://check-api-github/codeclimate/$GITHUB_REPO/$GITHUB_BUILD_NUMBER/codeclimate.$GITHUB_JOB_NAME.json
