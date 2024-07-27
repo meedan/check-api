1c5f906a5d86a4128f2a9aebbf4c2d37ea98e982
Jay Joshua <7008757+jayjay-w@users.noreply.github.com>
2024-07-27 10:17:51 +0200

    update _checkdesk_session cookie permissions to entire domain (#1973)
    * [CV2-4007] Set check-api session cookie based on environment
    
    Set check-api session cookie based on SSM value
    
    
    ---------
    
    Co-authored-by: Skye Bender-deMoll <skye@meedan.org>

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_checkdesk_session'
+# Retrieve the session key and domain based on the environment using CheckConfig.
+cookie_key = CheckConfig.get('session_store_key', '_checkdesk_session')
+domain_setting = CheckConfig.get('session_store_domain', Rails.env.development? ? 'localhost' : 'checkmedia.org')
+
+# Configure the session store with the dynamically obtained session key and domain.
+Rails.application.config.session_store :cookie_store, key: cookie_key, domain: domain_setting
