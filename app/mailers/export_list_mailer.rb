1726979812704975487f3826cdaee572a15be864
Caio <117518+caiosba@users.noreply.github.com>
2024-08-23 19:48:57 -0300

    [WIP] Ticket CV2-5067: Fixing e-mail template

--- app/mailers/export_list_mailer.rb
+++ app/mailers/export_list_mailer.rb
@@ -4,7 +4,8 @@ class ExportListMailer < ApplicationMailer
   def send_csv(csv_file_url, user)
     @csv_file_url = csv_file_url
     @user = user
-    @expire_in = CheckConfig.get('export_csv_expire', 7.days.to_i, :integer) / (60 * 60 * 24)
+    expire_in = Time.now.to_i + CheckConfig.get('export_csv_expire', 7.days.to_i, :integer)
+    @expire_in = I18n.l(Time.at(expire_in), format: :email)
     subject = I18n.t('mails_notifications.export_list.subject')
     Rails.logger.info "Sending export e-mail to #{@user.email}"
     mail(to: @user.email, email_type: 'export_list', subject: subject)
