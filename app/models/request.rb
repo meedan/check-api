a77f00c797c820bad7ec03924987fd8f022f1f99
Devin Gaffney <itsme@devingaffney.com>
2024-09-04 13:04:33 -0700

    Cv2 5080 request model to presto (#2015)
    * CV2-5080 update request model alegre calls to use presto-based alegre querying
    
    * move to sync
    
    * update for bypassing async calls in tests

--- app/models/request.rb
+++ app/models/request.rb
@@ -53,7 +53,7 @@ class Request < ApplicationRecord
         models_thresholds = self.text_similarity_settings.reject{ |_k, v| v['min_words'] > words }
         if models_thresholds.count > 0
           params = { text: media.quote, models: models_thresholds.keys, per_model_threshold: models_thresholds.transform_values{ |v| v['threshold'] }, limit: alegre_limit, context: context }
-          similar_request_id = ::Bot::Alegre.request('post', '/text/similarity/search/', params)&.dig('result').to_a.collect{ |result| result&.dig('_source', 'context', 'request_id').to_i }.find{ |id| id != 0 && id < self.id }
+          similar_request_id = ::Bot::Alegre.get_sync_with_params(params, "text")&.dig('result').to_a.collect{ |result| result&.dig('_source', 'context', 'request_id').to_i }.find{ |id| id != 0 && id < self.id }
         end
       # elsif ['UploadedImage', 'UploadedAudio', 'UploadedVideo'].include?(media.type)
       #   threshold = 0.85 #FIXME: Should be feed setting
@@ -194,7 +194,7 @@ class Request < ApplicationRecord
         models: request.text_similarity_settings.keys(),
         context: context
       }
-      ::Bot::Alegre.request('post', '/text/similarity/', params)
+      ::Bot::Alegre.get_async_with_params(params, "text")
     # elsif ['UploadedImage', 'UploadedAudio', 'UploadedVideo'].include?(media.type)
     #   type = media.type.gsub(/^Uploaded/, '').downcase
     #   url = media.file&.file&.public_url
