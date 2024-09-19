935f44f6cc572d6494bf4f6389a3ba5cbae7e692
manu vasconcelos <manu@meedan.com>
2024-09-19 16:54:09 -0300

    move run_later methods to a module

--- config/initializers/class_extensions.rb
+++ config/initializers/class_extensions.rb
@@ -1,5 +1,3 @@
 class Class
-  def run_later_in(time, klass_method, *method_args)
-    GenericWorker.perform_in(time, self.to_s, klass_method, *method_args)
-  end
+include GenericWorkerHelpers
 end
