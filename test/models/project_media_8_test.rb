2c9c1d0e8c8e93d86657a7cc3b060059990f2a8e
manu vasconcelos <manu@meedan.com>
2024-09-20 09:21:59 -0300

    add GenericWorkerHelpersTest

--- test/models/project_media_8_test.rb
+++ test/models/project_media_8_test.rb
@@ -69,19 +69,4 @@ class ProjectMedia8Test < ActiveSupport::TestCase
 
     assert_equal 1, GenericWorker.jobs.size
   end
-
-  test "when using :run_later_in to schedule multiple tags creation, tags should be created" do
-    Sidekiq::Testing.inline!
-
-    team = create_team
-    project = create_project team: team
-    pm = create_project_media project: project
-
-    project_media_id = pm.id
-    tags_json = ['one', 'two', 'three'].to_json
-
-    ProjectMedia.run_later_in(0.second, 'create_tags', project_media_id, tags_json, user_id: pm.user_id)
-
-    assert_equal 3, pm.annotations('tag').count
-  end
 end
