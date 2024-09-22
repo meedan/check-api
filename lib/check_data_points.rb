c704455e10ce9d1f2dd4f9c1fe385ecd53ba9d69
Sawy <melsawy.be7eri@gmail.com>
2024-09-22 10:52:07 +0300

    CV2-5138: add users report

--- lib/check_data_points.rb
+++ lib/check_data_points.rb
@@ -1,4 +1,6 @@
 class CheckDataPoints
+  SEARCH_RESULT_TYPES = ['relevant_search_result_requests', 'irrelevant_search_result_requests', 'timeout_search_requests']
+
   # 1) Number of tipline messages
   def self.tipline_messages(team_id, start_date, end_date, granularity = nil)
     start_date, end_date = self.parse_start_end_dates(start_date, end_date)
@@ -18,7 +20,7 @@ class CheckDataPoints
     start_date, end_date = self.parse_start_end_dates(start_date, end_date)
     TiplineRequest.where(
       team_id: team_id,
-      smooch_request_type: ['relevant_search_result_requests', 'irrelevant_search_result_requests'],
+      smooch_request_type: SEARCH_RESULT_TYPES,
       created_at: start_date..end_date,
     ).group('smooch_request_type').count
   end
@@ -34,8 +36,8 @@ class CheckDataPoints
   def self.newsletters_sent(team_id, start_date, end_date, granularity = nil)
     start_date, end_date = self.parse_start_end_dates(start_date, end_date)
     query = TiplineNewsletterDelivery
-    .joins("INNER JOIN tipline_newsletters tnl ON tipline_newsletter_deliveries.tipline_newsletter_id = tnl.id")
-    .where('tnl.team_id = ?', team_id)
+    .joins(:tipline_newsletter)
+    .where('tipline_newsletters.team_id': team.id)
     .where(created_at: start_date..end_date)
     self.query_based_on_granularity(query, granularity, 'tipline_newsletter_deliveries.created_at')
   end
@@ -44,15 +46,58 @@ class CheckDataPoints
   def self.media_received_by_type(team_id, start_date, end_date)
     bot = BotUser.smooch_user
     start_date, end_date = self.parse_start_end_dates(start_date, end_date)
-    query = ProjectMedia.where(team_id: team_id, user_id: bot.id, created_at: start_date..end_date)
-    .joins(:media).group('medias.type')
-    query.count
+    ProjectMedia.where(team_id: team_id, user_id: bot.id, created_at: start_date..end_date)
+    .joins(:media).group('medias.type').count
   end
 
-  # Top clusters
-  def self.top_clusters(team_id, start_date, end_date)
-
+  # 7) Top clusters
+  def self.top_clusters(team_id, start_date, end_date, limit = 5)
+    result = CheckSearch.new({sort: 'demand', eslimit: limit }.to_json, nil, team.id)
+    result.medias.collect{|pm| { id: pm.id, demand: pm.demand } }
   end
+
+  # 8) Top media tags
+  def self.top_media_tags(team_id, start_date, end_date, limit = 5)
+    result = CheckSearch.new({sort: 'demand', eslimit: limit }.to_json, nil, team.id)
+    result.medias.collect{|pm| { id: pm.id, demand: pm.demand } }
+  end
+
+  # 9) Articles sent
+  def self.articles_sent()
+  end
+
+  # 10) Average response time
+  def average_response_time()
+  end
+
+  # 11) Number of users
+  # 11.1) All users
+  def all_users(team_id, start_date, end_date, granularity = nil)
+    start_date, end_date = self.parse_start_end_dates(start_date, end_date)
+    query = TiplineRequest.where(team_id: team_id, created_at: start_date..end_date)
+    self.query_tipline_users_based_on_granularity(query, granularity)
+  end
+
+  # 11.2) Returning users
+  def returning_users(team_id, start_date, end_date, granularity = nil)
+    # Number of returning users (at least one session in the current month, and at least one session in the last previous 2 months)
+    start_date, end_date = self.parse_start_end_dates(start_date, end_date)
+    uids = TiplineRequest.where(team_id: team_id, created_at: start_date.ago(2.months)..start_date).map(&:tipline_user_uid).uniq
+    query = TiplineRequest.where(team_id: team_id, tipline_user_uid: uids, created_at: start_date..end_date)
+    self.query_tipline_users_based_on_granularity(query, granularity)
+  end
+
+  # 11.3) New users
+  def new_users(team_id, start_date, end_date, granularity = nil)
+    start_date, end_date = self.parse_start_end_dates(start_date, end_date)
+    query = TiplineRequest.where(team_id: team_id)
+    .joins("INNER JOIN annotations a ON tipline_requests.team_id = a.annotated_id")
+    .where('a.annotation_type': 'smooch_user', 'a.annotated_type': 'Team')
+    .where('a.created_at': start_date..end_date)
+    self.query_tipline_users_based_on_granularity(query, granularity)
+  end
+
+  private
 
   def self.parse_start_end_dates(start_date, end_date)
     # date format is `2023-08-23`
@@ -71,6 +116,14 @@ class CheckDataPoints
       query.group("date_trunc('#{granularity}', #{col_name})").count
     else
       query.count
+    end
+  end
+
+  def self.query_tipline_users_based_on_granularity(query, granularity)
+    if %w(year quarter month week day).include?(granularity)
+      query.group("date_trunc('#{granularity}', created_at)").count('DISTINCT(tipline_user_uid)')
+    else
+      query.count('DISTINCT(tipline_user_uid)')
     end
   end
 end
