0da135ca28332c983722bf6bad0449ba538e5aaa
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2024-10-28 07:04:34 +0300

    CV2-5532: Move all Explainers from the target to the source (#2102)
    * CV2-5532: move all explainers from the target to the source
    
    * CV2-5532: apply PR comment
    
    * CV2-5532: apply PR comments

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -24,6 +24,7 @@ class Relationship < ApplicationRecord
   after_update :reset_counters, prepend: true
   after_update :propagate_inversion
   after_save :turn_off_unmatched_field, if: proc { |r| r.is_confirmed? || r.is_suggested? }
+  after_save :move_explainers_to_source, if: proc { |r| r.is_confirmed? }
   before_destroy :archive_detach_to_list
   after_destroy :update_counters, prepend: true
   after_destroy :turn_on_unmatched_field, if: proc { |r| r.is_confirmed? || r.is_suggested? }
@@ -323,6 +324,27 @@ class Relationship < ApplicationRecord
       secondary.project_id = main.project_id
       secondary.save!
       CheckNotification::InfoMessages.send('moved_to_private_folder', item_title: secondary.title)
+    end
+  end
+
+  def move_explainers_to_source
+    # Three cases to move explainers
+    # 1) Relationship is new and confirmed
+    # 2) Item is being confirmed (move from suggested to confirmed)
+    # 3) Pin item (sawp source_id & target_id)
+    if self.relationship_type_before_last_save.nil? || self.is_being_confirmed? || (self.source_id_before_last_save && self.source_id_before_last_save == self.target_id)
+      ExplainerItem.transaction do
+        # Destroy common Explainer from target item (use destroy to log this event)
+        explainer_ids = ExplainerItem.where(project_media_id: [self.source_id, self.target_id])
+        .group('explainer_id').having("count(explainer_id) = ?", 2).pluck(:explainer_id)
+        ExplainerItem.where(explainer_id: explainer_ids, project_media_id: self.target_id).destroy_all
+        # Move the Explainer from target to source by using update_all(as no callbacks) and then update logs
+        ExplainerItem.where(project_media_id: self.target_id).update_all(project_media_id: self.source_id)
+        # Update logs (to make item history consistent with Explainers attached to item)
+        Version.from_partition(self.source.team_id)
+        .where(event_type: 'create_explaineritem', associated_type: 'ProjectMedia', associated_id: self.target_id)
+        .update_all(associated_id: self.source_id)
+      end
     end
   end
 
