402929d608b4c5ff7097b0d9310adf82a16a2584
Sawy <melsawy.be7eri@gmail.com>
2024-10-28 12:10:45 +0300

    CV2-5529: de-duplicate requests for matched items

--- app/models/concerns/smooch_messages.rb
+++ app/models/concerns/smooch_messages.rb
@@ -291,16 +291,20 @@ module SmoochMessages
         if message['type'] == 'text'
           # Get an item for long text (message that match number of words condition)
           if message['payload'].nil?
-            contains_link = Twitter::TwitterText::Extractor.extract_urls(message['text'])
+            contains_link = self.extract_url(message['text'])
             messages << message if !contains_link.blank? || ::Bot::Alegre.get_number_of_words(message['text'].to_s) > self.min_number_of_words_for_tipline_long_text
-            text << message['text']
+            # Text should be a link only in case we have two matched items (link and long text)
+            text << (contains_link.blank? ? message['text'] : contains_link.url)
           end
         elsif !message['mediaUrl'].blank?
           # Get an item for each media file
           if !message['text'].blank? && ::Bot::Alegre.get_number_of_words(message['text'].to_s) > self.min_number_of_words_for_tipline_long_text
             message['caption'] = message['text']
+            # Text should be a media url in case we have two matched items (media and caption)
+            message['text'] = message['mediaUrl'].to_s
+          else
+            message['text'] = [message['text'], message['mediaUrl'].to_s].compact.join("\n#{Bot::Smooch::MESSAGE_BOUNDARY}")
           end
-          message['text'] = [message['text'], message['mediaUrl'].to_s].compact.join("\n#{Bot::Smooch::MESSAGE_BOUNDARY}")
           text << message['text']
           messages << self.adjust_media_type(message)
         end
