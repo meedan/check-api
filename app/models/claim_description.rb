09b274cda67e95c05573214fe45f96508ae88bcb
Caio <117518+caiosba@users.noreply.github.com>
2024-11-11 19:01:35 -0300

    Reset report fields when fact-check is removed from item.
    Previously, we were already changing the report status to "paused" when a fact-check was removed from item. This PR does one more thing: also resets all text fields related to the report.
    
    Reference: CV2-5630.

--- app/models/claim_description.rb
+++ app/models/claim_description.rb
@@ -16,7 +16,7 @@ class ClaimDescription < ApplicationRecord
   validates_uniqueness_of :project_media_id, allow_nil: true
   validate :cant_apply_article_to_item_if_article_is_in_the_trash
   after_commit :update_fact_check, on: [:update]
-  after_update :update_report_status
+  after_update :update_report
   after_update :reset_item_rating_if_removed
   after_update :replace_media, unless: proc { |cd| cd.disable_replace_media }
   after_update :migrate_claim_and_fact_check_logs, if: proc { |cd| cd.saved_change_to_project_media_id? && !cd.project_media_id.nil? }
@@ -69,8 +69,8 @@ class ClaimDescription < ApplicationRecord
     end
   end
 
-  # Pause report when claim/fact-check is removed
-  def update_report_status
+  # Pause and update eport when claim/fact-check is removed
+  def update_report
     if self.project_media_id.nil? && !self.project_media_id_before_last_save.nil?
       # Update report status
       pm = ProjectMedia.find(self.project_media_id_before_last_save)
@@ -79,6 +79,7 @@ class ClaimDescription < ApplicationRecord
         report = report.load
         data = report.data.clone.with_indifferent_access
         data[:state] = 'paused'
+        data[:options] = data[:options].to_h.merge({ description: '', headline: '', title: '', text: '' })
         report.data = data
         report.save!
       end
