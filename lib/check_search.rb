0bcf8306c35a7dd511a2b73a0a80022d87655214
Caio <117518+caiosba@users.noreply.github.com>
2024-11-14 21:16:23 -0300

    Fixing RegExp and fixing two flaky tests not related to this PR really

--- lib/check_search.rb
+++ lib/check_search.rb
@@ -60,7 +60,7 @@ class CheckSearch
     unless @options['keyword'].blank?
       # This regex removes all characters except letters, numbers, hashtag, search operators, emojis and whitespace
       # in any language - stripping out special characters can improve match results
-      @options['keyword'].gsub!(/[^[:word:]\s#~+-|()"\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1F1E6}-\u{1F1FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/, ' ')
+      @options['keyword'].gsub!(/[^[:word:]\s#~+\-|()"\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1F1E6}-\u{1F1FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/, ' ')
 
       # Set fuzzy matching for keyword search, right now with automatic Levenshtein Edit Distance
       # https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-simple-query-string-query.html
