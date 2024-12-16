dbf9ca36f6d5ac0ec6d2c17363a670aed37b2020
Caio Almeida <117518+caiosba@users.noreply.github.com>
2024-12-16 10:47:57 -0300

    Handle single quote when searching by keyword. (#2158)
    Similarly to other characters, add single quotes to the list of characters to handle when searching by keyword.
    
    Fixes: CV2-5808.

--- test/lib/check_search_test.rb
+++ test/lib/check_search_test.rb
@@ -16,6 +16,10 @@ class CheckSearchTest < ActiveSupport::TestCase
     query = 'Something is going to happen on 04/11, reportedly'
     search = CheckSearch.new({ keyword: query }.to_json, nil, @team.id)
     assert_equal 'Something is going to happen on 04 11  reportedly', search.instance_variable_get('@options')['keyword']
+
+    query = "Something is going to happen on Foo's house"
+    search = CheckSearch.new({ keyword: query }.to_json, nil, @team.id)
+    assert_equal "Something is going to happen on Foo's house", search.instance_variable_get('@options')['keyword']
   end
 
   test "should search for array field containing nil values" do
