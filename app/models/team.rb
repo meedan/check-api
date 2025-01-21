ac49ba50deec08d7fb2aa3742a2cc6af43a5b20d
Caio <117518+caiosba@users.noreply.github.com>
2025-01-21 19:31:30 -0300

    Fixing conflicts for real

--- app/models/team.rb
+++ app/models/team.rb
@@ -569,35 +569,33 @@ class Team < ApplicationRecord
     pm.nil? ? CheckConfig.get('most_relevant_team_limit', 3, :integer) : CheckConfig.get('most_relevant_item_limit', 10, :integer)
   end
 
-  def search_for_similar_articles(query, pm = nil)
+  def search_for_similar_articles(query, pm = nil, settings = nil)
     # query:  expected to be text
     # pm: to request a most relevant to specific item and also include both FactCheck & Explainer
     limit = self.similar_articles_search_limit(pm)
     threads = []
     fc_items = []
-    # FIXME: Threads approach not working locally - requests from GraphiQL hang forever.
-    # ex_items = []
-    # threads = []
-    # threads << Thread.new {
-    result_ids = Bot::Smooch.search_for_similar_published_fact_checks_no_cache('text', query, [self.id], limit, nil, nil, nil, false, settings).map(&:id)
-    unless result_ids.blank?
-      fc_items = FactCheck.joins(claim_description: :project_media).where('project_medias.id': result_ids)
-      if pm.nil?
-        # This means we obtain relevant items for the Bot preview, so we should limit FactChecks to published articles;
-        # otherwise, relevant articles for ProjectMedia should include all FactChecks.
-        fc_items = fc_items.where(report_status: 'published')
-      elsif !pm.fact_check_id.nil?
-        # Exclude the ones already applied to a target item if exists.
-        fc_items = fc_items.where.not('fact_checks.id' => pm.fact_check_id) unless pm&.fact_check_id.nil?
+    ex_items = []
+    threads << Thread.new {
+      result_ids = Bot::Smooch.search_for_similar_published_fact_checks_no_cache('text', query, [self.id], limit, nil, nil, nil, false, settings).map(&:id)
+      unless result_ids.blank?
+        fc_items = FactCheck.joins(claim_description: :project_media).where('project_medias.id': result_ids)
+        if pm.nil?
+          # This means we obtain relevant items for the Bot preview, so we should limit FactChecks to published articles;
+          # otherwise, relevant articles for ProjectMedia should include all FactChecks.
+          fc_items = fc_items.where(report_status: 'published')
+        elsif !pm.fact_check_id.nil?
+          # Exclude the ones already applied to a target item if exists.
+          fc_items = fc_items.where.not('fact_checks.id' => pm.fact_check_id) unless pm&.fact_check_id.nil?
+        end
       end
-    end
-    # }
-    # threads << Thread.new {
-    ex_items = Bot::Smooch.search_for_explainers(nil, query, self.id, limit, nil, settings).distinct
-    # Exclude the ones already applied to a target item
-    ex_items = ex_items.where.not(id: pm.explainer_ids) unless pm&.explainer_ids.blank?
-    # }
-    # threads.map(&:join)
+    }
+    threads << Thread.new {
+      ex_items = Bot::Smooch.search_for_explainers(nil, query, self.id, limit, nil, settings).distinct
+      # Exclude the ones already applied to a target item
+      ex_items = ex_items.where.not(id: pm.explainer_ids) unless pm&.explainer_ids.blank?
+    }
+    threads.map(&:join)
     items = fc_items
     # Get Explainers if no fact-check returned or get similar_articles for a ProjectMedia
     items += ex_items if items.blank? || !pm.nil?
