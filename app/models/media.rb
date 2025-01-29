ba3eb7ed299993c22a29090c1f0bb33d2de33a75
manu vasconcelos <manu@meedan.com>
2025-01-29 09:55:33 -0300

    rename so it's clear media is created from original_claim
    We have very similar methods in ProjectMediaCreators.
    I think it probably makes sense to move those to Media
    as well, and try to deduplicate.
    
    But for now, so there is no confusion, I want to make
    it very clear that we are using these methods when
    creating from original_claim.

--- app/models/media.rb
+++ app/models/media.rb
@@ -84,16 +84,16 @@ class Media < ApplicationRecord
 
       case content_type
       when /^image\//
-        create_uploaded_file_media_from_url('UploadedImage', claim, ext)
+        create_uploaded_file_media_from_original_claim('UploadedImage', claim, ext)
       when /^video\//
-        create_uploaded_file_media_from_url('UploadedVideo', claim, ext)
+        create_uploaded_file_media_from_original_claim('UploadedVideo', claim, ext)
       when /^audio\//
-        create_uploaded_file_media_from_url('UploadedAudio', claim, ext)
+        create_uploaded_file_media_from_original_claim('UploadedAudio', claim, ext)
       else
-        create_link_media(claim, project_media_team)
+        create_link_media_from_original_claim(claim, project_media_team)
       end
     else
-      create_claim_media(claim)
+      create_claim_media_from_original_claim(claim)
     end
   end
 
@@ -134,7 +134,7 @@ class Media < ApplicationRecord
     response['content-type']
   end
 
-  def self.create_uploaded_file_media_from_url(media_type, url, ext)
+  def self.create_uploaded_file_media_from_original_claim(media_type, url, ext)
     klass = media_type.constantize
     file = download_file(url, ext)
     existing_media = klass.find_by(original_claim_hash: Digest::MD5.hexdigest(url))
@@ -156,11 +156,11 @@ class Media < ApplicationRecord
     file
   end
 
-  def self.create_claim_media(text)
+  def self.create_claim_media_from_original_claim(text)
     Claim.find_by(original_claim_hash: Digest::MD5.hexdigest(text)) || Claim.create!(quote: text, original_claim: text)
   end
 
-  def self.create_link_media(url, project_media_team)
+  def self.create_link_media_from_original_claim(url, project_media_team)
     pender_key = project_media_team.get_pender_key if project_media_team
     url_from_pender = Link.normalized(url, pender_key)
     Link.find_by(url: url_from_pender) || Link.find_by(original_claim_hash: Digest::MD5.hexdigest(url)) || Link.create!(url: url, pender_key: pender_key, original_claim: url)
