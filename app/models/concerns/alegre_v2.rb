90e05131bbd2988d7c65b2b04af1919b0c0500aa
Caio Almeida <117518+caiosba@users.noreply.github.com>
2025-01-31 15:59:43 -0300

    Notify Sentry when a request to Alegre fails. (#2198)
    Notify for every retry.
    
    Fixes: CV2-6070.

--- app/models/concerns/alegre_v2.rb
+++ app/models/concerns/alegre_v2.rb
@@ -124,11 +124,12 @@ module AlegreV2
         Rails.logger.info("[Alegre Bot] Alegre response: #{parsed_response.inspect}")
         parsed_response
       rescue StandardError => e
+        Rails.logger.error("[Alegre Bot] Alegre error: (#{method}, #{path}, #{params.inspect}, #{retries}), #{e.inspect} #{e.message}")
+        CheckSentry.notify(e, bot: 'alegre', method: method, path: path, params: params, retries: retries)
         if retries > 0
           sleep 1
           self.request(method, path, params, retries - 1)
         end
-        Rails.logger.error("[Alegre Bot] Alegre error: (#{method}, #{path}, #{params.inspect}, #{retries}), #{e.inspect} #{e.message}")
         { 'type' => 'error', 'data' => { 'message' => e.message } }
       end
     end
