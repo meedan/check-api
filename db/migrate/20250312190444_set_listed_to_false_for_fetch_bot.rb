cc8cfd24f169ace5131955e4a3d44f953bb13d0a
manu vasconcelos <manu@meedan.com>
2025-03-12 16:44:39 -0300

    doesn't need to be an array

--- db/migrate/20250312190444_set_listed_to_false_for_fetch_bot.rb
+++ db/migrate/20250312190444_set_listed_to_false_for_fetch_bot.rb
@@ -1,7 +1,7 @@
 class SetListedToFalseForFetchBot < ActiveRecord::Migration[6.1]
   def change
     # Fetch should not be marked as "listed", so it does not appear on the UI
-    BotUser.where(login: ['fetch']).each do |bot|
+    BotUser.where(login: 'fetch').each do |bot|
       bot.set_listed = false
       bot.save!
     end
