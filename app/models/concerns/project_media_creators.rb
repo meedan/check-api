cceabb933ed12dc3a5ee9e88352ce169747d30b5
manu vasconcelos <manu@meedan.com>
2025-03-25 18:43:29 -0300

    Remove the logic to set the channel from create_claim_description_and_fact_check
    - We shouldn't run logic for setting the channel in this method
            - that method either returns fc = nil or fetches a fact_check
            - we are only getting existing data here
            - so it should only try to fetch the channel

--- app/models/concerns/project_media_creators.rb
+++ app/models/concerns/project_media_creators.rb
@@ -16,7 +16,6 @@ module ProjectMediaCreators
     fc = nil
     unless self.set_fact_check.blank?
       fact_check = self.set_fact_check.with_indifferent_access
-      fact_check['channel'] ||= (User.current && User.current.is_a?(BotUser)) ? 'api' : 'manual'
       fc = FactCheck.create!({
         title: fact_check['title'],
         summary: fact_check['summary'],
