403b76bc26173a558dd4abb1f518a9710c0c123c
manu vasconcelos <manu@meedan.com>
2025-04-01 11:21:56 -0300

    enable array of strings

--- app/graph/types/team_type.rb
+++ app/graph/types/team_type.rb
@@ -326,7 +326,7 @@ class TeamType < DefaultObject
     argument :imported, GraphQL::Types::Boolean, required: false, camelize: false # Only for fact-checks
     argument :target_id, GraphQL::Types::Int, required: false, camelize: false # Exclude articles already applied to the `ProjectMedia` with this ID
     argument :trashed, GraphQL::Types::Boolean, required: false, camelize: false, default_value: false
-    argument :channel, GraphQL::Types::String, required: false, camelize: false
+    argument :channel, [GraphQL::Types::String, null: true], required: false, camelize: false
   end
 
   def articles(**args)
@@ -364,7 +364,7 @@ class TeamType < DefaultObject
     argument :imported, GraphQL::Types::Boolean, required: false, camelize: false # Only for fact-checks
     argument :target_id, GraphQL::Types::Int, required: false, camelize: false # Exclude articles already applied to the `ProjectMedia` with this ID
     argument :trashed, GraphQL::Types::Boolean, required: false, camelize: false, default_value: false
-    argument :channel, GraphQL::Types::String, required: false, camelize: false
+    argument :channel, [GraphQL::Types::String, null: true], required: false, camelize: false
   end
 
   def articles_count(**args)
