61621f8f471bbdc0907aea24679085d9cea17b72
manu vasconcelos <manu@meedan.com>
2025-04-15 17:58:07 -0300

    add events and get_request_url

--- app/graph/types/webhook_type.rb
+++ app/graph/types/webhook_type.rb
@@ -5,4 +5,10 @@ class WebhookType < DefaultObject
 
   field :dbid, GraphQL::Types::Int, null: true
   field :name, GraphQL::Types::String, null: true
+  field :events, GraphQL::Types::String, null: true
+  field :get_request_url, GraphQL::Types::String, null: true
+
+  def get_request_url
+    object.get_request_url
+  end
 end
