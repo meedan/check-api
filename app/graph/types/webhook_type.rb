c81e7071f744b783e30696364b8b9e7bb8330659
manu vasconcelos <manu@meedan.com>
2025-04-17 13:30:01 -0300

    return headers and return json objects
    we want both events and headers to be a json object, instead of ruby

--- app/graph/types/webhook_type.rb
+++ app/graph/types/webhook_type.rb
@@ -6,9 +6,18 @@ class WebhookType < DefaultObject
   field :dbid, GraphQL::Types::Int, null: true
   field :name, GraphQL::Types::String, null: true
   field :events, GraphQL::Types::String, null: true
-  field :get_request_url, GraphQL::Types::String, null: true
+  field :request_url, GraphQL::Types::String, null: true
+  field :headers, GraphQL::Types::String, null: true
 
-  def get_request_url
+  def events
+    object.get_events.to_json
+  end
+
+  def request_url
     object.get_request_url
+  end
+
+  def headers
+    object.get_headers.to_json
   end
 end
