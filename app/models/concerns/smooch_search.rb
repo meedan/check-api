f43875f3c06374f0ae2470c0e2c29045cd6d242f
Sawy <melsawy.be7eri@gmail.com>
2025-04-17 09:26:23 +0200

    CV2-5171: fallback to send the report as a template message if 24 hours have passed

--- app/models/concerns/smooch_search.rb
+++ app/models/concerns/smooch_search.rb
@@ -323,9 +323,10 @@ module SmoochSearch
       self.get_installation(self.installation_setting_id_keys, data['app_id']) if self.config.blank?
       no_body = (tipline_request.platform == 'Facebook Messenger' && !report.url.blank?)
       Rails.logger.info "[Smooch Bot] Sending explainer report to user #{uid} for ExplainerItem with ID #{ex_item_id}..."
-      self.send_message_to_user(uid, report.text(nil, no_body), {}, false, true, 'explainer_report')
+      last_smooch_response = self.send_message_to_user(uid, report.text(nil, no_body), {}, false, true, 'explainer_report')
       # Set smooch_report_sent_at to Time.now to prevent re-sending for the same request.
       tipline_request.update_column(:smooch_report_sent_at, Time.now.to_i)
+      self.save_smooch_response(last_smooch_response, tipline_request.associated, data['received'], 'fact_check_report', tipline_request.language)
     end
 
     def user_received_search_result(message)
