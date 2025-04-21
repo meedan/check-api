dcbcc52d9af67a8f776f2426231198084875adfc
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2025-04-21 13:51:29 +0200

    CV2-6342: Refactor SavedSearch to accept list_type (#2291)
    * CV2-6342: refactor SavedSearch to accept list_type
    
    * CV2-6342: update schema

--- app/models/saved_search.rb
+++ app/models/saved_search.rb
@@ -1,6 +1,8 @@
 class SavedSearch < ApplicationRecord
   attr_accessor :is_being_copied
 
+  enum list_type: { media: 0, article: 1 }
+
   validates_presence_of :title, :team_id
   validates :title, uniqueness: { scope: :team_id }, unless: proc { |ss| ss.is_being_copied }
 
