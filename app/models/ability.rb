cbc8e3199069581b024c54f4707588469f1b832f
manu vasconcelos <manu@meedan.com>
2025-04-28 14:19:29 -0300

    restrict Dynamic,DynamicAnnotation::Field destroy by team
    We can look at Dynamic.team

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -67,10 +67,8 @@ class Ability
       obj.team_id == @context_team.id || obj.feed.team_id == @context_team.id
     end
     can [:create, :update, :read, :destroy], ApiKey, :team_id => @context_team.id
-    # can :destroy, [Dynamic, DynamicAnnotation::Field], annotation_type: 'metadata' # FIXME: Restrict by team
     can :destroy, [Dynamic, DynamicAnnotation::Field] do |obj|
-      source = Source.find(obj.id)
-      source.team_id == @context_team.id
+      obj.team.present? && obj.team == @context_team
     end
   end
 
