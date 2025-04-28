51d803940c0be52a907cee9f2453d61e4b289c96
manu vasconcelos <manu@meedan.com>
2025-04-28 10:36:36 -0300

    extract SharedCreateAndUpdateFields

--- app/graph/mutations/webhook_mutations.rb
+++ app/graph/mutations/webhook_mutations.rb
@@ -2,11 +2,19 @@ module WebhookMutations
   MUTATION_TARGET = 'bot_user'.freeze
   PARENTS = ['team'].freeze
 
+  module SharedCreateAndUpdateFields
+    extend ActiveSupport::Concern
+
+    included do
+      argument :name, GraphQL::Types::String, required: true
+      argument :request_url, GraphQL::Types::String, required: true, camelize: false
+      argument :events, JsonStringType, required: true
+      argument :headers, JsonStringType, required: false
+    end
+  end
+
   class Create < Mutations::CreateMutation
-    argument :name, GraphQL::Types::String, required: true
-    argument :request_url, GraphQL::Types::String, required: true, camelize: false
-    argument :events, JsonStringType, required: true
-    argument :headers, JsonStringType, required: false
+    include SharedCreateAndUpdateFields
   end
 
   class Destroy < Mutations::DestroyMutation; end 
