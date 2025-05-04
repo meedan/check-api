076706583fe55059bdaa8edbc3db8511222f9d7e
Caio <117518+caiosba@users.noreply.github.com>
2025-05-04 17:55:58 -0300

    Fix

--- app/models/project_media.rb
+++ app/models/project_media.rb
@@ -555,8 +555,9 @@ class ProjectMedia < ApplicationRecord
   end
 
   def has_tipline_requests_that_never_received_articles
+    ids = ProjectMedia.where(id: self.related_items_ids).map(&:id) # Including child items
     # As we check against the range with 1, 7 and 30 days so I check the exists with the max range (30 days)
-    TiplineRequest.no_articles_sent(self.id).where(created_at: Time.now.ago(30.days)..Time.now).exists?
+    TiplineRequest.no_articles_sent(ids).where(created_at: Time.now.ago(30.days)..Time.now).exists?
   end
 
   def number_of_tipline_requests_that_never_received_articles_by_time
