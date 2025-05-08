b695162357e2c59567a312c6910b8d4f03dc8b79
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2025-05-08 08:02:19 +0300

    CV2-6018: filter SavedSearch by list_type (#2293)
    * CV2-6018: filter SavedSearch by list_type
    
    * CV2-6018: fix tests
    
    * CV2-6081: add a permission for SavedSearch
    
    * CV2-6018: unique list name based on team and type, also fix items_count for article lists
    
    * CV2-6018: fix tests
    
    * CV2-6018: add missing tests
    
    * Clear permissions cache
    
    * CV2-6018: fix article list
    
    * CV2-6018: apply PR comment
    
    ---------
    
    Co-authored-by: Caio <117518+caiosba@users.noreply.github.com>

--- app/models/saved_search.rb
+++ app/models/saved_search.rb
@@ -4,13 +4,18 @@ class SavedSearch < ApplicationRecord
   enum list_type: { media: 0, article: 1 }
 
   validates_presence_of :title, :team_id
-  validates :title, uniqueness: { scope: :team_id }, unless: proc { |ss| ss.is_being_copied }
+  validates :title, uniqueness: { scope: [:team_id, :list_type] }, unless: proc { |ss| ss.is_being_copied }
 
   belongs_to :team, optional: true
   has_many :feeds, dependent: :nullify
   has_many :feed_teams, dependent: :nullify
 
   def items_count
-    CheckSearch.new(self.filters.to_json, nil, self.team_id).number_of_results
+    if self.list_type == 'article'
+      filters = self.filters || {}
+      self.team.team_articles_count(filters.with_indifferent_access)
+    elsif self.list_type == 'media'
+      CheckSearch.new(self.filters.to_json, nil, self.team_id).number_of_results
+    end
   end
 end
