9505e3bab0e632b2909e595fd872d8b3e07269b2
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2025-05-09 21:29:47 +0300

    CV2-6018: fix item_counts for channel and users filter (#2302)

--- app/models/saved_search.rb
+++ app/models/saved_search.rb
@@ -12,7 +12,10 @@ class SavedSearch < ApplicationRecord
 
   def items_count
     if self.list_type == 'article'
-      filters = self.filters || {}
+      filters = begin self.filters.with_indifferent_access rescue {} end
+      # Add filters with same name in team articles filters i.e map channels to channel and users to user_ids
+      filters[:channel] = filters[:channels] unless filters[:channels].blank?
+      filters[:user_ids] = filters[:users] unless filters[:users].blank?
       self.team.team_articles_count(filters.with_indifferent_access)
     elsif self.list_type == 'media'
       CheckSearch.new(self.filters.to_json, nil, self.team_id).number_of_results
