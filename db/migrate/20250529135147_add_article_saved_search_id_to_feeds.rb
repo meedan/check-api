5b8c13c29f203eb348e03c505162b55579137c55
manu vasconcelos <manu@meedan.com>
2025-05-29 14:44:51 -0300

    Fix Migration
    since we are adding reference we don't need to explicitly add _id

--- db/migrate/20250529135147_add_article_saved_search_id_to_feeds.rb
+++
@@ -1,5 +0,0 @@
-class AddArticleSavedSearchIdToFeeds < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :feeds, :article_saved_search_id, foreign_key: { to_table: :saved_searches }
-  end
-end
