1265f60c07477835ad6f40330aeabf2c4ad2a7b8
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2025-06-23 20:41:58 +0300

    CV2-6375: unsubscribe user based on error code and platform (#2318)
    * CV2-6375: unsubscribe user based on error code and platform
    
    * CV2-6375: unsbuscribe user based on platform and error code
    
    * CV2-6375: apply PR comments

--- app/models/concerns/smooch_newsletter.rb
+++ app/models/concerns/smooch_newsletter.rb
@@ -24,5 +24,21 @@ module SmoochNewsletter
     def get_newsletter(team_id, language)
       TiplineNewsletter.where(team_id: team_id, language: language).last
     end
+
+    def unsubscribe_user_on_optout(json)
+      # unsubscribing user from all newsletters if the error code is 131050 and the platform is WhatsApp
+      if json.dig('destination', 'type') == 'whatsapp' && json.dig('error', 'underlyingError', 'errors', 0, 'code') == 131050
+        uid = json['appUser']['_id']
+        language = self.get_user_language(uid)
+        self.unsubscribe_from_all_language(uid, language, self.config['team_id'].to_i, self.get_workflow(language))
+      end
+    end
+
+    def unsubscribe_from_all_language(uid, language, team_id, workflow)
+      TiplineSubscription.where(uid: uid, team_id: team_id).destroy_all
+      # Call these methods to sync with `toggle_subscription` method
+      self.send_final_message_to_user(uid, self.subscription_message(uid, language, false, false), workflow, language)
+      self.clear_user_bundled_messages(uid)
+    end
   end
 end
