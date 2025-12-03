0284c0c6d4dd83dee9b4e37353bc33b52df8676d
Sawy <melsawy.be7eri@gmail.com>
2025-12-03 17:03:10 +0200

    CV2-6604: remove team_id condition

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -86,7 +86,7 @@ module Api
       def authenticate_from_token!
         header = CheckConfig.get('authorization_header', 'X-Token')
         token = request.headers[header]
-        @key = ApiKey.where(access_token: token).where('team_id IS NOT NULL').where('expire_at > ?', Time.now).first
+        @key = ApiKey.where(access_token: token).where('expire_at > ?', Time.now).first
         (render_unauthorized and return false) if @key.nil?
       end
 
@@ -102,7 +102,7 @@ module Api
       def identify_user(mandatory)
         header = CheckConfig.get('authorization_header', 'X-Token')
         token = request.headers[header].to_s
-        key = ApiKey.where(access_token: token).where('team_id IS NOT NULL').where('expire_at > ?', Time.now).first
+        key = ApiKey.where(access_token: token).where('expire_at > ?', Time.now).first
         if key.nil?
           ApiKey.current = nil
           user = User.find_with_token(token)
