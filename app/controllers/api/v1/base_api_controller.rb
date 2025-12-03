43e5dac3574ff638d194b9ee8d99d0fe3fbbc5fa
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2025-12-03 06:36:53 +0200

    CV2-6604: update last_active_at for api key (#2389)

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -109,6 +109,8 @@ module Api
           User.current = user
           (!token.blank? && user) ? sign_in(user, store: false) : (authenticate_api_user! if mandatory)
         else
+          # Update last_active_at for ApiKey
+          key.update_column(:last_active_at, Time.now)
           User.current = key.bot_user
           ApiKey.current = key
         end
