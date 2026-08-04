b25e3d6f4b48fe92342b580718818aa8c15b4373
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2026-08-04 22:07:56 +0300

    CV2-6719: Facebook Authentication logs (#2448)
    * CV2-6719: add more logs
    
    * CV2-6719: log omniauth.auth

--- app/controllers/concerns/facebook_authentication.rb
+++ app/controllers/concerns/facebook_authentication.rb
@@ -5,7 +5,7 @@ module FacebookAuthentication
     # pages_manage_metadata is for Facebook API > 7
     # manage_pages is for Facebook API < 7
     # An error will be displayed for Facebook users that are admins of the Facebook app, but should be transparent for other users
-    request.env['omniauth.strategy'].options[:scope] = 'pages_manage_metadata,manage_pages,pages_messaging,instagram_manage_messages,instagram_basic' if params[:context] == 'smooch'
+    request.env['omniauth.strategy'].options[:scope] = 'pages_manage_metadata,manage_pages,pages_messaging,instagram_manage_messages,instagram_basic,pages_utility_messaging' if params[:context] == 'smooch'
     prefix = facebook_context == 'smooch' ? 'smooch_' : ''
     request.env['omniauth.strategy'].options[:client_id] = CheckConfig.get("#{prefix}facebook_app_id")
     request.env['omniauth.strategy'].options[:client_secret] = CheckConfig.get("#{prefix}facebook_app_secret")
@@ -18,6 +18,7 @@ module FacebookAuthentication
 
   # OAuth callback
   def facebook
+    Rails.logger.info "FacebookAuthentication-facebook:oauthcallback:: #{request.env['omniauth.auth'].to_json}"
     request.env['omniauth.auth']['url'] = 'https://facebook.com/' + request.env['omniauth.auth'].uid
     start_session_and_redirect
   end
