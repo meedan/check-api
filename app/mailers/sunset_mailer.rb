32f2626d8aae08a1f4b787a810966c80e763e0e4
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2026-08-30 19:26:59 +0300

    CV2-6724 : Sunset Check notification (#2447)
    * CV2-6724: add rake task for sunset mails
    
    * CV2-6724: adjust sending sunset email
    
    * CV2-6724: cleanup
    
    * CV2-6724: add slug as argument to rake task
    
    * CV2-6724: update copy for sunset mails
    
    * CV2-6724: update mail copy
    
    * CV2-6724: set .text template
    
    * CV2-6724: cleanup
    
    * CV2-6724: apply PR comments

---
+++ app/mailers/sunset_mailer.rb
@@ -0,0 +1,25 @@
+class SunsetMailer < ApplicationMailer
+  layout nil
+
+  def notify(type, user, workspace, workspace_url)
+    @name = user.name
+    # Set subject based on type (notify or download)
+    subject = (type == 'download') ? "#{CheckConfig.get('app_name')} exported data for #{workspace} workspace is ready to download" : "Sunset alert: #{CheckConfig.get('app_name')} sunset for #{workspace} workspace"
+    # Dates for low usage workspaces
+    low_usage_end_date = begin Time.parse(CheckConfig.get('check_sunset_low_usage_date')) rescue Time.parse('2026-11-03') end
+    low_usage_download_link_date = begin Time.parse(CheckConfig.get('check_sunset_low_usage_download_link_date')) rescue Time.parse('2026-11-04') end
+    # Dates for high usage workspaces
+    high_usage_end_date = begin Time.parse(CheckConfig.get('check_sunset_high_usage_date')) rescue Time.parse('2027-01-31') end
+    @info = {
+      mail_copy: "#{type}_copy",
+      high_usage_end_date: high_usage_end_date,
+      low_usage_end_date: low_usage_end_date,
+      low_usage_download_link_date: low_usage_download_link_date,
+      workspace: workspace,
+      workspace_url: workspace_url,
+      download_expire_days: CheckConfig.get('check_sunset_download_expire_days', 14, :integer),
+      download_expire_extended_days: CheckConfig.get('check_sunset_download_expire_extended_days', 10, :integer),
+    }
+    mail(to: user.email, subject: subject)
+  end
+end
