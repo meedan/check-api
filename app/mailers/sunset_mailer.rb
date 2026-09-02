325afc8765631342e22e1fcfd62d9c9d5247d04b
Sawy <melsawy.be7eri@gmail.com>
2026-09-02 18:55:32 +0300

    CV2-6724: update mail copy

--- app/mailers/sunset_mailer.rb
+++ app/mailers/sunset_mailer.rb
@@ -4,7 +4,13 @@ class SunsetMailer < ApplicationMailer
   def notify(type, user, workspace, workspace_url)
     @name = user.name
     # Set subject based on type (notify or download)
-    subject = (type == 'download') ? "#{CheckConfig.get('app_name')} exported data for #{workspace} workspace is ready to download" : "Sunset alert: #{CheckConfig.get('app_name')} sunset for #{workspace} workspace"
+    if type == 'download'
+      subject = "#{CheckConfig.get('app_name')} exported data for #{workspace} workspace is ready to download"
+    elsif type == 'notify_low_usage'
+      subject = "Housekeeping update for inactive #{CheckConfig.get('app_name')} workspaces (#{workspace})"
+    else
+      subject = "Sunset alert: #{CheckConfig.get('app_name')} sunset for #{workspace} workspace"
+    end
     # Dates for low usage workspaces
     low_usage_end_date = begin Time.parse(CheckConfig.get('check_sunset_low_usage_date')) rescue Time.parse('2026-11-03') end
     low_usage_download_link_date = begin Time.parse(CheckConfig.get('check_sunset_low_usage_download_link_date')) rescue Time.parse('2026-11-04') end
@@ -17,7 +23,7 @@ class SunsetMailer < ApplicationMailer
       low_usage_download_link_date: low_usage_download_link_date,
       workspace: workspace,
       workspace_url: workspace_url,
-      download_expire_days: CheckConfig.get('check_sunset_download_expire_days', 14, :integer),
+      download_expire_days: CheckConfig.get('check_sunset_download_expire_days', 15, :integer),
       download_expire_extended_days: CheckConfig.get('check_sunset_download_expire_extended_days', 10, :integer),
     }
     mail(to: user.email, subject: subject)
