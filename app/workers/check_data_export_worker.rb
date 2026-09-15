304892edfa3f9843da8670602bd336822f8a224c
Sawy <melsawy.be7eri@gmail.com>
2026-09-16 00:01:04 +0300

    CV2-6741: add export tasks

--- app/workers/check_data_export_worker.rb
+++ app/workers/check_data_export_worker.rb
@@ -3,8 +3,28 @@ class CheckDataExportWorker
 
   sidekiq_options retry: 0
 
-  def perform(id)
-    de = CheckDataExport.find_by_id(id)
-    de.regenerate_download_url unless de.nil?
+  def perform(id, action)
+    object = CheckDataExport.find_by_id(id)
+    unless object.nil?
+      next_action = {
+        'initiate_readme' => 'export_workspace_data',
+        'export_workspace_data' => 'export_workspace_user_data',
+        'export_workspace_user_data' => 'export_workspace_articles_data',
+        'export_workspace_articles_data' => 'export_workspace_annotations_data',
+        'export_workspace_annotations_data' => 'export_workspace_tipline_requests_data',
+        'export_workspace_tipline_requests_data' => 'export_workspace_tipline_newsletter_data',
+        'export_workspace_tipline_newsletter_data' => 'export_workspace_item_data',
+        'export_workspace_item_data' => nil,
+      }
+      if object.respond_to?(action)
+        object.send(action, object.team)
+        if next_action[action].nil?
+          # Upload and send notification mail
+          object.delay.upload_and_send_workspace_data(object.team)
+        else
+          CheckDataExportWorker.perform_in(1.second, object.id, next_action[action])
+        end
+      end
+    end
   end
 end
